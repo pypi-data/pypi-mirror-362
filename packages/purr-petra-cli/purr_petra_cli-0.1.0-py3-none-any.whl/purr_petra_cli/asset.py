1c2611be0621157111f05d1c73f941e771ae96f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:13 +0000

    Add package purr-petra-cli/0.1.0/purr_petra_cli-0.1.0-py3-none-any.whl

---
+++ packages/purr-petra-cli/purr_petra_cli-0.1.0-py3-none-any.whl/purr_petra_cli/asset.py
@@ -0,0 +1,167 @@
+import json
+from dataclasses import dataclass
+from pathlib import Path
+from typing import List, Dict, Any, Optional
+import numpy as np
+import pandas as pd
+import pyodbc
+from purr_petra_cli.util import (
+    chunk_ids,
+    create_selectors,
+    get_id_list,
+    get_column_info,
+    get_recipe,
+    make_repo_id,
+    make_where_clause,
+    timestamp_filename,
+)
+from purr_petra_cli.dbisam import make_conn_params
+from purr_petra_cli.post_process import post_process
+from purr_petra_cli.xformer import PURR_WHERE, formatters, transform_dataframe_to_json
+
+
+######################
+import psutil
+from concurrent.futures import ThreadPoolExecutor
+
+
+# Function to estimate number of threads based on available memory (in MB)
+def estimate_thread_count(memory_per_thread_mb=500):
+    mem = psutil.virtual_memory()
+    available_mb = mem.available / (1024 * 1024)  # Convert bytes to MB
+    max_threads = int(available_mb // memory_per_thread_mb)
+    return max(1, max_threads)  # At least 1 thread
+
+
+@dataclass
+class AssetConfig:
+    asset: str
+    proj: str
+    uwis_list: Optional[List[str]]
+
+    def out_file(self):
+        return timestamp_filename(self.proj, self.asset)
+
+    @property
+    def repo_id(self):
+        return make_repo_id(self.proj)
+
+    @property
+    def recipe(self):
+        return get_recipe(self.asset)
+
+    @property
+    def conn(self):
+        return make_conn_params(self.proj)
+
+    @property
+    def xforms(self):
+        return self.recipe.get("xforms", {})
+
+    @property
+    def prefixes(self):
+        return self.recipe.get("prefixes", {})
+
+    @property
+    def selectors(self):
+        where = make_where_clause(self.uwis_list)
+
+        id_sql = self.recipe["identifier"].replace(PURR_WHERE, where)
+        chunk_size = self.recipe["chunk_size"] if "chunk_size" in self.recipe else 1000
+
+        ids = get_id_list(self.conn, id_sql)
+
+        chunked_ids = chunk_ids(ids, chunk_size)
+
+        selectors = create_selectors(
+            chunked_ids=chunked_ids,
+            identifier_keys=self.recipe["identifier_keys"],
+            selector=self.recipe["selector"],
+        )
+
+        return selectors
+
+    @property
+    def post_processor(self):
+        if proc_name := self.recipe.get("post_process"):
+            return post_process[proc_name]
+
+
+def compose_and_write_docs(
+    cfg: AssetConfig, select: str, out_file: str
+) -> tuple[str, int]:
+    all_columns = set()
+
+    with open(out_file, "w", encoding="utf-8") as f:
+        f.write("[")  # Start of JSON array
+        docs_written = 0
+
+        with pyodbc.connect(**cfg.conn) as con:
+            cursor = con.cursor()
+            cursor.execute(select)
+            column_names, column_types = get_column_info(cursor)
+            df = pd.DataFrame(
+                [tuple(row) for row in cursor.fetchall()], columns=column_names
+            )
+
+            all_columns.update(df.columns)
+
+            for col in df.columns:
+                col_type = str(df.dtypes[col])
+                xform = cfg.xforms.get(col, col_type)
+                formatter = formatters.get(xform, lambda x: x)
+                df[col] = df[col].apply(formatter)
+
+            df = df.replace({np.nan: None})
+
+            if cfg.post_processor:
+                df = cfg.post_processor(df)
+
+            json_data: List[Dict[str, Any]] = transform_dataframe_to_json(
+                df, cfg.prefixes
+            )
+
+            for json_obj in json_data:
+                json_obj["proj"] = cfg.proj
+                json_obj["repo_id"] = cfg.repo_id
+                json_str = json.dumps(json_obj, default=str)
+                f.write(json_str + ",")
+                docs_written += 1
+
+        if docs_written > 0:
+            f.seek(f.tell() - 1, 0)  # Remove the last comma
+        f.write("]")  # End JSON array
+
+    return out_file, docs_written
+
+
+######################
+def run_in_parallel(cfg, output_dir: str):
+    num_threads = estimate_thread_count()
+    out_files = []
+
+    def task(select):
+        out_file = str(Path(output_dir, cfg.out_file()).with_suffix(".json"))
+        result = compose_and_write_docs(cfg, select, out_file)
+        out_files.append(out_file)
+        return result
+
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        results = list(executor.map(task, cfg.selectors))
+
+    return results
+
+
+######################
+
+
+def select_assets(proj: str, asset: str, uwis_list: List[str] | None, output_dir: str):
+    cfg = AssetConfig(asset=asset, proj=proj, uwis_list=uwis_list)
+    # for x in cfg.selectors:
+    #     print(x)
+
+    # return
+    x = run_in_parallel(cfg, output_dir)
+    print("xxxxxxxxxxxxxxxx")
+    print(x)
+    print("xxxxxxxxxxxxxxxx")
---
+++ packages/purr-petra-cli/purr_petra_cli-0.1.0-py3-none-any.whl/purr_petra_cli/cli.py
@@ -0,0 +1,104 @@
+import json
+import tempfile
+from enum import Enum
+from pathlib import Path
+from typing import List, Optional, cast
+import typer
+from purr_petra_cli.setup import prepare
+from purr_petra_cli.proj import validate_proj_dir
+from purr_petra_cli.util import parse_uwis, ensure_dir
+from purr_petra_cli.asset import select_assets
+
+
+app = typer.Typer()
+
+
+class Asset(str, Enum):
+    core = "core"
+    dst = "dst"
+    formation = "formation"
+    ip = "ip"
+    perforation = "perforation"
+    production = "production"
+    raster_log = "raster_log"
+    survey = "survey"
+    vector_log = "vector_log"
+    well = "well"
+    zone = "zone"
+
+
+asset_members_str = ", ".join([member.value for member in Asset])
+
+
+@app.command()
+def collect(
+    proj: str = typer.Option(
+        ...,
+        help="Path to project directory, It's probably a UNC path",
+        callback=validate_proj_dir,
+        show_envvar=False,
+    ),
+    asset: Asset = typer.Option(
+        Asset.well, help=f"The asset (choose from: {asset_members_str})"
+    ),
+    uwis: Optional[str] = typer.Option(
+        None,
+        help="Full or partial UWI list. Separate UWIs with spaces or commas. "
+        + "Use * or % as wildcards. This is a single arg, so wrap it in quotes."
+        + '  Example: --uwis "05010001102000 350112* 350110*"  '
+        + "omitting the --uwis arg selects everything--BEWARE!",
+        callback=parse_uwis,
+    ),
+    output_dir: str = typer.Option(
+        Path(tempfile.gettempdir()),
+        help="Define an output directory store exported json data. "
+        + "Enclose paths with spaces in quotes."
+        + "(Defaults to System TEMP).",
+        callback=ensure_dir,
+    ),
+):
+    # asset = asset.value # to get a real string from the Enum causes type error
+
+    uwis_list = cast(Optional[List[str]], uwis)
+
+    print("proj=", proj)
+    print("asset=", asset.value)
+    print("uwis=", uwis)
+    print("uwis_list=", uwis_list)
+    print("output_dir=", output_dir)
+
+    # epsg = get_storage_epsg(proj)
+
+    res = select_assets(
+        proj=proj,
+        asset=asset.value,
+        uwis_list=uwis_list,
+        output_dir=output_dir,
+    )
+    print(json.dumps(res, indent=4))
+    # print(res)
+
+
+@app.command()
+def setup():
+    """
+    Register the Elevate Software dbodbc.dll to enable querying Petra databases.
+
+    RUN THIS COMMAND IN A CONSOLE AS ADMINISTRATOR. It writes to the registry.
+    This command requires no additional options or configuration.
+    """
+    prepare()
+
+
+if __name__ == "__main__":
+    app()
+
+
+# C:\Users\bryan\dev\purr_petra_cli>uv pip install -e .
+# C:\Users\bryan\dev\purr_petra_cli>uv run purr-petra-cli collect --proj IAMaproj --asset dst
+# INTERSTATE has 05009052460000,
+# --uwis "1234*,050090524*" should return
+#   [3074, 3075, 3076, 3077, 3078, 3079, 3080, 3081, 3082, 3083]
+
+# uv run purr-petra-cli collect --asset well --proj C:\dev_data\petra\projects\INTERSTATE
+# stack: 35087204750001
