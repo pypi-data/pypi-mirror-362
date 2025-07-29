09c1d5f9bfec4a20317066092796abd51a9a2ae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:31 +0000

    Add package renderg-sdk/0.1.24/renderg-sdk-0.1.24.tar.gz

---
+++ packages/renderg-sdk/renderg-sdk-0.1.24.tar.gz/renderg-sdk-0.1.24/setup.py
@@ -0,0 +1,37 @@
+from os import path
+
+from setuptools import setup, find_packages
+
+
+def get_readme():
+    this_directory = path.abspath(path.dirname(__file__))
+    with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+    return long_description
+
+
+setup(
+    name='renderg-sdk',
+    version='0.1.24',
+    url='https://github.com/renderg-repo/renderg-sdk.git',
+    author='RenderG',
+    author_email='support@renderg.com',
+    description='www.renderg.com',
+    long_description=get_readme(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),    
+    install_requires=[
+        'requests',
+        'tenacity',
+        'paho-mqtt'
+    ],
+    package_data={
+        'analyze_houdini': ["./houdini/*"],
+        'ascp': ["./*/*"],
+    },
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+)
