ca30a9bea5a78e348a676b9a7b99ace96f10eb2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:26 +0000

    Add package square-database-structure/2.5.9/square_database_structure-2.5.9.tar.gz

---
+++ packages/square-database-structure/square_database_structure-2.5.9.tar.gz/square_database_structure-2.5.9/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = "square_database_structure"
+
+setup(
+    name=package_name,
+    version="2.5.9",
+    packages=find_packages(),
+    package_data={
+        package_name: ["**/**/stored_procedures_and_functions/*.sql"],
+    },
+    install_requires=[
+        "sqlalchemy>=2.0.23",
+        "psycopg2-binary>=2.9.9",
+        "pytest>=8.0.0",
+    ],
+    author="thePmSquare",
+    author_email="thepmsquare@gmail.com",
+    description="database layer for my personal server.",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/thepmsquare/{package_name}",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
