b6c8af08101743b330d70c3b9346653c831dc6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:08:28 +0000

    Add package lark-cython/0.0.17/lark_cython-0.0.17.tar.gz

---
+++ packages/lark-cython/lark_cython-0.0.17.tar.gz/lark_cython-0.0.17/README.md
@@ -0,0 +1,44 @@
+# Lark-Cython
+
+Cython plugin for [Lark](https://github.com/lark-parser/lark), reimplementing the LALR parser &amp; lexer for better performance on CPython.
+
+Install:
+
+```python
+pip install lark-cython
+```
+
+Usage:
+
+```python
+import lark_cython
+
+parser = Lark(grammar, parser="lalr", _plugins=lark_cython.plugins)
+
+# Use Lark as you usually would, with a huge performance boost
+```
+
+See the [examples](https://github.com/lark-parser/lark_cython/tree/master/examples) for more.
+
+
+## Differences from Lark
+
+- `Token` instances do not inherit from `str`. You must use the `value` attribute to get the string.
+
+## Other caveats
+
+- Postlexer isn't currently implemented
+
+## Speed
+
+In current benchmarks, lark-cython is about 50% to 80% faster than Lark.
+
+We're still in the early stages, and in the future, lark-cython might go a lot faster.
+
+## Other
+
+License: MIT
+
+Author: [Erez Shinan](https://github.com/erezsh/)
+
+Special thanks goes to [Datafold](https://github.com/datafold) for commissioning the draft for lark-cython, and allowing me to release it as open-source.
---
+++ packages/lark-cython/lark_cython-0.0.17.tar.gz/lark_cython-0.0.17/setup.py
@@ -0,0 +1,61 @@
+import re
+from setuptools import find_packages, setup
+
+(__version__,) = re.findall('__version__ = "(.*)"', open("lark_cython/__init__.py").read())
+
+# python .\setup.py build_ext --inplace
+
+from Cython.Build import cythonize
+
+
+def parse_description():
+    """
+    Parse the description in the README file
+    """
+    from os.path import dirname, join, exists
+
+    readme_fpath = join(dirname(__file__), "README.md")
+    # This breaks on pip install, so check that it exists.
+    if exists(readme_fpath):
+        with open(readme_fpath, "r") as f:
+            text = f.read()
+        return text
+    return ""
+
+
+setup(
+    name="lark-cython",
+    version=__version__,
+    packages=find_packages(),
+    ext_modules=cythonize("lark_cython/*.pyx"),  # accepts a glob pattern
+    requires=["Cython"],
+    install_requires=["lark>=1.1.7"],
+    setup_requires=["Cython>=3.0,<3.1"],
+    author="Erez Shinan",
+    author_email="lark@erezsh.com",
+    description="A Lark plugin that optimizes LALR parsing using Cython",
+    keywords="Lark LALR parser optimized Cython",
+    url="https://github.com/lark-parser/lark_cython",
+    long_description=parse_description(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    python_requires=">=3.6",
+    classifiers=[
+        # List of classifiers available at:
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: MIT License",
+        # Supported Python versions
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
