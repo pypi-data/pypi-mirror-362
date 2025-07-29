fa7068a61d4c1fdaa32558a55c045df942bde760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:17:25 +0000

    Add package nkunyim-util/0.1.1/nkunyim_util-0.1.1.tar.gz

---
+++ packages/nkunyim-util/nkunyim_util-0.1.1.tar.gz/nkunyim_util-0.1.1/src/nkunyim_util/models/access_model.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+from typing import List, Optional
+from pydantic import BaseModel
+from uuid import UUID
+
+
+
+class ItemModel(BaseModel):
+    _id: UUID
+    sequence: int
+    id: UUID
+    name: str
+    title: str
+    caption: str
+    icon: str
+    path: str
+    route: str
+    tags: Optional[str]
+
+
+class MenuModel(BaseModel):
+    _id: UUID
+    node: str
+    sequence: int
+    id: UUID
+    name: str
+    title: str
+    caption: str
+    icon: str
+    path: str
+    route: str
+    colour: str
+    tags: Optional[str]
+    items: Optional[List[ItemModel]]
+
+
+class NavModel(BaseModel):
+    node: str
+    menus: List[MenuModel]
+    
+    
+class AccessModel(BaseModel):
+    navs: Optional[List[NavModel]]
+    uix: dict
