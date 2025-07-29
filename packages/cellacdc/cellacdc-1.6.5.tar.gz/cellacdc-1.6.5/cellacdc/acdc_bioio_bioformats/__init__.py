df82a217e4dcae283e473af9b16bf5e8ad6a788f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:14:09 +0000

    Add package cellacdc/1.6.5/cellacdc-1.6.5.tar.gz

---
+++ packages/cellacdc/cellacdc-1.6.5.tar.gz/cellacdc-1.6.5/cellacdc/acdc_bioio_bioformats/__init__.py
@@ -0,0 +1,22 @@
+import os
+
+from .. import is_win64
+
+conda_prefix = os.environ.get("CONDA_PREFIX")
+if conda_prefix is not None:
+    if is_win64:
+        os.environ["JAVA_HOME"] = rf'{conda_prefix}\Library'
+    else:
+        os.environ["JAVA_HOME"] = conda_prefix
+    
+    print('Setting JAVA_HOME:', os.environ["JAVA_HOME"])
+
+EXTENSION_PACKAGE_MAPPER = {
+    '.czi': 'bioio-czi',
+    '.dv': 'bioio-dv',
+    '.lif': 'bioio-lif',
+    '.nd2': 'bioio-nd2',
+}
+
+from .reader import ImageReader, get_omexml_metadata, OMEXML, Metadata
+from . import _utils
