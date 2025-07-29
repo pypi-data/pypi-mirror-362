3c151af4fed06a64ad6798f7d49688ed7763d77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 20:06:50 +0000

    Add package NS-Common-Keywords/0.1.44/ns_common_keywords-0.1.44.tar.gz

---
+++ packages/NS-Common-Keywords/ns_common_keywords-0.1.44.tar.gz/ns_common_keywords-0.1.44/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NS_Common_Keywords',
+    version='0.1.44',
+    author='Erik ten Asbroek',
+    packages=find_packages(),
+    install_requires=[],
+)
