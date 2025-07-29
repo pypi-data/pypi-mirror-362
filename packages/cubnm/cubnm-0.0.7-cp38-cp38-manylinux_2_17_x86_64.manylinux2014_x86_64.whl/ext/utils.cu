d6571a37ee06c101f05dcbf41e77cfcf2fb9b6bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:14 +0000

    Add package cubnm/0.0.7/cubnm-0.0.7-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cubnm/cubnm-0.0.7-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/ext/utils.cu
@@ -0,0 +1,37 @@
+#include "cubnm/includes.cuh"
+#include "cubnm/defines.h"
+#include "cubnm/utils.cuh"
+
+cudaDeviceProp get_device_prop(int verbose) {
+    /*
+        Gets GPU device properties and prints them to the console.
+        Also throws an error if no GPU is found.
+    */
+    int count;
+    cudaError_t error = cudaGetDeviceCount(&count);
+    struct cudaDeviceProp prop;
+    if (error == cudaSuccess) {
+        int device = 0;
+        CUDA_CHECK_RETURN( cudaGetDeviceProperties(&prop, device) );
+        if (verbose > 0) {
+            std::cout << std::endl << "CUDA device #" << device << ": " << prop.name << std::endl;
+        }
+        if (verbose > 1) {
+            std::cout << "totalGlobalMem: " << prop.totalGlobalMem << ", sharedMemPerBlock: " << prop.sharedMemPerBlock; 
+            std::cout << ", regsPerBlock: " << prop.regsPerBlock << ", warpSize: " << prop.warpSize << ", memPitch: " << prop.memPitch << std::endl;
+            std::cout << "maxThreadsPerBlock: " << prop.maxThreadsPerBlock << ", maxThreadsDim[0]: " << prop.maxThreadsDim[0] 
+                << ", maxThreadsDim[1]: " << prop.maxThreadsDim[1] << ", maxThreadsDim[2]: " << prop.maxThreadsDim[2] << std::endl; 
+            std::cout << "maxGridSize[0]: " << prop.maxGridSize[0] << ", maxGridSize[1]: " << prop.maxGridSize[1] << ", maxGridSize[2]: " 
+                << prop.maxGridSize[2] << ", totalConstMem: " << prop.totalConstMem << std::endl;
+            std::cout << "major: " << prop.major << ", minor: " << prop.minor << ", clockRate: " << prop.clockRate << ", textureAlignment: " 
+                << prop.textureAlignment << ", deviceOverlap: " << prop.deviceOverlap << ", multiProcessorCount: " << prop.multiProcessorCount << std::endl; 
+            std::cout << "kernelExecTimeoutEnabled: " << prop.kernelExecTimeoutEnabled << ", integrated: " << prop.integrated  
+                << ", canMapHostMemory: " << prop.canMapHostMemory << ", computeMode: " << prop.computeMode <<  std::endl; 
+            std::cout << "concurrentKernels: " << prop.concurrentKernels << ", ECCEnabled: " << prop.ECCEnabled << ", pciBusID: " << prop.pciBusID
+                << ", pciDeviceID: " << prop.pciDeviceID << ", tccDriver: " << prop.tccDriver  << std::endl;
+        }
+    } else {
+        throw std::runtime_error(std::string("No CUDA devices was found"));
+    }
+    return prop;
+}
