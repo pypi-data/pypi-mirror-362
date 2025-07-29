159d79d3b4051d911a7fb0a328ae1050eb46eca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:27 +0000

    Add package elinor/0.0.18/elinor-0.0.18.tar.gz

---
+++ packages/elinor/elinor-0.0.18.tar.gz/elinor-0.0.18/elinor/misc/__init__.py
@@ -0,0 +1,10 @@
+from datetime import datetime, timezone
+
+def o_d(td:int=0, tz:timezone=None):
+    if tz is not None:
+        return datetime.now(tz)
+    if td !=0:
+        tz = timezone(datetime.timedelta(hours=td))
+        return datetime.now(tz)
+    now = datetime.now()
+    return now.astimezone()
---
+++ packages/elinor/elinor-0.0.18.tar.gz/elinor-0.0.18/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="elinor",
+    version="0.0.18",
+    author="Dashvvood",
+    author_email="mathismottis@gmail.com",
+    description="some util functions",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "tqdm",
+        "dotenv",
+        "deprecated",
+    ],
+    extra_require={
+        "dev": [
+            "pybase64",
+            "pillow",
+            "pandas",
+            "pyyaml",
+            "bs4",
+            "requests",
+            "regex",
+            "rapidfuzz",
+        ]
+    }
+)
