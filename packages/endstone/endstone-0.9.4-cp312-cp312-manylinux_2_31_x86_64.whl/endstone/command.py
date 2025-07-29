c737f68631ed6baa0e52ce5ea51ae6b42f0f797a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:28 +0000

    Add package endstone/0.9.4/endstone-0.9.4-cp312-cp312-manylinux_2_31_x86_64.whl

---
+++ packages/endstone/endstone-0.9.4-cp312-cp312-manylinux_2_31_x86_64.whl/endstone/command.py
@@ -0,0 +1,17 @@
+from endstone._internal.endstone_python import (
+    Command,
+    CommandExecutor,
+    CommandSender,
+    CommandSenderWrapper,
+    ConsoleCommandSender,
+    ProxiedCommandSender,
+)
+
+__all__ = [
+    "Command",
+    "CommandExecutor",
+    "CommandSender",
+    "CommandSenderWrapper",
+    "ConsoleCommandSender",
+    "ProxiedCommandSender",
+]
