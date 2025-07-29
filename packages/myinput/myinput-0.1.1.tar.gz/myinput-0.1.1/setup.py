ad85c81d4a2f77c93b07725ea8f54b30f5dd8f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:24 +0000

    Add package myinput/0.1.1/myinput-0.1.1.tar.gz

---
+++ packages/myinput/myinput-0.1.1.tar.gz/myinput-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myinput',
+    version='0.1.1',
+    description='A customizable input function that mimics and extends Python’s built-in input()',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Muhammad',
+    author_email='sultanmuhammadarslan72@gmail.com',
+    url='https://github.com/yourusername/myinput',  # Replace with actual GitHub repo if public
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    license='MIT',
+    python_requires='>=3.6',
+)
