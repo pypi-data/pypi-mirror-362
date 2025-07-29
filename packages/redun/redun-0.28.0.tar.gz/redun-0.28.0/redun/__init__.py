c2744a719d18745a07d4974f98d10bbbc8a165aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:06:59 +0000

    Add package redun/0.28.0/redun-0.28.0.tar.gz

---
+++ packages/redun/redun-0.28.0.tar.gz/redun-0.28.0/redun/__init__.py
@@ -0,0 +1,57 @@
+from redun.context import get_context
+from redun.executors.alias import AliasExecutor
+from redun.executors.aws_batch import AWSBatchExecutor
+from redun.executors.aws_glue import AWSGlueExecutor
+from redun.executors.docker import DockerExecutor
+
+try:
+    from redun.executors.k8s import K8SExecutor
+except (ImportError, ModuleNotFoundError):
+    # Skip k8s executor if kubernetes is not installed.
+    pass
+try:
+    from redun.executors.gcp_batch import GCPBatchExecutor
+except (ImportError, ModuleNotFoundError):
+    # Skip gcp_batch executor if google-cloud-batch is not installed.
+    pass
+from redun.executors.local import LocalExecutor
+from redun.file import Dir, File, ShardedS3Dataset
+from redun.handle import Handle
+from redun.namespace import get_current_namespace, namespace
+from redun.scheduler import (
+    Scheduler,
+    apply_tags,
+    catch,
+    cond,
+    get_current_scheduler,
+    merge_handles,
+    set_current_scheduler,
+    throw,
+)
+from redun.scripting import script
+from redun.task import PartialTask, Task, get_task_registry, task
+from redun.version import version
+
+__version__ = version
+__all__ = [
+    "Dir",
+    "File",
+    "Handle",
+    "PartialTask",
+    "Scheduler",
+    "ShardedS3Dataset",
+    "Task",
+    "apply_tags",
+    "catch",
+    "cond",
+    "get_current_namespace",
+    "get_current_scheduler",
+    "get_task_registry",
+    "merge_handles",
+    "namespace",
+    "script",
+    "set_current_scheduler",
+    "task",
+    "throw",
+    "version",
+]
---
+++ packages/redun/redun-0.28.0.tar.gz/redun-0.28.0/redun/executors/base.py
@@ -0,0 +1,161 @@
+import importlib
+import typing
+from typing import Any, Callable, Dict, Iterator, Optional, Type, cast
+
+if typing.TYPE_CHECKING:
+    from redun.scheduler import Job, Scheduler
+
+
+class ExecutorError(Exception):
+    pass
+
+
+class Executor:
+    """Note that most Executors should Register themselves with the method `register_executor`,
+    below."""
+
+    def __init__(
+        self,
+        name: str,
+        scheduler: Optional["Scheduler"] = None,
+        config=None,
+    ):
+        self.name = name
+        self._scheduler = scheduler
+
+    def set_scheduler(self, scheduler: "Scheduler") -> None:
+        self._scheduler = scheduler
+
+    def supports_async(self) -> bool:
+        """
+        Returns True if Executor supports async tasks.
+        """
+        return False
+
+    def log(self, *messages: Any, **kwargs) -> None:
+        """
+        Display log message through Scheduler.
+        """
+        assert self._scheduler
+        self._scheduler.log(f"Executor[{self.name}]:", *messages, **kwargs)
+
+    def submit(self, job: "Job") -> None:
+        """Execute the provided job.
+
+        Implementations must provide results back to the scheduler by either calling `done_job` or
+        `reject_job`."""
+        assert self._scheduler
+        self._scheduler.reject_job(
+            job,
+            ExecutorError("Executor {} does not support submitting tasks.".format(type(self))),
+        )
+
+    def submit_script(self, job: "Job") -> None:
+        """Execute the provided script job.
+
+        Implementations must provide results back to the scheduler by either calling `done_job` or
+        `reject_job`."""
+
+        assert self._scheduler
+        self._scheduler.reject_job(
+            job,
+            ExecutorError(
+                "Executor {} does not support submitting script tasks.".format(type(self))
+            ),
+        )
+
+    def start(self) -> None:
+        pass
+
+    def stop(self) -> None:
+        pass
+
+    def scratch_root(self) -> str:
+        raise NotImplementedError()
+
+
+# Singleton executor registry.
+_executor_classes: Dict[str, Type[Executor]] = {}
+
+
+def get_executor_class(executor_name: str, required: bool = True) -> Optional[Type[Executor]]:
+    """
+    Get an Executor by name from the executor registry.
+
+    Parameters
+    ----------
+    executor_name : str
+        Name of executor class to retrieve.
+    required : bool
+        If True, raises error if executor is not registered.
+        If False, None is returned for unknown executor name.
+    """
+    executor_class = _executor_classes.get(executor_name)
+    if required and not executor_class:
+        raise ExecutorError("Unknown executor {}".format(executor_name))
+    return executor_class
+
+
+def _register_executor(executor_name: str, executor_class: Type[Executor]) -> None:
+    """
+    Register an Executor class to be used by the scheduler.
+    """
+    _executor_classes[executor_name] = executor_class
+
+
+def register_executor(executor_name: str) -> Callable:
+    """
+    Register an Executor class to be used by the scheduler.
+
+    Note that registered classes are responsible for ensuring their modules get loaded, so this
+    decorator is actually run. For example, this can be done by adding them to the redun
+    module `__init__.py`.
+    """
+
+    def deco(executor_class: Type[Executor]):
+        _register_executor(executor_name, executor_class)
+        return executor_class
+
+    return deco
+
+
+def get_executors_from_config(executors_config: dict) -> Iterator[Executor]:
+    """
+    Instantiate executors defined in an executors config section.
+    """
+    for executor_name, executor_config in executors_config.items():
+        executor_class = cast(Type[Executor], get_executor_class(executor_config["type"]))
+        executor = executor_class(executor_name, config=executor_config)
+        yield executor
+
+
+def get_executor_from_config(executors_config: dict, executor_name: str) -> Executor:
+    """Create and return the executor by name. Raise an error if it is not present."""
+    for name, executor_config in executors_config.items():
+        if name != executor_name:
+            continue
+
+        executor_class = cast(Type[Executor], get_executor_class(executor_config["type"]))
+        executor = executor_class(executor_name, config=executor_config)
+        return executor
+    raise RuntimeError(f"Unknown Executor {executor_name}.")
+
+
+def load_task_module(module_name: str, task_name: str) -> None:
+    """
+    Helper method that Executors may call to load a task's module code.
+
+    Presently, the main benefit is this method provides better error handling.
+
+    Args:
+          module_name: Task module name.
+          task_name: Task name
+    """
+    try:
+        importlib.import_module(module_name)
+    except ModuleNotFoundError as exc:
+        # Append user-friendly error to original exception
+        raise ModuleNotFoundError(
+            f"Failed to find module {module_name} for task {task_name}. "
+            "If this is remotely executed, ensure module is included in code packaging."
+        ) from exc
