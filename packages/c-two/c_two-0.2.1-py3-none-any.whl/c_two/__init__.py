f9cf792b7cb20539492cd95d9567548e64856e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:04 +0000

    Add package c-two/0.2.1/c_two-0.2.1-py3-none-any.whl

---
+++ packages/c-two/c_two-0.2.1-py3-none-any.whl/c_two/__init__.py
@@ -0,0 +1,7 @@
+from . import rpc
+from . import mcp
+from . import crm
+from . import compo
+from . import error
+from .crm.meta import icrm, iicrm
+from .rpc.transferable import transfer, auto_transfer, Transferable, transferable
