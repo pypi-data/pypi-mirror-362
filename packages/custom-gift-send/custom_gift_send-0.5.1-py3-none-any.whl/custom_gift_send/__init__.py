2abb816809f132e5f0d08616bf96741bf20f0211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:28 +0000

    Add package custom-gift-send/0.5.1/custom_gift_send-0.5.1-py3-none-any.whl

---
+++ packages/custom-gift-send/custom_gift_send-0.5.1-py3-none-any.whl/custom_gift_send/__init__.py
@@ -0,0 +1,17 @@
+from .custom_gift_send import (
+    CustomGiftSend,
+    TelegramAPIError,
+    TelegramUnauthorizedError,
+    TelegramForbiddenError,
+    TelegramBadRequestError,
+    TelegramNotFoundError,
+    TelegramTooManyRequestsError,
+    SendGiftParams,
+    GiftPremiumParams,
+    TransferGiftParams,
+    GetUpdatesParams,
+    SetWebhookParams,
+    DeleteWebhookParams,
+    GetUserChatBoostsParams,
+    RefundStarPaymentParams
+)
