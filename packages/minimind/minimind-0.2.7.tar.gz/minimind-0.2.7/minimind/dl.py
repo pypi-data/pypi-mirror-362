1909a49ace3532a8032dc4856d67f878f3de7d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:49 +0000

    Add package minimind/0.2.7/minimind-0.2.7.tar.gz

---
+++ packages/minimind/minimind-0.2.7.tar.gz/minimind-0.2.7/minimind/dl.py
@@ -0,0 +1,19 @@
+from .TinyMLP import NeuralGenerator, MLPTrainer
+from .seprod import SeProD, SeProDTrainer
+from .Conv2D import conv2d, CNN_MNIST, predict, load_mnist, accuracy
+from vae import VAE, upsample, init_params
+__all__ = [
+    "NeuralGenerator",
+    "SeProD",
+    "MLPTrainer",
+    "SeProDTrainer",
+    "conv2d",
+    "CNN_MNIST",
+    "predict",
+    "load_mnist",
+    "accuracy",
+    "VAE",
+    "upsample",
+    "init_params"
+    
+]
