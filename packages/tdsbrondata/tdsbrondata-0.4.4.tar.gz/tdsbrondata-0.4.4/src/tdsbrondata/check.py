b3582800b9626016e975591b415deb92811fdc8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:31 +0000

    Add package tdsbrondata/0.4.4/tdsbrondata-0.4.4.tar.gz

---
+++ packages/tdsbrondata/tdsbrondata-0.4.4.tar.gz/tdsbrondata-0.4.4/src/tdsbrondata/check.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import tdsbrondata
+from pyspark.sql import functions as F
+from pyspark.sql.types import *
+
+def afasDebtor(dataToCheck, lakehouseName, schemaName, tableName, afasDebtors, idColumn, debtorIdColumn):
+    afasTrimmed = afasDebtors.select(
+        F.col("Verkooprelatie_id").alias("afas_id"),
+        "Geblokkeerd_voor_levering",
+        "Volledig_blokkeren"
+    )
+
+    result = dataToCheck.join(afasTrimmed, dataToCheck[debtorIdColumn] == F.col("afas_id"), how="left") \
+        .withColumn("remarks",
+            F.when(F.col(debtorIdColumn).isNull(), "Debtor is empty")
+             .when(F.col("afas_id").isNull(), "Debtor not found in AFAS")
+             .when((F.col("Geblokkeerd_voor_levering") == True) | (F.col("Volledig_blokkeren") == True), "Debtor is blocked in AFAS")
+        ) \
+        .filter(F.col("remarks").isNotNull()) \
+        .withColumn("lakehouseName", F.lit(lakehouseName)) \
+        .withColumn("schemaName", F.lit(schemaName)) \
+        .withColumn("tableName", F.lit(tableName)) \
+        .withColumn("id", F.lit(idColumn)) \
+        .withColumn("Verkooprelatie_id", F.col(debtorIdColumn)) \
+        .select("lakehouseName", "schemaName", "tableName", idColumn, "SurrogateKey", "Verkooprelatie_id", "remarks")
+
+    return result
