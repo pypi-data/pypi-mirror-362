2777527bed8e84b3db58c7d7a41f9b5703b9d3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:49 +0000

    Add package solvice-vrp-solver/0.1.0a2/solvice_vrp_solver-0.1.0a2-py3-none-any.whl

---
+++ packages/solvice-vrp-solver/solvice_vrp_solver-0.1.0a2-py3-none-any.whl/solvice_vrp_solver/resources/vrp/__init__.py
@@ -0,0 +1,33 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .vrp import (
+    VrpResource,
+    AsyncVrpResource,
+    VrpResourceWithRawResponse,
+    AsyncVrpResourceWithRawResponse,
+    VrpResourceWithStreamingResponse,
+    AsyncVrpResourceWithStreamingResponse,
+)
+from .solves import (
+    SolvesResource,
+    AsyncSolvesResource,
+    SolvesResourceWithRawResponse,
+    AsyncSolvesResourceWithRawResponse,
+    SolvesResourceWithStreamingResponse,
+    AsyncSolvesResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "SolvesResource",
+    "AsyncSolvesResource",
+    "SolvesResourceWithRawResponse",
+    "AsyncSolvesResourceWithRawResponse",
+    "SolvesResourceWithStreamingResponse",
+    "AsyncSolvesResourceWithStreamingResponse",
+    "VrpResource",
+    "AsyncVrpResource",
+    "VrpResourceWithRawResponse",
+    "AsyncVrpResourceWithRawResponse",
+    "VrpResourceWithStreamingResponse",
+    "AsyncVrpResourceWithStreamingResponse",
+]
