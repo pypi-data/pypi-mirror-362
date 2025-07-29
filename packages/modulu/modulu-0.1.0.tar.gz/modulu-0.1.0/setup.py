b16e02d572f8bd55cffb7ceab347e4bd68a592b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:47 +0000

    Add package modulu/0.1.0/modulu-0.1.0.tar.gz

---
+++ packages/modulu/modulu-0.1.0.tar.gz/modulu-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="modulu",
+    version="0.1.0",
+    py_modules=["modulu"],
+    author="Your Name",
+    description="Working on it!",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
