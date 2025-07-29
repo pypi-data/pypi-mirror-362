eb66c76a4b23daba17cdc6c569e2cfe0f3687e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:43 +0000

    Add package dsp-tools/16.3.0.post12/dsp_tools-16.3.0.post12-py3-none-any.whl

---
+++ packages/dsp-tools/dsp_tools-16.3.0.post12-py3-none-any.whl/dsp_tools/commands/xmlupload/write_diagnostic_info.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import json
+from datetime import datetime
+
+from loguru import logger
+
+from dsp_tools.commands.xmlupload.upload_config import DiagnosticsConfig
+
+
+def write_id2iri_mapping(
+    id2iri_mapping: dict[str, str],
+    shortcode: str,
+    diagnostics: DiagnosticsConfig,
+) -> None:
+    """Writes the mapping of internal IDs to IRIs to a file."""
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H%M%S")
+    servername = diagnostics.server_as_foldername
+    id2iri_filename = f"id2iri_{shortcode}_{servername}_{timestamp}.json"
+    with open(id2iri_filename, "x", encoding="utf-8") as f:
+        json.dump(id2iri_mapping, f, ensure_ascii=False, indent=4)
+        print(f"{datetime.now()}: The mapping of internal IDs to IRIs was written to {id2iri_filename}")
+        logger.info(f"The mapping of internal IDs to IRIs was written to {id2iri_filename}")
