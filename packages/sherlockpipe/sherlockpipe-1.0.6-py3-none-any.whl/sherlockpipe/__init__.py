76005826c54554b8eb21b68412c1bda69c6a3bdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:43 +0000

    Add package sherlockpipe/1.0.6/sherlockpipe-1.0.6-py3-none-any.whl

---
+++ packages/sherlockpipe/sherlockpipe-1.0.6-py3-none-any.whl/sherlockpipe/__init__.py
@@ -0,0 +1,27 @@
+__version__ = "1.0.6"
+
+import sys
+
+#Patching ellc with submodule
+import sherlockpipe.ellc as _mypackage_ellc
+
+# Override the 'ellc' name in sys.modules to point to your internal package module
+sys.modules['ellc'] = _mypackage_ellc
+
+# Patching all errors due to SSL certificates
+import requests
+import warnings
+import urllib3
+warnings.filterwarnings("ignore", category=urllib3.exceptions.InsecureRequestWarning)
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Original request method
+_original_request = requests.Session.request
+# Overwrite to enforce verify=False by default
+def no_verify_request(self, *args, **kwargs):
+    kwargs.setdefault("verify", False)  # solo cambia si no se pasa ya 'verify'
+    return _original_request(self, *args, **kwargs)
+# Apply patch
+requests.Session.request = no_verify_request
