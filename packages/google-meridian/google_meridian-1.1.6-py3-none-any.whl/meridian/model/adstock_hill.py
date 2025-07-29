9036c98aea27d768953f4e7f7fa6957274d7a550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:01 +0000

    Add package google-meridian/1.1.6/google_meridian-1.1.6-py3-none-any.whl

---
+++ packages/google-meridian/google_meridian-1.1.6-py3-none-any.whl/meridian/model/adstock_hill.py
@@ -0,0 +1,236 @@
+# Copyright 2025 The Meridian Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Function definitions for Adstock and Hill calculations."""
+
+import abc
+import tensorflow as tf
+
+__all__ = [
+    'AdstockHillTransformer',
+    'AdstockTransformer',
+    'HillTransformer',
+]
+
+
+def _validate_arguments(
+    media: tf.Tensor, alpha: tf.Tensor, max_lag: int, n_times_output: int
+) -> None:
+  batch_dims = alpha.shape[:-1]
+  n_media_times = media.shape[-2]
+
+  if n_times_output > n_media_times:
+    raise ValueError(
+        '`n_times_output` cannot exceed number of time periods in the media'
+        ' data.'
+    )
+  if media.shape[:-3] not in [tf.TensorShape([]), tf.TensorShape(batch_dims)]:
+    raise ValueError(
+        '`media` batch dims do not match `alpha` batch dims. If `media` '
+        'has batch dims, then they must match `alpha`.'
+    )
+  if media.shape[-1] != alpha.shape[-1]:
+    raise ValueError(
+        '`media` contains a different number of channels than `alpha`.'
+    )
+  if n_times_output <= 0:
+    raise ValueError('`n_times_output` must be positive.')
+  if max_lag < 0:
+    raise ValueError('`max_lag` must be non-negative.')
+
+
+def _adstock(
+    media: tf.Tensor,
+    alpha: tf.Tensor,
+    max_lag: int,
+    n_times_output: int,
+) -> tf.Tensor:
+  """Computes the Adstock function."""
+  _validate_arguments(
+      media=media, alpha=alpha, max_lag=max_lag, n_times_output=n_times_output
+  )
+  # alpha dims: batch_dims, n_media_channels.
+  # media dims: batch_dims (optional), n_geos, n_media_times, n_channels.
+  n_media_times = media.shape[-2]
+
+  # The window size is the number of time periods that go into the adstock
+  # weighted average for each output time period.
+  window_size = min(max_lag + 1, n_media_times)
+
+  # Drop any excess historical time periods that do not affect output.
+  required_n_media_times = n_times_output + window_size - 1
+  if n_media_times > required_n_media_times:
+    # Note that ProductCoverage believes that unit tests should cover the case
+    # that both conditions (1) `n_media_times > required_n_media_times` and
+    # (2) `window_size = n_media_times`. However, this combination of conditions
+    # is not possible.
+    media = media[..., -required_n_media_times:, :]
+
+  # If necessary, pad the media tensor with zeros. For each output time period,
+  # we need a media history of at least `window_size` time periods from which to
+  # calculate the adstock. The purpose of padding is to allow us to apply
+  # a fixed window size calculation to all time periods. The purpose is NOT to
+  # ensure that we have `max_lag` historical time periods for each output time
+  # period. If `max_lag` is set to a huge value, it is not necessary to pad the
+  # data with a huge number of zeros. The `normalization_factors` normalize
+  # the weights to the correct values even if `window_size` < `max_lag`+1.
+  if n_media_times < required_n_media_times:
+    pad_shape = (
+        media.shape[:-2]
+        + (required_n_media_times - n_media_times,)
+        + (media.shape[-1],)
+    )
+    media = tf.concat([tf.zeros(pad_shape), media], axis=-2)
+
+  # Adstock calculation.
+  window_list = [None] * window_size
+  for i in range(window_size):
+    window_list[i] = media[..., i:i+n_times_output, :]
+  windowed = tf.stack(window_list)
+  l_range = tf.range(window_size - 1, -1, -1, dtype=tf.float32)
+  weights = tf.expand_dims(alpha, -1) ** l_range
+  normalization_factors = tf.expand_dims(
+      (1 - alpha ** (window_size)) / (1 - alpha), -1
+  )
+  weights = tf.divide(weights, normalization_factors)
+  return tf.einsum('...mw,w...gtm->...gtm', weights, windowed)
+
+
+def _hill(
+    media: tf.Tensor,
+    ec: tf.Tensor,
+    slope: tf.Tensor,
+) -> tf.Tensor:
+  """Computes the Hill function."""
+  batch_dims = slope.shape[:-1]
+
+  # Argument checks.
+  if slope.shape != ec.shape:
+    raise ValueError('`slope` and `ec` dimensions do not match.')
+  if media.shape[:-3] not in [tf.TensorShape([]), tf.TensorShape(batch_dims)]:
+    raise ValueError(
+        '`media` batch dims do not match `slope` and `ec` batch dims. '
+        'If `media` has batch dims, then they must match `slope` and '
+        '`ec`.'
+    )
+  if media.shape[-1] != slope.shape[-1]:
+    raise ValueError(
+        '`media` contains a different number of channels than `slope` and `ec`.'
+    )
+
+  t1 = media ** slope[..., tf.newaxis, tf.newaxis, :]
+  t2 = (ec**slope)[..., tf.newaxis, tf.newaxis, :]
+  return t1 / (t1 + t2)
+
+
+class AdstockHillTransformer(metaclass=abc.ABCMeta):
+  """Abstract class to compute the Adstock and Hill transformation of media."""
+
+  @abc.abstractmethod
+  def forward(self, media: tf.Tensor) -> tf.Tensor:
+    """Computes the Adstock and Hill transformation of a given media tensor."""
+    pass
+
+
+class AdstockTransformer(AdstockHillTransformer):
+  """Computes the Adstock transformation of media."""
+
+  def __init__(self, alpha: tf.Tensor, max_lag: int, n_times_output: int):
+    """Initializes this transformer based on Adstock function parameters.
+
+    Args:
+      alpha: Tensor of `alpha` parameters taking values ≥ `[0, 1)` with
+        dimensions `[..., n_media_channels]`. Batch dimensions `(...)` are
+        optional. Note that `alpha = 0` is allowed, so it is possible to put a
+        point mass prior at zero (effectively no Adstock). However, `alpha = 1`
+        is not allowed since the geometric sum formula is not defined, and there
+        is no practical reason to have point mass at `alpha = 1`.
+      max_lag: Integer indicating the maximum number of lag periods (≥ `0`) to
+        include in the Adstock calculation.
+      n_times_output: Integer indicating the number of time periods to include
+        in the output tensor. Cannot exceed the number of time periods of the
+        media argument, for example, `media.shape[-2]`. The output time periods
+        correspond to the most recent time periods of the media argument. For
+        example, `media[..., -n_times_output:, :]` represents the media
+        execution of the output weeks.
+    """
+    self._alpha = alpha
+    self._max_lag = max_lag
+    self._n_times_output = n_times_output
+
+  def forward(self, media: tf.Tensor) -> tf.Tensor:
+    """Computes the Adstock transformation of a given `media` tensor.
+
+    For geo `g`, time period `t`, and media channel `m`, Adstock is calculated
+    as `adstock_{g,t,m} = sum_{i=0}^max_lag media_{g,t-i,m} alpha^i`.
+
+    Note: The Hill function can be applied before or after Adstock. If Hill is
+    applied first, then the Adstock media input can contain batch dimensions
+    because the transformed media tensor will be different for each posterior
+    sample.
+
+    Args:
+      media: Tensor of media values with dimensions `[..., n_geos,
+        n_media_times, n_media_channels]`. Batch dimensions `(...)` are
+        optional, but if batch dimensions are included, they must match the
+        batch dimensions of `alpha`. Media is not required to have batch
+        dimensions even if `alpha` contains batch dimensions.
+
+    Returns:
+      Tensor with dimensions `[..., n_geos, n_times_output, n_media_channels]`
+      representing Adstock transformed media.
+    """
+    return _adstock(
+        media=media,
+        alpha=self._alpha,
+        max_lag=self._max_lag,
+        n_times_output=self._n_times_output,
+    )
+
+
+class HillTransformer(AdstockHillTransformer):
+  """Class to compute the Hill transformation of media."""
+
+  def __init__(self, ec: tf.Tensor, slope: tf.Tensor):
+    """Initializes the instance based on the Hill function parameters.
+
+    Args:
+      ec: Tensor with dimensions `[..., n_media_channels]`. Batch dimensions
+        `(...)` are optional, but if batch dimensions are included, they must
+        match the batch dimensions of `ec`.
+      slope: Tensor with dimensions `[..., n_media_channels]`. Batch dimensions
+        `(...)` are optional, but if batch dimensions are included, they must
+        match the batch dimensions of `slope`.
+    """
+    self._ec = ec
+    self._slope = slope
+
+  def forward(self, media: tf.Tensor) -> tf.Tensor:
+    """Computes the Hill transformation of a given `media` tensor.
+
+    Calculates results for the Hill function, which accounts for the diminishing
+    returns of media effects.
+
+    Args:
+      media: Tensor with dimensions `[..., n_geos, n_media_times,
+        n_media_channels]`. Batch dimensions `(...)` are optional, but if batch
+        dimensions are included, they must match the batch dimensions of `slope`
+        and `ec`. Media is not required to have batch dimensions even if `slope`
+        and `ec` contain batch dimensions.
+
+    Returns:
+      Tensor with dimensions `[..., n_geos, n_media_times, n_media_channels]`
+      representing Hill-transformed media.
+    """
+    return _hill(media=media, ec=self._ec, slope=self._slope)
---
+++ packages/google-meridian/google_meridian-1.1.6-py3-none-any.whl/meridian/model/knots.py
@@ -0,0 +1,210 @@
+# Copyright 2025 The Meridian Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Auxiliary functions for knots calculations."""
+
+import bisect
+from collections.abc import Collection, Sequence
+import dataclasses
+import numpy as np
+
+
+__all__ = [
+    'KnotInfo',
+    'get_knot_info',
+    'l1_distance_weights',
+]
+
+
+# TODO: Reimplement with a more readable method.
+def _find_neighboring_knots_indices(
+    times: np.ndarray,
+    knot_locations: np.ndarray,
+) -> Sequence[Sequence[int] | None]:
+  """Return indices of neighboring knot locations.
+
+  Returns indices in `knot_locations` that correspond to the neighboring knot
+  locations for each time period. If a time point is at or before the first
+  knot, the first knot is the only neighboring knot. If a time point is after
+  the last knot, the last knot is the only neighboring knot.
+
+  Args:
+    times: Times `0, 1, 2,..., (n_times-1)`.
+    knot_locations: The location of knots within `0, 1, 2,..., (n_times-1)`.
+
+  Returns:
+    List of length `n_times`. Each element is the indices of the neighboring
+    knot locations for the respective time period. If a time point is at or
+    before the first knot, the first knot is the only neighboring knot. If a
+    time point is after the last knot, the last knot is the only neighboring
+    knot.
+  """
+  neighboring_knots_indices = [None] * len(times)
+  for t in times:
+    # knot_locations assumed to be sorted.
+    if t <= knot_locations[0]:
+      neighboring_knots_indices[t] = [0]
+    elif t >= knot_locations[-1]:
+      neighboring_knots_indices[t] = [len(knot_locations) - 1]
+    else:
+      bisect_index = bisect.bisect_left(knot_locations, t)
+      neighboring_knots_indices[t] = [bisect_index - 1, bisect_index]
+  return neighboring_knots_indices
+
+
+def l1_distance_weights(
+    n_times: int, knot_locations: np.ndarray[int, np.dtype[int]]
+) -> np.ndarray:
+  """Computes weights at knots for every time period.
+
+  The two neighboring knots inform the weight estimate of a particular time
+  period. The amount each of the two neighboring knots inform the weight at a
+  time period depends on how close (L1 distance) they are to the time period. If
+  a time point coincides with a knot location, then 100% weight is given to that
+  knot. If a time point lies outside the range of knots, then 100% weight is
+  given to the nearest endpoint knot.
+
+  This function computes an `(n_knots, n_times)` array of weights that are used
+  to model trend and seasonality. For a given time, the array contains two
+  non-zero weights. The weights are inversely proportional to the L1 distance
+  from the given time to the neighboring knots. The two weights are normalized
+  such that they sum to 1.
+
+  Args:
+    n_times: The number of time points.
+    knot_locations: The location of knots within `0, 1, 2,..., (n_times-1)`.
+
+  Returns:
+    A weight array with dimensions `(n_knots, n_times)` with values summing up
+    to 1 for each time period when summing over knots.
+  """
+  if knot_locations.ndim != 1:
+    raise ValueError('`knot_locations` must be one-dimensional.')
+  if not np.all(knot_locations == np.sort(knot_locations)):
+    raise ValueError('`knot_locations` must be sorted.')
+  if len(knot_locations) <= 1:
+    raise ValueError('Number of knots must be greater than 1.')
+  if len(knot_locations) != len(np.unique(knot_locations)):
+    raise ValueError('`knot_locations` must be unique.')
+  if np.any(knot_locations < 0):
+    raise ValueError('knot_locations must be positive.')
+  if np.any(knot_locations >= n_times):
+    raise ValueError('knot_locations must be less than `n_times`.')
+
+  times = np.arange(n_times)
+  time_minus_knot = abs(knot_locations[:, np.newaxis] - times[np.newaxis, :])
+
+  w = np.zeros(time_minus_knot.shape, dtype=np.float32)
+  neighboring_knots_indices = _find_neighboring_knots_indices(
+      times, knot_locations
+  )
+  for t in times:
+    idx = neighboring_knots_indices[t]
+    if len(idx) == 1:
+      w[idx, t] = 1
+    else:
+      # Weight is in proportion to how close the two neighboring knots are.
+      w[idx, t] = 1 - (time_minus_knot[idx, t] / time_minus_knot[idx, t].sum())
+
+  return w
+
+
+def _get_equally_spaced_knot_locations(n_times, n_knots):
+  """Equally spaced knot locations starting at the endpoints."""
+  return np.linspace(0, n_times - 1, n_knots, dtype=int)
+
+
+@dataclasses.dataclass(frozen=True)
+class KnotInfo:
+  """Contains the number of knots, knot locations, and weights.
+
+  Attributes:
+    n_knots: The number of knots
+    knot_locations: The location of knots
+    weights: The weights used to multiply with the knot values to get time-
+      varying coefficients.
+  """
+
+  n_knots: int
+  knot_locations: np.ndarray[int, np.dtype[int]]
+  weights: np.ndarray[int, np.dtype[float]]
+
+
+def get_knot_info(
+    n_times: int,
+    knots: int | Collection[int] | None,
+    is_national: bool = False,
+) -> KnotInfo:
+  """Returns the number of knots, knot locations, and weights.
+
+  Args:
+    n_times: The number of time periods in the data.
+    knots: An optional integer or a collection of integers indicating the knots
+      used to estimate time effects. When `knots` is a collection of integers,
+      the knot locations are provided by that collection. Zero corresponds to a
+      knot at the first time period, one corresponds to a knot at the second
+      time, ..., and `(n_times - 1)` corresponds to a knot at the last time
+      period. When `knots` is an integer, then there are knots with locations
+      equally spaced across the time periods (including knots at zero and
+      `(n_times - 1)`. When `knots` is `1`, there is a single common regression
+      coefficient used for all time periods. If `knots` is `None`, then the
+      numbers of knots used is equal to the number of time periods. This is
+      equivalent to each time period having its own regression coefficient.
+    is_national: A boolean indicator whether to adapt the knot information for a
+      national model.
+
+  Returns:
+    A KnotInfo that contains the number of knots, the location of knots, and the
+    weights used to multiply with the knot values to get time-varying
+    coefficients.
+  """
+
+  if isinstance(knots, int):
+    if knots < 1:
+      raise ValueError('If knots is an integer, it must be at least 1.')
+    elif knots > n_times:
+      raise ValueError(
+          f'The number of knots ({knots}) cannot be greater than the number of'
+          f' time periods in the kpi ({n_times}).'
+      )
+    elif is_national and knots == n_times:
+      raise ValueError(
+          f'Number of knots ({knots}) must be less than number of time periods'
+          f' ({n_times}) in a nationally aggregated model.'
+      )
+    n_knots = knots
+    knot_locations = _get_equally_spaced_knot_locations(n_times, n_knots)
+  elif isinstance(knots, Collection) and knots:
+    if any(k < 0 for k in knots):
+      raise ValueError('Knots must be all non-negative.')
+    if any(k >= n_times for k in knots):
+      raise ValueError(
+          'Knots must all be less than the number of time periods.'
+      )
+    n_knots = len(knots)
+    # np.unique also sorts
+    knot_locations = np.unique(knots)
+  elif isinstance(knots, Collection):
+    raise ValueError('Knots cannot be empty.')
+  else:
+    # knots is None
+    n_knots = 1 if is_national else n_times
+    knot_locations = _get_equally_spaced_knot_locations(n_times, n_knots)
+
+  if n_knots == 1:
+    weights = np.ones((1, n_times), dtype=np.float32)
+  else:
+    weights = l1_distance_weights(n_times, knot_locations)
+
+  return KnotInfo(n_knots, knot_locations, weights)
