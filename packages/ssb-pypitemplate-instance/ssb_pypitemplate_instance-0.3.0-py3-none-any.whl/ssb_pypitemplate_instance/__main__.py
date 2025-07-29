fe293008bf0cfd983533c358cc39cc285d2b208e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:36 +0000

    Add package ssb-pypitemplate-instance/0.3.0/ssb_pypitemplate_instance-0.3.0-py3-none-any.whl

---
+++ packages/ssb-pypitemplate-instance/ssb_pypitemplate_instance-0.3.0-py3-none-any.whl/ssb_pypitemplate_instance/__main__.py
@@ -0,0 +1,13 @@
+"""Command-line interface."""
+
+import click
+
+
+@click.command()
+@click.version_option()
+def main() -> None:
+    """SSB Pypitemplate Instance."""
+
+
+if __name__ == "__main__":
+    main(prog_name="ssb-pypitemplate-instance")  # pragma: no cover
