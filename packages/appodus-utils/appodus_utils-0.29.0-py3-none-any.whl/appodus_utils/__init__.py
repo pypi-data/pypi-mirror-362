5d81d57a039858e69d7972963fda513605e79609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:33 +0000

    Add package appodus-utils/0.29.0/appodus_utils-0.29.0-py3-none-any.whl

---
+++ packages/appodus-utils/appodus_utils-0.29.0-py3-none-any.whl/appodus_utils/__init__.py
@@ -0,0 +1,25 @@
+from .common import RouterUtils, Utils, Base64Utils, FileUtils
+from .config.logger import LoggerFactory
+from .db.db_utils import DbUtils
+from .db.models import (
+    AutoRepr, Object, BaseEntity, ModelType, CreateSchemaType, UpdateSchemaType,
+    SearchSchemaType, QuerySchemaType, BaseQueryDto, Page, PageRequest
+)
+
+
+__all__ = [
+    "Utils", "RouterUtils", "Base64Utils", "FileUtils",
+    "DbUtils",
+    "AutoRepr",
+    "Object",
+    "BaseEntity",
+    "ModelType",
+    "CreateSchemaType",
+    "UpdateSchemaType",
+    "SearchSchemaType",
+    "QuerySchemaType",
+    "BaseQueryDto",
+    "Page",
+    "PageRequest",
+    "LoggerFactory"
+]
