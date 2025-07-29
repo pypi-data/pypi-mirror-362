9015dd067190785435b32cf18d9f61561442079a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:28 +0000

    Add package lino/25.7.1/lino-25.7.1-py3-none-any.whl

---
+++ packages/lino/lino-25.7.1-py3-none-any.whl/lino/modlib/office/roles.py
@@ -0,0 +1,17 @@
+# Copyright 2015-2023 Rumma & Ko Ltd
+# License: GNU Affero General Public License v3 (see file COPYING for details)
+
+from lino.core.roles import UserRole
+from lino.modlib.uploads.roles import UploadsReader
+
+
+class OfficeUser(UploadsReader):
+    pass
+
+
+class OfficeOperator(UploadsReader):
+    pass
+
+
+class OfficeStaff(OfficeUser, OfficeOperator):
+    pass
