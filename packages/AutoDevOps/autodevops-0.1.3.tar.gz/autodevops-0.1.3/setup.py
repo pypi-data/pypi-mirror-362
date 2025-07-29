4eefd93ae132108e0c24f3b67301c38e069d05ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:16 +0000

    Add package AutoDevOps/0.1.3/autodevops-0.1.3.tar.gz

---
+++ packages/AutoDevOps/autodevops-0.1.3.tar.gz/autodevops-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AutoDevOps',
+    version='0.1.3',
+    description='CLI tool to automate Git workflow',
+    author='Your Name',
+    author_email='your.email@example.com',
+    packages=find_packages(),
+    install_requires=['click'],
+    entry_points={
+        'console_scripts': [
+            'AutoDevOps=AutoDevOps.cli:AutoDevOps',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.6',
+)
