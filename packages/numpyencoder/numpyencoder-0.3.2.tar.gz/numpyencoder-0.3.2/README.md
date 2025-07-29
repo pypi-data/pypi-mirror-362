40d806c1ac064e163f68fbc991ee8e648347e5a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:34 +0000

    Add package numpyencoder/0.3.2/numpyencoder-0.3.2.tar.gz

---
+++ packages/numpyencoder/numpyencoder-0.3.2.tar.gz/numpyencoder-0.3.2/README.md
@@ -0,0 +1,29 @@
+# numpyencoder
+
+Custom Python JSON encoder for handling Numpy data types.
+
+**Example Use:**
+
+```python
+from numpyencoder import NumpyEncoder
+
+numpy_data = np.array([0, 1, 2, 3])
+
+with open(json_file, 'w') as file:
+    json.dump(
+        numpy_data,
+        file,
+        indent=4,
+        sort_keys=True,
+        separators=(', ', ': '),
+        ensure_ascii=False,
+        cls=NumpyEncoder
+    )
+```
+
+## Installation
+
+```
+pip install numpyencoder==0.3.2
+```
+
---
+++ packages/numpyencoder/numpyencoder-0.3.2.tar.gz/numpyencoder-0.3.2/numpyencoder/numpyencoder.py
@@ -0,0 +1,68 @@
+import warnings
+from packaging.version import parse as parse_version
+import json
+import numpy as np
+
+
+# Both np.float_ and np.complex_ were deprecated in Numpy 2.0.0
+if parse_version(np.__version__) < parse_version("2.0.0"):
+    TYPES = {
+        "float": (np.float_, np.float16, np.float32, np.float64),
+        "complex": (np.complex_, np.complex64, np.complex128),
+    }
+    warnings.warn(
+        f"You are using an old version of Numpy ({np.__version__}), "
+        "support for 'np.float_' and 'np.complex_' were deprecated "
+        "in Numpy 2.0.0. If you use these datatypes in your code "
+        "please consider updating them.",
+        category=DeprecationWarning,
+    )
+else:
+    TYPES = {
+        "float": (np.float16, np.float32, np.float64),
+        "complex": (np.complex64, np.complex128),
+    }
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """Custom encoder for numpy data types"""
+
+    def default(self, obj):
+        if isinstance(
+            obj,
+            (
+                np.int_,
+                np.intc,
+                np.intp,
+                np.int8,
+                np.int16,
+                np.int32,
+                np.int64,
+                np.uint8,
+                np.uint16,
+                np.uint32,
+                np.uint64,
+            ),
+        ):
+            return int(obj)
+
+        elif isinstance(obj, TYPES["float"]):
+            return float(obj)
+
+        elif isinstance(obj, TYPES["complex"]):
+            return {"real": obj.real, "imag": obj.imag}
+
+        elif isinstance(obj, (np.ndarray,)):
+            return obj.tolist()
+
+        elif isinstance(obj, (np.bool_)):
+            return bool(obj)
+
+        elif isinstance(obj, (np.void)):
+            return None
+
+        return json.JSONEncoder.default(self, obj)
+
+
+if __name__ == "__main__":
+    numpy_encoder = NumpyEncoder()
