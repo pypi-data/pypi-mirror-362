a83554638512563a62701609cdf23fc2dca43c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:35 +0000

    Add package agentcrew-ai/0.6.4.post1/agentcrew_ai-0.6.4.post1-py3-none-any.whl

---
+++ packages/agentcrew-ai/agentcrew_ai-0.6.4.post1-py3-none-any.whl/AgentCrew/modules/chat/constants.py
@@ -0,0 +1,10 @@
+# Terminal formatting
+COLORS_ENABLED = True  # Set to False to disable colors
+RICH_YELLOW = "yellow"
+RICH_GRAY = "grey66"
+# History settings
+DEFAULT_HISTORY_FILE = "~/.chat_histories"
+DEFAULT_HISTORY_LIMIT = 1000
+HISTORY_ENTRY_DELIMITER = "\n---ENTRY---\n"
+
+OBSERVABLE_EVENTS = {}
