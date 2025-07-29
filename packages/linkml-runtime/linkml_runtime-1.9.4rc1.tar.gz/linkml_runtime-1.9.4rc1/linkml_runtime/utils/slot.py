a686629a78b3ae7f82292ce6d9e31884f2744b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:59 +0000

    Add package linkml-runtime/1.9.4rc1/linkml_runtime-1.9.4rc1.tar.gz

---
+++ packages/linkml-runtime/linkml_runtime-1.9.4rc1.tar.gz/linkml_runtime-1.9.4rc1/linkml_runtime/utils/slot.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from typing import Optional, Any
+from re import Pattern
+
+from rdflib import URIRef
+
+
+@dataclass
+class Slot:
+    """Runtime slot definition"""
+
+    uri: URIRef
+    name: str
+    curie: Optional[str]
+    model_uri: URIRef
+
+    domain: Optional[type]
+    range: Any
+    mappings: Optional[list[URIRef]] = None
+    pattern: Optional[Pattern] = None
