ba18b46314fa665dc831714f5d2a83b90ccf8c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:12 +0000

    Add package dataframely/1.7.5/dataframely-1.7.5.tar.gz

---
+++ packages/dataframely/dataframely-1.7.5.tar.gz/dataframely-1.7.5/tests/collection/test_validate_input.py
@@ -0,0 +1,31 @@
+# Copyright (c) QuantCo 2025-2025
+# SPDX-License-Identifier: BSD-3-Clause
+
+import polars as pl
+import pytest
+
+import dataframely as dy
+
+
+class TestSchema(dy.Schema):
+    a = dy.Integer()
+
+
+class MyCollection(dy.Collection):
+    first: dy.LazyFrame[TestSchema]
+    second: dy.LazyFrame[TestSchema] | None
+
+
+def test_collection_missing_required_member() -> None:
+    with pytest.raises(ValueError):
+        MyCollection.validate({"second": pl.LazyFrame({"a": [1, 2, 3]})})
+
+
+def test_collection_superfluous_member() -> None:
+    with pytest.warns(Warning):
+        MyCollection.validate(
+            {
+                "first": pl.LazyFrame({"a": [1, 2, 3]}),
+                "third": pl.LazyFrame({"a": [1, 2, 3]}),
+            },
+        )
