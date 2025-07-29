9fc6fd14ecb7b7cbc31745b42ca1edf693f0fe4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:07 +0000

    Add package ReactFlow-CSS/1.0.5/reactflow_css-1.0.5-py3-none-any.whl

---
+++ packages/ReactFlow-CSS/reactflow_css-1.0.5-py3-none-any.whl/reactflow_css/__init__.py
@@ -0,0 +1,25 @@
+"""
+This package provides styling utilities for ReactPy applications,
+including integrations with Tailwind CSS and Bootstrap.
+"""
+
+__version__ = '1.0.5'
+
+from .tailwindcss.Configuration import configure as configure_tailwind, default_css as default_tailwind
+
+from .bootstrap.Configuration import configure as configure_boots, default_css as default_boots
+
+from .bootstrap.generate import Convert_style as convert_imports_to_link
+
+from . import tailwindcss
+from . import bootstrap
+
+__all__ = [
+    'tailwindcss',
+    'bootstrap',
+    'configure_boots',
+    'configure_tailwind',
+    'default_boots',
+    'default_tailwind',
+    'convert_imports_to_link'
+]
