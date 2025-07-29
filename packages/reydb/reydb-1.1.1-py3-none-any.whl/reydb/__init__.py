ac569d0e60778aa163545c67abc37dd979a1568b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:32 +0000

    Add package reydb/1.1.1/reydb-1.1.1-py3-none-any.whl

---
+++ packages/reydb/reydb-1.1.1-py3-none-any.whl/reydb/__init__.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2024-01-07 20:51:57
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Rey's database method set.
+
+Modules
+-------
+rall : All methods.
+rbuild : Database build methods.
+rdatabase : Database connection methods.
+rexecute : Database execute methods.
+rfile : Database file methods.
+rinformation : Database information methods.
+rparameter : Database parameter methods.
+"""
