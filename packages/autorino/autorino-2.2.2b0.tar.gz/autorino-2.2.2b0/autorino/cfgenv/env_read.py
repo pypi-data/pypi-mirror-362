06f8b6abf6c23817ca31f7e9bf23850c4eac9c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:05 +0000

    Add package autorino/2.2.2b0/autorino-2.2.2b0.tar.gz

---
+++ packages/autorino/autorino-2.2.2b0.tar.gz/autorino-2.2.2b0/autorino/cfgenv/env_read.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 22/04/2024 16:16:23
+
+@author: psakic
+"""
+# Create a logger object.
+import logging
+import os
+import collections.abc
+import yaml
+import mergedeep
+
+### we need to clear the root logger to avoid duplicate logs
+root_logger = logging.getLogger()
+root_logger.handlers.clear()  # Clear all handlers in the root logger
+
+logger = logging.getLogger("autorino")
+logger.setLevel("DEBUG")
+
+
+def read_env(envfile_path=None):
+    """
+    Reads an environment configuration file (YAML format) and returns the corresponding dictionary.
+
+    Priority for determining the environment file path:
+    1. Function argument
+    2. Environment variable `AUTORINO_ENV`
+    3. Default file in the current directory
+    """
+    # Default environment file path
+    default_env_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "autorino_env_default.yml"
+    )
+
+    # Determine the environment file path
+    envfile_path_use = (
+        envfile_path or os.environ.get("AUTORINO_ENV", "")
+    )
+
+    if not envfile_path_use or not os.path.exists(envfile_path_use):
+        logger.warning(
+            f"{'Custom environment configfile not defined' if not envfile_path_use else f'File not found: {envfile_path_use}'}"
+        )
+        logger.warning("Falling back to default values in %s", default_env_path)
+        envfile_path_use = default_env_path
+
+    logger.info("Loading environment configfile: %s", envfile_path_use)
+
+    # Load and merge environment configurations
+    env_default = yaml.safe_load(open(default_env_path))["environment"]
+    env_custom = yaml.safe_load(open(envfile_path_use))["environment"]
+    return mergedeep.merge({}, env_default, env_custom)
+
+# Global environment dictionary used throughout the package
+ARO_ENV_DIC = read_env()
