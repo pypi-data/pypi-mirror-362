3622e6f4906e2befeac57ee1030aa649e8a9be11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:22 +0000

    Add package phosadsorption/0.1.4/phosadsorption-0.1.4.tar.gz

---
+++ packages/phosadsorption/phosadsorption-0.1.4.tar.gz/phosadsorption-0.1.4/phosadsorption/utils.py
@@ -0,0 +1,14 @@
+
+import pandas as pd
+
+def preprocess_dataframe(df):
+    df.columns = df.columns.str.strip()
+    rename_map = {
+        'Οργανική ουσία': 'O.O.', 'Ηλεκτρική Αγωγιμότητα': 'Ec',
+        'Οργανική': 'Organic matter', 'Ec': 'EC', 'Ec (dS/m)': 'EC',
+    }
+    required_columns = ['S', 'C', 'pH', 'EC', 'Organic matter', 'P', 'Mg', 'Mn', 'Cu']
+    df = df.rename(columns=rename_map)
+    df = df[[col for col in required_columns if col in df.columns]].copy()
+    df['Mg'] = df['Mg'] / (12.1525 * 10)  # Mg to meq/100g
+    return df
---
+++ packages/phosadsorption/phosadsorption-0.1.4.tar.gz/phosadsorption-0.1.4/setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="phosadsorption",
+    version="0.1.4",
+    packages=find_packages(include=["phosadsorption", "phosadsorption.*"]),
+    include_package_data=True,
+    install_requires=[
+        "pandas",
+        "numpy",
+        "xgboost",
+        "openpyxl",
+        "matplotlib"
+    ],
+    author="Miltiadis Iatrou",
+    description="Library for predicting phosphorus adsorption in soil",
+    long_description="Predicts phosphorus adsorption and fertilizer productivity based on soil inputs.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mil-afk/soil_phosphorus_adsorption_data",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
