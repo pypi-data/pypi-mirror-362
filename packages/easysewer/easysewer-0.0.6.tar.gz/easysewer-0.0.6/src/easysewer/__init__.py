2e6e45a7596577e3a469178992836568c2a20774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:52 +0000

    Add package easysewer/0.0.6/easysewer-0.0.6.tar.gz

---
+++ packages/easysewer/easysewer-0.0.6.tar.gz/easysewer-0.0.6/src/easysewer/__init__.py
@@ -0,0 +1,8 @@
+"""
+pass
+"""
+from .UDM import UrbanDrainageModel
+from .ModelAPI import Model
+from .OutputAPI import SWMMOutputAPI
+from .SolverAPI import SWMMSolverAPI
+from .JsonHandler import JsonHandler
