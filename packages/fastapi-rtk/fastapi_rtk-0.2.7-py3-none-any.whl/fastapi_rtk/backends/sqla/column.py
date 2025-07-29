97ff503918f6b5480eb9070ca52a77381fd7f026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:50 +0000

    Add package fastapi-rtk/0.2.7/fastapi_rtk-0.2.7-py3-none-any.whl

---
+++ packages/fastapi-rtk/fastapi_rtk-0.2.7-py3-none-any.whl/fastapi_rtk/backends/sqla/column.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+# This file is just to keep the folder structure consistent.
+
+__all__ = [
+    "Column",
+    "Mapped",
+    "mapped_column",
+    "relationship",
+]
