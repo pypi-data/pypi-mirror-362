8d8c960ddfc866a343734226f07143b08cfd9a5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:16 +0000

    Add package blurpy/0.0.6/blurpy-0.0.6-py3-none-any.whl

---
+++ packages/blurpy/blurpy-0.0.6-py3-none-any.whl/blurpy/blur.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+import time
+
+CONFIG = 'blur-bins/config/blur-config.cfg'
+EXE = 'blur-bins/bin/blur.exe'
+
+def blur(input_: str, output: str = "output.mp4", config_loc: str = CONFIG):
+    start = time.perf_counter()
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(current_dir)
+    subprocess.run([EXE, "-i", input_, "-o", output, "-c", config_loc, "-v"])
+    end = time.perf_counter()
+    
+    output_path = os.path.abspath(output)
+    if os.path.exists(output_path):
+        elapsed = end - start
+        mins, secs = divmod(elapsed, 60)
+        print(f"✅ Done: {output_path}")
+        print(f"⏱️  Time taken: {int(mins)}m {secs:.2f}s")
+        os.startfile(output_path)
+        
