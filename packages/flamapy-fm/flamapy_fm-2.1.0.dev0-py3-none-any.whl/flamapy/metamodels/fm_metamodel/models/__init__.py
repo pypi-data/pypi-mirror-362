ae8d23a1d38b867b7a2923cf987aa13fb6043691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:07 +0000

    Add package flamapy-fm/2.1.0.dev0/flamapy_fm-2.1.0.dev0-py3-none-any.whl

---
+++ packages/flamapy-fm/flamapy_fm-2.1.0.dev0-py3-none-any.whl/flamapy/metamodels/fm_metamodel/models/__init__.py
@@ -0,0 +1,23 @@
+from .feature_model import (
+    Feature,
+    Constraint,
+    FeatureModel,
+    Relation,
+    Domain,
+    Range,
+    Attribute,
+    Cardinality,
+    FeatureType
+)
+
+__all__ = [
+    'Attribute',
+    'Cardinality',
+    'Constraint',
+    'Domain',
+    'Feature',
+    'FeatureModel',
+    'FeatureType',
+    'Range',
+    'Relation',
+]
