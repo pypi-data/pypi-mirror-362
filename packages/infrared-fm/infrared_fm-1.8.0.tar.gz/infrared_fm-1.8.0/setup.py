1d3209ffa4c5a9b8d4f554f91c593f6622978c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:42 +0000

    Add package infrared-fm/1.8.0/infrared_fm-1.8.0.tar.gz

---
+++ packages/infrared-fm/infrared_fm-1.8.0.tar.gz/infrared_fm-1.8.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+# we go dumm
+setup(
+    name="infrared-fm",
+    version="1.8.0",
+    description="Last.FM wrapper by Infrared LLC.",
+    author="infrared",
+    author_email="faneorg.official@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    url="https://github.com/infraredhuh/infrared-fm",
+    package_data={"infrafm": ["py.typed"]},
+    install_requires=[
+        "aiohttp>=3.9.0",
+        "pillow>=9.0.0"
+    ],
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Typing :: Typed",
+    ],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
