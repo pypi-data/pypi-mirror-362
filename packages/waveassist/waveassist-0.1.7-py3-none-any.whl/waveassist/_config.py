470974557da0e9762d65a1be4f02f81d5049da29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:43 +0000

    Add package waveassist/0.1.7/waveassist-0.1.7-py3-none-any.whl

---
+++ packages/waveassist/waveassist-0.1.7-py3-none-any.whl/waveassist/_config.py
@@ -0,0 +1,10 @@
+# waveassist/_config.py
+
+# Module-level variable to hold the login token
+LOGIN_TOKEN = None
+PROJECT_KEY = None
+ENVIRONMENT_KEY = None
+DEFAULT_ENVIRONMENT_KEY = None
+DEFAULT_LOGIN_TOKEN = None
+DEFAULT_PROJECT_KEY = None
+VERSION = "0.1.7"
