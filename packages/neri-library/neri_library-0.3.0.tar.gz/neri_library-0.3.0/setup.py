480fd4d4c66ef6d577af27599c05e5336a8b4c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:56 +0000

    Add package neri-library/0.3.0/neri_library-0.3.0.tar.gz

---
+++ packages/neri-library/neri_library-0.3.0.tar.gz/neri_library-0.3.0/setup.py
@@ -0,0 +1,74 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="neri_library",
+    version="0.3.0",
+    author="Guilherme Neri",
+    author_email="gui.neriaz@gmail.com",
+    description="Neri library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/NeriAzv/Neri-Library",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            *[f"{cmd}=neri_library.cli:main" for cmd in [
+                "neri_library",
+                "nerilibrary",
+                "neri-lib",
+                "nerilib",
+                "library",
+                "nlib",
+                "neri",
+                "ner"
+                "nl",
+                "nr",
+            ]],
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'setuptools',
+        'pyperclip',
+        'pyinstaller',
+        'selenium',
+    ],
+    extras_require={
+        "pynput": [
+            'pynput',
+        ],
+        "screeninfo": [
+            'screeninfo',
+        ],
+        "pywinauto": [
+            'pywinauto',
+        ],
+        "capmonstercloudclient":[
+            'capmonstercloudclient',
+        ],
+        "twocaptcha":[
+            "2captcha-python",
+        ],
+        "full": [
+            'undetected-chromedriver',
+            'webdriver-manager',
+            'opencv-python',
+            'pygetwindow',
+            'pyinstaller',
+            'screeninfo',
+            'pyscreeze',
+            'pyautogui',
+            'selenium',
+            'requests',
+            'pymupdf',
+            'Pillow',
+            'psutil',
+            'pynput',
+        ],
+    },
+)
