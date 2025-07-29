3970e0649f3207a1d9ca4ca80214f7ed7466a9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:05 +0000

    Add package antares-study-version/1.0.19/antares_study_version-1.0.19-py3-none-any.whl

---
+++ packages/antares-study-version/antares_study_version-1.0.19-py3-none-any.whl/antares/study/version/__about__.py
@@ -0,0 +1,10 @@
+"""
+Antares Study (and Solver) version models.
+"""
+
+# Standard project metadata
+
+__version__ = "1.0.19"
+__author__ = "RTE, Antares Web Team"
+__date__ = "2025-07-15"
+__credits__ = "© Réseau de Transport de l’Électricité (RTE)"
