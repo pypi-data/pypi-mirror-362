3d9fdf782bf78e5252992c06fddd93b0dd76f868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:49 +0000

    Add package buildfast/0.1.0/buildfast-0.1.0.tar.gz

---
+++ packages/buildfast/buildfast-0.1.0.tar.gz/buildfast-0.1.0/README.md
@@ -0,0 +1,24 @@
+# buildfast
+
+
+## Installation
+
+```bash
+pip install buildfast
+```
+
+## Usage
+
+```python
+from buildfast import hello, build
+
+# Say hello
+print(hello())
+
+# Run build
+build()
+```
+
+## License
+
+MIT License
---
+++ packages/buildfast/buildfast-0.1.0.tar.gz/buildfast-0.1.0/buildfast/__init__.py
@@ -0,0 +1,16 @@
+"""
+buildfast
+"""
+
+__version__ = "0.1.0"
+__author__ = "Satvik"
+__email__ = "satvik@buildfastwithai.com"
+
+def hello():
+    """A simple hello function"""
+    return "Hello from buildfast!"
+
+def build():
+    """Main build function"""
+    print("Building fast...")
+    return True
