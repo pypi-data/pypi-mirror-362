850f85f1273c0a69c1bb64c88cc5801599104f3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:22 +0000

    Add package fdc-shared-kernel/0.0.109/fdc_shared_kernel-0.0.109-py3-none-any.whl

---
+++ packages/fdc-shared-kernel/fdc_shared_kernel-0.0.109-py3-none-any.whl/shared_kernel/datatype_mappings/connectors_to_system/postgres.py
@@ -0,0 +1,16 @@
+postgres_to_system = {
+    "INTEGER": "integer",
+    "REAL": "real",
+    "NUMERIC": "numeric",
+    "DOUBLE PRECISION": "double",
+    "BOOLEAN": "boolean",
+    "BYTEA": "binary",
+    "BIT": "bit",
+    "TEXT": "string",
+    "VARCHAR": "varchar",
+    "CHAR": "character",
+    "DATE": "date",
+    "TIMESTAMP": "datetime",
+    "TIME": "time",
+    "NULL": "null"
+}
