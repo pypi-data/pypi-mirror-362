545c02b842133a424249d56357299f9e15d22c51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:18 +0000

    Add package base-api-utils/1.0.71/base_api_utils-1.0.71.tar.gz

---
+++ packages/base-api-utils/base_api_utils-1.0.71.tar.gz/base_api_utils-1.0.71/src/base_api_utils/utils/external_errors.py
@@ -0,0 +1,30 @@
+import pybreaker
+import requests
+from rest_framework.response import Response
+from rest_framework import status
+
+class NamedCircuitBreakerError(pybreaker.CircuitBreakerError):
+    def __init__(self, service_name: str):
+        self.service_name = service_name
+        super().__init__(f"Circuit breaker for {service_name} is open.")
+
+def handle_external_service_errors(e):
+    if isinstance(e, NamedCircuitBreakerError):
+        return Response(
+            {
+                'message': f'The {e.service_name} is currently unavailable. Please try again later.',
+                'error': 'circuit_breaker_open'
+            },
+            status=status.HTTP_503_SERVICE_UNAVAILABLE
+        )
+
+    if isinstance(e, requests.RequestException):
+        return Response(
+            {
+                'message': 'Failed to connect to the external service.',
+                'error': 'external_service_connection_failed'
+            },
+            status=status.HTTP_502_BAD_GATEWAY
+        )
+
+    return None  # Not handled here
