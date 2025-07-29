0bdcd84bfacb65bb19ecec924530ac0f4ac8681d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:26 +0000

    Add package noiserandom/1.1/NoiseRandom-1.1.tar.gz

---
+++ packages/noiserandom/NoiseRandom-1.1.tar.gz/NoiseRandom-1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme:
+    description = readme.read()
+
+setup(
+    name="NoiseRandom",
+    author="Andreas Karageorgos",
+    version="1.1",
+    packages=find_packages(),
+    install_requires=[
+        "opencv-python",
+        "gmpy2"
+    ],
+    license="MIT",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AndreasKarageorgos/NoiseRandom/"
+)
