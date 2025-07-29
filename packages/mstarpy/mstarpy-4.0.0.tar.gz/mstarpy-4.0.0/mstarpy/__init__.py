441d1c8d85c45a34e791a7e693eab5ee0311a626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:32 +0000

    Add package mstarpy/4.0.0/mstarpy-4.0.0.tar.gz

---
+++ packages/mstarpy/mstarpy-4.0.0.tar.gz/mstarpy-4.0.0/mstarpy/__init__.py
@@ -0,0 +1,8 @@
+# flake8: noqa
+
+""" mstarpy init """
+from .funds import Funds
+from .stock import Stock
+from .search import search_field, search_filter, screener_universe
+
+__version__ = "4.0.0"
