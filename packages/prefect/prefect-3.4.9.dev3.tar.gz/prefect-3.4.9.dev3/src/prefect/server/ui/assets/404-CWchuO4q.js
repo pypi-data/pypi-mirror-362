8babfd1b5e2c256ca7f947804dda330000ddad85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:35 +0000

    Add package prefect/3.4.9.dev3/prefect-3.4.9.dev3.tar.gz

---
+++ packages/prefect/prefect-3.4.9.dev3.tar.gz/prefect-3.4.9.dev3/src/prefect/server/ui/assets/404-CWchuO4q.js
@@ -0,0 +1,2 @@
+import{d as e,c as o,o as a,n as t,dx as n}from"./index-ei-kaitd.js";import{u as r}from"./usePageTitle-LeBMnqrg.js";const m=e({__name:"404",setup(s){return r("Page Not Found"),(c,p)=>(a(),o(t(n)))}});export{m as default};
+//# sourceMappingURL=404-CWchuO4q.js.map
