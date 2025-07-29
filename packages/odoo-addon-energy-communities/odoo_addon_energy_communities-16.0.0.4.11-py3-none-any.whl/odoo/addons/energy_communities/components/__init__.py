09de1452dca8b0bf39828b2d067d01aa3839bdda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:22 +0000

    Add package odoo-addon-energy-communities/16.0.0.4.11/odoo_addon_energy_communities-16.0.0.4.11-py3-none-any.whl

---
+++ packages/odoo-addon-energy-communities/odoo_addon_energy_communities-16.0.0.4.11-py3-none-any.whl/odoo/addons/energy_communities/components/__init__.py
@@ -0,0 +1,5 @@
+from . import collections
+from .user_creator import UserCreator
+from .contract_utils import ContractUtils
+from .sale_order_utils import SaleOrderUtils
+from .user_role_utils import UserRoleUtils
