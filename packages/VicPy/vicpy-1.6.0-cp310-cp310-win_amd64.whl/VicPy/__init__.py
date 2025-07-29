9c91143b27ffb80a4e18e758fc9c7ce01f09590d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:04:53 +0000

    Add package VicPy/1.6.0/vicpy-1.6.0-cp310-cp310-win_amd64.whl

---
+++ packages/VicPy/vicpy-1.6.0-cp310-cp310-win_amd64.whl/VicPy/__init__.py
@@ -0,0 +1,12 @@
+# -----------------------------------------------------------------------------
+# Copyright (C) 2025 Correlated Solutions, Inc. All rights reserved.
+#
+# The information and source code contained herein is the exclusive property
+# of Correlated Solutions, Inc. and may not be disclosed, examined or
+# reproduced in whole or in part without explicit written authorization from
+# the company.
+# -----------------------------------------------------------------------------
+
+from .VicPy import *
+
+__version__ = "1.6.0"
