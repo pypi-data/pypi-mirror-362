74ec23b770a64b6ec3a6b53c4ab6f1d2b9e0afbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:37 +0000

    Add package motionminer/1.0.2/motionminer-1.0.2-py3-none-any.whl

---
+++ packages/motionminer/motionminer-1.0.2-py3-none-any.whl/motionminer/__main__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+MotionMiner - Main module entry point
+
+This module allows the package to be executed as a module with:
+python -m motionminer
+"""
+
+from .main import main
+
+if __name__ == "__main__":
+    main() 
