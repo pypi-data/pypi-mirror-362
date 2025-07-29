7c5d52c87fde7739100356f0d9f3d2693e55d211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:31 +0000

    Add package interval-tools/0.17.0/interval_tools-0.17.0-py3-none-any.whl

---
+++ packages/interval-tools/interval_tools-0.17.0-py3-none-any.whl/interval/__init__.py
@@ -0,0 +1,12 @@
+"""
+interval
+~~~~~~~~
+
+Utility functions and classes used in applications of Interval Design.
+
+:copyright: (c) 2022 by Interval Design.
+:license: MIT, see LICENSE for more details.
+"""
+
+from . import ddd
+from . import utils
