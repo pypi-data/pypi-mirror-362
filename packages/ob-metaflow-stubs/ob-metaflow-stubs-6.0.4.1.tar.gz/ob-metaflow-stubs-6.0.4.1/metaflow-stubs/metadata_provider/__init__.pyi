2a8b087940987c53b8ddcb6ca3ccac12574c2e21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:42 +0000

    Add package ob-metaflow-stubs/6.0.4.1/ob-metaflow-stubs-6.0.4.1.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.4.1.tar.gz/ob-metaflow-stubs-6.0.4.1/metaflow-stubs/metadata_provider/__init__.pyi
@@ -0,0 +1,16 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.16.0.1+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-07-14T20:31:43.548185                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import metadata as metadata
+from .metadata import DataArtifact as DataArtifact
+from .metadata import MetadataProvider as MetadataProvider
+from .metadata import MetaDatum as MetaDatum
+from . import util as util
+from . import heartbeat as heartbeat
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.4.1.tar.gz/ob-metaflow-stubs-6.0.4.1/metaflow-stubs/mf_extensions/obcheckpoint/plugins/machine_learning_utilities/checkpoints/exceptions.pyi
@@ -0,0 +1,25 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.16.0.1+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-07-14T20:31:43.599283                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import metaflow
+import typing
+if typing.TYPE_CHECKING:
+    import metaflow.exception
+
+from ......exception import MetaflowException as MetaflowException
+
+class CheckpointNotAvailableException(metaflow.exception.MetaflowException, metaclass=type):
+    def __init__(self, message):
+        ...
+    ...
+
+class CheckpointException(metaflow.exception.MetaflowException, metaclass=type):
+    def __init__(self, message):
+        ...
+    ...
+
