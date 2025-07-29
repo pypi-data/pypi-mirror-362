bbddad7b7fc2aa1c9d674632136ce015d9b35d75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:33 +0000

    Add package scale-gp-beta/0.1.0a29/scale_gp_beta-0.1.0a29-py3-none-any.whl

---
+++ packages/scale-gp-beta/scale_gp_beta-0.1.0a29-py3-none-any.whl/scale_gp_beta/types/evaluation_list_params.py
@@ -0,0 +1,26 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import List
+from typing_extensions import Literal, TypedDict
+
+__all__ = ["EvaluationListParams"]
+
+
+class EvaluationListParams(TypedDict, total=False):
+    ending_before: str
+
+    include_archived: bool
+
+    limit: int
+
+    name: str
+
+    sort_order: Literal["asc", "desc"]
+
+    starting_after: str
+
+    tags: List[str]
+
+    views: List[Literal["tasks"]]
