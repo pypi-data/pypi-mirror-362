c88c0c067f9aad2b10fe4eb5e0403f1c61edb084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:36 +0000

    Add package hyperskill-python-portfolio/0.6.0/hyperskill_python_portfolio-0.6.0-py3-none-any.whl

---
+++ packages/hyperskill-python-portfolio/hyperskill_python_portfolio-0.6.0-py3-none-any.whl/hyperskill_python_portfolio/chatbot/main.py
@@ -0,0 +1,15 @@
+"""Main entry point for the chatbot application."""
+
+from .bot import ChatBot
+
+
+def main() -> None:
+    """Initializes and runs the chatbot."""
+    # Create an instance of the bot with its configuration
+    simple_bot = ChatBot(bot_name="Aid", birth_year=2020)
+    # Start the conversation
+    simple_bot.run()
+
+
+if __name__ == "__main__":
+    main()
