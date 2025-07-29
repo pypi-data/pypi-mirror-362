fa4e768dcdd8eddcb7e6ee2dce48b0e0564c70b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:32 +0000

    Add package clapp-pm/1.0.0/clapp_pm-1.0.0.tar.gz

---
+++ packages/clapp-pm/clapp_pm-1.0.0.tar.gz/clapp_pm-1.0.0/version.py
@@ -0,0 +1,6 @@
+"""Version information for clapp package."""
+
+__version__ = "1.0.0"
+__author__ = "Melih Burak Memiş"
+__email__ = "mburakmemiscy@gmail.com"
+__description__ = "Lightweight cross-language app manager for Python and Lua" 
