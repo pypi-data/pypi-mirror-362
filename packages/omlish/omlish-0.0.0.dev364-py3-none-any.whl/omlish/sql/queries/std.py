f4b7491e7f4a57ca8b9e9dd6b48261d305231659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:05:46 +0000

    Add package omlish/0.0.0.dev364/omlish-0.0.0.dev364-py3-none-any.whl

---
+++ packages/omlish/omlish-0.0.0.dev364-py3-none-any.whl/omlish/sql/queries/std.py
@@ -0,0 +1,37 @@
+from .base import Builder
+from .binary import BinaryBuilder
+from .exprs import ExprBuilder
+from .idents import IdentBuilder
+from .inserts import InsertBuilder
+from .multi import MultiBuilder
+from .names import NameBuilder
+from .params import ParamBuilder
+from .relations import RelationBuilder
+from .selects import SelectBuilder
+from .stmts import StmtBuilder
+from .unary import UnaryBuilder
+
+
+##
+
+
+class StdBuilder(
+    InsertBuilder,
+    SelectBuilder,
+    StmtBuilder,
+
+    RelationBuilder,
+
+    MultiBuilder,
+    BinaryBuilder,
+    UnaryBuilder,
+    ExprBuilder,
+
+    ParamBuilder,
+
+    NameBuilder,
+    IdentBuilder,
+
+    Builder,
+):
+    pass
