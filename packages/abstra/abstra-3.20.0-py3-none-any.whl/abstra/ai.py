20be487ab04695f57a19d3ef285b5940ebb91476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:19 +0000

    Add package abstra/3.20.0/abstra-3.20.0-py3-none-any.whl

---
+++ packages/abstra/abstra-3.20.0-py3-none-any.whl/abstra/ai.py
@@ -0,0 +1,8 @@
+from abstra_internals.interface.sdk.ai import (
+    parse_boleto,
+    parse_document,
+    parse_nfse,
+    prompt,
+)
+
+__all__ = ["prompt", "parse_document", "parse_nfse", "parse_boleto"]
