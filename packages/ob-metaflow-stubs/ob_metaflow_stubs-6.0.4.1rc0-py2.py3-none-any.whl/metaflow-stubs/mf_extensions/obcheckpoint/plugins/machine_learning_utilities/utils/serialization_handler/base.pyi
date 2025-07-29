3e248d1d4691525d651b8d42a4a97524cf7d62ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:09 +0000

    Add package ob-metaflow-stubs/6.0.4.1rc0/ob_metaflow_stubs-6.0.4.1rc0-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.4.1rc0-py2.py3-none-any.whl/metaflow-stubs/mf_extensions/obcheckpoint/plugins/machine_learning_utilities/utils/serialization_handler/base.pyi
@@ -0,0 +1,20 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.16.0.1+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-07-14T20:03:25.702921                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+import typing
+if typing.TYPE_CHECKING:
+    import typing
+
+
+class SerializationHandler(object, metaclass=type):
+    def serialze(self, *args, **kwargs) -> typing.Union[str, bytes]:
+        ...
+    def deserialize(self, *args, **kwargs) -> typing.Any:
+        ...
+    ...
+
