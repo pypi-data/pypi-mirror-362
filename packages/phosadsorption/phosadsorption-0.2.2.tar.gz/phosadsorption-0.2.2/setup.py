8c5a61b4bcfc7ad3b0923afa96e6c3b3ad4404b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:15 +0000

    Add package phosadsorption/0.2.2/phosadsorption-0.2.2.tar.gz

---
+++ packages/phosadsorption/phosadsorption-0.2.2.tar.gz/phosadsorption-0.2.2/setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="phosadsorption",
+    version="0.2.2",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "pandas",
+        "numpy",
+        "xgboost>=1.7.0",
+        "matplotlib",
+        "openpyxl"
+    ],
+    author="Miltiadis Iatrou",
+    description="Library for predicting phosphorus adsorption in soil",
+    long_description="Phosadsorption is a Python package for predicting percentage phosphorus adsorption in soils using XGBoost models.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Mil-afk/soil_phosphorus_adsorption_data",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
