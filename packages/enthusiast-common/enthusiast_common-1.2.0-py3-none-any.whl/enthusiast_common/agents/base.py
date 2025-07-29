5e00183f7d5239a14400d76d13a39149ea23c4b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:05:20 +0000

    Add package enthusiast-common/1.2.0/enthusiast_common-1.2.0-py3-none-any.whl

---
+++ packages/enthusiast-common/enthusiast_common-1.2.0-py3-none-any.whl/enthusiast_common/agents/base.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+from langchain_core.callbacks import BaseCallbackHandler
+from langchain_core.language_models import BaseLanguageModel
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.tools import BaseTool
+
+
+class BaseAgent(ABC):
+    def __init__(
+        self,
+        tools: list[BaseTool],
+        llm: BaseLanguageModel,
+        prompt: ChatPromptTemplate,
+        conversation_id: Any,
+        injector: "BaseInjector",  # noqa: F821
+        callback_handler: BaseCallbackHandler | None = None,
+    ):
+        self._tools = tools
+        self._llm = llm
+        self._prompt = prompt
+        self._conversation_id = conversation_id
+        self._callback_handler = callback_handler
+        self._injector = injector
+
+    @abstractmethod
+    def get_answer(self, input_text: str) -> str:
+        pass
