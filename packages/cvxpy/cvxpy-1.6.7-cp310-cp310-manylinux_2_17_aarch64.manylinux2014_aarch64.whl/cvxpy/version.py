65a9447fab7f6bcf671e8ac2962c302fa79d270d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:09 +0000

    Add package cvxpy/1.6.7/cvxpy-1.6.7-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/cvxpy/cvxpy-1.6.7-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.6.7'
+version = '1.6.7'
+full_version = '1.6.7'
+git_revision = '8af1176'
+commit_count = '0'
+release = True
+if not release:
+    version = full_version
