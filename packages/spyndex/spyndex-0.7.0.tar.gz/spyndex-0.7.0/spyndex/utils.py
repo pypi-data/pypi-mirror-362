5fb0dd144699c465bf267ebe9fa0813e5eaecc3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 21:05:12 +0000

    Add package spyndex/0.7.0/spyndex-0.7.0.tar.gz

---
+++ packages/spyndex/spyndex-0.7.0.tar.gz/spyndex-0.7.0/spyndex/utils.py
@@ -0,0 +1,74 @@
+import json
+import os
+
+import requests
+
+import spyndex
+
+from importlib.resources import files
+
+
+def _load_JSON(file="spectral-indices-dict.json"):
+    """Loads the specified JSON file from the data folder.
+
+    Parameters
+    ----------
+    file : str
+        File name.
+
+    Returns
+    -------
+    object
+        JSON file.
+    """
+    data_file = files("spyndex.data") / file
+    with data_file.open("r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+def _get_indices(online=False):
+    """Retrieves the JSON of indices.
+
+    Parameters
+    ----------
+    online : boolean
+        Wheter to retrieve the most recent list of indices directly from the GitHub
+        repository and not from the local copy.
+
+    Returns
+    -------
+    dict
+        Indices.
+    """
+    if online:
+        indices = requests.get(
+            "https://raw.githubusercontent.com/awesome-spectral-indices/awesome-spectral-indices/main/output/spectral-indices-dict.json"
+        ).json()
+    else:
+        indices = _load_JSON()
+
+    return indices["SpectralIndices"]
+
+
+def _check_params(index: str, params: dict, indices: dict):
+    """Checks if the parameters dictionary contains all required bands for the index
+    computation.
+
+    Parameters
+    ----------
+    index : str
+        Index to check.
+    params : dict
+        Parameters dictionary to check.
+    indices : dict
+        Indices dictionary to check.
+
+    Returns
+    -------
+    None
+    """
+    for band in indices[index]["bands"]:
+        if band not in list(params.keys()):
+            raise Exception(
+                f"'{band}' is missing in the parameters for {index} computation!"
+            )
