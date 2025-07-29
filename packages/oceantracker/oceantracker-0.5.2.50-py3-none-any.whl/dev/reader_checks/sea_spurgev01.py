31539a820a63e69c7ec4a48cc3cacce3e6bc9234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:11 +0000

    Add package oceantracker/0.5.2.50/oceantracker-0.5.2.50-py3-none-any.whl

---
+++ packages/oceantracker/oceantracker-0.5.2.50-py3-none-any.whl/dev/reader_checks/sea_spurgev01.py
@@ -0,0 +1,59 @@
+from os import path
+
+from oceantracker.main import OceanTracker
+
+if __name__ == "__main__":
+    # nested NZ schisim with a  glorys subset
+    ot = OceanTracker()
+    ot.settings(output_file_base = 'sea_spurge_test01',
+                root_output_dir= r'D:\OceanTrackerOutput',
+                time_step=3600,
+                max_run_duration = 30*24*3600., # 10 days
+                #display_grid_at_start=True
+                )
+    # Glorys outer grid
+    ot.add_class('reader',
+                 input_dir=r'D:\Hindcast_reader_tests\Glorys\glorysRemySeaSpurgeSurfaceTestData2D',
+                 file_mask = '*.nc')  # mask covering hindcast and static variables
+
+    #NZ surface hindcast
+    ot.add_class('nested_readers',
+                input_dir=r'D:\Hindcast_reader_tests\Schisim\NZsurface2D_seaspurge',
+                file_mask = 'schism_*.nc',
+                grid_variable_map= dict(x='longitude', y= 'latitude'), # remap x to long lat
+                field_variable_map=dict(water_velocity =['vsurf']),  # remap vel to surf values in file
+                # hgrid needed for open boundary info
+                hgrid_file_name= path.join(r'D:\Hindcast_reader_tests\Schisim\NZsurface2D_seaspurge','hgridNZ_run.gr3')
+                )
+    x0 =  [ [174.665532083399,-35.922300421719214],
+            [167.70585302583135, -41.09760403942677],
+            [168.18486957886807, -41.126477553835635],
+            [178.78311081480544, -34.83205141270341],
+            [179.74114392087887, -35.81375090260477],
+            [ 178.9627420221942, -41.47295972674199]
+           ]
+    ot.add_class('release_groups',
+                 points = x0,
+                 pulse_size=10,
+                 release_interval=3600)
+
+    if False:
+        case_info_file= ot.run()
+    else:
+        # plot only
+        case_info_file = r'D:\OceanTrackerOutput\sea_spurge_test01\sea_spurge_test01_caseInfo.json'
+
+    if True:
+        from oceantracker.plot_output import plot_tracks
+        from oceantracker.read_output.python import load_output_files
+
+        tracks = load_output_files.load_track_data(case_info_file,  gridID=0, fraction_to_read=0.1) # plot inner gridID=1, not outer gridID = 0
+        anim = plot_tracks.animate_particles(tracks,
+                                             colour_using_data=tracks['hydro_model_gridID'],
+                                             back_ground_depth=False, vmin=0, vmax=1,
+                                             min_status=tracks['particle_status_flags']['outside_open_boundary'],
+                                             show_grid=True, show_dry_cells=False, axis_labels=True,
+                                             )
+
+
+
---
+++ packages/oceantracker/oceantracker-0.5.2.50-py3-none-any.whl/oceantracker/particle_properties/inside_polygons.py
@@ -0,0 +1,47 @@
+from oceantracker.particle_properties._base_particle_properties import ManuallyUpdatedParticleProperty
+import numpy as np
+from oceantracker.util.parameter_checking import ParamValueChecker as PVC, ParameterListChecker as PLC
+from oceantracker.util.polygon_util import set_up_list_of_polygon_instances, InsidePolygon
+
+from oceantracker.shared_info import shared_info as si
+
+class InsidePolygonsNonOverlapping2D(ManuallyUpdatedParticleProperty):
+    '''
+    particle property giving ID of 2D polygon which particle is inside. -1 if in no polygon
+    assumes non-overlapping polygons, ie so only inside one at a time, ie the first it is found inside,
+    does not check if polygons overlap
+    '''
+    def __init__(self):
+        super().__init__()
+        self.add_default_params({'initial_value': PVC(-1, int),
+                                 'dtype':PVC('int32',str)})
+
+        self.add_default_params({'polygon_list':[]})
+
+    def check_requirements(self):
+        self.check_class_required_fields_prop_etc(required_props_list=['x'])
+
+    def initial_setup(self, **kwargs):
+        super().initial_setup()
+        # set up polygons instances
+
+        self.polygons, msg = set_up_list_of_polygon_instances(self.params['polygon_list'], geographic_coords=si.settings.use_geographic_coords)
+
+    def update(self,n_time_step, time_sec, active):
+        # find polygon each particle is inside
+        part_prop = si.class_roles.particle_properties
+
+        # make all inside no polyg
+        self.set_values(-1, active)
+
+        # loop over polygons
+        # faster if those already found to be inside previously polygon are eliminated
+        to_search = active # for first polygon search all those IDs which are active
+
+        for n, poly in enumerate(self.polygons):
+            # assumes non-overlapping polygons, ie so only inside one at a time, the first polygon it is inside
+            inside, out_side = poly.inside_indices(part_prop['x'].used_buffer(), active=to_search, out=self.get_partID_buffer('B1'),
+                                                   also_return_indices_outside=True, out_outside=self.get_partID_subset_buffer('B1'))
+            self.set_values(n, inside)
+            to_search = out_side # on next polygon only search those not in precceding polygons
+
