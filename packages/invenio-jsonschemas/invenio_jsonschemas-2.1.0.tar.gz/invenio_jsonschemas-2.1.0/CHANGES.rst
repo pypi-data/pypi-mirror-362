6800c2f143e873a98aa09c160c9e68fdc3a4364d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:07:08 +0000

    Add package invenio-jsonschemas/2.1.0/invenio_jsonschemas-2.1.0.tar.gz

---
+++ packages/invenio-jsonschemas/invenio_jsonschemas-2.1.0.tar.gz/invenio_jsonschemas-2.1.0/CHANGES.rst
@@ -0,0 +1,65 @@
+..
+    This file is part of Invenio.
+    Copyright (C) 2015-2018 CERN.
+    Copyright (C) 2024 Graz University of Technology.
+
+    Invenio is free software; you can redistribute it and/or modify it
+    under the terms of the MIT License; see LICENSE file for more details.
+
+Changes
+=======
+
+Version v2.1.0 (released 2025-07-14)
+
+- global: use entry_points wrapper
+
+Version 2.0.0 (release 2024-12-05)
+
+- setup: bump major dependencies
+
+Version 1.1.5 (release 2024-11-30)
+
+- fix: SphinxWarning
+- global: remove examples directory
+- global: remove six usage
+- setup: change to reusable workflows
+- setup: pin dependencies
+- fix extlinks warning of compatibility with version 6
+- global: clean test infrastructure
+- increase minimal python version to 3.7
+- move check_manifest configuration to setup.cfg.
+- fix docs compatibilty problem with Sphinx>=5.0.0
+- add .git-blame-ignore-revs
+- migrate to use black as opinionated auto formater
+- migrate setup.py to setup.cfg
+
+Version 1.1.4 (released 2022-02-28)
+
+- Changes from pkg_resources to importlib for entry points iteration.
+
+Version 1.1.3 (released 2021-06-17)
+
+- Fix string interpolation on error messages.
+
+Version 1.1.2 (released 2021-04-06)
+
+- Adds method to build a ref resolver store from the registry with a local
+  URI scheme (e.g. local://).
+
+Version 1.1.1 (released 2020-12-09)
+
+- Migrates CI to GitHub actions.
+- Fixes issue related to nested `allOf` being ignored, and added tests for it.
+
+Version 1.1.0 (released 2020-03-12)
+
+- Removes support for Python 2.7
+- Changes Flask dependency to centrally managed by invenio-base
+
+Version 1.0.1 (released 2019-09-04)
+
+- Adds config ``JSONSCHEMAS_SCHEMAS`` to whitelist entrypoint names
+
+Version 1.0.0 (released 2018-03-23)
+
+- Initial public release.
---
+++ packages/invenio-jsonschemas/invenio_jsonschemas-2.1.0.tar.gz/invenio_jsonschemas-2.1.0/tests/conftest.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015-2018 CERN.
+# Copyright (C) 2022 RERO.
+# Copyright (C) 2025 Graz University of Technology.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Pytest configuration."""
+
+from __future__ import absolute_import, print_function
+
+import os
+import shutil
+import sys
+import tempfile
+from contextlib import contextmanager
+from importlib.metadata import EntryPoint
+
+import pytest
+from flask import Flask
+from mock import patch
+
+
+@pytest.fixture()
+def app():
+    """Flask application fixture."""
+    app = Flask("testapp")
+    app.config.update(
+        JSONSCHEMAS_LOADER_CLS="helpers:LOADER_CLS",
+        JSONSCHEMAS_REGISTER_ENDPOINTS_UI=True,
+        TESTING=True,
+    )
+    return app
+
+
+def create_file_hierarchy(root_dir_path, files):
+    """Create a hierarchy of files in a directory.
+
+    :param root_dir_path: directory in which all files will be created
+    :param files: dict of <relative file path> -> <file content>
+    """
+    for path, content in files.items():
+        if os.path.isabs(path):
+            raise Exception("Path {} cannot be absolute".format(path))
+        dir_path = os.path.join(root_dir_path, os.path.dirname(path))
+        file_path = os.path.join(root_dir_path, path)
+        if dir_path and not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        elif os.path.isfile(file_path):
+            raise Exception("Path {} is a directory".format(file_path))
+        with open(file_path, "w") as f:
+            f.write(content)
+
+
+@pytest.fixture()
+def dir_factory(tmpdir_factory):
+    """Context manager enabling the creation of temporary directories."""
+
+    @contextmanager
+    def dir_builder(files):
+        root_dir_path = str(tmpdir_factory.mktemp("test", numbered=True))
+        create_file_hierarchy(root_dir_path, files)
+        try:
+            yield os.path.abspath(root_dir_path)
+        finally:
+            shutil.rmtree(root_dir_path)
+
+    return dir_builder
+
+
+@pytest.yield_fixture
+def pkg_factory(tmpdir_factory):
+    """Context manager enabling the creation of temporary modules."""
+    modules_path = str(tmpdir_factory.mktemp("test_modules", numbered=True))
+
+    @contextmanager
+    def pkg_builder(files):
+        mod_path = tempfile.mkdtemp(dir=modules_path)
+        create_file_hierarchy(mod_path, files)
+        # create __init__.py file
+        with open(os.path.join(mod_path, "__init__.py"), "a"):
+            pass
+        # return the module name
+        try:
+            yield os.path.split(mod_path)[1]
+        finally:
+            shutil.rmtree(mod_path)
+
+    sys.path.append(modules_path)
+    try:
+        yield pkg_builder
+    finally:
+        sys.path.remove(modules_path)
+
+
+class MockEntryPoint(EntryPoint):
+    """Mocking of entrypoint."""
+
+    def load(self):
+        """Mock load entry point."""
+        return __import__(self.module)
+
+
+@pytest.yield_fixture
+def mock_entry_points():
+    """Mock of the JSONSchemas entry points."""
+    entry_points = {}
+
+    class EntryPointBuilder(object):
+        """Manipulate mock Entrypoints."""
+
+        def add(self, group, name, module):
+            """Register additional entrypoints."""
+            if group not in entry_points:
+                entry_points[group] = []
+            entry_points[group].append(
+                MockEntryPoint(name=name, value=module, group=group)
+            )
+
+    def fn(group=None):
+        if group:
+            entry_points_group = []
+            for eps in entry_points.values():
+                for ep in eps:
+                    if ep.group == group:
+                        entry_points_group.append(ep)
+            return entry_points_group
+        return entry_points
+
+    with patch("importlib.metadata.entry_points", fn):
+        yield EntryPointBuilder()
