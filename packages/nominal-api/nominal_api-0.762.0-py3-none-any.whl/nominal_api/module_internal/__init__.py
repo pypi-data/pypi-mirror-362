dc65e4f1ae9f10c4164b1eb7d3fd4b9d1dc7816f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:29 +0000

    Add package nominal-api/0.762.0/nominal_api-0.762.0-py3-none-any.whl

---
+++ packages/nominal-api/nominal_api-0.762.0-py3-none-any.whl/nominal_api/module_internal/__init__.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+from .._impl import (
+    module_internal_BatchGetResolvedModuleDefinitionsRequest as BatchGetResolvedModuleDefinitionsRequest,
+    module_internal_InternalModuleService as InternalModuleService,
+    module_internal_ModuleApplicationReference as ModuleApplicationReference,
+    module_internal_ResolvedModuleVersionDefinition as ResolvedModuleVersionDefinition,
+)
+
+__all__ = [
+    'BatchGetResolvedModuleDefinitionsRequest',
+    'ModuleApplicationReference',
+    'ResolvedModuleVersionDefinition',
+    'InternalModuleService',
+]
+
