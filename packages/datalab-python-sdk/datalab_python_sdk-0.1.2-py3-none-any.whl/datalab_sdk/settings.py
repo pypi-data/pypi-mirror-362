8227690818337c84a5098434e84362cc466eb92d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:09 +0000

    Add package datalab-python-sdk/0.1.2/datalab_python_sdk-0.1.2-py3-none-any.whl

---
+++ packages/datalab-python-sdk/datalab_python_sdk-0.1.2-py3-none-any.whl/datalab_sdk/settings.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings
+import os
+
+
+class Settings(BaseSettings):
+    # Paths
+    BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    LOGLEVEL: str = "DEBUG"
+
+    # Base settings
+    DATALAB_API_KEY: str | None = None
+    DATALAB_HOST: str = "https://www.datalab.to"
+
+
+settings = Settings()
