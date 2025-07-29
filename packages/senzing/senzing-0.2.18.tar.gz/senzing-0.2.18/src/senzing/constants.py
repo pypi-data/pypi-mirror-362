7bd0db172e3d92ad8f2698a0561f02c75d3fe914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:08:22 +0000

    Add package senzing/0.2.18/senzing-0.2.18.tar.gz

---
+++ packages/senzing/senzing-0.2.18.tar.gz/senzing-0.2.18/src/senzing/constants.py
@@ -0,0 +1,39 @@
+"""
+szconstants.py has convenience constant declarations.
+"""
+
+# Metadata
+
+__all__ = [
+    "SZ_INITIALIZE_WITH_DEFAULT_CONFIGURATION",
+    "SZ_NO_ATTRIBUTES",
+    "SZ_NO_AVOIDANCES",
+    "SZ_NO_INFO",
+    "SZ_NO_LOGGING",
+    "SZ_NO_REQUIRED_DATASOURCES",
+    "SZ_NO_SEARCH_PROFILE",
+    "SZ_VERBOSE_LOGGING",
+    "SZ_WITHOUT_INFO",
+]
+__version__ = "0.0.1"  # See https://www.python.org/dev/peps/pep-0396/
+__date__ = "2023-10-30"
+__updated__ = "2023-10-30"
+
+# -----------------------------------------------------------------------------
+# Constant helper values useful for flags
+# -----------------------------------------------------------------------------
+
+SZ_WITHOUT_INFO = 0
+
+# -----------------------------------------------------------------------------
+# Constant helper values
+# -----------------------------------------------------------------------------
+
+SZ_INITIALIZE_WITH_DEFAULT_CONFIGURATION = 0
+SZ_NO_ATTRIBUTES = ""
+SZ_NO_AVOIDANCES = ""
+SZ_NO_INFO = ""
+SZ_NO_LOGGING = 0
+SZ_NO_REQUIRED_DATASOURCES = ""
+SZ_NO_SEARCH_PROFILE = ""
+SZ_VERBOSE_LOGGING = 1
