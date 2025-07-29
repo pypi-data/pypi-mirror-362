e0965be3740e3e046f5b7bc6e424aee60b99eea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:27 +0000

    Add package thehellmaker-lib-1/0.1.1/thehellmaker_lib_1-0.1.1-py3-none-any.whl

---
+++ packages/thehellmaker-lib-1/thehellmaker_lib_1-0.1.1-py3-none-any.whl/main.py
@@ -0,0 +1,11 @@
+class Lib1:
+    def __init__(self):
+        pass
+
+    def main(self):
+        print("Hello from lib-1 version 0.1.1!")
+
+
+if __name__ == "__main__":
+    lib1 = Lib1()
+    lib1.main()
