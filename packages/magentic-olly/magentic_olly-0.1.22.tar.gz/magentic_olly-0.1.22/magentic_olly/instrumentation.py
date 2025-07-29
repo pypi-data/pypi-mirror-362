05ab37ecd45419a411c8d2a5f13b7e561665a35f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:04:48 +0000

    Add package magentic-olly/0.1.22/magentic_olly-0.1.22.tar.gz

---
+++ packages/magentic-olly/magentic_olly-0.1.22.tar.gz/magentic_olly-0.1.22/magentic_olly/instrumentation.py
@@ -0,0 +1,30 @@
+from functools import wraps
+import sys
+from .details.model import model
+from .details import wrapper_service, initializer
+
+def magentic_wrapper(func):
+    @wraps(func)
+    def wrapper(event, context, *args, **kwargs):
+        service_params = wrapper_service.MagenticWrapperServiceParams(
+            event=event,
+            lambda_context=context
+        )
+        
+        magentic_service = wrapper_service.MagenticWrapperService(service_params)
+        response = magentic_service.run_lambda_handler(func, *args, **kwargs)
+        return response
+    
+    return wrapper
+
+def initialize(params: model.InstrumentationParams):
+    tracing_initializer = initializer.InitializerProvider.create_tracing_initializer(params)
+    tracing_initializer.initialize()
+    print("OpenTelemetry tracing initialized.")
+    if params.enable_logging:
+        logging_initializer = initializer.InitializerProvider.create_logging_initializer(params)
+        logging_initializer.initialize()
+        print("OpenTelemetry logging initialized.")
+    
+def _is_pytest():
+    return "pytest" in sys.modules
