7622101565fba34689d02d5f77abe61be9cc3ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:34 +0000

    Add package odoo-addon-account-invoice-margin-sale/16.0.1.0.4/odoo_addon_account_invoice_margin_sale-16.0.1.0.4-py3-none-any.whl

---
+++ packages/odoo-addon-account-invoice-margin-sale/odoo_addon_account_invoice_margin_sale-16.0.1.0.4-py3-none-any.whl/odoo/addons/account_invoice_margin_sale/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2017-2018 Tecnativa - Sergio Teruel
+# Copyright 2019 Tecnativa - Carlos Dauden
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Account Invoice Margin Sale",
+    "summary": "Set margin in invoices from sale orders",
+    "version": "16.0.1.0.4",
+    "development_status": "Production/Stable",
+    "maintainers": ["sergio-teruel", "carlosdauden"],
+    "category": "Account",
+    "website": "https://github.com/OCA/margin-analysis",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "auto_install": True,
+    "post_init_hook": "post_init_hook",
+    "depends": ["sale_margin", "account_invoice_margin"],
+}
