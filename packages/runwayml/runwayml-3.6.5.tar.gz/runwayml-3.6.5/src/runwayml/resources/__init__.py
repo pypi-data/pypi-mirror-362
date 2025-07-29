d527b34042353d90c3d8a6deac1e666136b3938e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:43 +0000

    Add package runwayml/3.6.5/runwayml-3.6.5.tar.gz

---
+++ packages/runwayml/runwayml-3.6.5.tar.gz/runwayml-3.6.5/src/runwayml/resources/__init__.py
@@ -0,0 +1,75 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .tasks import (
+    TasksResource,
+    AsyncTasksResource,
+    TasksResourceWithRawResponse,
+    AsyncTasksResourceWithRawResponse,
+    TasksResourceWithStreamingResponse,
+    AsyncTasksResourceWithStreamingResponse,
+)
+from .organization import (
+    OrganizationResource,
+    AsyncOrganizationResource,
+    OrganizationResourceWithRawResponse,
+    AsyncOrganizationResourceWithRawResponse,
+    OrganizationResourceWithStreamingResponse,
+    AsyncOrganizationResourceWithStreamingResponse,
+)
+from .text_to_image import (
+    TextToImageResource,
+    AsyncTextToImageResource,
+    TextToImageResourceWithRawResponse,
+    AsyncTextToImageResourceWithRawResponse,
+    TextToImageResourceWithStreamingResponse,
+    AsyncTextToImageResourceWithStreamingResponse,
+)
+from .video_upscale import (
+    VideoUpscaleResource,
+    AsyncVideoUpscaleResource,
+    VideoUpscaleResourceWithRawResponse,
+    AsyncVideoUpscaleResourceWithRawResponse,
+    VideoUpscaleResourceWithStreamingResponse,
+    AsyncVideoUpscaleResourceWithStreamingResponse,
+)
+from .image_to_video import (
+    ImageToVideoResource,
+    AsyncImageToVideoResource,
+    ImageToVideoResourceWithRawResponse,
+    AsyncImageToVideoResourceWithRawResponse,
+    ImageToVideoResourceWithStreamingResponse,
+    AsyncImageToVideoResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "TasksResource",
+    "AsyncTasksResource",
+    "TasksResourceWithRawResponse",
+    "AsyncTasksResourceWithRawResponse",
+    "TasksResourceWithStreamingResponse",
+    "AsyncTasksResourceWithStreamingResponse",
+    "ImageToVideoResource",
+    "AsyncImageToVideoResource",
+    "ImageToVideoResourceWithRawResponse",
+    "AsyncImageToVideoResourceWithRawResponse",
+    "ImageToVideoResourceWithStreamingResponse",
+    "AsyncImageToVideoResourceWithStreamingResponse",
+    "TextToImageResource",
+    "AsyncTextToImageResource",
+    "TextToImageResourceWithRawResponse",
+    "AsyncTextToImageResourceWithRawResponse",
+    "TextToImageResourceWithStreamingResponse",
+    "AsyncTextToImageResourceWithStreamingResponse",
+    "VideoUpscaleResource",
+    "AsyncVideoUpscaleResource",
+    "VideoUpscaleResourceWithRawResponse",
+    "AsyncVideoUpscaleResourceWithRawResponse",
+    "VideoUpscaleResourceWithStreamingResponse",
+    "AsyncVideoUpscaleResourceWithStreamingResponse",
+    "OrganizationResource",
+    "AsyncOrganizationResource",
+    "OrganizationResourceWithRawResponse",
+    "AsyncOrganizationResourceWithRawResponse",
+    "OrganizationResourceWithStreamingResponse",
+    "AsyncOrganizationResourceWithStreamingResponse",
+]
