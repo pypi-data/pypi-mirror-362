4a17f4a55b3df63e494a58d7e2c90763ec5f1b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:19:48 +0000

    Add package jupyterlab-firefox-launcher/0.2.6/jupyterlab_firefox_launcher-0.2.6-py3-none-any.whl

---
+++ packages/jupyterlab-firefox-launcher/jupyterlab_firefox_launcher-0.2.6-py3-none-any.whl/jupyterlab_firefox_launcher/server.py
@@ -0,0 +1,14 @@
+# jupyterlab_firefox_launcher/server.py
+from jupyterlab_firefox_launcher import setup_handlers
+
+def _load_jupyter_server_extension(app):
+    """Called when the extension is loaded"""
+    setup_handlers(app.web_app)
+    app.log.info("Loaded jupyterlab_firefox_launcher extension")
+
+
+def _jupyter_server_extension_paths():
+    return [{
+        "module": "jupyterlab_firefox_launcher"
+    }]
+
