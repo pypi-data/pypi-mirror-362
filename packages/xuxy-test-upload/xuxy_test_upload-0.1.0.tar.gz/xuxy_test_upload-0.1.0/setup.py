5807561ce82d406ce5a8e2ce161ddf099e05891a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:36 +0000

    Add package xuxy-test-upload/0.1.0/xuxy_test_upload-0.1.0.tar.gz

---
+++ packages/xuxy-test-upload/xuxy_test_upload-0.1.0.tar.gz/xuxy_test_upload-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="xuxy_test_upload",
+    version="0.1.0",
+    author="xuxy",
+    author_email="",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/你的用户名/hello-printer",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/xuxy-test-upload/xuxy_test_upload-0.1.0.tar.gz/xuxy_test_upload-0.1.0/xuxy_test_upload/__init__.py
@@ -0,0 +1,4 @@
+from .printer import print_hello
+
+__all__ = ['print_hello']
+__version__ = '0.1.0'
