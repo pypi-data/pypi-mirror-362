c6c10e6a5ac4637e8fd0efa2e2a0181ad0e219dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:29 +0000

    Add package policyengine-uk/2.37.0/policyengine_uk-2.37.0.tar.gz

---
+++ packages/policyengine-uk/policyengine_uk-2.37.0.tar.gz/policyengine_uk-2.37.0/policyengine_uk/variables/input/consumption/property/council_tax.py
@@ -0,0 +1,12 @@
+from policyengine_uk.model_api import *
+
+
+class council_tax(Variable):
+    value_type = float
+    entity = Household
+    label = "Council Tax"
+    documentation: str = "Gross amount spent on Council Tax, before discounts"
+    definition_period = YEAR
+    unit = GBP
+    quantity_type = FLOW
+    uprating = "gov.economic_assumptions.indices.obr.council_tax"
