5a646e228adceb20d25d95f7776e7252f72f5967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:04:59 +0000

    Add package minimind/0.2.9/minimind-0.2.9.tar.gz

---
+++ packages/minimind/minimind-0.2.9.tar.gz/minimind-0.2.9/minimind/ml.py
@@ -0,0 +1,10 @@
+from .gpm import GPMGenerator, RidgeMultiOutputRegressor
+from .sap import SAPGenerator, RidgeRegressor
+
+
+__all__ = [
+    "GPMGenerator",
+    "SAPGenerator",
+    "RidgeMultiOutputRegressor",
+    "RidgeRegressor"
+]
