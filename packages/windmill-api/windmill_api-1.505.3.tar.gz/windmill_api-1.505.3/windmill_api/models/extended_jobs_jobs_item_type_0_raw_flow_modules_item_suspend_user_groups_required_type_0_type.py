1310154a4405e82c157d56d053cb8f521f681eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:19 +0000

    Add package windmill-api/1.505.3/windmill_api-1.505.3.tar.gz

---
+++ packages/windmill-api/windmill_api-1.505.3.tar.gz/windmill_api-1.505.3/windmill_api/models/extended_jobs_jobs_item_type_0_raw_flow_modules_item_suspend_user_groups_required_type_0_type.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class ExtendedJobsJobsItemType0RawFlowModulesItemSuspendUserGroupsRequiredType0Type(str, Enum):
+    JAVASCRIPT = "javascript"
+
+    def __str__(self) -> str:
+        return str(self.value)
