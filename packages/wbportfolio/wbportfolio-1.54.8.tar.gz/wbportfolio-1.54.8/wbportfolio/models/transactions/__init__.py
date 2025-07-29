a89fbb40c2e9d16ffcca3e43f9ee167dfae864a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:05:39 +0000

    Add package wbportfolio/1.54.8/wbportfolio-1.54.8.tar.gz

---
+++ packages/wbportfolio/wbportfolio-1.54.8.tar.gz/wbportfolio-1.54.8/wbportfolio/models/transactions/__init__.py
@@ -0,0 +1,6 @@
+from .claim import Claim
+from .dividends import DividendTransaction
+from .fees import FeeCalculation, Fees
+from .trade_proposals import TradeProposal
+from .rebalancing import RebalancingModel, Rebalancer
+from .trades import Trade
