528519901f8a278ac69d7a34aa1d07ccba07f5d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:43 +0000

    Add package pdf-pagemerge/0.0.2/pdf_pagemerge-0.0.2-py3-none-any.whl

---
+++ packages/pdf-pagemerge/pdf_pagemerge-0.0.2-py3-none-any.whl/pdf_pagemerge/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from .pdf_pagemerge import PDFMerger
+
+__copyright__    = 'Copyright (c) 2025, Nanigashi Uji'
+__version__      = PDFMerger.VERSION
+__license__      = 'BSD-3-Clause'
+__author__       = 'Nanigashi Uji'
+__author_email__ = '53845049+nanigashi-uji@users.noreply.github.com,4423013-nanigashi_uji@users.noreply.gitlab.com'
+__url__          = 'https://github.com/nanigashi-uji/pdf-pagemerge.git,https://gitlab.com/nanigashi_uji/pdf-pagemerge'
+
+__all__ = ['PDFMerger', ]
