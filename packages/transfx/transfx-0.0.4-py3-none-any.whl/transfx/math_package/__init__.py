f116c2efcd54346ff1e58ed20931b08daa471453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:13 +0000

    Add package transfx/0.0.4/transfx-0.0.4-py3-none-any.whl

---
+++ packages/transfx/transfx-0.0.4-py3-none-any.whl/transfx/math_package/__init__.py
@@ -0,0 +1,16 @@
+"""transFX_math_package"""
+from .module import (
+    add,
+    sum,
+    sub,
+    mul,
+    div
+)
+
+__all__ = [
+    "add",
+    "sum",
+    "sub",
+    "mul",
+    "div"
+]
---
+++ packages/transfx/transfx-0.0.4-py3-none-any.whl/transfx/welcome_package/module.py
@@ -0,0 +1,5 @@
+"""transFX_welcom_package"""
+
+def hello(name="World"): 
+    """hello函数"""
+    print(f"Hello, {name}! This is from transFX Welcom Package.")
