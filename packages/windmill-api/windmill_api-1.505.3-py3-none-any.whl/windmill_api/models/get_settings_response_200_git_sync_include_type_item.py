441a9e127114176da96a28c3679fdab81cb6c708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:19 +0000

    Add package windmill-api/1.505.3/windmill_api-1.505.3-py3-none-any.whl

---
+++ packages/windmill-api/windmill_api-1.505.3-py3-none-any.whl/windmill_api/models/get_settings_response_200_git_sync_include_type_item.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class GetSettingsResponse200GitSyncIncludeTypeItem(str, Enum):
+    APP = "app"
+    FLOW = "flow"
+    FOLDER = "folder"
+    GROUP = "group"
+    RESOURCE = "resource"
+    RESOURCETYPE = "resourcetype"
+    SCHEDULE = "schedule"
+    SCRIPT = "script"
+    SECRET = "secret"
+    TRIGGER = "trigger"
+    USER = "user"
+    VARIABLE = "variable"
+
+    def __str__(self) -> str:
+        return str(self.value)
