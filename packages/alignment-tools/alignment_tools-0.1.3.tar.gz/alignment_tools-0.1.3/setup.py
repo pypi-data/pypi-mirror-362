ee5b9a341bc4f2f101976500bce083e5c6fdebe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:06:17 +0000

    Add package alignment-tools/0.1.3/alignment_tools-0.1.3.tar.gz

---
+++ packages/alignment-tools/alignment_tools-0.1.3.tar.gz/alignment_tools-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+install_requires=[
+          "pandas",
+          "numpy",
+          "matplotlib",
+          "seaborn",
+          "scipy",
+          "biopython",
+          "freesasa"
+            ]
+
+
+setup(
+    name="alignment_tools",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=install_requires,
+    author="Amirhossein Sakhteman",
+    author_email="amirhossein.sakhteman@tume.de",
+    description="Tools for AlphaFold PDB processing, SASA, Clustal, and InterPro annotation",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires='>=3.7',
+)
