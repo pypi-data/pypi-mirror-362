910a2696d1fe6d2b000babd075e8ccb885c352d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 13:18:43 +0000

    Add package dojo-beam-transforms/2.0.5/dojo-beam-transforms-2.0.5.tar.gz

---
+++ packages/dojo-beam-transforms/dojo-beam-transforms-2.0.5.tar.gz/dojo-beam-transforms-2.0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dojo-beam-transforms',
+    version='2.0.5',
+    packages=find_packages(),
+    install_requires=[
+        'apache-beam[dataframe,gcp,interactive]==2.58.1',
+        'pandas==2.0.3',
+        'pandas-datareader==0.10.0',
+        'PyMuPDF==1.23.22',
+        'pypinyin==0.51.0',
+        'unidecode==1.3.8',
+        'openpyxl==3.0.10',
+        'fsspec==2024.6.1',
+        'gcsfs==2024.6.1',
+        'google-cloud-secret-manager==2.22.0'
+    ],
+)
