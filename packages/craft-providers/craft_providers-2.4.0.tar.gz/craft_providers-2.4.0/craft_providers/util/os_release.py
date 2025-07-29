16e1d757b64dacbcddfe9bc627717b02734d3cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:06:55 +0000

    Add package craft-providers/2.4.0/craft_providers-2.4.0.tar.gz

---
+++ packages/craft-providers/craft_providers-2.4.0.tar.gz/craft_providers-2.4.0/craft_providers/util/os_release.py
@@ -0,0 +1,80 @@
+#
+# Copyright 2021-2023 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+"""Parser for /etc/os-release."""
+
+from pathlib import Path
+
+OS_RELEASE_FILE = Path("/etc/os-release")
+
+
+def parse_os_release(content: str | None = None) -> dict[str, str]:
+    """Parser for /etc/os-release.
+
+    Format documentation at:
+
+    https://www.freedesktop.org/software/systemd/man/os-release.html
+
+    Example os-release contents::
+
+        NAME="Ubuntu"
+        VERSION="22.04 (Jammy Jellyfish)"
+        ID=ubuntu
+        ID_LIKE=debian
+        PRETTY_NAME="Ubuntu 22.04"
+        VERSION_ID="22.04"
+        HOME_URL="https://www.ubuntu.com/"
+        SUPPORT_URL="https://help.ubuntu.com/"
+        BUG_REPORT_URL="https://bugs.launchpad.net/ubuntu/"
+        PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
+        VERSION_CODENAME=jammy
+        UBUNTU_CODENAME=jammy
+
+    :param content: String contents of os-release file.  If None, will read contents of
+    file from host.
+
+    :returns: Dictionary of key-mappings found in os-release. Values are
+    stripped of encapsulating quotes.
+
+    """
+    if content is None:
+        with OS_RELEASE_FILE.open() as f:
+            content = f.read()
+
+    mappings: dict[str, str] = {}
+
+    for line in content.splitlines():
+        line = line.strip()
+
+        # Ignore commented lines.
+        if line.startswith("#"):
+            continue
+
+        # Ignore empty lines.
+        if not line:
+            continue
+
+        if "=" in line:
+            key, value = line.split("=", maxsplit=1)
+
+            # Strip encapsulating quotes, single or double.
+            if value[0] == value[-1] and value[0] in ("'", '"'):
+                value = value[1:-1]
+
+            mappings[key] = value
+
+    return mappings
---
+++ packages/craft-providers/craft_providers-2.4.0.tar.gz/craft_providers-2.4.0/docs/index.rst
@@ -0,0 +1,55 @@
+Craft Providers
+===========================================
+
+Craft Providers is a Python package for managing software builds in containers
+on behalf of tools using the Craft Parts framework.
+
+Craft Providers aims to provide Python interfaces for instantiating build
+environments, configuring base images and executing builds for a variety of
+target environments.
+
+The aim of this package is to provide a uniform, extensible set of interfaces
+that other tools and packages can use to build software without needing to
+know the particular details of each build environment or system.
+
+This package is most useful for implementers of tools using the Craft Parts
+framework that need to provide support for additional build environments.
+
+.. toctree::
+   :maxdepth: 1
+   :hidden:
+
+   tutorials/index
+   howto/index
+   reference/index
+   explanation/index
+
+.. list-table::
+
+    * - | :ref:`Tutorial <tutorial>`
+        | **Get started** with a hands-on introduction to Craft Providers
+      - | :ref:`How-to guides <howto>`
+        | **Step-by-step guides** covering key operations and common tasks
+    * - | :ref:`Reference <reference>`
+        | **Technical information** about Craft Providers
+      - | :ref:`Explanation <explanation>`
+        | **Discussion and clarification** of key topics
+
+Project and community
+=====================
+
+Craft Providers is a member of the Canonical family. It's an open source project
+that warmly welcomes community projects, contributions, suggestions, fixes
+and constructive feedback.
+
+* `Ubuntu Code of Conduct
+  <https://ubuntu.com/community/ethos/code-of-conduct>`_.
+* `Canonical contributor licenses agreement
+  <https://ubuntu.com/legal/contributors>`_.
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
