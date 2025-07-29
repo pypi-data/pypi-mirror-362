927b47008ab6df260fcdb015f8f83fcedfb1b7e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:06:18 +0000

    Add package wyrmx-cli/1.0.73/wyrmx_cli-1.0.73-py3-none-any.whl

---
+++ packages/wyrmx-cli/wyrmx_cli-1.0.73-py3-none-any.whl/wyrmx_cli/commands/run.py
@@ -0,0 +1,59 @@
+from rich.console import Console
+import typer
+
+
+def run(
+    app_module: str = "src.main:app",
+    host: str = "127.0.0.1",
+    port: int = 8000,
+    reload: bool = True
+):
+
+    """
+    Run Wyrmx Server.
+    """
+    from pathlib import Path
+    import subprocess, os, sys
+
+
+    projectRoot = Path.cwd()
+    if not (projectRoot / "src").exists(): raise RuntimeError(f"ERROR: No `src` in {projectRoot}. Run from your project root.")
+
+    os.chdir(projectRoot)
+    sys.path.insert(0, str(projectRoot))
+    
+    '''subprocess.run(
+        ["poetry", "run", "pyright"],
+        cwd=str(projectRoot),
+        check=True
+    )
+
+    subprocess.run(
+        ["poetry", "run", "uvicorn", app_module, "--host", host, "--port", str(port), "--reload" if reload else "--no-reload"],
+        cwd=str(projectRoot),
+        check=True
+    )'''
+
+
+    
+    with Console().status("[bold green][INFO]🔍 Running static type check with Pyright..."):
+        subprocess.run(
+            [
+                "poetry",
+                "run",
+                "pyright"
+            ],
+            cwd=str(projectRoot),
+            check=True
+        )
+    
+    typer.secho("[INFO]✅ Type check passed. 🚀 Starting Wyrmx server...", fg=typer.colors.GREEN)
+
+    subprocess.run(
+        ["poetry", "run", "uvicorn", app_module, "--host", host, "--port", str(port), "--reload" if reload else "--no-reload"],
+        cwd=str(projectRoot),
+        check=True
+    )
+
+
+    
