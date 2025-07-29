b53f97a593cf483901f4793c764db59a9c1bc202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:22 +0000

    Add package dragon-ml-toolbox/3.12.2/dragon_ml_toolbox-3.12.2-py3-none-any.whl

---
+++ packages/dragon-ml-toolbox/dragon_ml_toolbox-3.12.2-py3-none-any.whl/ml_tools/keys.py
@@ -0,0 +1,34 @@
+class LogKeys:
+    """
+    Used internally for ML scripts module.
+    
+    Centralized keys for logging and history.
+    """
+    # --- Epoch Level ---
+    TRAIN_LOSS = 'train_loss'
+    VAL_LOSS = 'val_loss'
+
+    # --- Batch Level ---
+    BATCH_LOSS = 'loss'
+    BATCH_INDEX = 'batch'
+    BATCH_SIZE = 'size'
+
+
+class ModelSaveKeys:
+    """
+    Used internally for ensemble_learning module.
+    """
+    # Serializing a trained model metadata.
+    MODEL = "model"
+    FEATURES = "feature_names"
+    TARGET = "target_name"
+    
+    # Classification keys
+    CLASSIFICATION_LABEL = "label"
+    CLASSIFICATION_PROBABILITIES = "probabilities"
+
+
+class _OneHotOtherPlaceholder:
+    OTHER_GUI = "OTHER"
+    OTHER_MODEL = "one hot OTHER placeholder"
+    OTHER_DICT = {OTHER_GUI: OTHER_MODEL}
