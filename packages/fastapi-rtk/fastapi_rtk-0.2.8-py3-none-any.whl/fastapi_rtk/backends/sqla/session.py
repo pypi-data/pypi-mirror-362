65d68758a5affc99b76d3b2bf4979db85c879785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:30 +0000

    Add package fastapi-rtk/0.2.8/fastapi_rtk-0.2.8-py3-none-any.whl

---
+++ packages/fastapi-rtk/fastapi_rtk-0.2.8-py3-none-any.whl/fastapi_rtk/backends/sqla/session.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session
+
+# This file is just to keep the folder structure consistent.
+
+__all__ = [
+    "Session",
+    "AsyncSession",
+]
