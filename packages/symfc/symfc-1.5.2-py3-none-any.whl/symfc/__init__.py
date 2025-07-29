22b8f38f1d1bd51887ae41e3ac4200e3f19eebe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:05:34 +0000

    Add package symfc/1.5.2/symfc-1.5.2-py3-none-any.whl

---
+++ packages/symfc/symfc-1.5.2-py3-none-any.whl/symfc/__init__.py
@@ -0,0 +1,6 @@
+"""Forcd constants calculation code: Symfc."""
+
+from .api_symfc import Symfc  # noqa F401
+from .version import __version__  # noqa F401
+
+from .api_symfc import eigh, eigsh  # noqa F401
