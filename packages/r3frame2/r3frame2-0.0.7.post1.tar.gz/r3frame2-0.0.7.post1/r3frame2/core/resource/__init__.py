90e6fb53cf10d1666d0d9a0e34d2510d84f5691c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 16:06:13 +0000

    Add package r3frame2/0.0.7.post1/r3frame2-0.0.7.post1.tar.gz

---
+++ packages/r3frame2/r3frame2-0.0.7.post1.tar.gz/r3frame2-0.0.7.post1/r3frame2/core/resource/__init__.py
@@ -0,0 +1,17 @@
+from .base import R3database
+
+from .world import R3world
+from .world.ecs import R3ecs
+from .world.partition import R3gridPartition
+from .world.partition import R3treePartition
+
+from .pipeline import R3entity
+
+from .pipeline.physics import R3aabb
+
+from .pipeline.render import R3renderCall
+from .pipeline.render import R3surf
+from .pipeline.render import R3anim
+from .pipeline.render import R3atlas
+
+from .pipeline.ui import R3element
