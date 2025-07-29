491d3c96b96240d94e17c80256610c2be100f87d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 22:06:10 +0000

    Add package VecPosLib/1.0.3.post2/vecposlib-1.0.3.post2-py3-none-any.whl

---
+++ packages/VecPosLib/vecposlib-1.0.3.post2-py3-none-any.whl/vecposlib/vectorlib/__init__.py
@@ -0,0 +1,11 @@
+"""
+VecPosLib サブパッケージ
+
+- Vector: 汎用ベクトルクラス
+- Vec2, Vec3, Vec4: n次元ベクトルクラス
+"""
+
+# ローカルモジュール
+from .vector import Vector, Vec2, Vec3, Vec4
+
+__all__ = ["Vector", "Vec2", "Vec3", "Vec4"]
