a481a05201d9e73c492a104c50cdf26e7fd27834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:31 +0000

    Add package marge-mri-example/0.0.4/marge_mri_example-0.0.4.tar.gz

---
+++ packages/marge-mri-example/marge_mri_example-0.0.4.tar.gz/marge_mri_example-0.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="marge-mri-example",
+    version="0.0.4",
+    packages=['marge'],
+    install_requires=[],
+    author="Nihal Ouahabi",
+    description="Une description de ton package MaRGE",
+)
+
