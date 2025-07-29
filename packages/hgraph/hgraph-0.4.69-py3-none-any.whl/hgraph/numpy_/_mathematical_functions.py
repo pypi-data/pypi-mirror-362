f5c5f642790f69ccf43b7510a807a54530d5cc81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 18:05:07 +0000

    Add package hgraph/0.4.69/hgraph-0.4.69-py3-none-any.whl

---
+++ packages/hgraph/hgraph-0.4.69-py3-none-any.whl/hgraph/numpy_/_mathematical_functions.py
@@ -0,0 +1,21 @@
+__all__ = ["cumsum"]
+
+from functools import wraps
+
+import numpy as np
+
+from hgraph import TS, lift, graph, AUTO_RESOLVE
+from hgraph.numpy_._constants import ARRAY
+from hgraph.numpy_._utils import add_docs
+
+
+@graph
+@add_docs(np.cumsum)
+def cumsum(a: TS[ARRAY], axis: int = None, _tp: type[ARRAY] = AUTO_RESOLVE) -> TS[ARRAY]:
+    if axis is None:
+        return lift(np.cumsum, {"a": TS[_tp]}, TS[_tp])(a=a)
+    else:
+        return lift(np.cumsum, {"a": TS[_tp], "axis": TS[int]}, TS[_tp])(a=a, axis=axis)
+
+
+
