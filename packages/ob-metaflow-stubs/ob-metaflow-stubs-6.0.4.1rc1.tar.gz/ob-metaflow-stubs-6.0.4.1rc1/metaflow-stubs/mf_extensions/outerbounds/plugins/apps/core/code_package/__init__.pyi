6137587b0b23e59a668a1b0790b81fba013ae750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 15:06:19 +0000

    Add package ob-metaflow-stubs/6.0.4.1rc1/ob-metaflow-stubs-6.0.4.1rc1.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.4.1rc1.tar.gz/ob-metaflow-stubs-6.0.4.1rc1/metaflow-stubs/mf_extensions/outerbounds/plugins/apps/core/code_package/__init__.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.16.0.1+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-07-14T20:15:55.044997                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import code_packager as code_packager
+from .code_packager import CodePackager as CodePackager
+
