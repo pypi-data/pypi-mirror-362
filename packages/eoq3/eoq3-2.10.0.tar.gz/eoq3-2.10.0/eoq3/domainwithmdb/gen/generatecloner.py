e87d58efe2facf541bdf0eb143eb46c3c8d26b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:51 +0000

    Add package eoq3/2.10.0/eoq3-2.10.0.tar.gz

---
+++ packages/eoq3/eoq3-2.10.0.tar.gz/eoq3-2.10.0/eoq3/domainwithmdb/gen/generatecloner.py
@@ -0,0 +1,16 @@
+'''
+ script to create
+ - conecepts.py
+ - __init__.py
+ 
+ Bjoern Annighoefer 2023
+'''
+
+import os
+import shutil
+
+if __name__ == "__main__":
+    GEN_FILE = "../cloner.py"
+    GEN_TEMP = "cloner.py.mako"
+    
+    os.system("python -m eoq3conceptsgen.generatefromconceptscli -i %s -o %s"%(GEN_TEMP,GEN_FILE))
