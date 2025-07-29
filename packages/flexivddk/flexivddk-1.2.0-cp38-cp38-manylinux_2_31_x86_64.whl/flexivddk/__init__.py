ce8391ae4ecca95cfd479634acdc83ecdcfcc8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:08:11 +0000

    Add package flexivddk/1.2.0/flexivddk-1.2.0-cp38-cp38-manylinux_2_31_x86_64.whl

---
+++ packages/flexivddk/flexivddk-1.2.0-cp38-cp38-manylinux_2_31_x86_64.whl/flexivddk/__init__.py
@@ -0,0 +1,11 @@
+# flexivddk/__init__.py
+
+"""
+Python wrapper of Flexiv DDK C++ library (https://github.com/flexivrobotics/flexiv_ddk)
+
+"""
+
+__version__ = "1.2.0"
+__author__ = "Flexiv Ltd"
+
+from .flexivddk import *
