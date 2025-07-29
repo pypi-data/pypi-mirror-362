3ec9ce6477eefbae9ef287da8428dc7a680b646f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:51 +0000

    Add package bhfutils/0.1.20/bhfutils-0.1.20.tar.gz

---
+++ packages/bhfutils/bhfutils-0.1.20.tar.gz/bhfutils-0.1.20/bhfutils/crawler/items.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+
+from scrapy import Item, Field
+
+
+class RawResponseItem(Item):
+    appid = Field()
+    crawlid = Field()
+    url = Field()
+    responseUrl = Field()
+    statusCode = Field()
+    success = Field()
+    exception = Field()
+    encoding = Field()
+    attrs = Field()
+
+
+class MenuResponseItem(RawResponseItem):
+    playgroundId = Field()
+    groupCategoryName = Field()
+    groupName = Field()
+    groupUrl = Field()
+
+
+class ScreenshotResponseItem(RawResponseItem):
+    productUrl = Field()
+    price = Field()
+    priceDatetime = Field()
+
+
+class ProductResponseItem(RawResponseItem):
+    playgroundId = Field()
+    productUrl = Field()
+    groupId = Field()
+    price = Field()
+
+
+class ProductDetailsResponseItem(RawResponseItem):
+    playgroundId = Field()
+    productUrl = Field()
+    groupId = Field()
+    imageUrls = Field()
+    type = Field()
+    name = Field()
+    brandName = Field()
+    sellerName = Field()
+    description = Field()
+    details = Field()
+    comments = Field()
+
+
+class ReviewResponseItem(RawResponseItem):
+    technoBlogId = Field()
+    reviewUrl = Field()
+    createDate = Field()
+    name = Field()
+    category = Field()
+    imageUrl = Field()
+
+
+class ReviewDetailsResponseItem(ReviewResponseItem):
+    author = Field()
+    keywords = Field()
+    description = Field()
+    pros = Field()
+    cons = Field()
+    productName = Field()
+    productParameters = Field()
+    verdict = Field()
+    comments = Field()
+    baseParameters = Field()
+
+
+class BrandProductResponseItem(RawResponseItem):
+    brandId = Field()
+    groupId = Field()
+    productUrl = Field()
+    code = Field()
+    name = Field()
+    imageUrls = Field()
+    minPrice = Field()
+
+
+class BrandProductDetailsResponseItem(BrandProductResponseItem):
+    description = Field()
+    variations = Field()
+    productParameters = Field()
