7accd2cabb10b81bdd32821095d01f59aa79aa36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:47 +0000

    Add package sdkit/2.0.15.18/sdkit-2.0.15.18-py3-none-any.whl

---
+++ packages/sdkit/sdkit-2.0.15.18-py3-none-any.whl/sdkit/models/model_loader/codeformer/__init__.py
@@ -0,0 +1,22 @@
+from .codeformer_arch import CodeFormer
+
+from sdkit import Context
+from sdkit.utils import load_tensor_file
+
+
+def load_model(context: Context, **kwargs):
+    codeformer_path = context.model_paths["codeformer"]
+    sd = load_tensor_file(codeformer_path)
+    sd = sd["params_ema"]
+
+    model = CodeFormer(dim_embd=512, codebook_size=1024, n_head=8, n_layers=9, connect_list=["32", "64", "128", "256"])
+    model = model.to(context.torch_device)
+
+    model.load_state_dict(sd)
+    model.eval()
+
+    return model
+
+
+def unload_model(context: Context, **kwargs):
+    pass
