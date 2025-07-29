a36172cd1721e95dbae7b9c5963e6b851c787426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:41 +0000

    Add package virtuals-acp/0.1.18/virtuals_acp-0.1.18-py3-none-any.whl

---
+++ packages/virtuals-acp/virtuals_acp-0.1.18-py3-none-any.whl/virtuals_acp/__init__.py
@@ -0,0 +1,49 @@
+# virtuals_acp/__init__.py
+
+from .models import (
+    IACPAgent,
+    ACPJobPhase,
+    MemoType,
+    ACPAgentSort
+)
+from .configs import (
+    ACPContractConfig,
+    BASE_SEPOLIA_CONFIG,
+    BASE_MAINNET_CONFIG,
+    DEFAULT_CONFIG
+)
+from .exceptions import (
+    ACPError,
+    ACPApiError,
+    ACPContractError,
+    TransactionFailedError
+)
+from .client import VirtualsACP
+from .job import ACPJob
+from .offering import ACPJobOffering
+from .memo import ACPMemo
+from .abi import ACP_ABI, ERC20_ABI
+
+__all__ = [
+    "VirtualsACP",
+    "IACPAgent",
+    "ACPJobPhase",
+    "MemoType",
+    "IACPOffering",
+    "ACPContractConfig",
+    "BASE_SEPOLIA_CONFIG",
+    "BASE_MAINNET_CONFIG",
+    "DEFAULT_CONFIG",
+    "ACPError",
+    "ACPApiError",
+    "ACPContractError",
+    "TransactionFailedError",
+    "ACP_ABI",
+    "ERC20_ABI",
+    "ACPJob",
+    "ACPMemo",
+    "ACPJobOffering",
+    "ACPAgentSort"
+]
+
+__version__ = "0.1.0"
