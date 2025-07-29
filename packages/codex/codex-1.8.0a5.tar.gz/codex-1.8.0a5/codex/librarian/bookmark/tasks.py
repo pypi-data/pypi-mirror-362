5dd518b18d05e7ea7514b6bd646c7f34e6b86cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 17:08:32 +0000

    Add package codex/1.8.0a5/codex-1.8.0a5.tar.gz

---
+++ packages/codex/codex-1.8.0a5.tar.gz/codex-1.8.0a5/codex/librarian/bookmark/tasks.py
@@ -0,0 +1,36 @@
+"""Bookmark Tasks."""
+
+from collections.abc import Mapping
+from dataclasses import dataclass
+from typing import Any
+
+
+class BookmarkTask:
+    """Bookmark Base Class."""
+
+
+@dataclass
+class BookmarkUpdateTask(BookmarkTask):
+    """Bookmark a page."""
+
+    auth_filter: Mapping[str, int | str | None]
+    comic_pks: tuple[int]
+    updates: Mapping[str, Any]
+
+
+@dataclass
+class UserActiveTask(BookmarkTask):
+    """Update the user's last active status."""
+
+    pk: int
+
+
+class ClearLibrarianStatusTask(BookmarkTask):
+    """Clear all librarian statuses."""
+
+
+@dataclass
+class CodexLatestVersionTask(BookmarkTask):
+    """Get the latest version."""
+
+    force: bool = False
