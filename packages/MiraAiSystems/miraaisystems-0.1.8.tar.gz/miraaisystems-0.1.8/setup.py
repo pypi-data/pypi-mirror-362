4df899ac2e75e6c4e3ba59aee5c3ee8fe3ef0fb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:03 +0000

    Add package MiraAiSystems/0.1.8/miraaisystems-0.1.8.tar.gz

---
+++ packages/MiraAiSystems/miraaisystems-0.1.8.tar.gz/miraaisystems-0.1.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MiraAiSystems",
+    version="0.1.8",
+    packages=find_packages(),
+    install_requires=[
+        "python-dotenv",
+        "google-generativeai",
+        "rich",
+        "ultralytics",
+        "pyttsx3",
+        "SpeechRecognition",
+    ],
+    author="MoonTech",
+    description="An AI system that using Gemini AI to do various tasks",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
