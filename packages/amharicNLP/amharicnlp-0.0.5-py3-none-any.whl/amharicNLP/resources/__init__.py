9f1e46a9b294e08e42700d99cd17cca1b9300084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:55:50 +0000

    Add package amharicNLP/0.0.5/amharicnlp-0.0.5-py3-none-any.whl

---
+++ packages/amharicNLP/amharicnlp-0.0.5-py3-none-any.whl/amharicNLP/resources/__init__.py
@@ -0,0 +1,9 @@
+from .cleaner import AmharicCleaner
+from .normalizer import AmharicNormalizer
+from .lemmatizer import AmharicLemmatizer
+from .stemmer import AmharicStemmer
+from.utils import AmharicLanguageDetector
+from .stopwrod import AmharicStopwordProcessor
+
+
+__all__ = ['AmharicCleaner', 'AmharicNormalizer', 'AmharicLemmatizer', 'AmharicStemmer','AmharicLanguageDetector', 'AmharicStopwordProcessor','AmharicLemmatizer']
