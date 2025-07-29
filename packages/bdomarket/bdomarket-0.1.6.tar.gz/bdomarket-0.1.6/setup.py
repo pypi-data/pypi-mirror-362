e27fa0810d53069905a540af5e45ccce4f6aa22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 19:07:05 +0000

    Add package bdomarket/0.1.6/bdomarket-0.1.6.tar.gz

---
+++ packages/bdomarket/bdomarket-0.1.6.tar.gz/bdomarket-0.1.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+readme_path = os.path.join(here, "../README.md")
+
+with open(readme_path, "r") as f:
+    description = f.read()
+
+setup(
+    name='bdomarket',
+    version='0.1.6',
+    author="Szőke Dominik",
+    packages=find_packages(),
+    install_reqires=[
+        "requests>=2.25.1",
+        "beautifulsoup4>=4.13.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/Fizzor96/bdomarket",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8"
+)
