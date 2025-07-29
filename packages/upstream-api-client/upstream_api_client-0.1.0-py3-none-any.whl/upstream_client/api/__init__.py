e15a51bb984f5f7fcb4fb4a8daeeb2abda23d152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 23:09:15 +0000

    Add package upstream-api-client/0.1.0/upstream_api_client-0.1.0-py3-none-any.whl

---
+++ packages/upstream-api-client/upstream_api_client-0.1.0-py3-none-any.whl/upstream_client/api/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+# import apis into api package
+from upstream_client.api.auth_api import AuthApi
+from upstream_client.api.campaigns_api import CampaignsApi
+from upstream_client.api.measurements_api import MeasurementsApi
+from upstream_client.api.projects_api import ProjectsApi
+from upstream_client.api.sensor_variables_api import SensorVariablesApi
+from upstream_client.api.sensors_api import SensorsApi
+from upstream_client.api.stations_api import StationsApi
+from upstream_client.api.uploadfile_csv_api import UploadfileCsvApi
+
