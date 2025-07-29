5ce8d9fcea5ad1c65890ceb8e67e47dbeb18dc5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:31 +0000

    Add package pyzurecli/0.3.0/pyzurecli-0.3.0-py3-none-any.whl

---
+++ packages/pyzurecli/pyzurecli-0.3.0-py3-none-any.whl/pyzurecli/__init__.py
@@ -0,0 +1,5 @@
+from .factory import az, AzureCLI
+from .user import AzureCLIUser, UserSession
+from .sp import AzureCLIServicePrincipal
+from .app_registration import AzureCLIAppRegistration
+from .util import json_to_dataclass
