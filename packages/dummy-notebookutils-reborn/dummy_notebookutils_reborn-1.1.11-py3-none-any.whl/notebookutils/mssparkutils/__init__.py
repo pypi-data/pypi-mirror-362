04d5f644054f0146e93d06c316fd1247a2de1b8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:06 +0000

    Add package dummy-notebookutils-reborn/1.1.11/dummy_notebookutils_reborn-1.1.11-py3-none-any.whl

---
+++ packages/dummy-notebookutils-reborn/dummy_notebookutils_reborn-1.1.11-py3-none-any.whl/notebookutils/mssparkutils/__init__.py
@@ -0,0 +1,18 @@
+__all__ = ["fs", "notebook", "env", "credentials", "runtime", "session", "lakehouse"]
+
+from notebookutils.mssparkutils import (
+    credentials,
+    env,
+    fs,
+    lakehouse,
+    notebook,
+    runtime,
+    session,
+)
+
+
+def help(module_name: str = ""):
+    pass
+
+
+nbResPath = ""
