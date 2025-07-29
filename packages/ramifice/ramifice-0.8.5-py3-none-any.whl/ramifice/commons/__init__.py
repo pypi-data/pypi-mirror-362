3f1488b4a315b62e94b94be626273e141c5c492d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 12:05:25 +0000

    Add package ramifice/0.8.5/ramifice-0.8.5-py3-none-any.whl

---
+++ packages/ramifice/ramifice-0.8.5-py3-none-any.whl/ramifice/commons/__init__.py
@@ -0,0 +1,20 @@
+"""Commons - Model class methods."""
+
+from ramifice.commons.general import GeneralMixin
+from ramifice.commons.indexes import IndexMixin
+from ramifice.commons.many import ManyMixin
+from ramifice.commons.one import OneMixin
+from ramifice.commons.unit_manager import UnitMixin
+
+
+class QCommonsMixin(
+    GeneralMixin,
+    OneMixin,
+    ManyMixin,
+    IndexMixin,
+    UnitMixin,
+):
+    """Commons - Model class methods."""
+
+    def __init__(self) -> None:  # noqa: D107
+        super().__init__()
