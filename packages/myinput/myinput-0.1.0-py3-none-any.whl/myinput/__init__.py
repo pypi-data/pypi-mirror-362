e23966a9bcbeb4ff568de1b3c7da7ae9e603aca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 10:56:39 +0000

    Add package myinput/0.1.0/myinput-0.1.0-py3-none-any.whl

---
+++ packages/myinput/myinput-0.1.0-py3-none-any.whl/myinput/__init__.py
@@ -0,0 +1,12 @@
+# myinput/__init__.py
+
+def my_input(prompt="", end=""):
+    """
+    Custom function to mimic Python's built-in input().
+    Prints a prompt and returns user input.
+    """
+    if not isinstance(end, str):
+        end = ""  # Default to empty string if incorrect type passed
+    print(prompt, end=end)
+    return __builtins__.input()
+
