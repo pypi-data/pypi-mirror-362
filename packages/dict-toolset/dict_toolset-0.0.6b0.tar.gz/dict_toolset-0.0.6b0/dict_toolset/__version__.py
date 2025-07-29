0350841a7cdf67e253a6ab7eee43098af9733140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 11:04:47 +0000

    Add package dict-toolset/0.0.6b0/dict_toolset-0.0.6b0.tar.gz

---
+++ packages/dict-toolset/dict_toolset-0.0.6b0.tar.gz/dict_toolset-0.0.6b0/dict_toolset/__version__.py
@@ -0,0 +1,10 @@
+__title__ = "dict_toolset"
+__description__ = ""
+__url__ = "https://dict_tools.readthedocs.io"
+__version__ = "0.0.6b"
+__build__ = 0x023100
+__author__ = "George Haddad"
+__author_email__ = "georgeh87@live.de"
+__license__ = "Apache-2.0"
+__copyright__ = "Copyright George Haddad"
+__cake__ = "\u2728 \U0001f370 \u2728"
