9e56da7dc092d7b5f16fa0fdf959e64f2ea3f9bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-29 14:07:23 +0000

    Add package futureagi/0.6.4/futureagi-0.6.4.tar.gz

---
+++ packages/futureagi/futureagi-0.6.4.tar.gz/futureagi-0.6.4/fi/prompt/__init__.py
@@ -0,0 +1,19 @@
+from .client import Prompt
+from .types import (
+    MessageBase,
+    SystemMessage,
+    AssistantMessage,
+    UserMessage,
+    ModelConfig,
+    PromptTemplate,
+)
+
+__all__ = [
+    "Prompt",
+    "MessageBase",
+    "SystemMessage",
+    "AssistantMessage",
+    "UserMessage",
+    "ModelConfig",
+    "PromptTemplate",
+]
