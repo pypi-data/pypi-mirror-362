089e54d5fd52c3dbcf6668db63dcc7c2ec07d186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:47 +0000

    Add package VecPosLib/1.0.4/vecposlib-1.0.4.tar.gz

---
+++ packages/VecPosLib/vecposlib-1.0.4.tar.gz/vecposlib-1.0.4/package/vecposlib/__init__.py
@@ -0,0 +1,11 @@
+"""
+VecPosLib package
+
+- positionlib: position vector module
+- vectorlib: general vector module
+"""
+
+# Local modules
+from . import positionlib, vectorlib
+
+__all__ = ["positionlib", "vectorlib"]
