59de719ac698aab1128a6b96867d71c1595323e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:56 +0000

    Add package wbportfolio/1.54.14/wbportfolio-1.54.14.tar.gz

---
+++ packages/wbportfolio/wbportfolio-1.54.14.tar.gz/wbportfolio-1.54.14/wbportfolio/admin/__init__.py
@@ -0,0 +1,12 @@
+# Import CRM Relevant Data
+from .portfolio_relationships import PortfolioInstrumentPreferredClassificationThroughInlineModelAdmin
+from .asset import AssetPositionModelAdmin
+from .custodians import CustodianModelAdmin
+from .products import ProductAdmin
+from .indexes import IndexAdmin
+from .product_groups import ProductGroupAdmin
+from .portfolio import PortfolioModelAdmin
+from .registers import RegisterModelAdmin
+from .roles import PortfolioRoleAdmin
+from .transactions import DividendAdmin, FeesAdmin, TradeAdmin
+from .reconciliations import AccountReconciliationAdmin
