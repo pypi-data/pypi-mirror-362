ce742161df730fd42a706591be2d8ac9fb65d9ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:55 +0000

    Add package superduper-snowflake/0.9.0/superduper_snowflake-0.9.0.tar.gz

---
+++ packages/superduper-snowflake/superduper_snowflake-0.9.0.tar.gz/superduper_snowflake-0.9.0/superduper_snowflake/__init__.py
@@ -0,0 +1,14 @@
+from .data_backend import SnowflakeDataBackend as DataBackend
+from .secrets import check_secret_updates, raise_if_secrets_pending, secrets_not_ready
+from .vector_search import SnowflakeVectorSearcher as VectorSearcher
+
+__version__ = "0.9.0"
+
+
+__all__ = [
+    "VectorSearcher",
+    "DataBackend",
+    "check_secret_updates",
+    "raise_if_secrets_pending",
+    "secrets_not_ready",
+]
