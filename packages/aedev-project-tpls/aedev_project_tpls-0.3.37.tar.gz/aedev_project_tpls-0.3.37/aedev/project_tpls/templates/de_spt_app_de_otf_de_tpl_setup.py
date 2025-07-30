fd59c32e5986d1cd532cdafbcf5d656475356703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:30 +0000

    Add package aedev-project-tpls/0.3.37/aedev_project_tpls-0.3.37.tar.gz

---
+++ packages/aedev-project-tpls/aedev_project_tpls-0.3.37.tar.gz/aedev_project_tpls-0.3.37/aedev/project_tpls/templates/de_spt_app_de_otf_de_tpl_setup.py
@@ -0,0 +1,12 @@
+""" setup of {project_desc}. """
+# ReplaceWith#({'import sys' if cae.debug else ''})#
+# ReplaceWith#({'print(f"SetUp {__name__=} {sys.executable=} {sys.argv=} {sys.path=}")' if cae.debug else ''})#
+
+# noinspection PyUnresolvedReferences
+import setuptools
+
+# ReplaceWith#(setup_kwargs = {setup_kwargs_literal(setup_kwargs)})#
+
+if __name__ == "__main__":
+    # ReplaceWith#(setuptools.setup(**setup_kwargs))#
+    pass
