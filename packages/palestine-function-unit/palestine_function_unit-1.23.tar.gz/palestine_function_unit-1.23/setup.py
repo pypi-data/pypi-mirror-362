a01b0011f418c0effe46e0b292826c9b63388854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:02 +0000

    Add package palestine-function-unit/1.23/palestine_function_unit-1.23.tar.gz

---
+++ packages/palestine-function-unit/palestine_function_unit-1.23.tar.gz/palestine_function_unit-1.23/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="palestine_function_unit",
+    version="1.23",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "pconvert=punit.cli:main"
+        ]
+    },
+    install_requires=[],
+    author="Palestine Equation",
+    author_email="1@leat.xyz",
+    description="P-Unit Converter based on recursive persistence and geometric scaling",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ileathan/punit ",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
