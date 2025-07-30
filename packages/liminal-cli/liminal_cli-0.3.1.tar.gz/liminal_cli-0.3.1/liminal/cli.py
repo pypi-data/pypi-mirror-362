ffa6107a3d6439651293917f4e9d1c9eb9ce3f7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:18 +0000

    Add package liminal-cli/0.3.1/liminal_cli-0.3.1.tar.gz

---
+++ packages/liminal-cli/liminal_cli-0.3.1.tar.gz/liminal_cli-0.3.1/liminal/cli.py
@@ -0,0 +1,39 @@
+import json
+import typer
+
+
+app = typer.Typer()
+
+@app.command()
+def info():
+	from .env import debug
+
+	print('Liminal\'s ShellSync is powered by atuin: https://atuin.sh/')
+	print(json.dumps(debug(), indent='\t'))
+
+
+# @app.command()
+# def prompt_upgrade():
+# 	input('Would you like to upgrade from Liminal v{} to v{}?')
+
+
+@app.command()
+def install():
+	from liminal import installer
+	installer.main()
+
+
+@app.command()
+def backup():
+	"""
+	# uninstall
+	cp .local/share/atuin/history.db ./
+	rm -rf .atuin/ .local/share/atuin/ .config/atuin ; rm -rf .liminal-tools/ ; rm lminstall.py
+
+	# if installed atuin without us previously, remove the sourceing of atuin
+	"""
+	pass
+
+
+if __name__ == '__main__':
+	app()
