bd2a889d2ba3f804bb5aff7c281857bff74eadbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:26 +0000

    Add package pysru-accounting/0.0.1/pysru_accounting-0.0.1.tar.gz

---
+++ packages/pysru-accounting/pysru_accounting-0.0.1.tar.gz/pysru_accounting-0.0.1/README.md
@@ -0,0 +1,8 @@
+Content
+=======
+This package contains writers for SRU files for the Swedish tax agency
+(Skatteverket)
+See: SKV 269 Teknisk beskrivning för elektronisk redovisning av Inkomstdeklaration 1 samt bilagor till Inkomstdeklaration 1, 2, 3 och 4
+
+1. infosru.py:
+   Writer for info.sru
---
+++ packages/pysru-accounting/pysru_accounting-0.0.1.tar.gz/pysru_accounting-0.0.1/setup.py
@@ -0,0 +1,23 @@
+""" package setup """
+import setuptools
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+#    name="pysru-accounting",
+#    version="0.0.1",
+#    author="Thomas Petig",
+#    author_email="thomas@petig.eu",
+#    description="SRU file encoder",
+#    long_description=long_description,
+#    long_description_content_type="text/markdown",
+#    url="https://github.com/thpe/pysru-accounting",
+#
+#    classifiers=[
+#        "Programming Language :: Python :: 3",
+#        "License :: OSI Approved :: MIT License",
+#        "Operating System :: OS Independent",
+#    ],
+    packages=setuptools.find_packages(),
+)
