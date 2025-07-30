62a5b64de9bc3a987bcb4159622c42cb4fc0cb8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:52 +0000

    Add package bjpmodel/0.2.1/bjpmodel-0.2.1-cp311-cp311-win32.whl

---
+++ packages/bjpmodel/bjpmodel-0.2.1-cp311-cp311-win32.whl/bjpmodel/dummy_bjp.py
@@ -0,0 +1,20 @@
+import math
+import os, sys, shutil
+from datetime import datetime
+
+# import numpy as np
+
+# from normtransform import pytrans
+# from bjpmodel import pybjp
+
+def do_something():
+    a = 3
+    b = 4
+    c = math.sqrt(a**2 + b**2)
+    return c
+
+def do_something_else():
+    a = 5
+    b = 12  
+    c = math.sqrt(a**2 + b**2)
+    return c
