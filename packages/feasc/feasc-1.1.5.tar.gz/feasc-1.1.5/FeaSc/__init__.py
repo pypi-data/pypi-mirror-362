e0144606987efc25a48e6c4fda25f40451f33267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:31 +0000

    Add package feasc/1.1.5/feasc-1.1.5.tar.gz

---
+++ packages/feasc/feasc-1.1.5.tar.gz/feasc-1.1.5/FeaSc/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "1.1.4"  # 版本号
+
+from FeaSc.pyFeaSc import *
+from FeaSc.pyPaaSc import *
+from FeaSc.pySaaSc import *
+from FeaSc.util_harmony import *
+from FeaSc.util_mca import *
+from FeaSc.util_stamp import *
