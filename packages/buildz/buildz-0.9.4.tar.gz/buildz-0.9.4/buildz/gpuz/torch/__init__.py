f99353683b9634943ec9add060f886a6a37876cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:17 +0000

    Add package buildz/0.9.4/buildz-0.9.4.tar.gz

---
+++ packages/buildz/buildz-0.9.4.tar.gz/buildz-0.9.4/buildz/gpuz/torch/__init__.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+
+__version__="0.0.1"
+
+__author__ = "Zzz, emails: 1174534295@qq.com, 1309458652@qq.com"
+# 小号多
+
+from .dict_middle import DictCache, Fcs
+#from .dict_middle import *
+Dict = DictCache
