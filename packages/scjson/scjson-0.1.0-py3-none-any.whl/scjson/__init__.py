98655a3e23dd1a364cc7cb3b33890d3edaedbb0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:36 +0000

    Add package scjson/0.1.0/scjson-0.1.0-py3-none-any.whl

---
+++ packages/scjson/scjson-0.1.0-py3-none-any.whl/scjson/__init__.py
@@ -0,0 +1,7 @@
+"""scjson conversion tools."""
+
+__progname__ = "scjson"
+__description__ = "Convert between scjson and SCXML"
+__version__ = "0.1.0"
+
+__all__ = ["__progname__", "__description__", "__version__"]
