69ee69288e5fc2da19552cb27dc220b2215262d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:44 +0000

    Add package rebel-eval/0.1.6/rebel_eval-0.1.6.tar.gz

---
+++ packages/rebel-eval/rebel_eval-0.1.6.tar.gz/rebel_eval-0.1.6/rebel/models/__init__.py
@@ -0,0 +1,30 @@
+from .api import (
+    Function,
+    ToolCall,
+    Message,
+    AssistantInput,
+    AssistantOutput,
+    RoleEnum,
+)
+from .metric import (
+    EvaluationVerdict,
+    EvaluationResult,
+    Metric,
+    SerializableMetric,
+)
+from .test import (
+    RetryParams,
+    RetryAggregationStrategy,
+    ParameterGrid,
+    TestAttempt,
+    TestAttemptExecuted,
+    TestSuite,
+    TestCase,
+    TestInfo,
+)
+from .evaluation import (
+    EvaluationAttempt,
+    EvaluationAttemptEvaluated,
+    EvaluationResult,
+    TestCaseEvaluated
+)
