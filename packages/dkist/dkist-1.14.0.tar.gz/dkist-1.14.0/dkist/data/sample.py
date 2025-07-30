46ed21602bef3695384d2d2864f3852f8e265b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:20 +0000

    Add package dkist/1.14.0/dkist-1.14.0.tar.gz

---
+++ packages/dkist/dkist-1.14.0.tar.gz/dkist-1.14.0/dkist/data/sample.py
@@ -0,0 +1,32 @@
+"""
+This module provides some (partial) sample datasets.
+"""
+
+from ._sample import _SAMPLE_DATASETS, VISP_HEADER, _get_sample_datasets
+
+__all__ = ["download_all_sample_data", *sorted(_SAMPLE_DATASETS.keys()), "VISP_HEADER"]  # noqa: PLE0604
+
+
+# See PEP 562 (https://peps.python.org/pep-0562/) for module-level __dir__()
+def __dir__():
+    return __all__
+
+
+# See PEP 562 (https://peps.python.org/pep-0562/) for module-level __getattr__()
+def __getattr__(name):
+    if name in _SAMPLE_DATASETS:
+        return _get_sample_datasets(name)[0]
+
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
+
+
+def download_all_sample_data(overwrite=False):
+    """
+    Download all sample data at once that has not already been downloaded.
+
+    Parameters
+    ----------
+    overwrite : `bool`
+        Re-download and overwrite any existing files.
+    """
+    return _get_sample_datasets(_SAMPLE_DATASETS.keys(), force_download=not overwrite)
---
+++ packages/dkist/dkist-1.14.0.tar.gz/dkist-1.14.0/dkist/data/tests/test_sample.py
@@ -0,0 +1,40 @@
+import os
+import platform
+from unittest.mock import call
+
+import pytest
+
+
+@pytest.fixture
+def tmp_sample_dir(tmp_path):
+    old_path = os.environ.get("DKIST_SAMPLE_DIR", "")
+    os.environ["DKIST_SAMPLE_DIR"] = str(tmp_path)
+    yield tmp_path
+    os.environ["DKIST_SAMPLE_DIR"] = old_path
+
+
+def test_module_dir():
+    import dkist.data.sample  # noqa: PLC0415
+
+    assert "VBI_AJQWW" in dir(dkist.data.sample)
+    assert "VISP_BKPLX" in dir(dkist.data.sample)
+
+
+@pytest.mark.parametrize("attrname", ["VBI_AJQWW", "VISP_BKPLX"])
+def test_module_getattr(mocker, attrname):
+    mock = mocker.patch("dkist.data.sample._get_sample_datasets")
+    import dkist.data.sample  # noqa: PLC0415
+
+    getattr(dkist.data.sample, attrname)
+
+    mock.assert_has_calls([call(attrname), call().__getitem__(0)])
+
+
+@pytest.mark.skipif(platform.system() == "Windows", reason="Internet not properly disabled on Windows")
+@pytest.mark.internet_off
+def test_fail(tmp_sample_dir):
+    """
+    No remote data means this test should fail.
+    """
+    with pytest.raises(RuntimeError, match="1 sample data files failed"):
+        from dkist.data.sample import VISP_BKPLX  # noqa: F401 PLC0415
