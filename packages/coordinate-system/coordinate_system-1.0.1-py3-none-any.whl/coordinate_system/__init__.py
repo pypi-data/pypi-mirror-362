26561ce793d2dd237908ecce8324dcde9aae2a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:58 +0000

    Add package coordinate-system/1.0.1/coordinate_system-1.0.1-py3-none-any.whl

---
+++ packages/coordinate-system/coordinate_system-1.0.1-py3-none-any.whl/coordinate_system/__init__.py
@@ -0,0 +1,7 @@
+# coordinate_system/__init__.py
+
+from .coordinate_system import vec3
+from .coordinate_system import quat
+from .coordinate_system import coord3
+
+__all__ = ['vec3', 'quat', 'coord3']
