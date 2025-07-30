42d70e71c63228e652052e439626965a0863e1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:14 +0000

    Add package sapiens-infinite-context-window/1.0.0/sapiens_infinite_context_window-1.0.0-py3-none-any.whl

---
+++ packages/sapiens-infinite-context-window/sapiens_infinite_context_window-1.0.0-py3-none-any.whl/sapiens_infinite_context_window/__init__.py
@@ -0,0 +1,29 @@
+"""
+This algorithm was developed and programmed by Ben-Hur Varriano for Sapiens Technology®️
+with the goal of providing an “infinite context window” for language models.
+The logical foundation of the code is based on extracting features from the prompt and/or a list of dialogues,
+retaining only the tokens that best define the message within a predetermined limit. In this way,
+the model becomes capable of generalizing the surplus context, assimilating only the general meaning of everything
+that was discussed instead of the entire conversation.
+This behavior is similar to that of humans, who, although they do not memorize every word in a long conversation,
+are able to remember the key points that define what was discussed.
+
+Note: Any public disclosure or comment on the logic and/or operation of this code is strictly prohibited and
+the author will be subject to legal proceedings and measures by our team of lawyers.
+"""
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
+from .sapiens_infinite_context_window import *
+"""
+This algorithm was developed and programmed by Ben-Hur Varriano for Sapiens Technology®️
+with the goal of providing an “infinite context window” for language models.
+The logical foundation of the code is based on extracting features from the prompt and/or a list of dialogues,
+retaining only the tokens that best define the message within a predetermined limit. In this way,
+the model becomes capable of generalizing the surplus context, assimilating only the general meaning of everything
+that was discussed instead of the entire conversation.
+This behavior is similar to that of humans, who, although they do not memorize every word in a long conversation,
+are able to remember the key points that define what was discussed.
+
+Note: Any public disclosure or comment on the logic and/or operation of this code is strictly prohibited and
+the author will be subject to legal proceedings and measures by our team of lawyers.
+"""
+# --------------------------> A SAPIENS TECHNOLOGY®️ PRODUCTION) <--------------------------
