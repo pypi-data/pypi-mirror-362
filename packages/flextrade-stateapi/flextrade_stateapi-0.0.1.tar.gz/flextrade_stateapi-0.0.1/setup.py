9f0bca668372f430c2f6805cdfff5da45353c1d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:39 +0000

    Add package flextrade-stateapi/0.0.1/flextrade_stateapi-0.0.1.tar.gz

---
+++ packages/flextrade-stateapi/flextrade_stateapi-0.0.1.tar.gz/flextrade_stateapi-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="flextrade-stateapi",
+    version="0.0.1",
+    description="Placeholder package",
+    long_description="Placeholder package",
+    long_description_content_type="text/markdown",
+    author="FlexTrade Systems Inc",
+    packages=["flextrade-stateapi"],
+    python_requires=">=3.6",
+)
