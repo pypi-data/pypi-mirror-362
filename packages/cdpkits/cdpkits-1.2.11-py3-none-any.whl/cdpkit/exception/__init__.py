643a3d112dc768e24b57779cd620ccafe1370e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:40 +0000

    Add package cdpkits/1.2.11/cdpkits-1.2.11-py3-none-any.whl

---
+++ packages/cdpkits/cdpkits-1.2.11-py3-none-any.whl/cdpkit/exception/__init__.py
@@ -0,0 +1,39 @@
+from .browser import (
+    ArgumentAlreadyExistsInOptions,
+    BrowserLaunchError,
+    ElementNotFileInput,
+    ExecutableNotFoundError,
+    NoSuchElement,
+    NoValidTabError,
+    ParamsMustSpecified,
+    ScriptRunError,
+    TabNotFoundError,
+)
+from .connection import (
+    CallbackParameterError,
+    CommandExecutionTimeout,
+    InvalidCallback,
+    InvalidResponse,
+    NetworkError,
+    WebSocketConnectionClosed,
+)
+from .generate import GeneratorNameNotFound
+
+__all__ = [
+    'ExecutableNotFoundError',
+    'GeneratorNameNotFound',
+    'InvalidCallback',
+    'NetworkError',
+    'NoSuchElement',
+    'TabNotFoundError',
+    'CallbackParameterError',
+    'InvalidResponse',
+    'CommandExecutionTimeout',
+    'WebSocketConnectionClosed',
+    'BrowserLaunchError',
+    'NoValidTabError',
+    'ElementNotFileInput',
+    'ArgumentAlreadyExistsInOptions',
+    'ParamsMustSpecified',
+    'ScriptRunError'
+]
