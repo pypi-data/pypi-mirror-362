6abb3e263c49c28e95d45c066f17437424ad1e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:01 +0000

    Add package llm-tavily/0.0.1/llm_tavily-0.0.1.tar.gz

---
+++ packages/llm-tavily/llm_tavily-0.0.1.tar.gz/llm_tavily-0.0.1/tests/test_tavily.py
@@ -0,0 +1,46 @@
+from unittest.mock import MagicMock
+
+import httpx
+import respx
+from llm.plugins import load_plugins, pm
+
+from llm_tavily import TavilyQnASearch
+
+
+def test_plugin_is_installed():
+    load_plugins()
+
+    names = [mod.__name__ for mod in pm.get_plugins()]
+    assert "llm_tavily" in names
+
+
+@respx.mock(assert_all_called=True, assert_all_mocked=True)
+def test_execute(respx_mock):
+    respx_mock.post(
+        "https://api.tavily.com/search",
+        headers__contains={"Authorization": "Bearer tvly-test-key"},
+        json__query="Who is Leo Messi?",
+        json__search_depth="advanced",
+        json__include_answer=True,
+    ).mock(
+        return_value=httpx.Response(
+            status_code=200,
+            json={
+                "answer": "Lionel Messi is ...",
+            },
+        )
+    )
+
+    sut = TavilyQnASearch()
+    prompt = MagicMock()
+    prompt.prompt = "Who is Leo Messi?"
+
+    actual = sut.execute(
+        prompt,
+        stream=False,
+        response=MagicMock(),
+        conversation=MagicMock(),
+        key="tvly-test-key",
+    )
+
+    assert actual == "Lionel Messi is ..."
