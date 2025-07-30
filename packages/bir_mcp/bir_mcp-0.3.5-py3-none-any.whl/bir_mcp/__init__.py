e739a2e2f5b8594adacdd21ce99f1ea76d0aa795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:28 +0000

    Add package bir_mcp/0.3.5/bir_mcp-0.3.5-py3-none-any.whl

---
+++ packages/bir_mcp/bir_mcp-0.3.5-py3-none-any.whl/bir_mcp/__init__.py
@@ -0,0 +1,7 @@
+import bir_mcp.atlassian
+import bir_mcp.charts
+import bir_mcp.config
+import bir_mcp.dataiku
+import bir_mcp.git_lab
+import bir_mcp.local
+import bir_mcp.sql
---
+++ packages/bir_mcp/bir_mcp-0.3.5-py3-none-any.whl/bir_mcp/local/__init__.py
@@ -0,0 +1,2 @@
+from bir_mcp.local.filesystem import Filesystem
+from bir_mcp.local.git_local import Git
