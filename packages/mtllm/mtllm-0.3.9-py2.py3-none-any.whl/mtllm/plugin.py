cb6af6205dd14488e861c07e4d0465281f5817f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:58 +0000

    Add package mtllm/0.3.9/mtllm-0.3.9-py2.py3-none-any.whl

---
+++ packages/mtllm/mtllm-0.3.9-py2.py3-none-any.whl/mtllm/plugin.py
@@ -0,0 +1,36 @@
+"""Plugin for Jac's with_llm feature."""
+
+from typing import Callable
+
+from jaclang.runtimelib.machine import hookimpl
+
+from mtllm.llm import Model
+
+
+class JacMachine:
+    """Jac's with_llm feature."""
+
+    @staticmethod
+    @hookimpl
+    def call_llm(
+        model: Model, caller: Callable, args: dict[str | int, object]
+    ) -> object:
+        """Call JacLLM and return the result."""
+        return model.invoke(caller, args)
+
+
+def by(model: Model) -> Callable:
+    """Python library mode decorator for Jac's by llm() syntax."""
+
+    def _decorator(caller: Callable) -> Callable:
+        def _wrapped_caller(*args: object, **kwargs: object) -> object:
+            invoke_args: dict[int | str, object] = {}
+            for i, arg in enumerate(args):
+                invoke_args[i] = arg
+            for key, value in kwargs.items():
+                invoke_args[key] = value
+            return JacMachine.call_llm(model, caller, invoke_args)
+
+        return _wrapped_caller
+
+    return _decorator
