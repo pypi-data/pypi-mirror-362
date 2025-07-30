fe3bf695c50fb89fa8a4a38f4fd45483047bd5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:20 +0000

    Add package xa-izipy/0.0.5/xa_izipy-0.0.5.tar.gz

---
+++ packages/xa-izipy/xa_izipy-0.0.5.tar.gz/xa_izipy-0.0.5/xa_izipy/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.5"
+
+from .iziinfo import help, getdirectory, fast_import, get_platform_info, typing_text
+__all__ = [
+    "help",
+    "getdirectory",
+    "fast_import",
+    "get_platform_info",
+    "typing_text"
+]
