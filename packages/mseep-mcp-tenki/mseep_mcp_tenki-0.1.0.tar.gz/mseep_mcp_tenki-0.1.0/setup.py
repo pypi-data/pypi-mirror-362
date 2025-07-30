87811a07fb8408338c4978d2e82467935efe7da7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:48 +0000

    Add package mseep-mcp-tenki/0.1.0/mseep_mcp_tenki-0.1.0.tar.gz

---
+++ packages/mseep-mcp-tenki/mseep_mcp_tenki-0.1.0.tar.gz/mseep_mcp_tenki-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mseep-mcp-tenki',
+    version='0.1.0',
+    description='Get weather of Japan',
+    long_description="Package managed by MseeP.ai",
+    long_description_content_type="text/plain",
+    author='mseep',
+    author_email='support@skydeck.ai',
+    maintainer='mseep',
+    maintainer_email='support@skydeck.ai',
+    url='https://github.com/mseep',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['httpx>=0.28.1', 'mcp[cli]>=1.2.0', 'pydantic>=2.10.5'],
+    keywords=['mseep'],
+)
