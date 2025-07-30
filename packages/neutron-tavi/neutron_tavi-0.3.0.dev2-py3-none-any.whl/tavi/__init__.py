a8ee7330b872adfef669cfc0fc4b2fa1271bf49c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:38 +0000

    Add package neutron-tavi/0.3.0.dev2/neutron_tavi-0.3.0.dev2-py3-none-any.whl

---
+++ packages/neutron-tavi/neutron_tavi-0.3.0.dev2-py3-none-any.whl/tavi/__init__.py
@@ -0,0 +1,9 @@
+"""
+Contains the entry point for the application
+"""
+
+# TODO template
+try:
+    from ._version import __version__  # noqa: F401
+except ImportError:
+    __version__ = "unknown"
