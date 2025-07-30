01fdd23ab4bef6c077352b7280dddc86b3b1c32d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:49 +0000

    Add package xray-uploader/2.7.1/xray_uploader-2.7.1.tar.gz

---
+++ packages/xray-uploader/xray_uploader-2.7.1.tar.gz/xray_uploader-2.7.1/xray/logger.py
@@ -0,0 +1,8 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(levelname)s:  %(message)s')
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
