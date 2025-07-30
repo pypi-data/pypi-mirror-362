2cc6870efe374d1f30a6e7dee89cbbb29f1295d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:02 +0000

    Add package quick-actions/0.0.6a0/quick_actions-0.0.6a0-py3-none-any.whl

---
+++ packages/quick-actions/quick_actions-0.0.6a0-py3-none-any.whl/quick_actions/config_processing/config_classes/Action.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from typing import Dict
+from pathlib import Path
+from quick_actions import constants
+import copy
+
+
+@dataclass
+class Action:
+    id: str
+    label: str
+    exec: str | None = None
+    script: Path | str | None = None
+    sleep_before: float | int | None = None
+    search_tags: str | None = None
+    prefix: str | None = None
+    populate_options: str | None = ""
+    show_response: bool = False
+
+    @staticmethod
+    def is_action(action_candidate):
+        # print(action_candidate)
+        if (not isinstance(action_candidate, dict)) or action_candidate.get("label") is None:
+            return False
+        if action_candidate.get(constants.INLINE_SCRIPT_PREFIX) == None and \
+            action_candidate.get(constants.SCRIPT_PREFIX) == None:
+            # TODO: use proper warning log
+            print(f"[WARNING]: Action '{action_candidate["label"]}' has no command")
+        return True
+
+    @property
+    def tags(self):
+        if self.search_tags is None:
+            return []
+        return self.search_tags.strip().split(",")
+
+    def with_arguments(self, arguments):
+        new_action = copy.copy(self)
+
+        if self.script is not None:
+            new_action.script = None
+            new_action.exec = str(self.script) + " " + f'"{arguments}"'
+        elif self.exec is not None:
+            new_action.exec += " " + f'"{arguments}"'
+        return new_action
