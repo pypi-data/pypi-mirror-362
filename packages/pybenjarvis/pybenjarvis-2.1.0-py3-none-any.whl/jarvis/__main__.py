acf660452e8c5ba3bc8d846545f1853c55460edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:10:29 +0000

    Add package pybenjarvis/2.1.0/pybenjarvis-2.1.0-py3-none-any.whl

---
+++ packages/pybenjarvis/pybenjarvis-2.1.0-py3-none-any.whl/jarvis/__main__.py
@@ -0,0 +1,12 @@
+from jarvis.install import auto_etp_install
+
+try:
+    from auto_etp.jarvis import main_new_window
+except ModuleNotFoundError as ex:
+    print(ex)
+    auto_etp_install()
+finally:
+    from auto_etp.jarvis import main_new_window
+from auto_etp.jarvis import main_new_window
+
+main_new_window()
