bc2f3f9b18003263ba6cbb927f80f085b0e97ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:43 +0000

    Add package SynMem/0.0.8/synmem-0.0.8.tar.gz

---
+++ packages/SynMem/synmem-0.0.8.tar.gz/synmem-0.0.8/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name="SynMem",
+    version="0.0.8",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+    author="Tristan McBride Sr.",
+    author_email="TristanMcBrideSr@users.noreply.github.com",
+    description="A Modern 4-Stage Synthetic Memory",
+)
