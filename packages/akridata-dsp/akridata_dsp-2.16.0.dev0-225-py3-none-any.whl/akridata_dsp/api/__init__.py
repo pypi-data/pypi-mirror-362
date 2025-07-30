f83e72829da9f34ed85018b8e62ad55625a4e0dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:12 +0000

    Add package akridata-dsp/2.16.0.dev0/akridata_dsp-2.16.0.dev0-225-py3-none-any.whl

---
+++ packages/akridata-dsp/akridata_dsp-2.16.0.dev0-225-py3-none-any.whl/akridata_dsp/api/__init__.py
@@ -0,0 +1,41 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from akridata_dsp.api.anomaly_segmentation_api import AnomalySegmentationApi
+from akridata_dsp.api.catalog_source_tag_api import CatalogSourceTagApi
+from akridata_dsp.api.cluster_snapshot_api import ClusterSnapshotApi
+from akridata_dsp.api.coresets_api import CoresetsApi
+from akridata_dsp.api.customers_api import CustomersApi
+from akridata_dsp.api.dataset_api import DatasetApi
+from akridata_dsp.api.entity_api import EntityApi
+from akridata_dsp.api.er_update_annotations_state_api import ErUpdateAnnotationsStateApi
+from akridata_dsp.api.expert_review_stats_api import ExpertReviewStatsApi
+from akridata_dsp.api.get_annotations_count_api import GetAnnotationsCountApi
+from akridata_dsp.api.get_er_annotations_count_api import GetErAnnotationsCountApi
+from akridata_dsp.api.get_er_review_state_count_api import GetErReviewStateCountApi
+from akridata_dsp.api.health_api import HealthApi
+from akridata_dsp.api.image_fetch_api import ImageFetchApi
+from akridata_dsp.api.image_server_api import ImageServerApi
+from akridata_dsp.api.image_tags_api import ImageTagsApi
+from akridata_dsp.api.images_api import ImagesApi
+from akridata_dsp.api.job_requests_api import JobRequestsApi
+from akridata_dsp.api.label_instructions_api import LabelInstructionsApi
+from akridata_dsp.api.labelspec_api import LabelspecApi
+from akridata_dsp.api.model_training_api import ModelTrainingApi
+from akridata_dsp.api.multi_class_stats_api import MultiClassStatsApi
+from akridata_dsp.api.request_api import RequestApi
+from akridata_dsp.api.request_statistics_api import RequestStatisticsApi
+from akridata_dsp.api.resultset_api import ResultsetApi
+from akridata_dsp.api.saliency_map_api import SaliencyMapApi
+from akridata_dsp.api.scenario_api import ScenarioApi
+from akridata_dsp.api.scenario_execution_api import ScenarioExecutionApi
+from akridata_dsp.api.sim_search_request_v1_api import SimSearchRequestV1Api
+from akridata_dsp.api.sim_search_request_v2_api import SimSearchRequestV2Api
+from akridata_dsp.api.summary_stats_api import SummaryStatsApi
+from akridata_dsp.api.synthesis_api import SynthesisApi
+from akridata_dsp.api.tag_api import TagApi
+from akridata_dsp.api.top_class_stats_api import TopClassStatsApi
+from akridata_dsp.api.update_annotations_state_api import UpdateAnnotationsStateApi
+from akridata_dsp.api.video_api import VideoApi
