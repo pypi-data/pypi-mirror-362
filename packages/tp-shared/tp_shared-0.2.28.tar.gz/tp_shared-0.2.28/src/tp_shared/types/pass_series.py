36237a51286c4e93433a299ea2785eb680c215b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:12 +0000

    Add package tp-shared/0.2.28/tp_shared-0.2.28.tar.gz

---
+++ packages/tp-shared/tp_shared-0.2.28.tar.gz/tp_shared-0.2.28/src/tp_shared/types/pass_series.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class PassSeries(str, Enum):
+    AA = "AA"
+    BA = "БА"
+    AB = "АБ"
+    BB = "ББ"
+    MB = "МБ"
+    MK = "МК"
+    MA = "МА"
+    MO = "MO"
+    II = "ЯЯ"
+    MOJD = "МОЖД"
