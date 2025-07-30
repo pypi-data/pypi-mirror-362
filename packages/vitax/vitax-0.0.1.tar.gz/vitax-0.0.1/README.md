81af44c0cc79a96dc891576c2847f0e282bc87c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:51 +0000

    Add package vitax/0.0.1/vitax-0.0.1.tar.gz

---
+++ packages/vitax/vitax-0.0.1.tar.gz/vitax-0.0.1/README.md
@@ -0,0 +1,2 @@
+# vitax
+ViTax: Open source platform for training and inference of vanilla Vision Transformers with Flax NNX
---
+++ packages/vitax/vitax-0.0.1.tar.gz/vitax-0.0.1/vitax/training.py
@@ -0,0 +1,58 @@
+from flax import nnx
+import optax
+import jax, jax.numpy as jnp
+
+
+def compute_losses_and_logits(model: nnx.Module, images: jax.Array, labels: jax.Array):
+    logits = model(images)
+    loss = optax.softmax_cross_entropy_with_integer_labels(
+        logits=logits, labels=labels
+    ).mean()
+    return loss, logits
+
+
+@nnx.jit
+def train_step(
+        model: nnx.Module, optimizer: nnx.Optimizer, batch
+):
+    images = batch[0]
+    labels = batch[1]
+
+    grad_fn = nnx.value_and_grad(compute_losses_and_logits, has_aux=True)
+    (loss, logits), grads = grad_fn(model, images, labels)
+
+    optimizer.update(grads)  # In-place updates.
+
+    return loss
+
+
+@nnx.jit
+def eval_step(
+        model: nnx.Module, batch, eval_metrics: nnx.MultiMetric
+):
+    images = batch[0]
+    labels = batch[1]
+    loss, logits = compute_losses_and_logits(model, images, labels)
+
+    eval_metrics.update(
+        loss=loss,
+        logits=logits,
+        labels=labels,
+    )
+
+
+def train_one_epoch(model, train_loader, optimizer):
+
+    losses = []
+    model.train()  # Set model to the training mode: e.g. update batch statistics
+
+    for batch in train_loader:
+        batch = (jnp.array(batch['img']), jnp.array(batch['fine_label']))
+
+        loss = train_step(model, optimizer, batch)
+
+        losses.append(loss)
+
+    return losses
+
+
