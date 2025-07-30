665eece92babf1c4cb255e54bc580cc3b96e9f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:48 +0000

    Add package asyauth/0.0.22/asyauth-0.0.22.tar.gz

---
+++ packages/asyauth/asyauth-0.0.22.tar.gz/asyauth-0.0.22/setup.py
@@ -0,0 +1,53 @@
+from setuptools import setup, find_packages
+import re
+
+VERSIONFILE="asyauth/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+
+setup(
+	# Application name:
+	name="asyauth",
+
+	# Version number (initial):
+	version=verstr,
+
+	# Application author details:
+	author="Tamas Jos",
+	author_email="info@skelsecprojects.com",
+
+	# Packages
+	packages=find_packages(exclude=["tests*"]),
+
+	# Include additional files into the package
+	include_package_data=True,
+
+	# Details
+	url="https://github.com/skelsec/asyauth",
+
+	zip_safe = False,
+	#
+	# license="LICENSE.txt",
+	description="Unified authentication library",
+	long_description="Unified authentication library",
+
+	# long_description=open("README.txt").read(),
+	python_requires='>=3.7',
+	classifiers=[
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Operating System :: OS Independent",
+	],
+	install_requires=[
+		'unicrypto>=0.0.10',
+		'asn1crypto>=1.3.0',
+		'asysocks>=0.2.17',
+		'minikerberos>=0.4.7',
+	],
+)
