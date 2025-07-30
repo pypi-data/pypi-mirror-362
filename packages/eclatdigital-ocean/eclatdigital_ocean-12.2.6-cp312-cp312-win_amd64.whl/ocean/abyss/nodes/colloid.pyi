b6334cf0897135355eecbce812c08a46e08e2476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:07:36 +0000

    Add package eclatdigital-ocean/12.2.6/eclatdigital_ocean-12.2.6-cp312-cp312-win_amd64.whl

---
+++ packages/eclatdigital-ocean/eclatdigital_ocean-12.2.6-cp312-cp312-win_amd64.whl/ocean/abyss/nodes/colloid.pyi
@@ -0,0 +1,59 @@
+from __future__ import annotations
+import ocean.abyss.nodes
+__all__ = ['Default', 'Linked']
+class Default(ocean.abyss.nodes.Node, ocean.abyss.nodes.CNodeHandler):
+    """
+    
+        Default
+        
+        .. raw:: html
+    
+            <iframe id=Default" src="https://docs.eclat-digital.com/ocean2024-docs/reference/nodes/colloid.html"loading="lazy" alt="https://docs.eclat-digital.com/ocean2024-docs/reference/nodes/colloid.html"></iframe>
+    
+        
+    """
+    def __init__(self, name: str) -> None:
+        ...
+    def getDensity(self) -> float:
+        """
+        Get the density parameter
+        """
+    def getParticle(self) -> ocean.abyss.nodes.Node:
+        """
+                    Retrieve the Particle node 
+        """
+    def getSizedistribution(self) -> dict:
+        """
+        Get the sizedistribution parameter
+        """
+    def setDensity(self, density: float) -> bool:
+        """
+        Set the density parameter
+        """
+    def setParticle(self, arg0: ocean.abyss.nodes.Node) -> bool:
+        """
+                    Set the Particle node 
+        """
+    def setSizedistribution(self, sizedistribution: dict) -> bool:
+        """
+        Set the sizedistribution parameter
+                            Table is passed as a dict with this format:
+        
+                            .. code-block:: python
+        
+                                table = {"colname_0": np.array(...),"colname_1": np.array(...)}
+        """
+class Linked(ocean.abyss.nodes.Node, ocean.abyss.nodes.CNodeHandler):
+    """
+    Linked
+    """
+    def __init__(self, name: str) -> None:
+        ...
+    def getTarget(self) -> str:
+        """
+        Get the target parameter
+        """
+    def setTarget(self, target: str) -> bool:
+        """
+        Set the target parameter
+        """
