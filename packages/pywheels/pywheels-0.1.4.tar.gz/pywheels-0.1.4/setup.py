a094172eba2a98cb17d0fcb975821c3a1223ec26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:12:08 +0000

    Add package pywheels/0.1.4/pywheels-0.1.4.tar.gz

---
+++ packages/pywheels/pywheels-0.1.4.tar.gz/pywheels-0.1.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name = 'pywheels',
+    version = '0.1.4',
+    packages = find_packages(),
+    description = 'debug',
+    author = 'parkcai',
+    author_email = 'sun_retailer@163.com',
+    url = 'https://github.com/parkcai/pywheels',
+)
