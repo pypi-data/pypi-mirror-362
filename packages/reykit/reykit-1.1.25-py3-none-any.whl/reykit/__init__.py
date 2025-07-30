429ffdf8ca066ded4098bdbb5cb01137aebd55e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:18 +0000

    Add package reykit/1.1.25/reykit-1.1.25-py3-none-any.whl

---
+++ packages/reykit/reykit-1.1.25-py3-none-any.whl/reykit/__init__.py
@@ -0,0 +1,35 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 2022-12-05 14:09:21
+@Author  : Rey
+@Contact : reyxbo@163.com
+@Explain : Rey's kit method set.
+
+Modules
+-------
+rall : All methods.
+rdata : Data methods.
+rdll : DLL file methods.
+remail : E-mail methods.
+rexc : Exception methods.
+rimage : Image methods.
+rlog : Log methods.
+rmonkey : Monkey patch methods.
+rnet : Network methods.
+rnum : Number methods.
+ros: Operation system methods.
+rrand : Random methods.
+rre : Regular expression methods.
+rschedule : Schedule methods.
+rstdout : Standard output and input methods.
+rsys : Interpreter system methods.
+rtable : Table methods.
+rtask : Multi task methods.
+rtext : Text methods.
+rtime : Time methods.
+rtype : Type methods.
+rwrap : Decorators.
+rzip : File compress methods.
+"""
