ad40a48f0a96da7f0e3e172637a6d937e96f065b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:01 +0000

    Add package invenio-subjects-mesh/2025.1.15.2/invenio_subjects_mesh-2025.1.15.2-py3-none-any.whl

---
+++ packages/invenio-subjects-mesh/invenio_subjects_mesh-2025.1.15.2-py3-none-any.whl/invenio_subjects_mesh/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021-2025 Northwestern University.
+#
+# invenio-subjects-mesh is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""MeSH subject terms for InvenioRDM."""
