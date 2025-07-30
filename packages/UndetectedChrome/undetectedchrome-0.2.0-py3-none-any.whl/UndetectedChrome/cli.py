f4fe7af191f3a0ec5964d64d83e42ca4af1e7ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:35 +0000

    Add package UndetectedChrome/0.2.0/undetectedchrome-0.2.0-py3-none-any.whl

---
+++ packages/UndetectedChrome/undetectedchrome-0.2.0-py3-none-any.whl/UndetectedChrome/cli.py
@@ -0,0 +1,14 @@
+import argparse
+from UndetectedChrome.undetected_chrome import UndetectedChromeSync
+
+def main():
+    parser = argparse.ArgumentParser(description="Fetch HTML from a URL using UndetectedChrome.")
+    parser.add_argument("url", help="URL to fetch")
+    args = parser.parse_args()
+
+    browser = UndetectedChromeSync()
+    html = browser.get_html(args.url)
+    print(html)
+
+if __name__ == "__main__":
+    main()
