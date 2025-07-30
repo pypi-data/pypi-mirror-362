eb5062ce0c4b0c05f8cef208f183342491d2cb64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:43 +0000

    Add package pyview-web/0.2.5/pyview_web-0.2.5.tar.gz

---
+++ packages/pyview-web/pyview_web-0.2.5.tar.gz/pyview_web-0.2.5/pyview/live_socket.py
@@ -0,0 +1,173 @@
+from __future__ import annotations
+from starlette.websockets import WebSocket
+import json
+from typing import (
+    Any,
+    TypeVar,
+    Generic,
+    TYPE_CHECKING,
+    Optional,
+    Union,
+    TypeAlias,
+    TypeGuard,
+)
+from urllib.parse import urlencode
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from pyview.vendor.flet.pubsub import PubSubHub, PubSub
+from pyview.events import InfoEvent
+from pyview.uploads import UploadConstraints, UploadConfig, UploadManager
+from pyview.meta import PyViewMeta
+from pyview.template.render_diff import calc_diff
+import datetime
+from pyview.async_stream_runner import AsyncStreamRunner
+
+
+if TYPE_CHECKING:
+    from .live_view import LiveView
+
+scheduler = AsyncIOScheduler()
+scheduler.start()
+
+pub_sub_hub = PubSubHub()
+
+T = TypeVar("T")
+
+
+def is_connected(socket: LiveViewSocket[T]) -> TypeGuard["ConnectedLiveViewSocket[T]"]:
+    return socket.connected
+
+
+class UnconnectedSocket(Generic[T]):
+    context: T
+    live_title: Optional[str] = None
+    connected: bool = False
+
+    def allow_upload(
+        self, upload_name: str, constraints: UploadConstraints
+    ) -> UploadConfig:
+        return UploadConfig(name=upload_name, constraints=constraints)
+
+
+class ConnectedLiveViewSocket(Generic[T]):
+    context: T
+    live_title: Optional[str] = None
+    pending_events: list[tuple[str, Any]]
+    upload_manager: UploadManager
+    prev_rendered: Optional[dict[str, Any]] = None
+
+    def __init__(self, websocket: WebSocket, topic: str, liveview: LiveView):
+        self.websocket = websocket
+        self.topic = topic
+        self.liveview = liveview
+        self.scheduled_jobs = []
+        self.connected = True
+        self.pub_sub = PubSub(pub_sub_hub, topic)
+        self.pending_events = []
+        self.upload_manager = UploadManager()
+        self.stream_runner = AsyncStreamRunner(self)
+
+    @property
+    def meta(self) -> PyViewMeta:
+        return PyViewMeta()
+
+    async def subscribe(self, topic: str):
+        await self.pub_sub.subscribe_topic_async(topic, self._topic_callback_internal)
+
+    async def broadcast(self, topic: str, message: Any):
+        await self.pub_sub.send_all_on_topic_async(topic, message)
+
+    async def _topic_callback_internal(self, topic, message):
+        await self.send_info(InfoEvent(topic, message))
+
+    def schedule_info(self, event, seconds):
+        id = f"{self.topic}:{event}"
+        scheduler.add_job(
+            self.send_info, args=[event], id=id, trigger="interval", seconds=seconds
+        )
+        self.scheduled_jobs.append(id)
+
+    def schedule_info_once(self, event, seconds=None):
+        scheduler.add_job(
+            self.send_info,
+            args=[event],
+            trigger="date",
+            run_date=datetime.datetime.now() + datetime.timedelta(seconds=seconds or 0),
+            misfire_grace_time=None,
+        )
+
+    def diff(self, render: dict[str, Any]) -> dict[str, Any]:
+        if self.prev_rendered:
+            diff = calc_diff(self.prev_rendered, render)
+        else:
+            diff = render
+
+        self.prev_rendered = render
+        return diff
+
+    async def send_info(self, event: InfoEvent):
+        await self.liveview.handle_info(event, self)
+        r = await self.liveview.render(self.context, self.meta)
+        resp = [None, None, self.topic, "diff", self.diff(r.tree())]
+
+        try:
+            await self.websocket.send_text(json.dumps(resp))
+        except Exception:
+            for id in self.scheduled_jobs:
+                print("Removing job", id)
+                scheduler.remove_job(id)
+
+    async def push_patch(self, path: str, params: dict[str, Any] = {}):
+        # or "replace"
+        kind = "push"
+
+        to = path
+        if params:
+            to = to + "?" + urlencode(params)
+
+        message = [
+            None,
+            None,
+            self.topic,
+            "live_patch",  # or "live_redirect"
+            {
+                "kind": kind,
+                "to": to,
+            },
+        ]
+
+        # TODO another way to marshall this
+        for k in params:
+            params[k] = [params[k]]
+
+        await self.liveview.handle_params(to, params, self)
+        try:
+            await self.websocket.send_text(json.dumps(message))
+        except Exception as e:
+            print("Error sending message", e)
+
+    async def push_event(self, event: str, value: dict[str, Any]):
+        self.pending_events.append((event, value))
+
+    def allow_upload(
+        self, upload_name: str, constraints: UploadConstraints
+    ) -> UploadConfig:
+        return self.upload_manager.allow_upload(upload_name, constraints)
+
+    async def close(self):
+        self.connected = False
+        for id in self.scheduled_jobs:
+            scheduler.remove_job(id)
+        await self.pub_sub.unsubscribe_all_async()
+
+        try:
+            self.upload_manager.close()
+        except Exception:
+            pass
+
+        try:
+            await self.liveview.disconnect(self)
+        except Exception:
+            pass
+
+
+LiveViewSocket: TypeAlias = Union[ConnectedLiveViewSocket[T], UnconnectedSocket[T]]
---
+++ packages/pyview-web/pyview_web-0.2.5.tar.gz/pyview_web-0.2.5/pyview/ws_handler.py
@@ -0,0 +1,269 @@
+from typing import Optional
+import json
+from starlette.websockets import WebSocket, WebSocketDisconnect
+from urllib.parse import urlparse, parse_qs
+from pyview.live_socket import ConnectedLiveViewSocket, LiveViewSocket
+from pyview.live_routes import LiveViewLookup
+from pyview.csrf import validate_csrf_token
+from pyview.session import deserialize_session
+from pyview.auth import AuthProviderFactory
+from pyview.phx_message import parse_message
+
+
+class AuthException(Exception):
+    pass
+
+
+class LiveSocketHandler:
+    def __init__(self, routes: LiveViewLookup):
+        self.routes = routes
+        self.manager = ConnectionManager()
+        self.sessions = 0
+
+    async def check_auth(self, websocket: WebSocket, lv):
+        if not await AuthProviderFactory.get(lv).has_required_auth(websocket):
+            raise AuthException()
+
+    async def handle(self, websocket: WebSocket):
+        await self.manager.connect(websocket)
+
+        self.sessions += 1
+        topic = None
+        socket: Optional[LiveViewSocket] = None
+
+        try:
+            data = await websocket.receive_text()
+            [joinRef, mesageRef, topic, event, payload] = json.loads(data)
+            if event == "phx_join":
+                if not validate_csrf_token(payload["params"]["_csrf_token"], topic):
+                    raise AuthException("Invalid CSRF token")
+
+                self.myJoinId = topic
+
+                url = urlparse(payload["url"])
+                lv, path_params = self.routes.get(url.path)
+                await self.check_auth(websocket, lv)
+                socket = ConnectedLiveViewSocket(websocket, topic, lv)
+
+                session = {}
+                if "session" in payload:
+                    session = deserialize_session(payload["session"])
+
+                await lv.mount(socket, session)
+
+                # Parse query parameters and merge with path parameters
+                query_params = parse_qs(url.query)
+                merged_params = {**query_params, **path_params}
+
+                # Pass merged parameters to handle_params
+                await lv.handle_params(url, merged_params, socket)
+
+                rendered = await _render(socket)
+                socket.prev_rendered = rendered
+
+                resp = [
+                    joinRef,
+                    mesageRef,
+                    topic,
+                    "phx_reply",
+                    {"response": {"rendered": rendered}, "status": "ok"},
+                ]
+
+                await self.manager.send_personal_message(json.dumps(resp), websocket)
+                await self.handle_connected(topic, socket)
+
+        except WebSocketDisconnect:
+            if socket:
+                await socket.close()
+            self.sessions -= 1
+        except AuthException:
+            await websocket.close()
+            self.sessions -= 1
+
+    async def handle_connected(self, myJoinId, socket: ConnectedLiveViewSocket):
+        while True:
+            message = await socket.websocket.receive()
+            [joinRef, mesageRef, topic, event, payload] = parse_message(message)
+
+            if event == "heartbeat":
+                resp = [
+                    None,
+                    mesageRef,
+                    "phoenix",
+                    "phx_reply",
+                    {"response": {}, "status": "ok"},
+                ]
+                await self.manager.send_personal_message(
+                    json.dumps(resp), socket.websocket
+                )
+                continue
+
+            if event == "event":
+                value = payload["value"]
+
+                if payload["type"] == "form":
+                    value = parse_qs(value)
+                    socket.upload_manager.maybe_process_uploads(value, payload)
+
+                await socket.liveview.handle_event(payload["event"], value, socket)
+                rendered = await _render(socket)
+
+                hook_events = (
+                    {} if not socket.pending_events else {"e": socket.pending_events}
+                )
+
+                diff = socket.diff(rendered)
+
+                socket.pending_events = []
+
+                resp = [
+                    joinRef,
+                    mesageRef,
+                    topic,
+                    "phx_reply",
+                    {"response": {"diff": diff | hook_events}, "status": "ok"},
+                ]
+                await self.manager.send_personal_message(
+                    json.dumps(resp), socket.websocket
+                )
+                continue
+
+            if event == "live_patch":
+                lv = socket.liveview
+                url = urlparse(payload["url"])
+
+                # Extract and merge parameters
+                query_params = parse_qs(url.query)
+                path_params = {}
+
+                # We need to get path params for the new URL
+                try:
+                    # TODO: I don't think this is actually going to work...
+                    _, path_params = self.routes.get(url.path)
+                except ValueError:
+                    pass  # Handle case where the path doesn't match any route
+
+                merged_params = {**query_params, **path_params}
+
+                await lv.handle_params(url, merged_params, socket)
+                rendered = await _render(socket)
+                diff = socket.diff(rendered)
+
+                resp = [
+                    joinRef,
+                    mesageRef,
+                    topic,
+                    "phx_reply",
+                    {"response": {"diff": diff}, "status": "ok"},
+                ]
+                await self.manager.send_personal_message(
+                    json.dumps(resp), socket.websocket
+                )
+                continue
+
+            if event == "allow_upload":
+                allow_upload_response = socket.upload_manager.process_allow_upload(
+                    payload
+                )
+
+                rendered = await _render(socket)
+                diff = socket.diff(rendered)
+
+                resp = [
+                    joinRef,
+                    mesageRef,
+                    topic,
+                    "phx_reply",
+                    {
+                        "response": {"diff": rendered} | allow_upload_response,
+                        "status": "ok",
+                    },
+                ]
+
+                await self.manager.send_personal_message(
+                    json.dumps(resp), socket.websocket
+                )
+                continue
+
+            # file upload
+            if event == "phx_join":
+                socket.upload_manager.add_upload(joinRef, payload)
+
+                resp = [
+                    joinRef,
+                    mesageRef,
+                    topic,
+                    "phx_reply",
+                    {"response": {}, "status": "ok"},
+                ]
+
+                await self.manager.send_personal_message(
+                    json.dumps(resp), socket.websocket
+                )
+
+            if event == "chunk":
+                socket.upload_manager.add_chunk(joinRef, payload)  # type: ignore
+
+                resp = [
+                    joinRef,
+                    mesageRef,
+                    topic,
+                    "phx_reply",
+                    {"response": {}, "status": "ok"},
+                ]
+
+                if socket.upload_manager.no_progress(joinRef):
+                    await self.manager.send_personal_message(
+                        json.dumps(
+                            [
+                                joinRef,
+                                None,
+                                myJoinId,
+                                "phx_reply",
+                                {"response": {"diff": {}}, "status": "ok"},
+                            ]
+                        ),
+                        socket.websocket,
+                    )
+
+                await self.manager.send_personal_message(
+                    json.dumps(resp), socket.websocket
+                )
+
+            if event == "progress":
+                socket.upload_manager.update_progress(joinRef, payload)
+                rendered = await _render(socket)
+                diff = socket.diff(rendered)
+
+                resp = [
+                    joinRef,
+                    mesageRef,
+                    topic,
+                    "phx_reply",
+                    {"response": {"diff": diff}, "status": "ok"},
+                ]
+
+                await self.manager.send_personal_message(
+                    json.dumps(resp), socket.websocket
+                )
+
+
+async def _render(socket: ConnectedLiveViewSocket):
+    rendered = (await socket.liveview.render(socket.context, socket.meta)).tree()
+
+    if socket.live_title:
+        rendered["t"] = socket.live_title
+        socket.live_title = None
+
+    return rendered
+
+
+class ConnectionManager:
+    def __init__(self):
+        pass
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
