618626e55cf4156bc2e095734e01bc27695e8d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:02 +0000

    Add package space-raccoon/1.0.0/space_raccoon-1.0.0-py2.py3-none-any.whl

---
+++ packages/space-raccoon/space_raccoon-1.0.0-py2.py3-none-any.whl/raccoon/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for raccoon."""
+
+__author__ = """Anowar J. Shajib"""
+__email__ = "ajshajib@gmail.com"
+__version__ = "1.0.0"
+
+from .wiggle_cleaner import WiggleCleaner
+
+
+__all__ = ["WiggleCleaner"]
