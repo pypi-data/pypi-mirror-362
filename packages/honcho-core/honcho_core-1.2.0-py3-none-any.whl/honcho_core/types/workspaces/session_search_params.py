44b5ac895115a877160fff7b977efc7c5e14cd0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:33 +0000

    Add package honcho-core/1.2.0/honcho_core-1.2.0-py3-none-any.whl

---
+++ packages/honcho-core/honcho_core-1.2.0-py3-none-any.whl/honcho_core/types/workspaces/session_search_params.py
@@ -0,0 +1,25 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Optional
+from typing_extensions import Required, TypedDict
+
+__all__ = ["SessionSearchParams"]
+
+
+class SessionSearchParams(TypedDict, total=False):
+    workspace_id: Required[str]
+    """ID of the workspace"""
+
+    query: Required[str]
+    """Search query"""
+
+    page: int
+    """Page number"""
+
+    size: int
+    """Page size"""
+
+    semantic: Optional[bool]
+    """Whether to explicitly use semantic search to filter the results"""
