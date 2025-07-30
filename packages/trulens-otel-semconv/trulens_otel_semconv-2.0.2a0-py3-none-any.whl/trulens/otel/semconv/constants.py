c95f9f8abf2bb7df10bc3d9184ee36d861c729fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:13 +0000

    Add package trulens-otel-semconv/2.0.2a0/trulens_otel_semconv-2.0.2a0-py3-none-any.whl

---
+++ packages/trulens-otel-semconv/trulens_otel_semconv-2.0.2a0-py3-none-any.whl/trulens/otel/semconv/constants.py
@@ -0,0 +1,7 @@
+TRULENS_INSTRUMENT_WRAPPER_FLAG = "__trulens_instrument_wrapper__"
+TRULENS_APP_SPECIFIC_INSTRUMENT_WRAPPER_FLAG = (
+    "__trulens_app_specific_instrument_wrapper__"
+)
+TRULENS_RECORD_ROOT_INSTRUMENT_WRAPPER_FLAG = (
+    "__trulens_record_root_instrument_wrapper__"
+)
