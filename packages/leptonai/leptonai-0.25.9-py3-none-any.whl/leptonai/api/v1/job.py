3634f91691312d8c0b3b4dc43462dab04d9adb91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:22 +0000

    Add package leptonai/0.25.9/leptonai-0.25.9-py3-none-any.whl

---
+++ packages/leptonai/leptonai-0.25.9-py3-none-any.whl/leptonai/api/v1/job.py
@@ -0,0 +1,79 @@
+from typing import Union, List, Iterator, Optional
+
+from .api_resource import APIResourse
+from .types.events import LeptonEvent
+
+from .types.job import LeptonJob
+from .types.replica import Replica
+
+
+class JobAPI(APIResourse):
+    def _to_id(self, name_or_job: Union[str, LeptonJob]) -> str:
+        return (  # type: ignore
+            name_or_job if isinstance(name_or_job, str) else name_or_job.metadata.id_
+        )
+
+    def list_all(self) -> List[LeptonJob]:
+        responses = self._get("/jobs")
+        return self.ensure_list(responses, LeptonJob)
+
+    def list_matching(self, pattern: str):
+        params = {
+            "query": pattern,
+        }
+        responses = self._get("/jobs", params=params)
+        return self.ensure_list(responses, LeptonJob)
+
+    def create(self, spec: LeptonJob) -> LeptonJob:
+        """
+        Run a photon with the given job spec.
+        """
+        response = self._post("/jobs", json=self.safe_json(spec))
+        return self.ensure_type(response, LeptonJob)
+
+    def get(self, id_or_job: Union[str, LeptonJob]) -> LeptonJob:
+        response = self._get(f"/jobs/{self._to_id(id_or_job)}")
+        return self.ensure_type(response, LeptonJob)
+
+    def update(self, name_or_job: Union[str, LeptonJob], spec: LeptonJob) -> bool:
+        response = self._patch(f"/jobs/{self._to_id(name_or_job)}", json=spec)
+        return self.ensure_ok(response)
+
+    def delete(self, name_or_job: Union[str, LeptonJob]) -> bool:
+        response = self._delete(f"/jobs/{self._to_id(name_or_job)}")
+        return self.ensure_ok(response)
+
+    def get_events(self, name_or_job: Union[str, LeptonJob]) -> List[LeptonEvent]:
+        response = self._get(f"/jobs/{self._to_id(name_or_job)}/events")
+        return self.ensure_list(response, LeptonEvent)
+
+    def get_replicas(self, name_or_job: Union[str, LeptonJob]) -> List[Replica]:
+        response = self._get(f"/jobs/{self._to_id(name_or_job)}/replicas")
+        return self.ensure_list(response, Replica)
+
+    def get_log(
+        self,
+        id_or_job: Union[str, LeptonJob],
+        replica: Union[str, Replica],
+        timeout: Optional[int] = None,
+    ) -> Iterator[str]:
+        """
+        Gets the log of the given job's specified replica. The log is streamed
+        in chunks until timeout is reached. If timeout is not specified, the log will be
+        streamed indefinitely, although you should not rely on this behavior as connections
+        can be dropped when streamed for a long time.
+        """
+        replica_id = replica if isinstance(replica, str) else replica.metadata.id_
+        response = self._get(
+            f"/jobs/{self._to_id(id_or_job)}/replicas/{replica_id}/log",
+            stream=True,
+            timeout=timeout,
+        )
+        if not response.ok:
+            raise RuntimeError(
+                f"API call failed with status code {response.status_code}. Details:"
+                f" {response.text}"
+            )
+        for chunk in response.iter_content(chunk_size=None):
+            if chunk:
+                yield chunk.decode("utf8")
---
+++ packages/leptonai/leptonai-0.25.9-py3-none-any.whl/leptonai/cli/util.py
@@ -0,0 +1,206 @@
+"""
+Common utilities for the CLI.
+"""
+
+import sys
+from typing import Any, Dict
+from urllib.parse import urlparse
+
+import click
+from loguru import logger
+
+from rich.console import Console
+from leptonai.api.v2.client import APIClient
+
+
+console = Console(highlight=False)
+
+
+def catch_deprecated_flag(old_name, new_name):
+    def warn_old_name(ctx, param, value):
+        if ctx.get_parameter_source(old_name) == click.core.ParameterSource.COMMANDLINE:
+            console.print(
+                f"[yellow]Warning:[/] Flag [yellow]--{old_name}[/] is deprecated. Use"
+                f" [green]--{new_name}[/] instead."
+            )
+        return value
+
+    return warn_old_name
+
+
+def click_group(*args, **kwargs):
+    """
+    A wrapper around click.group that allows for command shorthands as long as
+    they are unambiguous. For example, in the lepton case, the command `lep deployment`
+    can be shortened to `lep depl` as `depl` uniquely identifies the `deployment` command.
+    """
+
+    class ClickAliasedGroup(click.Group):
+        def get_command(self, ctx, cmd_name):
+            rv = click.Group.get_command(self, ctx, cmd_name)
+            if rv is not None:
+                return rv
+
+            def is_abbrev(x, y):
+                # first char must match
+                if x[0] != y[0]:
+                    return False
+                it = iter(y)
+                return all(any(c == ch for c in it) for ch in x)
+
+            matches = [x for x in self.list_commands(ctx) if is_abbrev(cmd_name, x)]
+
+            if not matches:
+                return None
+            elif len(matches) == 1:
+                return click.Group.get_command(self, ctx, matches[0])
+            ctx.fail(f"'{cmd_name}' is ambiguous: {', '.join(sorted(matches))}")
+
+        def resolve_command(self, ctx, args):
+            # always return the full command name
+            _, cmd, args = super().resolve_command(ctx, args)
+            return cmd.name, cmd, args
+
+    return click.group(*args, cls=ClickAliasedGroup, **kwargs)
+
+
+def is_valid_url(candidate_str: str) -> bool:
+    parsed = urlparse(candidate_str)
+    return parsed.scheme != "" and parsed.netloc != ""
+
+
+def check(condition: Any, message: str) -> None:
+    """
+    Checks a condition and prints a message if the condition is false.
+
+    :param condition: The condition to check.
+    :param message: The message to print if the condition is false.
+    """
+    if not condition:
+        console.print(message)
+        sys.exit(1)
+
+
+def explain_response(response, if_2xx, if_4xx, if_others, exit_if_4xx=False):
+    """
+    A wrapper function that prints a message based on the response status code
+    If the response status code is 2xx or 3xx, print if_2xx, and return.
+    If the response status code is 4xx, print if_4xx, and exit if exit_if_4xx is true.
+    If the response status code is anything else, print if_others and always exit(1).
+
+    This is useful for apis that directly return a response object.
+    """
+    if response.ok or response.is_redirect:
+        if if_2xx:
+            console.print(if_2xx)
+        return
+    else:
+        if response.status_code >= 400 and response.status_code <= 499:
+            errmsg = if_4xx
+        else:
+            errmsg = if_others
+        try:
+            # convert response text to json
+            content = response.json()
+            console.print(
+                f"{response.status_code} {content['code']}:"
+                f" {content['message']}\n{errmsg}"
+            )
+        except Exception:
+            # fallback to display raw message
+            console.print(f"{response.status_code}: {response.text}\n{errmsg}")
+
+        if (
+            response.status_code >= 400
+            and response.status_code <= 499
+            and not exit_if_4xx
+        ):
+            return
+        else:
+            sys.exit(1)
+
+
+def sizeof_fmt(num, suffix="B"):
+    """
+    Convert a quantity of bytes to a human readable format.
+    ref: https://web.archive.org/web/20111010015624/http://blogmag.net/blog/read/38/Print_human_readable_file_size
+    """
+    for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
+
+
+def _get_only_replica_public_ip(name: str):
+    client = APIClient()
+    replicas = client.deployment.get_replicas(name)
+    logger.trace(f"Replicas for {name}:\n{replicas}")
+
+    if len(replicas) != 1:
+        console.print(f"Pod {name} has more than one replica. This is not supported.")
+        sys.exit(1)
+    return replicas[0].status.public_ip
+
+
+def _get_valid_nodegroup_ids(node_groups: [str], need_queue_priority=False):
+    client = APIClient()
+    valid_ng = client.nodegroup.list_all()
+
+    valid_ng_map: Dict[str, str] = {ng.metadata.name: ng for ng in valid_ng}
+    node_group_ids = []
+    for ng in node_groups:
+        if ng not in valid_ng_map:
+            console.print(
+                f"Invalid node group: [red]{ng}[/] (valid node groups:"
+                f" {', '.join(valid_ng_map.keys())})"
+            )
+            sys.exit(1)
+
+        current_ng = valid_ng_map[ng]
+
+        if not need_queue_priority or current_ng.status.quota_enabled is True:
+            node_group_ids.append(current_ng.metadata.id_)
+        else:
+            console.print(
+                f"[red]Warning[/red]: Node group '{ng}' does not support"
+                " queue_priority."
+            )
+
+    if len(node_group_ids) == 0:
+        console.print("[red]Warning[/red]: No valid node groups found.")
+        sys.exit(1)
+
+    return node_group_ids
+
+
+def _get_valid_node_ids(node_group_ids: [str], node_ids: [str]):
+    if (
+        not node_group_ids
+        or len(node_group_ids) == 0
+        or not node_ids
+        or len(node_ids) == 0
+    ):
+        return None
+    node_ids_set = set(node_ids)
+    client = APIClient()
+    valid_nodes_id = set()
+    for ng_id in node_group_ids:
+        cur_all_nodes = client.nodegroup.list_nodes(name_or_ng=ng_id)
+        for node in cur_all_nodes:
+            if node.metadata.id_ in node_ids_set:
+                valid_nodes_id.add(node.metadata.id_)
+    invalid_node_ids = node_ids_set - valid_nodes_id
+    if invalid_node_ids and len(invalid_node_ids) > 0:
+        console.print(
+            f"Invalid node ids: [red]{', '.join(invalid_node_ids)}[/]\nPlease try to"
+            " use [green]'lep node list -d'[/] to check your node groups and nodes"
+        )
+
+        # We will stop this creation operation if user entered a wrong node id
+        console.print(
+            "[red]Creation process halted. Please enter a valid node ID and try"
+            " again.[/]"
+        )
+        sys.exit(1)
+    return valid_nodes_id
