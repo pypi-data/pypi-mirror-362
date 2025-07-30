077541ab15a76c99d0a504fdd882939d5f272fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:00 +0000

    Add package lagom/2.7.7/lagom-2.7.7-cp310-cp310-win_amd64.whl

---
+++ packages/lagom/lagom-2.7.7-cp310-cp310-win_amd64.whl/lagom/__init__.py
@@ -0,0 +1,36 @@
+"""Lagom, a type based dependency injection container"""
+
+from .container import Container, ExplicitContainer
+from .context_based import ContextContainer
+from .debug import get_build_info
+from .decorators import (
+    bind_to_container,
+    magic_bind_to_container,
+    dependency_definition,
+    context_dependency_definition,
+)
+from .definitions import Singleton, Alias, UnresolvableTypeDefinition
+from .markers import injectable
+from .util.functional import FunctionCollection
+from .version import __version__
+
+__all__ = [
+    "__version__",
+    "get_build_info",
+    "Singleton",
+    "Alias",
+    "UnresolvableTypeDefinition",
+    "Container",
+    "ExplicitContainer",
+    "FunctionCollection",
+    "bind_to_container",
+    "magic_bind_to_container",
+    "dependency_definition",
+    "context_dependency_definition",
+    "integrations",
+    "experimental",
+    "exceptions",
+    "environment",
+    "injectable",
+    "ContextContainer",
+]
