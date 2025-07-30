4f2d12e8520f369dd5d171ac5f25230b34054e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:16:46 +0000

    Add package lolalytics-api/0.0.4/lolalytics_api-0.0.4.tar.gz

---
+++ packages/lolalytics-api/lolalytics_api-0.0.4.tar.gz/lolalytics_api-0.0.4/src/lolalytics_api/__init__.py
@@ -0,0 +1,7 @@
+from .main import get_tierlist, get_counters, _sort_by_rank, display_lanes, display_ranks
+from .errors import InvalidLane, InvalidRank
+
+__version__ = "0.0.3"
+__author__ = "xPerSki"
+
+__all__ = ["get_tierlist", "get_counters", "display_lanes", "display_ranks", "InvalidRank", "InvalidLane"]
