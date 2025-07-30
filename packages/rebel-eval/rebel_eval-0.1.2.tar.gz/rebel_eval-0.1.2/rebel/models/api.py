18805ca3bcd15da62ab080b47f52547b538ad2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:20:25 +0000

    Add package rebel-eval/0.1.2/rebel_eval-0.1.2.tar.gz

---
+++ packages/rebel-eval/rebel_eval-0.1.2.tar.gz/rebel_eval-0.1.2/rebel/models/api.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel
+from enum import Enum
+from typing import Literal, List, Optional, Dict, Any
+import json
+
+
+class RoleEnum(str, Enum):
+    user = 'user'
+    assistant = 'assistant'
+    tool = 'tool'
+    system = 'system'
+
+
+class Function(BaseModel):
+    name: str
+    arguments: str
+    
+    def parse_arguments(self) -> Dict[str, Any]:
+        return json.loads(self.arguments)
+
+class ToolCall(BaseModel):
+    id: str
+    type: Literal['function']
+    function: Function
+
+
+class Message(BaseModel):
+    role: RoleEnum
+    content: str
+    tool_calls: Optional[List[ToolCall]] = None
+    tool_call_id: Optional[str] = None
+
+
+class AssistantInput(BaseModel):
+    messages: List[Message]
+    api_params: Dict[str, Any]
+
+
+class AssistantOutput(BaseModel):
+    output: str
+    tools_called: List[ToolCall]
+    context: List[str]
+    execution_time: Optional[float] = None # None for expected output
