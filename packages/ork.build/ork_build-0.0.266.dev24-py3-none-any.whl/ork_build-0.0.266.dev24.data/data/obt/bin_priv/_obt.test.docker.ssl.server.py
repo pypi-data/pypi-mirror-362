d2c3c6def42a589b0be7fa8248fb761b4e8704f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:14 +0000

    Add package ork.build/0.0.266.dev24/ork_build-0.0.266.dev24-py3-none-any.whl

---
+++ packages/ork.build/ork_build-0.0.266.dev24-py3-none-any.whl/ork_build-0.0.266.dev24.data/data/obt/bin_priv/_obt.test.docker.ssl.server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from obt import path, command, docker
+
+module = docker.descriptor("ssltest")
+module.build()
+module.launch()
+try:
+  while True:
+    time.sleep(1)
+except KeyboardInterrupt:
+  module.stop()
+  exit(0)
+
+ 
