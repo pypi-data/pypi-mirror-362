a1b8c1e8a86cb696d13817a098eae2e015374025
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:09 +0000

    Add package aspyx/1.6.0/aspyx-1.6.0-py3-none-any.whl

---
+++ packages/aspyx/aspyx-1.6.0-py3-none-any.whl/aspyx/di/configuration/yaml_configuration_source.py
@@ -0,0 +1,26 @@
+"""
+YamlConfigurationSource - Loads variables from a YAML configuration file.
+"""
+import yaml
+
+from .configuration import ConfigurationSource
+
+class YamlConfigurationSource(ConfigurationSource):
+    """
+    YamlConfigurationSource loads variables from a YAML configuration file.
+    """
+
+    __slots__ = ["file"]
+
+    # constructor
+
+    def __init__(self, file: str):
+        super().__init__()
+
+        self.file = file
+
+    # implement
+
+    def load(self) -> dict:
+        with open(self.file, "r") as file:
+            return yaml.safe_load(file)
