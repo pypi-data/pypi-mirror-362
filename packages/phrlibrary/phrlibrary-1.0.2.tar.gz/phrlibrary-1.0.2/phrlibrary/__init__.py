f3f2de12db4433de84609e7fbacfb5444af2bb12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:25 +0000

    Add package phrlibrary/1.0.2/phrlibrary-1.0.2.tar.gz

---
+++ packages/phrlibrary/phrlibrary-1.0.2.tar.gz/phrlibrary-1.0.2/phrlibrary/__init__.py
@@ -0,0 +1,12 @@
+from .core import hello_world, calculate_sum, DataProcessor, game, GamePy
+
+
+__version__ = "1.0.2"
+__mine__ = "phrlibrary"
+__library__ = __mine__ + " " + __version__
+__ini__ = None
+__1__ = None
+
+game = game()
+gamepy = GamePy()
+dataprocessor = DataProcessor()
---
+++ packages/phrlibrary/phrlibrary-1.0.2.tar.gz/phrlibrary-1.0.2/phrlibrary/core.py
@@ -0,0 +1,88 @@
+import pygame
+import os
+import sys
+import time
+import threading
+from PIL import Image
+
+
+def hello_world():
+    """示例函数：返回欢迎信息"""
+    return "Hello from mylibrary!"
+
+def calculate_sum(a, b):
+    """演示带类型提示的函数"""
+    return a + b
+
+class DataProcessor:
+    """示例类：数据处理工具"""
+    def __init__(self, multiplier=1):
+        self.multiplier = multiplier
+    def process(self, x):
+        return x * self.multiplier
+
+class GamePy:
+    class Game:
+        def __init__(self, width=800, height=600, title="GamePy Window", R=0, G=0, B=0):
+            self.init()
+            self.running = False
+            self.R = R
+            self.G = G
+            self.B = B
+            self.window = self.Window(width, height, title)
+            self.event = self.Event()
+            self.time = self.Time()
+        
+        def init(self):
+            pygame.init()
+            self.python__version__ = f"Python {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+            print("phrlibrary 1.0.2 gamepy 0.1.1 in " + str(self.python__version__) + "\nHello from the PHRLibrary GamePy World!")
+    
+        class Window:
+            def __init__(self, width, height, title="GamePy Window"):
+                self.screen = pygame.display.set_mode((width, height))
+                self.caption(title)
+            def caption(self, title):
+                pygame.display.set_caption(title)
+            def icon(self, iconfile):
+                pygame.display.set_icon(Image.open(iconfile))
+            def update(self):
+                pass
+            def render(self, fillR=0, fillG=0, fillB=0):
+                self.screen.fill((fillR, fillG, fillB))
+            def flip(self):
+                pygame.display.flip()
+    
+        class Event:
+            def __init__(self):
+                pass
+            def handle_events(self, game):
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        game.running = False
+    
+        class Time:
+            def __init__(self):
+                self.clock = pygame.time.Clock()
+            def set_fps(self, fps):
+                self.clock.tick(fps)
+            def delay(self, ms):
+                time.sleep(ms / 1000)
+    
+        def run(self):
+            self.running = True
+            while self.running:
+                self.event.handle_events(self)
+                self.window.update()
+                self.window.render(self.R, self.G, self.B)
+                self.window.flip()
+                self.time.set_fps(60)
+            pygame.quit()
+
+class game:
+    def __init__(self):
+        pass
+    def test(self, width=800, height=600, title="GamePy Window", R=0, G=0, B=0):
+        self.gamepy = GamePy()
+        self.game = self.gamepy.Game(width, height, title, R, G, B)
+        self.game.run()
