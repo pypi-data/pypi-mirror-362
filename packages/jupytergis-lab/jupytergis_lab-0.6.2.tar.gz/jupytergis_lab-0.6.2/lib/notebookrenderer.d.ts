52c6462326e618e364e298059a7d550b2a021997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:24 +0000

    Add package jupytergis-lab/0.6.2/jupytergis_lab-0.6.2.tar.gz

---
+++ packages/jupytergis-lab/jupytergis_lab-0.6.2.tar.gz/jupytergis_lab-0.6.2/lib/notebookrenderer.d.ts
@@ -0,0 +1,35 @@
+import { JupyterGISOutputWidget, JupyterGISTracker } from '@jupytergis/base';
+import { IJGISExternalCommandRegistry, JupyterGISModel } from '@jupytergis/schema';
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+import { CommandRegistry } from '@lumino/commands';
+import { Panel } from '@lumino/widgets';
+import { JupyterYModel } from 'yjs-widgets';
+export interface ICommMetadata {
+    create_ydoc: boolean;
+    path: string;
+    format: string;
+    contentType: string;
+    ymodel_name: string;
+}
+export declare const CLASS_NAME = "jupytergis-notebook-widget";
+export declare class YJupyterGISModel extends JupyterYModel {
+    jupyterGISModel: JupyterGISModel;
+}
+export declare class YJupyterGISLuminoWidget extends Panel {
+    constructor(options: IOptions);
+    get jgisWidget(): JupyterGISOutputWidget;
+    /**
+     * Build the widget and add it to the panel.
+     * @param options
+     */
+    private _buildWidget;
+    private _jgisWidget;
+}
+interface IOptions {
+    commands: CommandRegistry;
+    model: JupyterGISModel;
+    externalCommands?: IJGISExternalCommandRegistry;
+    tracker?: JupyterGISTracker;
+}
+export declare const notebookRendererPlugin: JupyterFrontEndPlugin<void>;
+export {};
