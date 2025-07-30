b3b08056e238750d33fa3ebc46776637271ac9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:11:09 +0000

    Add package dynamiq/0.21.0/dynamiq-0.21.0-py3-none-any.whl

---
+++ packages/dynamiq/dynamiq-0.21.0-py3-none-any.whl/dynamiq/utils/__init__.py
@@ -0,0 +1,11 @@
+from .duration import format_duration
+from .utils import (
+    JsonWorkflowEncoder,
+    TruncationMethod,
+    format_value,
+    generate_uuid,
+    is_called_from_async_context,
+    merge,
+    serialize,
+    truncate_text_for_embedding,
+)
