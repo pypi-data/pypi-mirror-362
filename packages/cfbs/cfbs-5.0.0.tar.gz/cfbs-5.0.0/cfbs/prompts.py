abb1d49e654e0fe80e04accaf299f67e816a45de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:40 +0000

    Add package cfbs/5.0.0/cfbs-5.0.0.tar.gz

---
+++ packages/cfbs/cfbs-5.0.0.tar.gz/cfbs-5.0.0/cfbs/prompts.py
@@ -0,0 +1,40 @@
+YES_NO_CHOICES = ("yes", "y", "no", "n")
+
+
+def prompt_user(non_interactive, prompt, choices=None, default=None):
+    if non_interactive:
+        if default is None:
+            raise ValueError(
+                "Missing default value for prompt '%s' in non-interactive mode" % prompt
+            )
+        else:
+            return default
+
+    prompt_separator = " " if prompt.endswith("?") else ": "
+    if choices:
+        assert (
+            default is None or str(default) in choices
+        ), "'%s' not 'None' and '%s' not in '%s'" % (default, default, choices)
+        choices_str = "/".join(
+            choice.upper() if choice == str(default) else choice for choice in choices
+        )
+        prompt += " [%s]%s" % (choices_str, prompt_separator)
+    elif default is not None:
+        prompt += " [%s]%s" % (default, prompt_separator)
+    else:
+        prompt += prompt_separator
+
+    answer = None
+    while answer is None:
+        try:
+            answer = input(prompt)
+        except EOFError:
+            answer = ""
+
+        if answer == "":
+            answer = default
+        elif choices and answer not in choices and answer.lower() not in choices:
+            print("Invalid value entered, must be one of: %s" % "/".join(choices))
+            answer = None
+
+    return answer
