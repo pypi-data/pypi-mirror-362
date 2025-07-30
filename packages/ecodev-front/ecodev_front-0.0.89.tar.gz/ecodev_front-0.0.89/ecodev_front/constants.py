2dcd5eddb4ff480621a1c16fe6d7aa46bd185d52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:15:26 +0000

    Add package ecodev-front/0.0.89/ecodev_front-0.0.89.tar.gz

---
+++ packages/ecodev-front/ecodev_front-0.0.89.tar.gz/ecodev_front-0.0.89/ecodev_front/constants.py
@@ -0,0 +1,62 @@
+"""
+Callbacks constants
+"""
+CHILDREN = 'children'
+DATA = 'data'
+PATHNAME = 'pathname'
+N_CLICKS = 'n_clicks'
+VALUE = 'value'
+DISABLED = 'disabled'
+STYLE = 'style'
+ACTIVE = 'active'
+LOCAL = 'local'
+OPENED = 'opened'
+CONTENTS = 'contents'
+FILENAME = 'filename'
+TYPE = 'type'
+INDEX = 'index'
+ID = 'id'
+VISIBLE = 'visible'
+CHECKED = 'checked'
+HIDDEN = 'hidden'
+HREF = 'href'
+FIGURE = 'figure'
+ROW_DATA = 'rowData'
+ROW_ID = 'rowId'
+COL_ID = 'colId'
+CELL_CLICKED = 'cellClicked'
+CLICK_DATA = 'clickData'
+ERROR = 'error'
+LOADING = 'loading'
+ALLOW_STEP_CLICK = 'allowStepClick'
+STEPS = 'steps'
+LABEL = 'label'
+OPTIONS = 'options'
+REQUIRED = 'required'
+DISPLAY = 'display'
+
+"""
+Table constants
+"""
+CELL_RENDERER_DATA = 'cellRendererData'
+ROW_DATA = 'rowData'
+ROW_INDEX = 'rowIndex'
+CELL_CLICKED = 'cellClicked'
+
+"""
+Stores
+"""
+SESSION_STORE = 'session'
+LOCAL_STORE = 'local'
+
+NOTIFICATION_ID = 'notification-id'
+MAIN_PAGE_URL = '/'
+LOGIN_PAGE_URL = '/login'
+
+"""
+Unit Converters
+"""
+THOUSAND = 1000
+MILLION = 1_000_000
+BILLION = 1_000_000_000
+TRILLION = 1_000_000_000_000
