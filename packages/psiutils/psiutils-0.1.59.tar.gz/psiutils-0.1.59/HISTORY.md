a663ce5c6f80d64fb3dc89808394c1d93cf83b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:18 +0000

    Add package psiutils/0.1.59/psiutils-0.1.59.tar.gz

---
+++ packages/psiutils/psiutils-0.1.59.tar.gz/psiutils-0.1.59/HISTORY.md
@@ -0,0 +1,302 @@
+# History
+
+Version 0.1.59 - 16 July 2025
+
+1. Upgrade and UPdate buttons
+------------------------------
+
+Version 0.1.58 - 16 July 2025
+
+1. New icon buttons and sorted text
+------------------------------
+
+Version 0.1.57 - 02 July 2025
+
+1. Allw defn of icon dir in IconButtons
+------------------------------
+
+Version 0.1.56 - 01 July 2025
+
+1. Add Icons
+------------------------------
+
+Version 0.1.55 - 01 July 2025
+
+1. IconButtons
+------------------------------
+
+Version 0.1.54 - 20 April 2025
+
+1. Move DEFAULT_GEOMETRY
+------------------------------
+
+Version 0.1.53 - 20 April 2025
+
+1. Add new geometry
+------------------------------
+
+Version 0.1.52 - 07 April 2025
+
+1. Added AboutFrame to README.md
+------------------------------
+
+Version 0.1.50 - 07 April 2025
+
+1. Add AboutFrame
+------------------------------
+
+Version 0.1.49 - 23 March 2025
+
+1. Added undefined to DIALOG_STATUS
+------------------------------
+
+Version 0.1.46 - 19 March 2025
+
+1. Fix messagebox, add askyesno
+------------------------------
+
+Version 0.1.45 - 15 March 2025
+
+1. Add vertical offset to tooltip
+------------------------------
+
+Version 0.1.44 - 12 March 2025
+
+1. Bug in wondows resize
+------------------------------
+
+Version 0.1.43 - 20 February 2025
+
+1. Add window_resizer
+------------------------------
+
+Version 0.1.42 - 17 February 2025
+
+1. Pass missing icons to messagebox
+------------------------------
+
+Version 0.1.41 - 16 February 2025
+
+1. Allow for missing images in messagebox
+------------------------------
+
+Version 0.1.40 - 16 February 2025
+
+1. Refactor messagebox in line with tkinter
+------------------------------
+
+Version 0.1.39 - 15 February 2025
+
+1. Add messagebox
+------------------------------
+
+Version 0.1.38 - 10 February 2025
+
+1. Geometry and file type strings
+------------------------------
+
+Version 0.1.37 - 26 January 2025
+
+1. Add Path to resolve_paths
+------------------------------
+
+Version 0.1.36 - 21 January 2025
+
+1. Update README
+------------------------------
+
+Version 0.1.35 - 16 January 2025
+
+1. Dependent on python >= 3.11
+------------------------------
+
+Version 0.1.34 - 15 January 2025
+
+1. Dependant on python >= 3.11
+------------------------------
+
+Version 0.1.33 - 09 January 2025
+
+1. Readme added
+------------------------------
+
+Version 0.1.32 - 08 January 2025
+
+1. Use poetry add
+------------------------------
+
+Version 0.1.31 - 08 January 2025
+
+1. 
+------------------------------
+
+Version 0.1.30 - 08 January 2025
+
+1. Add setup.py
+------------------------------
+
+Version 0.1.29 - 05 January 2025
+
+1. Change to display_icon
+------------------------------
+
+Version 0.1.28 - 04 January 2025
+
+1. Add set_icon
+------------------------------
+
+Version 0.1.25 - 02 January 2025
+
+1. Remove extraneousr uuid4()
+------------------------------
+
+Version 0.1.24 - 02 January 2025
+
+1. Gte str from guid
+------------------------------
+
+Version 0.1.23 - 02 January 2025
+
+1. Improve Windows file location to pick up user
+------------------------------
+
+Version 0.1.22 - 21 December 2024
+
+1. Indentation problem fixed
+------------------------------
+
+Version 0.1.21 - 20 December 2024
+
+1. Use tk constants in ButtonFrame
+------------------------------
+
+Version 0.1.20 - 30 November 2024
+
+1. Scrolling frame and sourcery suggestions
+------------------------------
+
+Version 0.1.19 - 29 November 2024
+
+1. Tidy colours in styles
+------------------------------
+
+Version 0.1.18 - 25 November 2024
+
+1. Refactor Button and add constant colours
+------------------------------
+
+Version 0.1.17 - 25 November 2024
+
+1. Remove spurious ',' from buttons
+------------------------------
+
+Version 0.1.16 - 25 November 2024
+
+1. Add Enum and update styles
+------------------------------
+
+Version 0.1.15 - 07 November 2024
+
+1. Fix button padding
+------------------------------
+
+Version 0.1.14 - 06 November 2024
+
+1. Added enable_frame to utilities
+------------------------------
+
+Version 0.1.13 - 04 November 2024
+
+1.  New update
+------------------------------
+
+Version 0.1.12 - 04 November 2024
+
+1. Pad Class
+------------------------------
+
+Version 0.1.11 - 31 October 2024
+
+1. Add default enable to Menu
+------------------------------
+
+Version 0.1.8 - 28 October 2024
+
+1. Changes after documentation
+------------------------------
+
+Version 0.1.7 - 27 October 2024
+
+1. Add style to buttons
+------------------------------
+
+Version 0.1.6 - 26 October 2024
+
+1. Constants and tidy up widgets
+------------------------------
+
+Version 0.1.5 - 16 October 2024
+
+1. Add MODES to constants
+------------------------------
+
+Version 0.1.4 - 16 October 2024
+
+1. Reorganise
+2. Add tests
+
+------------------------------
+
+Version 0.1.3 - 8 October 2024
+
+Remove src
+
+------------------------------
+
+Version 0.1.2 - 8 October 2024
+
+Remove _version.py_
+
+------------------------------
+
+Version 0.1.1 - 8 October 2024
+
+Remove psiutils. from imports
+
+------------------------------
+
+Version 0.1.0 - 7 October 2024
+
+Initial version
+
+------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ packages/psiutils/psiutils-0.1.59.tar.gz/psiutils-0.1.59/psiutils/buttons.py
@@ -0,0 +1,224 @@
+"""Button class for Tkinter applications."""
+import tkinter as tk
+from tkinter import ttk
+from pathlib import Path
+from PIL import Image, ImageTk
+
+from .constants import PAD, Pad
+from .widgets import enter_widget, clickable_widget, HAND
+
+import psiutils.text as text
+
+
+class IconButton(ttk.Frame):
+    def __init__(
+            self,
+            master,
+            text,
+            icon,
+            command=None,
+            sticky: str = '',
+            dimmable: bool = False,
+            icon_path: str = '',
+            **kwargs):
+        super().__init__(master, borderwidth=1, relief="raised", **kwargs)
+        self.command = command
+        self._state = 'normal'
+
+        # Icon and text
+        if not icon_path:
+            icon_path = f'{Path(__file__).parent}/icons/'
+        image = Image.open(f'{icon_path}{icon}.png').resize((16, 16))
+        photo_image = ImageTk.PhotoImage(image)
+
+        self.button_label = ttk.Label(
+            self, text=text, image=photo_image, compound=tk.LEFT)
+        self.button_label.image = photo_image  # Prevent garbage collection
+        self.button_label.pack(padx=(3, 5), pady=5)
+        self.widget = self.button_label
+
+        # Make the whole frame clickable
+        self.bind_widgets()
+
+        self.sticky = sticky
+        self.dimmable = dimmable
+
+    def state(self) -> dict:
+        return self._state
+
+    def enable(self, enable: bool = True) -> None:
+        state = tk.NORMAL if enable else tk.DISABLED
+        self.button_label.configure(state=state)
+        self._state = state
+
+    def disable(self, disable: bool = True) -> None:
+        state = tk.DISABLED if disable else tk.NORMAL
+        self.button_label.configure(state=state)
+        self._state = state
+
+    def bind_widgets(self):
+        for widget in (self, self.button_label):
+            widget.bind("<Button-1>", self._on_click)
+            widget.bind("<Enter>", self._enter_button)
+            widget.bind("<Leave>", lambda e: self.config(relief="raised"))
+
+    def _enter_button(self, event) -> None:
+        if self._state == tk.DISABLED:
+            return
+        self.config(relief="sunken")
+        event.widget.winfo_toplevel().config(cursor=HAND)
+
+    def _on_click(self, *args):
+        if self.command:
+            self.command()
+
+
+class Button(ttk.Button):
+    def __init__(
+            self,
+            *args,
+            sticky: str = '',
+            dimmable: bool = False,
+            **kwargs: dict,
+            ) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.sticky = sticky
+        self.dimmable = dimmable
+
+    def enable(self, enable: bool = True) -> None:
+        state = tk.NORMAL if enable else tk.DISABLED
+        self['state'] = state
+
+    def disable(self, disable: bool = True) -> None:
+        state = tk.DISABLED if disable else tk.NORMAL
+        self['state'] = state
+
+
+class ButtonFrame(ttk.Frame):
+    def __init__(
+            self,
+            master: tk.Frame,
+            orientation: str = tk.HORIZONTAL,
+            **kwargs: dict) -> None:
+        super().__init__(master, **kwargs)
+        self._buttons = []
+        self._enabled = False
+        self.orientation = orientation
+
+        if 'enabled' in kwargs:
+            self._enabled = kwargs['enabled']
+
+        self.icon_buttons = {
+            'build': IconButton(self, text.BUILD, 'build'),
+            'close': IconButton(self, text.CLOSE, 'cancel'),
+            'compare': IconButton(self, text.COMPARE, 'compare'),
+            'copy_docs': IconButton(self, text.COPY, 'copy_docs'),
+            'copy_clipboard': IconButton(self, text.COPY, 'copy_clipboard'),
+            'delete': IconButton(self, text.DELETE, 'delete'),
+            'diff': IconButton(self, text.DIFF, 'diff'),
+            'done': IconButton(self, text.DONE, 'done'),
+            'edit': IconButton(self, text.EDIT, 'edit'),
+            'exit': IconButton(self, text.EXIT, 'cancel'),
+            'new': IconButton(self, text.NEW, 'new'),
+            'next': IconButton(self, text.NEXT, 'next'),
+            'open': IconButton(self, text.OPEN, 'open'),
+            'previous': IconButton(self, text.PREVIOUS, 'previous'),
+            'refresh': IconButton(self, text.REFRESH, 'refresh'),
+            'report': IconButton(self, text.REPORT, 'report'),
+            'save': IconButton(self, text.SAVE, 'save'),
+            'send': IconButton(self, text.SEND, 'send'),
+            'update': IconButton(self, text.UPDATE, 'update'),
+            'upgrade': IconButton(self, text.UPGRADE, 'upgrade'),
+            'use': IconButton(self, text.USE, 'done'),
+        }
+
+    def icon_button(
+            self, id: str, dimmable: bool = False, command=None) -> IconButton:
+        button = self.icon_buttons[id]
+        button.dimmable = dimmable
+        button.command = command
+        return button
+
+    @property
+    def buttons(self) -> list[Button]:
+        return self._buttons
+
+    @buttons.setter
+    def buttons(self, value: list[Button]) -> None:
+        self._buttons = value
+
+        if self.orientation == tk.VERTICAL:
+            self._vertical_buttons()
+        elif self.orientation == tk.HORIZONTAL:
+            self._horizontal_buttons()
+
+    @property
+    def enabled(self) -> bool:
+        return self._enabled
+
+    @enabled.setter
+    def enabled(self, value: bool) -> None:
+        self._enabled = value
+        state = tk.NORMAL if value else tk.DISABLED
+        for button in self. buttons:
+            button.widget['state'] = state
+
+    def enable(self, enable: bool = True) -> None:
+        self._enabled = enable
+        self._enable_buttons(self.buttons, enable)
+
+    def disable(self) -> None:
+        self._enabled = False
+        self._enable_buttons(self.buttons, False)
+
+    def _vertical_buttons(self) -> None:
+        self.rowconfigure(len(self.buttons)-1, weight=1)
+        for row, button in enumerate(self.buttons):
+            pady = PAD
+            if row == 0:
+                pady = Pad.S
+            if row == len(self.buttons) - 1:
+                self.rowconfigure(row, weight=1)
+                row += 1
+                pady = Pad.N
+
+            button.grid(row=row, column=0, sticky=tk.EW, pady=pady)
+            clickable_widget(button)
+
+    def _horizontal_buttons(self) -> None:
+        self.columnconfigure(len(self.buttons)-1, weight=1)
+        for col, button in enumerate(self.buttons):
+            padx = PAD
+            if col == 0:
+                padx = Pad.W
+            if col == len(self.buttons) - 1:
+                self.columnconfigure(col, weight=1)
+                col += 1
+            # if not button.sticky:
+            #     button.sticky = tk.W
+            button.grid(row=0, column=col, sticky=button.sticky, padx=padx)
+            clickable_widget(button)
+
+    @staticmethod
+    def _enable_buttons(buttons: list[Button], enable: bool = True):
+        state = tk.NORMAL if enable else tk.DISABLED
+        for button in buttons:
+            if button.dimmable:
+                if isinstance(button, Button):
+                    button['state'] = state
+                    button.bind('<Enter>', enter_widget)
+                elif isinstance(button, IconButton):
+                    if enable:
+                        button.enable()
+                    else:
+                        button.disable()
+
+
+
+def enable_buttons(buttons: list[Button], enable: bool = True):
+    state = tk.NORMAL if enable else tk.DISABLED
+    for button in buttons:
+        if button.dimmable:
+            button['state'] = state
+            button.bind('<Enter>', enter_widget)
