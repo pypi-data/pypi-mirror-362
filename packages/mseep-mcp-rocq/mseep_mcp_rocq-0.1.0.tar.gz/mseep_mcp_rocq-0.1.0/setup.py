954c6c6e9b7e747e692f98830b4510e8d4465b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:08:32 +0000

    Add package mseep-mcp-rocq/0.1.0/mseep_mcp_rocq-0.1.0.tar.gz

---
+++ packages/mseep-mcp-rocq/mseep_mcp_rocq-0.1.0.tar.gz/mseep_mcp_rocq-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mseep-mcp_rocq',
+    version='0.1.0',
+    description='MCP server for advanced logical reasoning using Coq',
+    long_description="Package managed by MseeP.ai",
+    long_description_content_type="text/plain",
+    author='mseep',
+    author_email='support@skydeck.ai',
+    maintainer='mseep',
+    maintainer_email='support@skydeck.ai',
+    url='https://github.com/mseep',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp>=1.0.0', 'pydantic>=2.0.0'],
+    keywords=['mseep'],
+)
