88080e42058cc85b48e0bc2482f2fb7cefaadefe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:11:01 +0000

    Add package pdal-piper/0.2.0/pdal_piper-0.2.0.tar.gz

---
+++ packages/pdal-piper/pdal_piper-0.2.0.tar.gz/pdal_piper-0.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pdal-piper',
+    version='0.2.0',
+    packages=['pdal_piper'],
+    url='https://github.com/j-tenny/pdal-piper',
+    license='MIT',
+    author='Johnathan Tenny (j-tenny)',
+    author_email='jt893@nau.edu',
+    description='Type stubs and utilities for PDAL (Point Data Abstraction Library) and USGS 3DEP lidar download.'
+)
