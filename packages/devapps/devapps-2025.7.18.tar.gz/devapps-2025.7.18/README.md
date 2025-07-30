f326bc217456a24aa9cd9d2bf537fecf254d33ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:34 +0000

    Add package devapps/2025.7.18/devapps-2025.7.18.tar.gz

---
+++ packages/devapps/devapps-2025.7.18.tar.gz/devapps-2025.7.18/README.md
@@ -0,0 +1,33 @@
+# devapps
+
+
+<!-- badges -->
+[![docs pages][docs pages_img]][docs pages] [![gh-ci][gh-ci_img]][gh-ci] [![pkg][pkg_img]][pkg] [![code_style][code_style_img]][code_style] 
+
+[docs pages]: https://axgkl.github.io/devapps/
+[docs pages_img]: https://axgkl.github.io/devapps/img/badge_docs.svg
+[gh-ci]: https://github.com/AXGKl/devapps/actions/workflows/ci.yml
+[gh-ci_img]: https://github.com/AXGKl/devapps/actions/workflows/ci.yml/badge.svg
+[pkg]: https://pypi.com/
+[pkg_img]: https://axgkl.github.io/devapps/img/badge_pypi.svg
+[code_style]: https://pypi.org/project/axblack/
+[code_style_img]: https://axgkl.github.io/devapps/img/badge_axblack.svg
+<!-- badges -->
+
+
+Enabler repo for dev *and* ops friendly apps, in a normalized way.
+
+Includes:
+
+- logging (structlog)
+- cli flags handling (abseil, with addons)
+- docutools (mkdocs-material)
+- project setup
+- (test) resources management, including daemons and container filesystem layers
+
+and more.
+
+
+
+
+Documentation: https://axgkl.github.io/devapps/
---
+++ packages/devapps/devapps-2025.7.18.tar.gz/devapps-2025.7.18/src/devapp/operations/resources.py
@@ -0,0 +1,89 @@
+from devapp.tools import offset_port, exists, project
+import os
+from devapp.app import app
+# from devapp.operations import binenv, asdf
+
+
+class tool:
+    conda_env = 'lctools'
+
+
+redis_pre = """
+test "$1" == "cli" && { shift; redis-cli "$@"; exit $?; }
+test -n "$1" && { redis-server "$@"; exit $?; }
+test -e "$host_conf_dir/redis.conf" && { redis-server "$host_conf_dir/redis.conf"; exit $?; }
+
+"""
+
+
+def redis_server(**kw):
+    if kw.get('cmd') == 'redis-cli':
+        m = {'cmd': ':redis-cli -p %s' % offset_port(kw['rsc'].port)}
+    else:
+        m = {'cmd': ':redis-server --port %s' % offset_port(kw['rsc'].port)}
+        m['cmd_pre'] = redis_pre
+    return m
+
+
+def verify_tools(path, rsc, **kw):
+    for p in rsc.provides:
+        d = path + '/' + p
+        if not exists(d):
+            app.error('Not found', cmd=d)
+            return False
+    return True
+
+
+def lc_tools(*a, **kw):
+    cmd = kw['cmd']
+    if cmd == 'tmux':
+        d = project.root() + '/tmp/tmux'
+        os.makedirs(d, exist_ok=True)
+        return {'cmd_pre': 'export TMUX_TMPDIR="%s"; ' % d, 'cmd': 'tmux'}
+    return cmd
+
+
+def slc(**kw):
+    os.environ.get('slc_lc_hubs')
+    return {'cmd': ':ops slc connect'}
+
+
+class rsc:
+    """For services we change dir to project."""
+
+    class slc:
+        cmd = slc
+        pkg = False
+        environ = ['lc_hubs']
+
+    class redis_server:
+        provides = ['redis-server', 'redis-cli']
+        cmd = 'redis-server'
+        run = redis_server
+        pkg = 'redis-server'
+        port = 6379
+        systemd = 'redis-server'
+
+    class lc_tools:
+        # httpd for rotatelogs
+        provides = ['git', 'fzf', 'jq', 'rg', 'fd', 'http', 'htop', 'tmux']
+        conda_chan = 'conda-forge'
+        conda_pkg = (
+            ' '.join(provides)
+            .replace(' rg ', ' ripgrep ')
+            .replace(' fd ', ' fd-find ')
+            .replace(' http ', ' httpie ')
+        )
+        run = lc_tools
+        verify_present = verify_tools
+
+    class lc_tools_kf:
+        # httpd for rotatelogs
+        provides = ['rotatelogs']
+        conda_chan = 'kalefranz'
+        conda_pkg = 'httpd'
+        verify_present = verify_tools
+
+
+# rsc.binenv = binenv.binenv
+# rsc.asdf = asdf.asdf
