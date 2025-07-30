308ea378753490bc8c179e3436ba30c033813d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:07:10 +0000

    Add package sneakydog-primitives-demo/0.1.5/sneakydog_primitives_demo-0.1.5-py3-none-any.whl

---
+++ packages/sneakydog-primitives-demo/sneakydog_primitives_demo-0.1.5-py3-none-any.whl/sneakydog_primitives_demo/password.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from sqlalchemy.types import TypeDecorator, String
+from sneakydog_primitives_demo.value_objects import ValueObject
+
+
+@dataclass(frozen=True)
+class Password(ValueObject[str]): ...
+
+
+# 2. 自定义 SQLAlchemy 类型
+class PasswordTypeDecorator(TypeDecorator):
+    impl = String
+    cache_ok = True  # ✅ SQLAlchemy 2.x 要求设置
+
+    def process_bind_param(self, value, dialect):
+        # 存到数据库前
+        if isinstance(value, Password):
+            return value.value
+        elif isinstance(value, str):
+            return value
+        elif value is None:
+            return None
+        else:
+            raise ValueError("Invalid Password")
+
+    def process_result_value(self, value, dialect):
+        # 从数据库读出时
+        if value is not None:
+            return Password(value)
+        return None
