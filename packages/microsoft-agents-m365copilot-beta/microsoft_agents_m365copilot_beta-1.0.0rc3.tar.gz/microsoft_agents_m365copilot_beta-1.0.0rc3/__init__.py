9cb44c452438f71f402e421f857f0743c42026f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:52 +0000

    Add package microsoft-agents-m365copilot-beta/1.0.0rc3/microsoft_agents_m365copilot_beta-1.0.0rc3.tar.gz

---
+++ packages/microsoft-agents-m365copilot-beta/microsoft_agents_m365copilot_beta-1.0.0rc3.tar.gz/microsoft_agents_m365copilot_beta-1.0.0rc3/__init__.py
@@ -0,0 +1,12 @@
+"""
+Python client for the Microsoft CCS Python SDK.
+"""
+from ._version import VERSION
+from .agents_m365_copilot_beta_request_adapter import AgentsM365CopilotBetaRequestAdapter
+from .agents_m365_copilot_beta_service_client import AgentsM365CopilotBetaServiceClient
+
+__version__ = VERSION
+
+__all__ = [
+    "AgentsM365CopilotBetaServiceClient", "AgentsM365CopilotBetaRequestAdapter"
+]
