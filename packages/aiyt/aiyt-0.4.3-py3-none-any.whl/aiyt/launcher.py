80fbbbd974235f52aa83a81a748e1b2116e90386
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:45 +0000

    Add package aiyt/0.4.3/aiyt-0.4.3-py3-none-any.whl

---
+++ packages/aiyt/aiyt-0.4.3-py3-none-any.whl/aiyt/launcher.py
@@ -0,0 +1,21 @@
+import click
+from pathlib import Path
+from subprocess import Popen
+
+
+@click.command()
+@click.version_option()
+def main():
+    """Launch the Streamlit app."""
+
+    cmd = f"uv run streamlit run {Path(__file__).parent / 'main.py'}"
+    process = Popen(cmd.split())
+    try:
+        process.wait()
+    except KeyboardInterrupt:
+        process.terminate()
+        exit(0)
+
+
+if __name__ == "__main__":
+    main()
