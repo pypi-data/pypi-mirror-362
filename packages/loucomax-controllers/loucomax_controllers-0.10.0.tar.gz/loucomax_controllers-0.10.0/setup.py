9c5b7d057a6dba756ed577777aa8d6a35fd05bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:54 +0000

    Add package loucomax-controllers/0.10.0/loucomax_controllers-0.10.0.tar.gz

---
+++ packages/loucomax-controllers/loucomax_controllers-0.10.0.tar.gz/loucomax_controllers-0.10.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="LouCOMAX_Controllers",
+    version="0.10.0",
+    description="Controllers for all devices used in the LouCOMAX (CNRS) project",
+    author="BLASIAK Antoine",
+    author_email="antoineblasiak66@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "overrides",
+        "pythonnet",
+        "h5py",
+        "matplotlib",
+        "configparser",
+        "pyusb",
+        "pyserial",
+        "PyQt5",
+        "csu2controller",
+        "zaber_motion",
+    ],
+    python_requires=">=3.8",
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
