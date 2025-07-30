b3564be637389c4794d9f4b37c4ce3a0f0bf2c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:25 +0000

    Add package fastml-contets/0.4.0/fastml_contets-0.4.0.tar.gz

---
+++ packages/fastml-contets/fastml_contets-0.4.0.tar.gz/fastml_contets-0.4.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="fastml_contets",
+    version="0.4.0",
+    author="xuxy",
+    author_email="",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/你的用户名/fastml_contest",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn"
+    ]
+)
