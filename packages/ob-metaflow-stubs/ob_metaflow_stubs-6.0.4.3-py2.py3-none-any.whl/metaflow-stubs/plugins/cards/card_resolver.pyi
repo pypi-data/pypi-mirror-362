9f658341c12e314549414f6343f7dc951dc12551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:04 +0000

    Add package ob-metaflow-stubs/6.0.4.3/ob_metaflow_stubs-6.0.4.3-py2.py3-none-any.whl

---
+++ packages/ob-metaflow-stubs/ob_metaflow_stubs-6.0.4.3-py2.py3-none-any.whl/metaflow-stubs/plugins/cards/card_resolver.pyi
@@ -0,0 +1,17 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.16.1.1+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-07-15T20:32:21.540022                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from .card_datastore import CardDatastore as CardDatastore
+
+def resumed_info(task):
+    ...
+
+def resolve_paths_from_task(flow_datastore, pathspec = None, type = None, hash = None, card_id = None):
+    ...
+
