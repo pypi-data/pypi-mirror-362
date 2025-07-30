9bf0353e39ad091d81268ae07817ccc7bef3d274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:18 +0000

    Add package s3vectors-embed-cli/0.1.0/s3vectors_embed_cli-0.1.0.tar.gz

---
+++ packages/s3vectors-embed-cli/s3vectors_embed_cli-0.1.0.tar.gz/s3vectors_embed_cli-0.1.0/s3vectors/__version__.py
@@ -0,0 +1,7 @@
+"""Version information for S3 Vectors CLI."""
+
+__version__ = "0.1.0"
+__version_info__ = tuple(int(i) for i in __version__.split('.'))
+
+# Version history
+# 0.1.0 - Amazon S3 Vectors Embed CLI is in preview release and is subject to change.
