68a59cf7b812ec35c680638c427793f1d924d3fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:15:09 +0000

    Add package ob-metaflow-stubs/6.0.4.4/ob-metaflow-stubs-6.0.4.4.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.4.4.tar.gz/ob-metaflow-stubs-6.0.4.4/metaflow-stubs/plugins/aws/step_functions/__init__.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.16.2.1+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-07-16T08:15:48.047850                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from . import schedule_decorator as schedule_decorator
+from . import step_functions_deployer as step_functions_deployer
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.4.4.tar.gz/ob-metaflow-stubs-6.0.4.4/metaflow-stubs/plugins/snowflake/__init__.pyi
@@ -0,0 +1,14 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.16.2.1+obcheckpoint(0.2.4);ob(v1)                                                    #
+# Generated on 2025-07-16T08:15:47.956877                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from ...mf_extensions.outerbounds.plugins.snowflake import snowflake as snowflake
+from ...mf_extensions.outerbounds.plugins.snowflake.snowflake import connect as connect
+from ...mf_extensions.outerbounds.plugins.snowflake.snowflake import get_snowflake_token as get_snowflake_token
+from ...mf_extensions.outerbounds.plugins.snowflake.snowflake import Snowflake as Snowflake
+
