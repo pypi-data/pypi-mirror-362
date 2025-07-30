f81a63594efbbccdf5b2d1581abc5d1f7cd34ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:53 +0000

    Add package dreamtools-dreamgeeker/2025.2.1/dreamtools_dreamgeeker-2025.2.1.tar.gz

---
+++ packages/dreamtools-dreamgeeker/dreamtools_dreamgeeker-2025.2.1.tar.gz/dreamtools_dreamgeeker-2025.2.1/dreamtools_dreamgeeker/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Liste de fonction standard reutilisable
+
+.. author: Dreamgeeker
+"""
+
+__author__ = "Ketsia LENTIN"
+__copyright__ = "Copyright 2021, CouleurWest IT"
+__credits__ = "Ketsia LENTIN"
+__license__ = "MIT"
+__version__ = "2025.2.1"
+__email__ = "contact@couleurwest-it"
+__status__ = "Production"
+__docformat__ = 'reStructuredText'
