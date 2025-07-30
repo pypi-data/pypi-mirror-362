d97ab318b4e83255e0f7da4d965248f9672d3d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:11 +0000

    Add package mseep-langroid/0.58.3/mseep_langroid-0.58.3.tar.gz

---
+++ packages/mseep-langroid/mseep_langroid-0.58.3.tar.gz/mseep_langroid-0.58.3/examples/docqa/doc-chat-simple.py
@@ -0,0 +1,44 @@
+"""
+Bare-bones example of using DocChatAgent to query a document.
+
+Run like this (omit the model to use default GPT-4o):
+    
+    python3 examples/docqa/doc-chat-simple.py --model ollama/qwen2.5:latest
+    
+"""
+
+from fire import Fire
+
+import langroid.language_models as lm
+from langroid.vector_store.chromadb import ChromaDBConfig
+from langroid.agent.special.doc_chat_agent import (
+    DocChatAgent,
+    DocChatAgentConfig,
+)
+
+
+def main(model: str = ""):
+    # set up the agent
+    agent = DocChatAgent(
+        DocChatAgentConfig(
+            vecdb=ChromaDBConfig(),
+            llm=lm.OpenAIGPTConfig(chat_model=model or lm.OpenAIChatModel.GPT4o),
+            # several configs possible here, omitted for brevity
+        ),
+    )
+
+    # ingest document(s), could be a local file/folder or URL
+    # Try Borges' "Library of Babel" short story
+    url = "https://xpressenglish.com/our-stories/library-of-babel/"
+
+    agent.ingest_doc_paths([url])
+
+    result = agent.llm_response("what is the shape of the rooms in the library?")
+
+    assert "hexagon" in result.content.lower()
+
+    print(result.content)
+
+
+if __name__ == "__main__":
+    Fire(main)
