7a4711dd00ad5f5aa6da5e83068e0553e95ef4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:38 +0000

    Add package fastjet/3.5.1.1/fastjet-3.5.1.1-cp312-cp312-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/fastjet/fastjet-3.5.1.1-cp312-cp312-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl/fastjet/lib64/cmake/fastjetcontrib/fastjetcontribTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "fastjet::contrib::fastjetcontribfragile" for configuration "Release"
+set_property(TARGET fastjet::contrib::fastjetcontribfragile APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(fastjet::contrib::fastjetcontribfragile PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libfastjetcontribfragile.so"
+  IMPORTED_SONAME_RELEASE "libfastjetcontribfragile.so"
+  )
+
+list(APPEND _cmake_import_check_targets fastjet::contrib::fastjetcontribfragile )
+list(APPEND _cmake_import_check_files_for_fastjet::contrib::fastjetcontribfragile "${_IMPORT_PREFIX}/lib64/libfastjetcontribfragile.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
