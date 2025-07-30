7d097fc6f6af48a40ac73bb06e503b7b7bcde0cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:44 +0000

    Add package bluer-ugv/6.448.1/bluer_ugv-6.448.1.tar.gz

---
+++ packages/bluer-ugv/bluer_ugv-6.448.1.tar.gz/bluer_ugv-6.448.1/bluer_ugv/sparrow/README.py
@@ -0,0 +1,14 @@
+assets2 = "https://github.com/kamangir/assets2/blob/main/bluer-sparrow"
+
+dict_of_images = {
+    f"{assets2}/20250712_114819.jpg?raw=true": "",
+    f"{assets2}/20250713_172325.jpg?raw=true": "",
+    f"{assets2}/20250713_172413.jpg?raw=true": "",
+    f"{assets2}/20250713_172442_1.gif?raw=true": "",
+    f"{assets2}/4-channel-remote-control.png?raw=true": "./analog/parts.md",
+}
+
+items = [
+    "[![image]({})]({})".format(image, url if url else image)
+    for image, url in dict_of_images.items()
+]
