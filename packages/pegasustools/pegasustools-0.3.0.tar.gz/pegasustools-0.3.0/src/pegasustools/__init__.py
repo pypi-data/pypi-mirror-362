186321ba0bf870d2db22ad5fa74ff3920ad1f940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:11:28 +0000

    Add package pegasustools/0.3.0/pegasustools-0.3.0.tar.gz

---
+++ packages/pegasustools/pegasustools-0.3.0.tar.gz/pegasustools-0.3.0/src/pegasustools/__init__.py
@@ -0,0 +1,26 @@
+"""Copyright (c) 2025 Robert Caddy. All rights reserved.
+
+PegasusTools: Analysis package for the Pegasus PIC code.
+"""
+
+# Import all relevant modules into the pegasustools namespacess
+from pegasustools.loading_nbf import PegasusNBFData
+from pegasustools.loading_spectra import PegasusSpectralData
+from pegasustools.loading_tracks import (
+    collate_tracks_from_ascii,
+    collate_tracks_from_binary,
+)
+from pegasustools.pt_logging import setup_pt_logger
+
+# Set version
+from ._version import version as __version__
+
+# Setup what gets imported with import *
+__all__ = [
+    "PegasusNBFData",
+    "PegasusSpectralData",
+    "__version__",
+    "collate_tracks_from_ascii",
+    "collate_tracks_from_binary",
+    "setup_pt_logger",
+]
