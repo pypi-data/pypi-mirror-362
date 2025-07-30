c9a5c03bc06659e82a0f725b3a4d8d57c5d1728d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:09:47 +0000

    Add package fairscape-models/1.0.5/fairscape_models-1.0.5.tar.gz

---
+++ packages/fairscape-models/fairscape_models-1.0.5.tar.gz/fairscape_models-1.0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fairscape-models",
+    version="1.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "pydantic",
+        "pymongo"
+    ],
+    python_requires=">=3.8",
+    description="Fairscape metadata models",
+    author="Justin Niestroy",
+    author_email="jniestroy@gmail.com",
+    url="https://github.com/fairscape/fairscape_models",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
