d7638b34e142b6dcc63d50213d65368676d5ca34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:13:46 +0000

    Add package use-notify/0.3.4/use_notify-0.3.4.tar.gz

---
+++ packages/use-notify/use_notify-0.3.4.tar.gz/use_notify-0.3.4/src/use_notify/channels/base.py
@@ -0,0 +1,16 @@
+from abc import ABCMeta, abstractmethod
+
+from usepy import useAdDict
+
+
+class BaseChannel(metaclass=ABCMeta):
+    def __init__(self, config: dict):
+        self.config = useAdDict(config)
+
+    @abstractmethod
+    def send(self, message):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def send_async(self, message):
+        raise NotImplementedError
