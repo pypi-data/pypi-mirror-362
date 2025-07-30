6283e191c02ba08817178714f767ffa9aae1ac18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:16 +0000

    Add package mseep-tdengine-mcp-server/0.0.8/mseep_tdengine_mcp_server-0.0.8.tar.gz

---
+++ packages/mseep-tdengine-mcp-server/mseep_tdengine_mcp_server-0.0.8.tar.gz/mseep_tdengine_mcp_server-0.0.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mseep-tdengine-mcp-server',
+    version='0.0.8',
+    description='Add your description here',
+    long_description="Package managed by MseeP.ai",
+    long_description_content_type="text/plain",
+    author='mseep',
+    author_email='support@skydeck.ai',
+    maintainer='mseep',
+    maintainer_email='support@skydeck.ai',
+    url='https://github.com/mseep',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['jinja2', 'mcp[cli]', 'taospy'],
+    keywords=['mseep'],
+)
