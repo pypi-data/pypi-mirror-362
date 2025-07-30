4fc0da74e3c25443587882fe144e0b079f22564b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:35 +0000

    Add package behave/1.2.7.dev7/behave-1.2.7.dev7.tar.gz

---
+++ packages/behave/behave-1.2.7.dev7.tar.gz/behave-1.2.7.dev7/more.features/steps/tutorial_steps.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+"""
+Step implementations for tutorial example.
+"""
+
+from __future__ import absolute_import, print_function
+from behave import given, when, then
+from assertpy import assert_that
+
+
+@given(u'we have behave installed')
+def step_impl(context):
+    pass
+
+
+@when(u'we implement a test')
+def step_impl(context):
+    assert_that(True).is_not_equal_to(False)
+
+
+@then(u'behave will test it for us!')
+def step_impl(context):
+    assert_that(context.failed).is_equal_to(False)
