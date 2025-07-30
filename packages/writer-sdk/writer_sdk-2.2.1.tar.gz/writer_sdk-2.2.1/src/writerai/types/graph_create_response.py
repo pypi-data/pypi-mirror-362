29fcdd3772d15bc849b1e049cd87cba2ea62417e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:09 +0000

    Add package writer-sdk/2.2.1/writer_sdk-2.2.1.tar.gz

---
+++ packages/writer-sdk/writer_sdk-2.2.1.tar.gz/writer_sdk-2.2.1/src/writerai/types/graph_create_response.py
@@ -0,0 +1,22 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import Optional
+from datetime import datetime
+
+from .._models import BaseModel
+
+__all__ = ["GraphCreateResponse"]
+
+
+class GraphCreateResponse(BaseModel):
+    id: str
+    """A unique identifier of the Knowledge Graph."""
+
+    created_at: datetime
+    """The timestamp when the Knowledge Graph was created."""
+
+    name: str
+    """The name of the Knowledge Graph (max 255 characters)."""
+
+    description: Optional[str] = None
+    """A description of the Knowledge Graph (max 255 characters)."""
