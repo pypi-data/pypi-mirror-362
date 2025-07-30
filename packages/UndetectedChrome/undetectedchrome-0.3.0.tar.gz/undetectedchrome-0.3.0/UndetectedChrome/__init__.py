18d4b388ea2a8240d84d1b8945ca7cef73cd7632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:11:04 +0000

    Add package UndetectedChrome/0.3.0/undetectedchrome-0.3.0.tar.gz

---
+++ packages/UndetectedChrome/undetectedchrome-0.3.0.tar.gz/undetectedchrome-0.3.0/UndetectedChrome/__init__.py
@@ -0,0 +1,5 @@
+from .undetected_chrome import UndetectedChrome
+from .undetected_chrome import UndetectedChromeSync
+from .undetected_chrome import quick_browse
+
+__all__ = ["UndetectedChrome", "UndetectedChromeSync", "quick_browse"]
