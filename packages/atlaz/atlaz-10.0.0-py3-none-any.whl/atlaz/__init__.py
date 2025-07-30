360a9ff5a1544348d49a3f06d076c78ceb7cac81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:02 +0000

    Add package atlaz/10.0.0/atlaz-10.0.0-py3-none-any.whl

---
+++ packages/atlaz/atlaz-10.0.0-py3-none-any.whl/atlaz/__init__.py
@@ -0,0 +1,11 @@
+"""
+atlaz - A minimal Python library placeholder
+"""
+
+__version__ = "10.0.0"
+__author__ = "Rasmus Bjersing"
+__email__ = "rasmus.bjersing@gmail.com"
+
+def hello():
+    """A simple hello function."""
+    return "Hello from atlaz!"
