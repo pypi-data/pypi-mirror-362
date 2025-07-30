f3423b1821ca95895cc64c7b560187bfbdb9cf70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:24 +0000

    Add package phrlibrary/1.0.0/phrlibrary-1.0.0.tar.gz

---
+++ packages/phrlibrary/phrlibrary-1.0.0.tar.gz/phrlibrary-1.0.0/setup.py
@@ -0,0 +1,22 @@
+﻿from setuptools import setup, find_packages
+
+
+setup(
+
+    name="phrlibrary",
+
+    version="1.0.0",
+
+    packages=find_packages(),
+
+    install_requires=[open("requirements.txt", "r").read()],  # 依赖项
+
+    author="Phr",
+
+    description="A demo library",
+
+    long_description=open("README.md").read(),
+
+    python_requires=">=3.6"
+
+)
