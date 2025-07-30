2f05d4b22be63a61e0c1d390d3c7ab43b7f3e9ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 20:05:43 +0000

    Add package walytis-identities/0.1.5/walytis_identities-0.1.5-py3-none-any.whl

---
+++ packages/walytis-identities/walytis_identities-0.1.5-py3-none-any.whl/walytis_identities/__init__.py
@@ -0,0 +1,4 @@
+import walytis_beta_embedded # configure walytis_beta_api & walytis_beta_tools via environment variable
+from .group_did_manager import GroupDidManager
+from .did_manager import DidManager
+from .did_manager_with_supers import DidManagerWithSupers
