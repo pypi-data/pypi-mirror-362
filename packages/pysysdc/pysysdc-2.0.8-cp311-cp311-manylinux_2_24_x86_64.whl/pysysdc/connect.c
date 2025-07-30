005c0b594ba2df8bb74edb624fc59bad74af8ef7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:15 +0000

    Add package pysysdc/2.0.8/pysysdc-2.0.8-cp311-cp311-manylinux_2_24_x86_64.whl

---
+++ packages/pysysdc/pysysdc-2.0.8-cp311-cp311-manylinux_2_24_x86_64.whl/pysysdc/connect.c
@@ -0,0 +1,136 @@
+//#include <stdio.h>
+//#include <pthread.h>
+//#include <stdlib.h>
+//#include <sys/types.h>
+//#include <unistd.h>
+//#include <sys/syscall.h>
+#include "connect.h"
+#include "dc_error_handling.h"
+
+//static pthread_mutex_t mutex;
+static sd_bus *g_client_bus = NULL;
+static sd_bus *g_server_bus = NULL;
+
+sd_bus* get_server_bus(void)
+{
+	return g_server_bus;
+}
+
+sd_bus* get_client_bus(void)
+{
+	return g_client_bus;
+}
+
+int is_server_bus_open(void)
+{
+	if(g_server_bus)
+		return 1;
+
+	return 0;
+}
+
+int is_client_bus_open(void)
+{
+	if(g_client_bus)
+		return 1;
+
+	return 0;
+}
+
+sd_bus* acquire_server_bus(void)
+{
+	if(!g_server_bus)
+	{
+		server_bus_open();
+	}
+	return g_server_bus;
+}
+
+sd_bus* acquire_client_bus(void)
+{
+	if(!g_client_bus)
+	{
+		client_bus_open();
+	}
+	return g_client_bus;
+}
+
+sd_bus* bus_open()
+{
+	//pthread_mutex_lock(&mutex);
+		sd_bus *bus = NULL;
+		int r = sd_bus_open_system(&bus);
+		if (r < 0) {
+			set_dc_error(FAILED_TO_CONNECT_TO_SYSTEM_BUS);
+			bus = NULL;
+		}
+
+	//	fprintf(stderr, "new bus connection: %p\n", g_bus);
+	//pthread_mutex_unlock(&mutex);
+
+	return bus;
+}
+
+sd_bus* client_bus_open(void)
+{
+	g_client_bus = bus_open();
+	if (dc_has_error())
+	{
+		return NULL;
+	}
+
+	return g_client_bus;
+}
+
+sd_bus* server_bus_open(void)
+{
+	g_server_bus = bus_open();
+	if (dc_has_error())
+	{
+		return NULL;
+	}
+
+	return g_server_bus;
+}
+
+void client_bus_close(void)
+{
+	if(g_client_bus)
+	{
+		bus_close(&g_client_bus);
+		g_client_bus = NULL;
+	}
+}
+
+void server_bus_close(void)
+{
+	if(g_server_bus)
+	{
+		bus_close(&g_server_bus);
+		g_server_bus = NULL;
+	}
+}
+
+void bus_close(sd_bus **bus)
+{
+	//pthread_mutex_lock(&mutex);
+		//pid_t x = syscall(__NR_gettid);
+		//fprintf(stderr, "unref %d\n", x);
+		if(bus && *bus)
+		{
+			sd_bus_flush_close_unref(*bus);
+			*bus = NULL;
+		}
+	//pthread_mutex_unlock(&mutex);
+}
+
+void server_bus_deinit(void)
+{
+	server_bus_close();
+}
+
+void client_bus_deinit(void)
+{
+	//fprintf(stdout, "CLIENT DEINIT\n");
+	client_bus_close();
+}
---
+++ packages/pysysdc/pysysdc-2.0.8-cp311-cp311-manylinux_2_24_x86_64.whl/pysysdc/dc_error_handling.c
@@ -0,0 +1,96 @@
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "dc_error_handling.h"
+
+#define STRERR_SIZE 1024
+
+enum dc_error_codes_t dc_error_code = DC_NO_ERROR;
+char *sd_error = NULL;
+
+static inline const char *enum_to_str(enum dc_error_codes_t err)
+{
+	static const char *strings[] = 
+	{
+		[DC_NO_ERROR] = "",
+		[BUS_VALUE_IS_NULL] = "bus value is NULL",
+		[FUNC_ARG_IS_NULL] = "func arg is NULL",
+		[FAILED_TO_PARSE_REPLY] = "Failed to parse response message",
+		[FAILED_TO_CONNECT_TO_SYSTEM_BUS] = "Failed to connect to system bus",
+		[FAILED_TO_GET_PROPERTY] = "Failed to get property",
+		[USER_DATA_IS_NULL] = "User data is null",
+		[FAILED_TO_PARSE_PARAMETERS] = "Failed to parse parameters",
+		[FAILED_TO_PROCESS_BUS] = "Failed to process bus",
+		[FAILED_TO_WAIT_BUS] = "Failed to wait bus",
+		[FAILED_TO_ISSUE_METHOD_CALL] = "Failed to issue method call",
+		[FAILED_TO_ACQUIRE_SERVICE_NAME] = "Failed to acquire service name",
+		[FAILED_TO_CALL_METHOD] = "Failed to call method",
+		[FAILED_TO_ADD_OBJECT_VTABLE] = "Failed to add object vtable",
+		[FAILED_TO_REPLY_METHOD_RETURN] = "Failed to reply method return",
+		[FAILED_TO_RUN_PYTHON_FUNCTION] = "Failed to run python function",
+		[FAILED_TO_GET_SD_FD] = "Failed to get bus file descriptor",
+		[FAILED_EPOLL_CREATE] = "Failed to call epoll_create()",
+		[FAILED_EPOLL_CTL] = "Failed to call epoll_ctl()"
+	};
+
+	return strings[err];
+}
+
+void set_dc_error(enum dc_error_codes_t err)
+{
+	dc_error_code = err;
+	//fprintf(stderr, "dc_error_msg %s\n", msg);
+}
+
+void set_dc_error_msg(enum dc_error_codes_t er_code, const char *err)
+{
+	dc_error_code = er_code;
+	sd_error = strdup(err);
+}
+
+char* get_last_error(int code)
+{
+	char *buf = NULL;
+	char *strerr_buf = (char*) calloc(STRERR_SIZE, sizeof(char));
+	const char *dc_err_str = enum_to_str(dc_error_code);
+
+	strerror_r(-code, strerr_buf, STRERR_SIZE);
+
+	if(sd_error == NULL)
+	{
+		buf = (char*) calloc(strlen(dc_err_str)+STRERR_SIZE, sizeof(char));
+		sprintf(buf, "%s, %s", dc_err_str, strerr_buf);
+	} else {
+		buf = (char*) calloc(strlen(dc_err_str)+strlen(sd_error)+STRERR_SIZE, sizeof(char));
+		sprintf(buf, "%s, %s, %s", dc_err_str, sd_error, strerr_buf);
+	}
+
+	//free(strerr_buf);
+
+	dc_clean_error();
+
+	return buf;
+}
+
+int dc_has_error(void)
+{
+	return dc_error_code != DC_NO_ERROR;
+}
+
+void dc_clean_error(void)
+{
+	dc_error_code = DC_NO_ERROR;
+	// python GC free?
+	if(sd_error)
+	{
+		free(sd_error);
+		sd_error = NULL;
+	}
+}
+
+void dc_error_init(void)
+{
+	//dc_errors = realloc(dc_errors, ++Naccounts * sizeof(*tmp))
+}
