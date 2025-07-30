22cfa0fb0faf08c93487a56a752b12c1b95c9f23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:41 +0000

    Add package turing-sdk/0.16.1rc3/turing-sdk-0.16.1rc3.tar.gz

---
+++ packages/turing-sdk/turing-sdk-0.16.1rc3.tar.gz/turing-sdk-0.16.1rc3/turing/generated/models/__init__.py
@@ -0,0 +1,94 @@
+# flake8: noqa
+
+# import all models into this package
+# if you have many models here with many references from one model to another this may
+# raise a RecursionError
+# to avoid this, import only the models that you directly need like:
+# from from turing.generated.model.pet import Pet
+# or import this package, but before doing it, use:
+# import sys
+# sys.setrecursionlimit(n)
+
+from turing.generated.model.autoscaling_policy import AutoscalingPolicy
+from turing.generated.model.big_query_config import BigQueryConfig
+from turing.generated.model.big_query_dataset import BigQueryDataset
+from turing.generated.model.big_query_dataset_all_of import BigQueryDatasetAllOf
+from turing.generated.model.big_query_dataset_config import BigQueryDatasetConfig
+from turing.generated.model.big_query_sink import BigQuerySink
+from turing.generated.model.big_query_sink_all_of import BigQuerySinkAllOf
+from turing.generated.model.big_query_sink_config import BigQuerySinkConfig
+from turing.generated.model.build_ensembler_image_request import BuildEnsemblerImageRequest
+from turing.generated.model.dataset import Dataset
+from turing.generated.model.default_traffic_rule import DefaultTrafficRule
+from turing.generated.model.enricher import Enricher
+from turing.generated.model.ensembler import Ensembler
+from turing.generated.model.ensembler_config import EnsemblerConfig
+from turing.generated.model.ensembler_config_kind import EnsemblerConfigKind
+from turing.generated.model.ensembler_docker_config import EnsemblerDockerConfig
+from turing.generated.model.ensembler_id import EnsemblerId
+from turing.generated.model.ensembler_image import EnsemblerImage
+from turing.generated.model.ensembler_image_runner_type import EnsemblerImageRunnerType
+from turing.generated.model.ensembler_images import EnsemblerImages
+from turing.generated.model.ensembler_infra_config import EnsemblerInfraConfig
+from turing.generated.model.ensembler_job_status import EnsemblerJobStatus
+from turing.generated.model.ensembler_pyfunc_config import EnsemblerPyfuncConfig
+from turing.generated.model.ensembler_standard_config import EnsemblerStandardConfig
+from turing.generated.model.ensembler_standard_config_experiment_mappings import EnsemblerStandardConfigExperimentMappings
+from turing.generated.model.ensembler_type import EnsemblerType
+from turing.generated.model.ensemblers_paginated_results import EnsemblersPaginatedResults
+from turing.generated.model.ensemblers_paginated_results_all_of import EnsemblersPaginatedResultsAllOf
+from turing.generated.model.ensemblers_paginated_results_all_of1 import EnsemblersPaginatedResultsAllOf1
+from turing.generated.model.ensembling_job import EnsemblingJob
+from turing.generated.model.ensembling_job_ensembler_spec import EnsemblingJobEnsemblerSpec
+from turing.generated.model.ensembling_job_ensembler_spec_result import EnsemblingJobEnsemblerSpecResult
+from turing.generated.model.ensembling_job_meta import EnsemblingJobMeta
+from turing.generated.model.ensembling_job_paginated_results import EnsemblingJobPaginatedResults
+from turing.generated.model.ensembling_job_paginated_results_all_of import EnsemblingJobPaginatedResultsAllOf
+from turing.generated.model.ensembling_job_prediction_source import EnsemblingJobPredictionSource
+from turing.generated.model.ensembling_job_prediction_source_all_of import EnsemblingJobPredictionSourceAllOf
+from turing.generated.model.ensembling_job_result_type import EnsemblingJobResultType
+from turing.generated.model.ensembling_job_sink import EnsemblingJobSink
+from turing.generated.model.ensembling_job_source import EnsemblingJobSource
+from turing.generated.model.ensembling_job_spec import EnsemblingJobSpec
+from turing.generated.model.ensembling_resources import EnsemblingResources
+from turing.generated.model.env_var import EnvVar
+from turing.generated.model.event import Event
+from turing.generated.model.experiment_config import ExperimentConfig
+from turing.generated.model.field_source import FieldSource
+from turing.generated.model.generic_dataset import GenericDataset
+from turing.generated.model.generic_ensembler import GenericEnsembler
+from turing.generated.model.generic_sink import GenericSink
+from turing.generated.model.id_object import IdObject
+from turing.generated.model.image_building_job_state import ImageBuildingJobState
+from turing.generated.model.image_building_job_status import ImageBuildingJobStatus
+from turing.generated.model.job_id import JobId
+from turing.generated.model.kafka_config import KafkaConfig
+from turing.generated.model.label import Label
+from turing.generated.model.log_level import LogLevel
+from turing.generated.model.mounted_mlp_secret import MountedMLPSecret
+from turing.generated.model.pagination_paging import PaginationPaging
+from turing.generated.model.project import Project
+from turing.generated.model.protocol import Protocol
+from turing.generated.model.py_func_ensembler import PyFuncEnsembler
+from turing.generated.model.py_func_ensembler_all_of import PyFuncEnsemblerAllOf
+from turing.generated.model.resource_request import ResourceRequest
+from turing.generated.model.result_logger_type import ResultLoggerType
+from turing.generated.model.route import Route
+from turing.generated.model.router import Router
+from turing.generated.model.router_config import RouterConfig
+from turing.generated.model.router_details import RouterDetails
+from turing.generated.model.router_details_all_of import RouterDetailsAllOf
+from turing.generated.model.router_ensembler_config import RouterEnsemblerConfig
+from turing.generated.model.router_events import RouterEvents
+from turing.generated.model.router_id import RouterId
+from turing.generated.model.router_id_and_version import RouterIdAndVersion
+from turing.generated.model.router_id_object import RouterIdObject
+from turing.generated.model.router_status import RouterStatus
+from turing.generated.model.router_version import RouterVersion
+from turing.generated.model.router_version_config import RouterVersionConfig
+from turing.generated.model.router_version_config_log_config import RouterVersionConfigLogConfig
+from turing.generated.model.router_version_log_config import RouterVersionLogConfig
+from turing.generated.model.router_version_status import RouterVersionStatus
+from turing.generated.model.save_mode import SaveMode
+from turing.generated.model.traffic_rule import TrafficRule
+from turing.generated.model.traffic_rule_condition import TrafficRuleCondition
