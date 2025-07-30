d964d90d29c20d66a793b48ea48dd347ce522e13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:23 +0000

    Add package sunset-mcp-calculator/0.1.4/sunset_mcp_calculator-0.1.4-py3-none-any.whl

---
+++ packages/sunset-mcp-calculator/sunset_mcp_calculator-0.1.4-py3-none-any.whl/sunset_mcp_calculator/__init__.py
@@ -0,0 +1,12 @@
+import argparse
+from .sunset_mcp_calculator import mcp
+def main():
+    """MCP Wiki kel: Read Wikipedia articles and convert them to Markdown."""
+    parser = argparse.ArgumentParser(
+        description="Gives you the ability to read Wikipedia articles and convert them to Markdown."
+    )
+    parser.parse_args()
+    mcp.run()
+
+if __name__ == "__main__":
+    main()
