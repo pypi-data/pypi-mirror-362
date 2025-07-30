e1fc49b65981028ed16124f80df41253531ccc68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:50 +0000

    Add package jupyterlab-firefox-launcher/0.6.7/jupyterlab_firefox_launcher-0.6.7-py3-none-any.whl

---
+++ packages/jupyterlab-firefox-launcher/jupyterlab_firefox_launcher-0.6.7-py3-none-any.whl/jupyterlab_firefox_launcher-0.6.7.data/data/share/jupyter/labextensions/jupyterlab-firefox-launcher/static/lib_index_js.3155c0707dfc74b34cf6.js
@@ -0,0 +1,15 @@
+"use strict";
+(self["webpackChunkjupyterlab_firefox_launcher"] = self["webpackChunkjupyterlab_firefox_launcher"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ (() => {
+
+
+
+
+/***/ })
+
+}]);
