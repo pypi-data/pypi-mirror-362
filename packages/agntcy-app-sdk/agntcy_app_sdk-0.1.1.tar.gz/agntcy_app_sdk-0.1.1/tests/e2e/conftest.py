8f650580be4128e38f22500a44d284ad12106430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:06 +0000

    Add package agntcy-app-sdk/0.1.1/agntcy_app_sdk-0.1.1.tar.gz

---
+++ packages/agntcy-app-sdk/agntcy_app_sdk-0.1.1.tar.gz/agntcy_app_sdk-0.1.1/tests/e2e/conftest.py
@@ -0,0 +1,43 @@
+# Copyright AGNTCY Contributors (https://github.com/agntcy)
+# SPDX-License-Identifier: Apache-2.0
+
+import subprocess
+import os
+import signal
+import time
+import pytest
+
+TRANSPORT_CONFIGS = {
+    "A2A": "http://localhost:9999",
+    "NATS": "localhost:4222",
+    "SLIM": "http://localhost:46357",
+}
+
+
+@pytest.fixture
+def run_server():
+    procs = []
+
+    def _run(transport, endpoint):
+        cmd = [
+            "uv",
+            "run",
+            "python",
+            "tests/server/__server__.py",
+            "--transport",
+            transport,
+            "--endpoint",
+            endpoint,
+        ]
+
+        proc = subprocess.Popen(cmd, preexec_fn=os.setsid)
+
+        procs.append(proc)
+        time.sleep(1)
+        return proc
+
+    yield _run
+
+    for proc in procs:
+        if proc.poll() is None:
+            os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
