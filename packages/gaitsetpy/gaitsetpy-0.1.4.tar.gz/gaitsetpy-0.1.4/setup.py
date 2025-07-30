6443f6108ff4cbc474527d56c110d135c882007c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:15 +0000

    Add package gaitsetpy/0.1.4/gaitsetpy-0.1.4.tar.gz

---
+++ packages/gaitsetpy/gaitsetpy-0.1.4.tar.gz/gaitsetpy-0.1.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# Read the README.md file for the long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='gaitsetpy',
+    version='0.1.4',
+    packages=find_packages(include=["gaitsetpy", "gaitsetpy.*"]),
+    description="A Python package for gait analysis using sensor data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Alohomora Labs",
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'requests',
+        'statsmodels',
+        'matplotlib',
+        'scikit-learn',
+        'joblib',
+    ],
+
+)
