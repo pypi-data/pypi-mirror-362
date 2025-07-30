67ed5bc504c872b7a796f6ccc8d26922b9fe6721
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:57 +0000

    Add package phrlibrary/1.0.1/phrlibrary-1.0.1.tar.gz

---
+++ packages/phrlibrary/phrlibrary-1.0.1.tar.gz/phrlibrary-1.0.1/phrlibrary/__init__.py
@@ -0,0 +1,11 @@
+from .core import hello_world, calculate_sum, DataProcessor, game, GamePy
+
+
+__version__ = "1.0.1"
+__mine__ = "phrlibrary"
+__library__ = __mine__ + " " + __version__
+__ini__ = None
+
+game = game()
+gamepy = GamePy()
+dataprocessor = DataProcessor()
---
+++ packages/phrlibrary/phrlibrary-1.0.1.tar.gz/phrlibrary-1.0.1/setup.py
@@ -0,0 +1,26 @@
+﻿from setuptools import setup, find_packages
+
+
+setup(
+
+    name="phrlibrary",
+
+    version="1.0.1",
+
+    packages=find_packages(),
+
+    install_requires=[open("requirements.txt", "r").read()],
+    author="Phr",
+
+    description="A demo library",
+
+    long_description=open("README.md").read(),
+
+    python_requires=">=3.6",
+    scripts=["scripts/custom_scripts.py"],
+    entry_points={
+        "console_scripts": [
+            "phrtools=phrlibrary.module:main"
+        ]
+    }
+)
