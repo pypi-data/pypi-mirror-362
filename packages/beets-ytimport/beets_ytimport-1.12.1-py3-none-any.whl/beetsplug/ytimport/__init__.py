0c0fdfee6392b3fe07cfab2ec5a83cd75bd9ac02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:29 +0000

    Add package beets-ytimport/1.12.1/beets_ytimport-1.12.1-py3-none-any.whl

---
+++ packages/beets-ytimport/beets_ytimport-1.12.1-py3-none-any.whl/beetsplug/ytimport/__init__.py
@@ -0,0 +1,226 @@
+import os
+import re
+import pathlib
+import mediafile
+import requests
+from beets.plugins import BeetsPlugin
+from beets.dbcore import types
+from beets.ui import Subcommand
+from beets.ui import _store_dict
+from beets.ui.commands import import_files
+from beets import config
+from optparse import OptionParser
+from confuse import ConfigSource, load_yaml
+import beetsplug.ytimport.youtube
+import beetsplug.ytimport.split
+
+class YtImportPlugin(BeetsPlugin):
+    item_types = {
+        'like': types.BOOLEAN,
+        'yt_id': types.STRING,
+        'yt_source': types.STRING,
+        'yt_split': types.BOOLEAN,
+        'yt_likes': types.INTEGER,
+        'yt_dislikes': types.INTEGER,
+        'yt_views': types.INTEGER,
+        'yt_rating': types.INTEGER,
+    }
+
+    @property
+    def album_types(self):
+        return self.item_types
+
+    def __init__(self):
+        super(YtImportPlugin, self).__init__()
+        config_file_path = os.path.join(os.path.dirname(__file__), 'config_default.yaml')
+        source = ConfigSource(load_yaml(config_file_path) or {}, config_file_path)
+        self.config.add(source)
+        # Import additional fields from audio file into DB
+        for f in self.item_types.keys():
+            self.add_media_field(f, mediafile.MediaField(
+                mediafile.MP3DescStorageStyle(f),
+                mediafile.StorageStyle(f)
+            ))
+
+    def commands(self):
+
+        def run_import_cmd(lib, opts, args):
+            ytdir = os.path.expanduser(opts.directory)
+            headers = os.path.expanduser(opts.auth_headers)
+            cookiefile = os.path.expanduser(opts.cookiefile)
+            if headers:
+                with open(headers, 'r') as f:
+                    headers = f.read()
+            urls = [] + args
+            if opts.url_file:
+                add_urls = []
+                if re.match(r'^https?://', opts.url_file):
+                    add_urls = requests.get(opts.url_file).text.strip('\n').split('\n')
+                else:
+                    with open(opts.url_file, 'r') as f:
+                        add_urls = f.readlines()
+                urls += add_urls
+                self._log.info('Found {:n} URLs within URL file', len(add_urls))
+            singles_dir = os.path.join(ytdir, 'singles')
+            albums_dir = os.path.join(ytdir, 'albums')
+            pathlib.Path(singles_dir).mkdir(parents=True, exist_ok=True)
+            pathlib.Path(albums_dir).mkdir(parents=False, exist_ok=True)
+            if opts.likes:
+                if opts.url_file or len(args) > 0:
+                    raise Exception('ytimport: using --likes option in conjunction with --url-file or URL args is not supported!')
+                # TODO: mark like as tag within downloaded file already (to be able to distinguish likes also on local reimport)
+                self._log.info('Obtaining your liked songs from Youtube...')
+                if not headers:
+                    self._log.info('Using interactive authentication. To enable non-interactive authentication, set --auth-headers')
+                if not opts.oauth_client_id or not opts.oauth_client_secret:
+                    raise Exception('ytimport: neither auth_headers nor oauth_client_id and oauth_client_secret configured, see https://ytmusicapi.readthedocs.io/en/stable/setup/oauth.html')
+                auth = youtube.login(
+                    headers=headers,
+                    oauth_client_id=opts.oauth_client_id,
+                    oauth_client_secret=opts.oauth_client_secret,
+                )
+                likedIds = youtube.likes(auth, opts.max_likes)
+                if len(likedIds) > opts.max_likes:
+                    likedIds = likedIds[:opts.max_likes]
+                self._log.info('Found {:n} liked songs', len(likedIds))
+                urls += ['https://www.youtube.com/watch?v='+id for id in likedIds]
+            if not opts.reimport:
+                unknown_urls = [u for u in urls if not lib.items('comments:'+u)]
+                self._log.info('Ignoring {:n} known URLs', len(urls)-len(unknown_urls))
+                urls = unknown_urls
+            if urls:
+                self._log.info('Downloading {:n} song(s) to {:s}', len(urls), ytdir)
+                h = {}
+                # TODO: authenticate download requests.
+                # The following makes download requests return a 400 reponse.
+                # Maybe a cookiefile with some picked cookies from the headers can be generated?
+                #if opts.auth and headers:
+                #    h = dict([l.split(': ', 1) for l in headers.strip().split('\n')[1:]])
+                youtube.download(urls, ytdir, cookiefile, format=opts.format, min_len=opts.min_length, max_len=opts.max_length, max_len_nochapter=opts.max_length_nochapter, split=opts.split_tracks, like=opts.likes, reimport=opts.reimport, auth_headers=h)
+            else:
+                self._log.info('Nothing to download')
+            if opts.do_import:
+                self._log.info('Importing downloaded songs into beets library')
+                if opts.group_albums:
+                    self._import_files(lib, opts, albums_dir, False)
+                    self._import_files(lib, opts, singles_dir, True)
+                else:
+                    self._import_files(lib, opts, ytdir, True)
+            else:
+                self._log.info('Skipping import')
+
+        p = OptionParser()
+        p.add_option('--directory', type='string', metavar='DIR',
+            default=self.config['directory'].get(),
+            dest='directory', help='directory to download Youtube files to')
+        p.add_option('--format', type='string', metavar='FORMAT',
+            default=self.config['format'].get(),
+            dest='format', help='preferred file format')
+        p.add_option('--auth-headers', type='string', metavar='FILE',
+            default=self.config['auth_headers'].get(),
+            dest='auth_headers', help="path to a file containing the HTTP headers of an authenticated POST request to music.youtube.com, copied from your browser's development tool")
+        p.add_option('--cookiefile', type='string', metavar='FILE',
+            default=self.config['cookiefile'].get(),
+            dest='cookiefile', help='path to a file containing the cookies for a logged in user on music.youtube.com in Netscape format')
+        p.add_option('--url-file', type='string', metavar='URL',
+            default=self.config['url_file'].get(),
+            dest='url_file', help='URL/path to a file containing a download URL per line')
+        p.add_option('--likes', action='store_true',
+            default=self.config['likes'].get(),
+            dest='likes', help='download liked songs')
+        p.add_option('--no-likes', action='store_false',
+            default=self.config['likes'].get(),
+            dest='likes', help="don't download liked songs")
+        p.add_option('--max-likes', type='int', metavar='LIKES',
+            default=self.config['max_likes'].get(),
+            dest='max_likes', help='maximum number of likes to obtain')
+        p.add_option('--split-tracks', action='store_true',
+            default=self.config['split_tracks'].get(),
+            dest='split_tracks', help='split tracks by chapter')
+        p.add_option('--no-split-tracks', action='store_false',
+            default=self.config['split_tracks'].get(),
+            dest='split_tracks', help="don't split tracks")
+        p.add_option('--group-albums', action='store_true',
+            default=self.config['group_albums'].get(),
+            dest='group_albums', help='import split tracks as albums')
+        p.add_option('--no-group-albums', action='store_false',
+            default=self.config['group_albums'].get(),
+            dest='group_albums', help="don't import split tracks as albums")
+        p.add_option('--import', action='store_true',
+            default=self.config['import'].get(),
+            dest='do_import', help='import downloaded songs into beets')
+        p.add_option('--no-import', action='store_false',
+            default=self.config['import'].get(),
+            dest='do_import', help="don't import downloaded songs into beets")
+        p.add_option('--reimport', action='store_true',
+            default=self.config['reimport'].get(),
+            dest='reimport', help="re-download and re-import tracks")
+        p.add_option('--no-reimport', action='store_false',
+            default=self.config['reimport'].get(),
+            dest='reimport', help="don't re-download and re-import tracks")
+        p.add_option('--set', type='string', metavar='KEY=VALUE',
+            default={k: str(v.get()) for k,v in self.config['set'].items()},
+            action="callback",
+            callback=_store_dict,
+            dest='set', help='set a field on import, using KEY=VALUE format')
+        p.add_option('--min-length', type='int', metavar='SECONDS',
+            default=self.config['min_length'].get(),
+            dest='min_length', help='minimum track length in seconds')
+        p.add_option('--max-length', type='int', metavar='SECONDS',
+            default=self.config['max_length'].get(),
+            dest='max_length', help='maximum track length in seconds')
+        p.add_option('--max-length-nochapter', type='int', metavar='SECONDS',
+            default=self.config['max_length_nochapter'].get(),
+            dest='max_length_nochapter', help='max track length in seconds when no chapters defined')
+        p.add_option('--oauth-client-id', type='string',
+            default=os.environ.get('BEETS_YTIMPORT_OAUTH_CLIENT_ID') or self.config['oauth_client_id'].get(),
+            dest='oauth_client_id', help='OAuth client ID [BEETS_YTIMPORT_OAUTH_CLIENT_ID]')
+        p.add_option('--oauth-client-secret', type='string',
+            default=os.environ.get('BEETS_YTIMPORT_OAUTH_CLIENT_SECRET') or self.config['oauth_client_secret'].get(),
+            dest='oauth_client_secret', help='OAuth client secret [BEETS_YTIMPORT_OAUTH_CLIENT_SECRET]')
+        p.add_option('-q', '--quiet', action='store_true',
+            default=False,
+            dest='quiet', help="don't prompt for input when importing")
+        p.add_option('--quiet-fallback', type='string', metavar='skip|asis',
+            default=('quiet_fallback' in self.config and self.config['quiet_fallback'].get() or None),
+            dest='quiet_fallback', help='decision in quiet mode when there is no strong match')
+        p.add_option('--pretend', action='store_true',
+            default=False,
+            dest='pretend', help="don't import but print the files when importing")
+
+        c = Subcommand('ytimport', parser=p, help='import songs from Youtube')
+        c.func = run_import_cmd
+        return [c]
+
+    def _import_files(self, lib, opts, src_dir, singletons):
+        user_incremental = config['import']['incremental'].get()
+        user_resume = config['import']['resume'].get()
+        user_move = config['import']['move'].get()
+        user_quiet = config['import']['quiet'].get()
+        user_quiet_fallback = config['import']['quiet_fallback'].get()
+        user_group_albums = config['import']['group_albums'].get()
+        user_singletons = config['import']['singletons'].get()
+        user_pretend = config['import']['pretend'].get()
+        user_set_fields = {k: v.get() for k,v in config['import']['set_fields'].items()}
+        try:
+            config['import']['incremental'] = not opts.reimport
+            config['import']['resume'] = True
+            config['import']['move'] = True
+            config['import']['pretend'] = opts.pretend
+            config['import']['group_albums'] = False
+            config['import']['singletons'] = singletons
+            config['import']['set_fields'] = opts.set
+            config['import']['quiet'] = opts.quiet
+            if opts.quiet_fallback:
+                config['import']['quiet_fallback'] = opts.quiet_fallback
+            import_files(lib, [src_dir], None)
+        finally: # revert global import configuration changes
+            config['import']['incremental'] = user_incremental
+            config['import']['resume'] = user_resume
+            config['import']['move'] = user_move
+            config['import']['pretend'] = user_pretend
+            config['import']['group_albums'] = user_group_albums
+            config['import']['singletons'] = user_singletons
+            config['import']['set_fields'] = user_set_fields
+            config['import']['quiet'] = user_quiet
+            config['import']['quiet_fallback'] = user_quiet_fallback
---
+++ packages/beets-ytimport/beets_ytimport-1.12.1-py3-none-any.whl/beetsplug/ytimport/youtube.py
@@ -0,0 +1,160 @@
+import os
+import re
+import json
+import pathlib
+import ytmusicapi
+import yt_dlp
+from beetsplug.ytimport.split import chapters2tracks
+from beetsplug.ytimport.safename import safe_name
+
+uploader_as_artist_rule = r'uploader:^(?P<artist>.+?)( (Official|Oficial))?( +- +Topic)?$'
+title_extraction_rule = r'title:(?i)^(\[[^\]]+\])? *((?P<track_number>0[1-9])\.? +)?(?P<artist>[^(]+?) +(-+|–|\||~|) +(?P<title>.+?) *((\(|\[)(HD|HQ|Official|FREE|video|lyrics?|music video)( .+|[^\w].*))?$|'
+
+def login(headers=None, oauth_client_id=None, oauth_client_secret=None):
+    if headers:
+        return ytmusicapi.setup(headers_raw=headers)
+    else:
+        a = ytmusicapi.setup_oauth(
+            client_id=oauth_client_id,
+            client_secret=oauth_client_secret,
+        )
+        return json.dumps(a.as_dict())
+
+def likes(auth, max_tracks):
+    yt = ytmusicapi.YTMusic(auth)
+    likes = yt.get_liked_songs(max_tracks)
+    return [t['videoId'] for t in likes['tracks'] if t['likeStatus'] == 'LIKE']
+
+class RenamePP(yt_dlp.postprocessor.PostProcessor):
+    def run(self, info):
+        oldname = info['filepath']
+        dir = os.path.dirname(oldname)
+        artist = info['artist']
+        artist = safe_name(artist)
+        title = safe_name(info['title'])
+        newname = '{} - {} [{}].{}'.format(artist, title, info['id'], info['ext'])
+        newname = os.path.join(dir, newname)
+        if newname != oldname:
+            self.to_screen("Renaming '{}' to '{}'".format(oldname, newname))
+            os.rename(oldname, newname)
+            info['filename'] = newname
+        return [], info
+
+class SplitChaptersToTracksPP(yt_dlp.postprocessor.PostProcessor):
+
+    def __init__(self, max_length_nochapter):
+        super(SplitChaptersToTracksPP, self).__init__()
+        self.max_length_nochapter = max_length_nochapter
+
+    def run(self, info):
+        fname = info['filename']
+        self.to_screen('Splitting ' + fname)
+        dir = os.path.join(os.path.dirname(fname), '..', 'albums')
+        pathlib.Path(dir).mkdir(parents=True, exist_ok=True)
+        dest_dir = os.path.join(dir, os.path.basename(os.path.splitext(fname)[0]))
+        if chapters2tracks(fname, dest_dir):
+            os.remove(fname)
+        return [], info
+
+def download(urls, target_dir, cookiefile, format='bestaudio/best', min_len=60, max_len=7200, max_len_nochapter=900, split=False, like=False, reimport=False, auth_headers={}):
+
+    def download_filter(info):
+        duration = info.get('duration')
+        chapters = info.get('chapters')
+        if duration and duration < min_len:
+            return 'Track is too short'
+        if duration and duration > max_len:
+            return 'Track is too long'
+        if duration and duration > max_len_nochapter and (not chapters or len(chapters) < 2):
+            return 'Track is too long and has no chapters'
+
+    transform_rules = [
+        # Add Youtube URL and original title to comment field.
+        # This is to preserve the information when importing it into beets where it could be useful for disambiguation later.
+        '%(webpage_url)s %(title)s:%(meta_comment)s',
+        # Use uploader name without suffix as artist tag.
+        uploader_as_artist_rule,
+        # Extract track number and artist from title tag.
+        title_extraction_rule,
+        # Use artist as album_artist
+        r'artist:^(?P<album_artist>.+?)$',
+        # Trim quotes from title
+        r'title:^(“|")(?P<title>[^“”"]+)(“|”|")$|',
+        # Trim trailing dot from title
+        r'title:^(?P<title>.+[^0-9])\.$|',
+        # Add additional Youtube fields to the file's metadata.
+        '%(id)s:%(meta_yt_id)s',
+        '%(webpage_url_domain)s:%(meta_yt_source)s',
+        r'webpage_url_domain:^(?P<meta_yt_source>[^\.]+?)\.[^\.]+$|',
+        '%(like_count)s:%(meta_yt_likes)s',
+        '%(dislike_count)s:%(meta_yt_dislikes)s',
+        '%(view_count)s:%(meta_yt_views)s',
+        '%(average_rating)s:%(meta_yt_rating)s',
+        '%(release_date>%Y-%m-%d,upload_date>%Y-%m-%d)s:%(meta_publish_date)s',
+    ]
+    if like:
+        transform_rules += ['1:%(meta_like)s']
+    ytdl_opts = {
+        'outtmpl': target_dir+'/singles/%(artist|)s%(artist& - |)s%(title)s [%(id)s].%(ext)s',
+        'format': format,
+        'continuedl': True,
+        'restrictfilenames': True,
+        'windowsfilenames': True,
+        'trim_file_name': 120,
+        'writethumbnail': True,
+        'keepvideo': False,
+        'ignoreerrors': True,
+        'nooverwrites': True,
+        'retries': 10,
+        'match_filter': download_filter,
+        'process': True,
+        'force_generic_extractor': False,
+        'postprocessors': [
+            { # Corresponds to the -x CLI option.
+                'key': 'FFmpegExtractAudio',
+                'nopostoverwrites': True
+                # Don't specify preferredcodec to prevent transcoding lossy audio
+            },
+            { # Corresponds to the --parse-metadata CLI option.
+                'key': 'MetadataFromField',
+                'formats': transform_rules,
+            },
+            { # Corresponds to the --add-metadata CLI option.
+                'key': 'FFmpegMetadata',
+            },
+            { # Corresponds to the --embed-thumbnail CLI option.
+                'key': 'EmbedThumbnail',
+            },
+            { # Corresponds to --xattrs CLI option.
+                'key': 'XAttrMetadata',
+            },
+        ],
+        'cookiefile': cookiefile,
+    }
+    if not reimport:
+        ytdl_opts['download_archive'] = target_dir+'/ytdownloads.txt'
+    if len(auth_headers) > 0:
+        ytdl_opts['http_headers'] = auth_headers
+    pathlib.Path(target_dir).mkdir(parents=True, exist_ok=True)
+    ydl = yt_dlp.YoutubeDL(ytdl_opts)
+    if split:
+        ydl.add_post_processor(RenamePP(), when='post_process')
+        ydl.add_post_processor(SplitChaptersToTracksPP(max_len_nochapter), when='post_process')
+    ydl.download(urls)
+
+#yt-dlp --batch-file=/data/youtube-likes.urls --download-archive=youtube/.youtube-download.log \
+#  -ciwx --audio-format=m4a \
+#  -o '/data/youtube/%(title)s.%(ext)s' \
+#  --parse-metadata '%(original_url)s %(title)s:%(meta_comment)s' \
+#  --parse-metadata 'title:^(\[[^\]]+\] *)?((?P<track_number>0[1-9])\.? +)?(?P<artist>.+?) +(-+|–|:|\||~|) +(?P<title>.+?)(\((HD|HQ|Official)([^\)]+)?\)|( +\[(HD|Official)([^\]]+)?\]))? *$|' \
+#  --parse-metadata '%(like_count)s:%(meta_likes)s' \
+#  --parse-metadata '%(dislike_count)s:%(meta_dislikes)s' \
+#  --parse-metadata '%(view_count)s:%(meta_views)s' \
+#  --parse-metadata '%(average_rating)s:%(meta_rating)s' \
+#  --parse-metadata '%(release_date>%Y-%m-%d,upload_date>%Y-%m-%d)s:%(meta_publish_date)s' \
+#  --add-metadata \
+#  --embed-metadata \
+#  --embed-thumbnail \
+#  --xattrs --no-overwrites \
+#  --restrict-filenames --windows-filenames \
+#  --progress
