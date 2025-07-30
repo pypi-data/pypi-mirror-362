2c25dfd3775ebbf7e10e6cd963fc29b246d33dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:21 +0000

    Add package devisay/0.1.0/devisay-0.1.0.tar.gz

---
+++ packages/devisay/devisay-0.1.0.tar.gz/devisay-0.1.0/README.md
@@ -0,0 +1,12 @@
+# devi
+
+A simple Python package that prints funny messages with ASCII faces.
+
+## Usage
+
+```python
+from devi import devi
+
+devi.bullies()
+devi.greets()
+devi.beingfunny(
---
+++ packages/devisay/devisay-0.1.0.tar.gz/devisay-0.1.0/devisay/devisay.py
@@ -0,0 +1,30 @@
+class devisay:
+    @staticmethod
+    def bullies():
+        print("randemaga, you're a dissapointment to your parents.")
+        print(r"""
+  >:(
+ (o_o)
+ /| |\
+  / \
+""")
+
+    @staticmethod
+    def greets():
+        print("Bevarsi encha ulla")
+        print(r"""
+  (^_^)
+  (   )
+  /   \
+""")
+
+    @staticmethod
+    def beingfunny():
+        print("thangabali punkbali")
+        print(r"""
+  XD
+ ( ^.^ )
+  (   )
+""")
+        
+
