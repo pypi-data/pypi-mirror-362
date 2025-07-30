a8e1c385db67021a416950641c3acb9ded456780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:50 +0000

    Add package clipstitcher/0.2.5/clipstitcher-0.2.5-py3-none-any.whl

---
+++ packages/clipstitcher/clipstitcher-0.2.5-py3-none-any.whl/clipstitcher/__init__.py
@@ -0,0 +1,10 @@
+from .scene_objects import (
+    default_options, clear_cache,
+    Image, Html_page, Tweet, 
+    Video, Overlay, Scene_sequence,
+    LinearTransform, LinearTransition,
+    )
+
+from .client_sync import (
+    ClientPlayer,
+    )
