8657fd683b73f430d95592e632a2924a7197edca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:10:30 +0000

    Add package comet-mpm/1.3.0b13/comet_mpm-1.3.0b13-py3-none-any.whl

---
+++ packages/comet-mpm/comet_mpm-1.3.0b13-py3-none-any.whl/comet_mpm/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# *******************************************************
+#   ____                     _               _
+#  / ___|___  _ __ ___   ___| |_   _ __ ___ | |
+# | |   / _ \| '_ ` _ \ / _ \ __| | '_ ` _ \| |
+# | |__| (_) | | | | | |  __/ |_ _| | | | | | |
+#  \____\___/|_| |_| |_|\___|\__(_)_| |_| |_|_|
+#
+#  Sign up for free at http://www.comet.ml
+#  Copyright (C) 2021-2025 Comet ML INC
+#  This file can not be copied and/or distributed without the express
+#  permission of Comet ML Inc.
+# *******************************************************
+
+__all__ = ["CometMPM", "API"]
+
+from ._logging import _setup_comet_mpm_logging
+from ._version import __version__  # noqa: F401
+from .api import API
+from .comet_mpm import CometMPM
+
+_setup_comet_mpm_logging()
