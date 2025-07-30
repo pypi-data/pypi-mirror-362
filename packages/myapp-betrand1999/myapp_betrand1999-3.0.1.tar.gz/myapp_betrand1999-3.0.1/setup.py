d938bd9a6fa2ea87dc1d113c6599297a2d2adb9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:34 +0000

    Add package myapp-betrand1999/3.0.1/myapp_betrand1999-3.0.1.tar.gz

---
+++ packages/myapp-betrand1999/myapp_betrand1999-3.0.1.tar.gz/myapp_betrand1999-3.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myapp-betrand1999',
+    version='3.0.1', # each new release need update version
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+    entry_points={
+        'console_scripts': [
+            'myapp=myapp.app:main',  # assumes main() exists in app.py
+        ],
+    },
+)
