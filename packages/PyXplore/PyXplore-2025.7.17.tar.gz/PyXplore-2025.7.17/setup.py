abe9ebcab756e95c742c2e6a670d3e1b88451d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:38 +0000

    Add package PyXplore/2025.7.17/PyXplore-2025.7.17.tar.gz

---
+++ packages/PyXplore/PyXplore-2025.7.17.tar.gz/PyXplore-2025.7.17/setup.py
@@ -0,0 +1,52 @@
+import os
+from setuptools import setup, find_packages
+
+# 读取 README 内容
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='PyXplore',
+    version='2025.7.17',
+    description='Toolkit for refining crystal structures using WPEM',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Bin CAO',
+    author_email='bincao4-c@my.cityu.edu.hk',
+    maintainer='Bin CAO',
+    maintainer_email='bincao4-c@my.cityu.edu.hk',
+    license='MIT License',
+    url='https://github.com/Bin-Cao/PyWPEM',
+    packages=find_packages(include=['PyXplore', 'PyXplore.*']),
+    include_package_data=True,
+    package_data={
+        # 包含所有 .pdf、.cif、.ipynb 文件作为数据资源
+        'PyXplore': [
+            'refs/*.pdf',
+            '**/*.ipynb',
+            '**/*.cif'
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'pymatgen',
+    ],
+)
