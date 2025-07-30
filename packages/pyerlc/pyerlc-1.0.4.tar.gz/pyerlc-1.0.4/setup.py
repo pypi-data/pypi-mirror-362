96b8e04439694e889b083af8f3f61aa6d140122a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:08:00 +0000

    Add package pyerlc/1.0.4/pyerlc-1.0.4.tar.gz

---
+++ packages/pyerlc/pyerlc-1.0.4.tar.gz/pyerlc-1.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyerlc",
+    version="1.0.4",
+    description="Python wrapper for the ERLC PRC API",
+    author="epell Development",
+    author_email="epell1@epelldevelopment.xyz",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "colorama"
+    ],
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.12",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+)
