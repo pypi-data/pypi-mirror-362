6219d5bde3ac104f222c235e907d895a8139c362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:27 +0000

    Add package ob-metaflow-stubs/6.0.4.6rc1/ob-metaflow-stubs-6.0.4.6rc1.tar.gz

---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.4.6rc1.tar.gz/ob-metaflow-stubs-6.0.4.6rc1/metaflow-stubs/mf_extensions/outerbounds/plugins/apps/core/click_importer.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.21.2+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-16T22:24:21.796814                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from ......_vendor import click as metaflow_click
+from ......_vendor import click as click
+
---
+++ packages/ob-metaflow-stubs/ob-metaflow-stubs-6.0.4.6rc1.tar.gz/ob-metaflow-stubs-6.0.4.6rc1/metaflow-stubs/profilers/__init__.pyi
@@ -0,0 +1,12 @@
+######################################################################################################
+#                                 Auto-generated Metaflow stub file                                  #
+# MF version: 2.15.21.2+obcheckpoint(0.2.4);ob(v1)                                                   #
+# Generated on 2025-07-16T22:24:21.734871                                                            #
+######################################################################################################
+
+from __future__ import annotations
+
+
+from ..mf_extensions.outerbounds.profilers import gpu as gpu
+from ..mf_extensions.outerbounds.profilers.gpu import gpu_profile as gpu_profile
+
