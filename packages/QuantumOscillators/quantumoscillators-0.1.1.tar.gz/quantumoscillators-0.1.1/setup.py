74686273fb426956b8f37389a50fdd4b82c55491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:09:59 +0000

    Add package QuantumOscillators/0.1.1/quantumoscillators-0.1.1.tar.gz

---
+++ packages/QuantumOscillators/quantumoscillators-0.1.1.tar.gz/quantumoscillators-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='QuantumOscillators',
+    version='0.1.1',
+    packages=find_packages(),
+    description='A package that deals with Quantum Finance, Provides plotting visuals',
+    author='SaduPostMagazine',
+    author_email='sadupostrivista@gmail.com',
+)
