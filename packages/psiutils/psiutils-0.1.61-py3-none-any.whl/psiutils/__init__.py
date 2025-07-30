71ede78bab899774c2aef4ece9f6a039424d5119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:30 +0000

    Add package psiutils/0.1.61/psiutils-0.1.61-py3-none-any.whl

---
+++ packages/psiutils/psiutils-0.1.61-py3-none-any.whl/psiutils/__init__.py
@@ -0,0 +1,19 @@
+# from _version import __version__
+
+# version = __version__
+
+# Dialog Status
+YES = True
+NO = False
+CANCEL = None
+NULL = 0
+EXIT = 1
+OK = 2
+UPDATED = 3
+ERROR = 4
+
+# Modes
+VIEW = 0
+NEW = 1
+EDIT = 2
+DELETE = 3
