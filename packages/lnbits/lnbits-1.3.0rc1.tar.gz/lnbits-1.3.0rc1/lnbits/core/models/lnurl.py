d272cf89d35faeed493726760803cd9f1e9da1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:09:01 +0000

    Add package lnbits/1.3.0rc1/lnbits-1.3.0rc1.tar.gz

---
+++ packages/lnbits/lnbits-1.3.0rc1.tar.gz/lnbits-1.3.0rc1/lnbits/core/models/lnurl.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+from lnurl import Lnurl, LnurlPayResponse
+from pydantic import BaseModel
+
+
+class CreateLnurlPayment(BaseModel):
+    res: LnurlPayResponse
+    amount: int
+    comment: Optional[str] = None
+    unit: Optional[str] = None
+    internal_memo: Optional[str] = None
+
+
+class CreateLnurlWithdraw(BaseModel):
+    lnurl_w: Lnurl
