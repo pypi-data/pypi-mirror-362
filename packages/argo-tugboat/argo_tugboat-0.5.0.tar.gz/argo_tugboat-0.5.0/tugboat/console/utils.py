43bf0c37ceaf5237d128468170db0ab609b2419c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:56 +0000

    Add package argo-tugboat/0.5.0/argo_tugboat-0.5.0.tar.gz

---
+++ packages/argo-tugboat/argo_tugboat-0.5.0.tar.gz/argo_tugboat-0.5.0/tugboat/console/utils.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+import functools
+import io
+import sys
+import typing
+
+from tugboat.types import PathLike
+
+if typing.TYPE_CHECKING:
+    from collections.abc import Sequence
+    from typing import TextIO
+
+
+def format_loc(loc: Sequence[str | int]) -> str:
+    return "." + ".".join(map(str, loc))
+
+
+class CachedStdin(PathLike):
+    """A virtual path that reads from stdin and caches the content."""
+
+    def __init__(self):
+        super().__init__("<stdin>")
+
+    @property
+    def name(self) -> str:
+        return "<stdin>"
+
+    def is_file(self) -> bool:
+        return True
+
+    def is_dir(self) -> bool:
+        return False
+
+    @functools.cache  # noqa: B019
+    def read_text(self) -> str:
+        return sys.stdin.read()
+
+    def open(self, mode: str = "r") -> TextIO:
+        if "r" not in mode:
+            return NotImplemented  # pragma: no cover
+        return io.StringIO(self.read_text())
