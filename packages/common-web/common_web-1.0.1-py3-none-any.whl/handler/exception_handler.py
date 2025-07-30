7cbdaeebb57968239f0493a16a980e0e8e617c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:08:12 +0000

    Add package common-web/1.0.1/common_web-1.0.1-py3-none-any.whl

---
+++ packages/common-web/common_web-1.0.1-py3-none-any.whl/handler/exception_handler.py
@@ -0,0 +1,21 @@
+from dto.res_dto import ResObj
+from exception.auth_exception import AuthException
+from exception.business_exception import BusinessException
+
+SYSTEM_EXCEPTION_CODE = 999
+BUSINESS_EXCEPTION_CODE = 500
+AUTH_EXCEPTION_CODE = 400
+
+
+def process_exception(exception):
+    print('exception is:', exception)
+    if isinstance(exception, BusinessException):
+        code = BUSINESS_EXCEPTION_CODE
+        message = exception.value
+    elif isinstance(exception, AuthException):
+        code = AUTH_EXCEPTION_CODE
+        message = exception.value
+    else:
+        code = SYSTEM_EXCEPTION_CODE
+        message = "system error"
+    return ResObj(code=code, message=message)
---
+++ packages/common-web/common_web-1.0.1-py3-none-any.whl/utils/festival_util.py
@@ -0,0 +1,42 @@
+import holidays
+
+from constants.festival import FESTIVAL_DAY
+
+
+def get_holiday_date_by_year_name(year, festival_name):
+    """
+    Obtain the holiday(China) date of the current year based on the year and holiday name
+    根据年份以及节日名获取当年的节日日期
+    :param year:
+    :param festival_name: reference constants/festival
+    :return: date
+    """
+    cn_holidays = holidays.CN(language='zh_CN', years=int(year))
+    for date, name in cn_holidays.items():
+        if name == FESTIVAL_DAY.get(festival_name):
+            return date
+
+
+def get_holiday_by_date(date_str):
+    """
+    get holiday name by date
+    根据日期获取节假日名
+    :param date_str: date str
+    :return: holiday name
+    """
+    year = date_str.split('-')[0]
+    cn_holidays = holidays.CN(language='zh_CN', years=int(year))
+    for date, name in cn_holidays.items():
+        if str(date) == date_str:
+            return name
+
+
+def is_holiday(date_str):
+    """
+    check the date is holiday or not
+    :param date_str: date str
+    :return:
+    """
+    year = date_str.split('-')[0]
+    cn_holidays = holidays.CN(language='zh_CN', years=int(year))
+    return date_str in cn_holidays
