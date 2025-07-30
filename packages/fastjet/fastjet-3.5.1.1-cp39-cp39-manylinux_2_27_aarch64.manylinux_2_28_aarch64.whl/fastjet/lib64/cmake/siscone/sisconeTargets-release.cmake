9e4cfbc2ccceabc91409c5e20f2ed8f1edbd500d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:48 +0000

    Add package fastjet/3.5.1.1/fastjet-3.5.1.1-cp39-cp39-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/fastjet/fastjet-3.5.1.1-cp39-cp39-manylinux_2_27_aarch64.manylinux_2_28_aarch64.whl/fastjet/lib64/cmake/siscone/sisconeTargets-release.cmake
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "siscone::siscone" for configuration "Release"
+set_property(TARGET siscone::siscone APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(siscone::siscone PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libsiscone.so"
+  IMPORTED_SONAME_RELEASE "libsiscone.so"
+  )
+
+list(APPEND _cmake_import_check_targets siscone::siscone )
+list(APPEND _cmake_import_check_files_for_siscone::siscone "${_IMPORT_PREFIX}/lib64/libsiscone.so" )
+
+# Import target "siscone::siscone_spherical" for configuration "Release"
+set_property(TARGET siscone::siscone_spherical APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(siscone::siscone_spherical PROPERTIES
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libsiscone_spherical.so"
+  IMPORTED_SONAME_RELEASE "libsiscone_spherical.so"
+  )
+
+list(APPEND _cmake_import_check_targets siscone::siscone_spherical )
+list(APPEND _cmake_import_check_files_for_siscone::siscone_spherical "${_IMPORT_PREFIX}/lib64/libsiscone_spherical.so" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
