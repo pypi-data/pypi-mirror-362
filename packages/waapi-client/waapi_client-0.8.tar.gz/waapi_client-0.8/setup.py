304d6f5f3b0bb8b74b63682fa63b62b5cecc0dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:53 +0000

    Add package waapi-client/0.8/waapi_client-0.8.tar.gz

---
+++ packages/waapi-client/waapi_client-0.8.tar.gz/waapi_client-0.8/setup.py
@@ -0,0 +1,40 @@
+import os
+from distutils.core import setup
+
+from setuptools import find_packages
+
+# Dump the README.md file
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='waapi-client',
+    packages=find_packages(exclude=['waapi.test']),
+    test_suite='waapi.test',
+    install_requires=[
+      'autobahn'
+    ],
+    license='Apache License 2.0',
+    platforms=['any'],
+    scripts=[],
+    version='0.8',
+    description='Wwise Authoring API client.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Audiokinetic',
+    maintainer='Samuel Longchamps',
+    maintainer_email='slongchamps@audiokinetic.com',
+    url='https://github.com/audiokinetic/waapi-client-python',
+    download_url='https://github.com/audiokinetic/waapi-client-python/releases',
+    keywords=['waapi', 'wwise', 'audiokinetic'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 4 - Beta',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux'
+    ],
+)
