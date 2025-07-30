f94533a188d7e9cfb723e4b37c52af245668c476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:26 +0000

    Add package chainix/1.0.0/chainix-1.0.0.tar.gz

---
+++ packages/chainix/chainix-1.0.0.tar.gz/chainix-1.0.0/chainix/__init__.py
@@ -0,0 +1,4 @@
+from .main import AsyncChainClient
+
+__version__ = "1.0.0"
+__all__ = ["AsyncChainClient"]
---
+++ packages/chainix/chainix-1.0.0.tar.gz/chainix-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='chainix',
+    version='1.0.0',
+    author='Chainix',
+    author_email='jack@chainix.ai',
+    description='A client library for executing asynchronous chains on chainix.ai with custom function callbacks',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ChainixDev/chainix-python",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        'requests>=2.25.0',
+    ],
+)
