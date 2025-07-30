6730616dc41d6b066fa9dce90e5f6ce8594a4392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:26 +0000

    Add package osShared/0.1.2/osshared-0.1.2.tar.gz

---
+++ packages/osShared/osshared-0.1.2.tar.gz/osshared-0.1.2/osshared/response/responseBuilder.py
@@ -0,0 +1,38 @@
+"""
+Response Builder 정의
+"""
+from typing import Any, Optional
+from datetime import datetime
+from osshared.response.enum import enumResponseStatus
+from osshared.response.response import ResponseBase
+
+import uuid
+
+class ResponseBuilder:
+    @staticmethod
+    def buildOk(
+        data: Optional[Any] = None,
+        message: Optional[str] = "backend.common.ok",
+        traceid: Optional[str] = None,
+    ) -> ResponseBase:
+        return ResponseBase(
+            status=enumResponseStatus.OK,
+            message=message,
+            data=data,
+            traceid=traceid or str(uuid.uuid4()),
+            timestamp=datetime.now().isoformat(),
+        )
+
+    @staticmethod
+    def buildError(
+        message: Optional[str] = "backend.common.error",
+        errors: Optional[Any] = None,
+        traceid: Optional[str] = None,
+    ) -> ResponseBase:
+        return ResponseBase(
+            status=enumResponseStatus.ERROR,
+            message=message,
+            errors=errors,
+            traceid=traceid or str(uuid.uuid4()),
+            timestamp=datetime.now().isoformat(),
+        )
