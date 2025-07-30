958f1be395ebac749b0f0c824e541e22c8fd097a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:14 +0000

    Add package alfinx-utils/0.3.5/alfinx_utils-0.3.5.tar.gz

---
+++ packages/alfinx-utils/alfinx_utils-0.3.5.tar.gz/alfinx_utils-0.3.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="alfinx_utils",
+    version="0.3.5",
+    description="HTTP headers auto detection tool.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Maharram",
+    author_email="maharram@alfinx.com",
+    packages=find_packages(),
+    install_requires=[
+        "fastapi",
+        "httpx",
+    ],
+    python_requires='>=3.8',
+    zip_safe=False
+)
