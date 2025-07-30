f7272d5bbeb956302e09e42fa6fc45ab97e9e261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:10:03 +0000

    Add package pyenet-sc/0.1/pyenet_sc-0.1.tar.gz

---
+++ packages/pyenet-sc/pyenet_sc-0.1.tar.gz/pyenet_sc-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyenet_sc",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "pyenet>=1.3.17"
+    ]
+)
