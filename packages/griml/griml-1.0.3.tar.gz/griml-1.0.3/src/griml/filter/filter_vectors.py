06f5033880a9a201915855604364a9b4773ebaec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:09:53 +0000

    Add package griml/1.0.3/griml-1.0.3.tar.gz

---
+++ packages/griml/griml-1.0.3.tar.gz/griml-1.0.3/src/griml/filter/filter_vectors.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from griml.filter import filter_margin, filter_area
+from griml.load import load
+import geopandas as gpd
+from pathlib import Path
+import os
+
+def filter_vectors(inlist, margin_file, outdir=None, min_area=0.05):
+    '''Filter vectors by area and margin proximity
+
+    Parameters
+    ----------
+    inlist : list
+        List of either file paths of GeoDataFrame objects to filter
+    margin_file : str, geopandas.GeoSeries
+        Bufferred margin to perform margin proximity filter
+    outdir : str, optional
+        Output directory to write files to
+    min_area: int, optional
+        Threshold area (sq km) to filter by
+        
+    Returns
+    -------
+    filtered : list
+        List of filtered GeoDataFrame objects
+
+    '''
+    
+    # Load margin
+    margin_buff = load(margin_file)
+    
+    # Iterate through input list
+    count=1
+    filtered=[]
+    for infile in inlist:
+    
+        # Load and define name
+        if type(infile)==str:
+            print('\n'+str(count)+'/'+str(len(inlist)) +
+                  ': Filtering vectors in '+str(Path(infile).name))   
+            name = str(Path(infile).stem)+"_filtered.shp"
+            
+        else:
+            print('\n'+str(count)+'/'+str(len(inlist))) 
+            name = 'lakes_' + str(count) + "_filtered.shp"
+        
+        vectors = load(infile)
+
+        # Perform filtering steps
+        vectors = filter_area(vectors, min_area)
+        print(f'{vectors.shape[0]} features over 0.05 sq km')
+        
+        vectors = filter_margin(vectors, margin_buff)
+        print(f'{vectors.shape[0]} features within 500 m of margin')    
+
+        # Retain and save if vectors are present after filtering
+        if vectors.shape[0]>0:
+            if outdir is not None:
+                vectors.to_file(Path(outdir).joinpath(name))
+            filtered.append(vectors)
+        else:
+        	print('No vectors present after filter. Moving to next file.')
+        count=count+1
+
+    return filtered
+        
+
+
+if __name__ == "__main__": 
+    infile1 = os.path.join(os.path.dirname(griml.__file__),'test/test_filter.shp')
+    infile2 = os.path.join(os.path.dirname(griml.__file__),'test/test_icemask.shp')       
+    filter_vectors([infile1], infile2)
---
+++ packages/griml/griml-1.0.3.tar.gz/griml-1.0.3/src/griml/metadata/add_metadata.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from griml.load import load
+from griml.metadata import assign_id, assign_sources, assign_certainty, \
+    assign_names, assign_regions
+import geopandas as gpd
+
+def add_metadata(iml, names, regions, outfile=None):
+    '''Add all metadata information to inventory
+
+    Parameters
+    ----------
+    iml : geopandas.GeoDataFrame or str
+        Inventory GeoDataFrame object or filepath
+    names : geopandas.GeoDataFrame or str
+        Placenames database GeoDataFrame object or filepath
+    regions : geopandas.GeoDataFrame or str
+        Regions identifier GeoDataFrame object or filepath
+    outfile : str
+        Filepath for output to be saved to
+
+    Returns
+    -------
+    iml : geopandas.GeoDataFrame
+        Inventory GeoDataFrame with metadata
+    '''
+    iml = load(iml)
+    names = load(names)
+    regions = load(regions)
+    
+    print('Assigning ID...')
+    iml = assign_id(iml)
+        
+    print('Assigning sources...')
+    iml = assign_sources(iml)
+        
+    print('Assigning certainty scores...')
+    n = ['S1','S2','ARCTICDEM']
+    scores = [0.298, 0.398, 0.304]
+    iml = assign_certainty(iml, n, scores)
+
+    print('Assigning regions...')
+    iml = assign_regions(iml, regions)
+        
+    print('Assigning placenames...')
+    iml = assign_names(iml, names)
+    
+    if outfile:    
+        print('Saving file...')
+        iml.to_file(outfile)
+        print('Saved to '+str(outfile)+"_metadata.shp")
+
+    return iml
+        
+        
+if __name__ == "__main__": 
+    infile1 = os.path.join(os.path.dirname(griml.__file__),'test/test_merge_2.shp')
+    infile2 = os.path.join(os.path.dirname(griml.__file__),'test/test_placenames.shp')
+    infile3 = os.path.join(os.path.dirname(griml.__file__),'test/greenland_basins_polarstereo.shp')
+    add_metadata(infile1, infile2, infile3)
