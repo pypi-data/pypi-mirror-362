e2b406ea0c7dae6975196ec929c9ec01485a49ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:27 +0000

    Add package pyerlc/1.0.2/pyerlc-1.0.2.tar.gz

---
+++ packages/pyerlc/pyerlc-1.0.2.tar.gz/pyerlc-1.0.2/erlc_py/__init__.py
@@ -0,0 +1,13 @@
+"""
+PRC API Wrapper
+===============
+
+A Python wrapper for the Police Roleplay Community API.
+"""
+
+from .client import PRCClient
+from .exceptions import PRCError
+from .models import PRCResponse, ErrorCode
+
+__version__ = "1.0.0"
+__all__ = ["PRCClient", "PRCError", "PRCResponse", "ErrorCode"]
