291b4c139f39be7ee96c6090de8eeed5e07de198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:11 +0000

    Add package c-temp-api/0.0.1/c_temp_api-0.0.1-py3-none-any.whl

---
+++ packages/c-temp-api/c_temp_api-0.0.1-py3-none-any.whl/current_temp_api/base.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+class WeatherAPIBase(ABC):
+
+    def __init__(self, latitude, longitude, **kwargs):
+        pass
+
+    @abstractmethod
+    def get_current_temperature(self):
+        pass
+
