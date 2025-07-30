3f9a3eff85375ce467eea81fd2e4846cef26e3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:27 +0000

    Add package ASqrtC/3.0.1/asqrtc-3.0.1.tar.gz

---
+++ packages/ASqrtC/asqrtc-3.0.1.tar.gz/asqrtc-3.0.1/README.md
@@ -0,0 +1,13 @@
+# Accurate-Sqrt-Calculator (ASqrtC)
+ASqrtC is a fast and accurate square root calculator with no size limit.
+It delivers exact results to any specified number of decimal places.
+It is 100% accurate.
+ASqrtC outperforms Python’s built-in math and decimal modules in speed when calculating less then ~7500 decimal places.
+
+**How to Use**
+1. Install the package using pip.
+2. Import it in your script.
+3. Call ASqrtC.sqrt(number, decimal_places) — replacing number with the value you want the square root of, and decimal_places with how many digits after the decimal you'd like.
+4. That’s it! It’s that easy.
+
+**Note: This project does not support negative, imaginary, or complex numbers.**
---
+++ packages/ASqrtC/asqrtc-3.0.1.tar.gz/asqrtc-3.0.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+setup(
+    name='ASqrtC',
+    version='3.0.1',
+    description=
+    (
+     'ASqrtC is a fast square root calculator with 100% accuracy. '
+     'It delivers exact results to any specified number of decimal places. '
+     'ASqrtC outperforms Python’s built-in math and decimal modules in speed when calculating less then ~7500 decimal places.'
+    ),
+    py_modules=["ASqrtC"],
+    package_dir={'': 'src'},
+    author='Sebastian Trumbore',
+    author_email='Trumbore.Sebastian@gmail.com',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/SebastianTrumbore/ASqrtC',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[],
+    keywords=[
+        'square root', 'sqrt', 'math', 'numerical methods', 'accurate',
+        'precision', 'high precision', 'exact decimal', 'arbitrary precision',
+        'fast math', 'root calculator', 'big number math',
+        'scientific computing', 'math utilities', 'python3', 'educational',
+        'lightweight'
+    ],
+)
