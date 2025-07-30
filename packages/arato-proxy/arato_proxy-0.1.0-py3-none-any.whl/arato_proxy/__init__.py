07b1542935481cd299b6bf086afc9c6ac9afa9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:30:10 +0000

    Add package arato-proxy/0.1.0/arato_proxy-0.1.0-py3-none-any.whl

---
+++ packages/arato-proxy/arato_proxy-0.1.0-py3-none-any.whl/arato_proxy/__init__.py
@@ -0,0 +1,9 @@
+"""
+Arato LiteLLM Proxy Package
+
+A Python package for running LiteLLM proxy with Arato logging callbacks.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Arato"
+__email__ = "dev@arato.com"
---
+++ packages/arato-proxy/arato_proxy-0.1.0-py3-none-any.whl/arato_proxy/__main__.py
@@ -0,0 +1,4 @@
+from arato_proxy.cli import main
+
+if __name__ == "__main__":
+    main()
