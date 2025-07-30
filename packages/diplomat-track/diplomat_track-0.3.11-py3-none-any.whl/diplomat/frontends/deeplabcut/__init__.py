2172d1c6c42ab7062a624eb35261d5b455f655a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:56 +0000

    Add package diplomat-track/0.3.11/diplomat_track-0.3.11-py3-none-any.whl

---
+++ packages/diplomat-track/diplomat_track-0.3.11-py3-none-any.whl/diplomat/frontends/deeplabcut/__init__.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from diplomat.frontends import DIPLOMATFrontend, DIPLOMATCommands
+from diplomat.utils import colormaps
+
+class DEEPLABCUTFrontend(DIPLOMATFrontend):
+    """
+    The DEEPLABCUT frontend for DIPLOMAT. Contains functions for running DIPLOMAT on DEEPLABCUT projects.
+    """
+    @classmethod
+    def init(cls) -> Optional[DIPLOMATCommands]:
+        try:
+            from diplomat.frontends.deeplabcut._verify_func import _verify_dlc_like
+            from diplomat.frontends.deeplabcut.load_model import load_model
+            from diplomat.frontends.deeplabcut.convert_tracks import _dlc_hdf_to_diplomat_table
+        except ImportError:
+            return None
+
+        return DIPLOMATCommands(
+            _verifier=_verify_dlc_like,
+            _load_model=load_model,
+            _load_tracks=_dlc_hdf_to_diplomat_table,
+        )
+
+    @classmethod
+    def get_package_name(cls) -> str:
+        return "deeplabcut"
+
+
