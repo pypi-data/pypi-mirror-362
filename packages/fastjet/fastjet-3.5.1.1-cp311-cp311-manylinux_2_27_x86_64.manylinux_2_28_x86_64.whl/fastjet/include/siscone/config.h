304f49d9e305c56709a3dd7bf0733d1470e2260e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:08:33 +0000

    Add package fastjet/3.5.1.1/fastjet-3.5.1.1-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/fastjet/fastjet-3.5.1.1-cp311-cp311-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/fastjet/include/siscone/config.h
@@ -0,0 +1,13 @@
+/* config.h.in for CMake build */
+
+/* Define to the full name of this package. */
+#define SISCONE_PACKAGE_NAME "SISCone"
+
+/* Define to the full name and version of this package. */
+#define SISCONE_PACKAGE_STRING "SISCone 3.1.2"
+
+/* Version number of package */
+#define SISCONE_VERSION "3.1.2"
+
+/* use unique_ptr instead of auto_ptr */
+#define SISCONE_USES_UNIQUE_PTR_AS_AUTO_PTR
