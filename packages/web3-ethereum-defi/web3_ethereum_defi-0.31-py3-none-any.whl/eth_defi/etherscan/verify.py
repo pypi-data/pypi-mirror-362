d8bc2ddd4c94a46508e45495f056c32bab9689f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:06:38 +0000

    Add package web3-ethereum-defi/0.31/web3_ethereum_defi-0.31-py3-none-any.whl

---
+++ packages/web3-ethereum-defi/web3_ethereum_defi-0.31-py3-none-any.whl/eth_defi/etherscan/verify.py
@@ -0,0 +1,9 @@
+"""Etherscan integration.
+
+
+.. note ::
+
+    Currently no Web3.py direct integration available.
+
+Use :py:func:`eth_defi.foundry.forge.deploy_contract_with_forge` to deploy contracts that are verified on Etherscan.
+"""
