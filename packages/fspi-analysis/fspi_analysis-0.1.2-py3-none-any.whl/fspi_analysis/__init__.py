81243fc69a37cb51b4a6810e796d610f94278bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 17:08:27 +0000

    Add package fspi-analysis/0.1.2/fspi_analysis-0.1.2-py3-none-any.whl

---
+++ packages/fspi-analysis/fspi_analysis-0.1.2-py3-none-any.whl/fspi_analysis/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.2"
+
+# src/fspi_analysis/__init__.py
+
+# ---- Start of new debug block ---- Used this to debug the import issue
+# import sys
+# print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+# print("DEBUG: TOP LEVEL OF src/fspi_analysis/__init__.py IS BEING EXECUTED NOW!")
+# print(f"DEBUG: Current time: {__import__('datetime').datetime.now()}")
+# sys.stdout.flush() # Force output to terminal
