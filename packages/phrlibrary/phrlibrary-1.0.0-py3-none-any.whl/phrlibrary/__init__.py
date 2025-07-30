d5c6f494e8cd9e082bc9d2ea40b1fdb900e177c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:24 +0000

    Add package phrlibrary/1.0.0/phrlibrary-1.0.0-py3-none-any.whl

---
+++ packages/phrlibrary/phrlibrary-1.0.0-py3-none-any.whl/phrlibrary/__init__.py
@@ -0,0 +1,4 @@
+﻿from .core import hello_world  # 暴露核心接口
+
+
+__version__ = "1.0.0"
---
+++ packages/phrlibrary/phrlibrary-1.0.0-py3-none-any.whl/phrlibrary/core.py
@@ -0,0 +1,28 @@
+﻿def hello_world():
+
+    """示例函数：返回欢迎信息"""
+
+    return "Hello from mylibrary!"
+
+
+
+def calculate_sum(a, b):
+
+    """演示带类型提示的函数"""
+
+    return a + b
+
+
+
+class DataProcessor:
+
+    """示例类：数据处理工具"""
+
+    def __init__(self, multiplier=1):
+
+        self.multiplier = multiplier
+    
+    
+def process(self, x):
+
+        return x * self.multiplier
