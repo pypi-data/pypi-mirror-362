91e7202c708b916b8ba9d9ccf15fe08c37648af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 19:10:21 +0000

    Add package qtliz/0.1.16/qtliz-0.1.16-py3-none-any.whl

---
+++ packages/qtliz/qtliz-0.1.16-py3-none-any.whl/qtlizlib/model/menuActionTool.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+from PySide6.QtWidgets import QMenu
+
+from qtlizlib.domain.menuActionTool import InstanceAction
+from qtlizlib.handler.menuAction import ToolbarHandler
+from qtlizlib.util.qtlizLogger import logger
+
+
+
+
