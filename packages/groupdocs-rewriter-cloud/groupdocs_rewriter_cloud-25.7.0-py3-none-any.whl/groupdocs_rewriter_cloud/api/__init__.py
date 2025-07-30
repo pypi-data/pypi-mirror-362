8beecd6eb18d5e301b81dfa17fc74769cc789c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:07:30 +0000

    Add package groupdocs-rewriter-cloud/25.7.0/groupdocs_rewriter_cloud-25.7.0-py3-none-any.whl

---
+++ packages/groupdocs-rewriter-cloud/groupdocs_rewriter_cloud-25.7.0-py3-none-any.whl/groupdocs_rewriter_cloud/api/__init__.py
@@ -0,0 +1,16 @@
+# flake8: noqa
+
+# import apis into api package
+from groupdocs_rewriter_cloud.api.compare_api import CompareApi
+from groupdocs_rewriter_cloud.api.detect_api import DetectApi
+from groupdocs_rewriter_cloud.api.estimate_api import EstimateApi
+from groupdocs_rewriter_cloud.api.file_api import FileApi
+from groupdocs_rewriter_cloud.api.formalize_api import FormalizeApi
+from groupdocs_rewriter_cloud.api.generate_api import GenerateApi
+from groupdocs_rewriter_cloud.api.info_api import InfoApi
+from groupdocs_rewriter_cloud.api.paraphrase_api import ParaphraseApi
+from groupdocs_rewriter_cloud.api.simplify_api import SimplifyApi
+from groupdocs_rewriter_cloud.api.summarize_api import SummarizeApi
+from groupdocs_rewriter_cloud.api.swagger_api import SwaggerApi
+from groupdocs_rewriter_cloud.api.synonymize_api import SynonymizeApi
+
