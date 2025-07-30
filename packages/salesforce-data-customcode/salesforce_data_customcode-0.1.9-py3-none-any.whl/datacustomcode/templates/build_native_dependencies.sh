d305a4052b907bf9fb9cbdb867ca06adc22de694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 00:07:51 +0000

    Add package salesforce-data-customcode/0.1.9/salesforce_data_customcode-0.1.9-py3-none-any.whl

---
+++ packages/salesforce-data-customcode/salesforce_data_customcode-0.1.9-py3-none-any.whl/datacustomcode/templates/build_native_dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Description: build native dependencies
+
+python3.11 -m venv --copies .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+venv-pack -o native_dependencies.tar.gz -f
