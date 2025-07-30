03d00c400fe5341efc49ee6d447feb358dec14d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:44 +0000

    Add package amplicon-coverage-inspector/1.15.250702/amplicon_coverage_inspector-1.15.250702.tar.gz

---
+++ packages/amplicon-coverage-inspector/amplicon_coverage_inspector-1.15.250702.tar.gz/amplicon_coverage_inspector-1.15.250702/tests/test_aci.py
@@ -0,0 +1,33 @@
+import pytest
+import subprocess
+
+
+def run_command(cmd):
+    stdout = subprocess.PIPE
+    stderr = subprocess.PIPE
+
+    proc = subprocess.Popen(cmd, shell=True, stdout=stdout, stderr=stderr)
+    _, err = proc.communicate()
+
+    if proc.returncode != 0:
+        raise RuntimeError(f"FAILED: {cmd}\n{err}")
+
+
+def test_aci():
+    """test aci"""
+    cmd = (
+        "python -m aci.aci -b tests/data/test.bam -d tests/data/test.bed -o tests -t 1"
+    )
+    run_command(cmd)
+
+
+def test_version():
+    """test aci"""
+    cmd = "python -m aci.aci -v"
+    run_command(cmd)
+
+
+def test_help():
+    """test aci"""
+    cmd = "python -m aci.aci -h"
+    run_command(cmd)
