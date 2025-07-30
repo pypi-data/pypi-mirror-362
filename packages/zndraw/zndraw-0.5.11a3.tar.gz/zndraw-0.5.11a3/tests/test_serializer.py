487bc77690b4f45623487c36e9275ec1ebc57166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:37 +0000

    Add package zndraw/0.5.11a3/zndraw-0.5.11a3.tar.gz

---
+++ packages/zndraw/zndraw-0.5.11a3.tar.gz/zndraw-0.5.11a3/tests/test_serializer.py
@@ -0,0 +1,214 @@
+import ase
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.testing as npt
+import znjson
+from ase.calculators.singlepoint import SinglePointCalculator
+from ase.constraints import FixAtoms
+import rdkit2ase
+import plotly.graph_objects as go
+
+from zndraw import Figure
+from zndraw.converter import ASEConverter
+import pytest
+
+
+@pytest.fixture
+def atoms():
+    """Create a simple ASE Atoms object for testing."""
+    return ase.Atoms("H2", positions=[[0, 0, 0], [0, 0, 1]])
+
+
+@pytest.fixture
+def atoms_x_info_str(atoms):
+    """Create an ASE Atoms object with additional info."""
+    atoms.info["key"] = "value"
+    return atoms
+
+
+@pytest.fixture
+def atoms_x_info_nested_dict(atoms):
+    """Create an ASE Atoms object with additional info."""
+    atoms.info["key"] = {"subkey": "subvalue"}
+    atoms.info["nested"] = {"key1": "value1", "key2": {"subkey": "subvalue"}, "key3": [1, 2, 3]}
+    return atoms
+
+
+@pytest.fixture
+def atoms_x_info_nested_list(atoms):
+    """Create an ASE Atoms object with a list in info."""
+    atoms.info["key"] = ["value1", "value2"]
+    atoms.info["nested"] = [{"subkey": "subvalue"}, [1, 2, 3]]
+    return atoms
+
+
+@pytest.fixture
+def atoms_x_info_nested_objects(atoms):
+    """Create an ASE Atoms object with nested objects in info."""
+    fig, ax = plt.subplots()
+    ax.plot([1, 2, 3], [1, 2, 3])
+
+    atoms.info["nested"] = {
+        "lvl1": {
+            "lvl2": {
+                "lvl3": {
+                    "array": np.array([1, 2, 3]),
+                    "figure": Figure.from_matplotlib(fig),
+                }
+            }
+        }
+    }
+    return atoms
+
+
+@pytest.fixture
+def atoms_x_info_figure(atoms):
+    """Create an ASE Atoms object with a Figure in info."""
+    fig, ax = plt.subplots()
+    ax.plot([1, 2, 3], [1, 2, 3])
+    atoms.info["figure"] = Figure.from_matplotlib(fig)
+    return atoms
+
+@pytest.fixture
+def atoms_x_info_plotly(atoms):
+    """Create an ASE Atoms object with a Plotly figure in info."""
+    fig = go.Figure(data=go.Scatter(x=[1, 2, 3], y=[1, 2, 3]))
+    atoms.info["plotly_figure"] = fig
+    return atoms
+
+
+@pytest.fixture
+def atoms_x_info_array(atoms):
+    """Create an ASE Atoms object with an array in info."""
+    atoms.info["array"] = np.array([1, 2, 3])
+    return atoms
+
+@pytest.fixture
+def atoms_x_info_np_generic(atoms):
+    """Create an ASE Atoms object with a numpy generic type in info."""
+    atoms.info["generic"] = np.int64(42)
+    return atoms
+
+@pytest.fixture
+def atoms_x_info_znsocket(atoms):
+    """Create an ASE Atoms object with a znsocket in info."""
+    
+
+@pytest.fixture
+def atoms_x_connectivity():
+    """Create an ASE Atoms object with connectivity info."""
+    atoms = rdkit2ase.smiles2atoms("CCO")
+    assert "connectivity" in atoms.info
+    return atoms
+
+
+@pytest.fixture
+def atoms_x_calc(atoms):
+    """Create an ASE Atoms object with a calculator."""
+    atoms.calc = SinglePointCalculator(atoms, energy=0.0, forces=np.zeros((2, 3)))
+    return atoms
+
+
+@pytest.fixture
+def atoms_x_constraints(atoms):
+    """Create an ASE Atoms object with constraints."""
+    atoms.set_constraint(FixAtoms([0]))
+    return atoms
+
+
+@pytest.fixture
+def atoms_x_arrays(atoms):
+    """Create an ASE Atoms object with additional arrays."""
+    atoms.arrays["colors"] = np.array(["#ff0000", "#00ff00"])
+    atoms.arrays["radii"] = np.array([0.3, 0.4])
+    return atoms
+
+@pytest.fixture
+def atoms_x_arrays_figure(atoms):
+    """Create an ASE Atoms object with a Figure in arrays."""
+    fig, ax = plt.subplots()
+    ax.plot([1, 2, 3], [1, 2, 3])
+    atoms.arrays["figure"] = [Figure.from_matplotlib(fig) for _ in range(len(atoms))]
+    return atoms
+
+@pytest.fixture
+def atoms_x_arrays_plotly(atoms):
+    """Create an ASE Atoms object with a Plotly figure in arrays."""
+    fig = go.Figure(data=go.Scatter(x=[1, 2, 3], y=[1, 2, 3]))
+    atoms.arrays["plotly_figure"] = [fig for _ in range(len(atoms))]
+    return atoms
+
+@pytest.fixture
+def atoms_x_cell(atoms):
+    """Create an ASE Atoms object with a cell."""
+    atoms.set_cell([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    atoms.set_pbc([True, True, False])
+    return atoms
+
+
+@pytest.mark.parametrize(
+    "myatoms",
+    [
+        "atoms_x_info_str",
+        "atoms_x_info_nested_dict",
+        "atoms_x_info_nested_list",
+        "atoms_x_info_nested_objects",
+        "atoms_x_info_figure",
+        "atoms_x_info_array",
+        "atoms_x_info_np_generic",
+        "atoms_x_info_plotly",
+        "atoms_x_connectivity",
+        "atoms_x_calc",
+        "atoms_x_constraints",
+        "atoms_x_arrays",
+        "atoms_x_arrays_figure",
+        "atoms_x_arrays_plotly",
+        "atoms_x_cell",
+    ],
+)
+def test_serialization(myatoms, request):
+    """Test serialization and deserialization of ASE Atoms objects."""
+    atoms = request.getfixturevalue(myatoms)
+    serialized = znjson.dumps(atoms, cls=znjson.ZnEncoder.from_converters([ASEConverter]))
+    deserialized = znjson.loads(serialized, cls=znjson.ZnDecoder.from_converters([ASEConverter]))
+
+    npt.assert_array_equal(atoms.get_atomic_numbers(), deserialized.get_atomic_numbers())
+    npt.assert_array_equal(atoms.get_positions(), deserialized.get_positions())
+    npt.assert_array_equal(atoms.get_pbc(), deserialized.get_pbc())
+    npt.assert_array_equal(atoms.get_cell(), deserialized.get_cell())
+
+    for key in atoms.info:
+        npt.assert_equal(atoms.info[key], deserialized.info[key])
+    for key in deserialized.info:
+        npt.assert_equal(atoms.info[key], deserialized.info[key])
+    for key in atoms.arrays:
+        npt.assert_equal(atoms.arrays[key], deserialized.arrays[key])
+    for key in deserialized.arrays:
+        npt.assert_equal(atoms.arrays[key], deserialized.arrays[key])
+    if atoms.calc is not None:
+        for key in atoms.calc.results:
+            npt.assert_equal(
+                atoms.calc.results[key], deserialized.calc.results[key]
+            )
+    if deserialized.calc is not None:
+        for key in deserialized.calc.results:
+            npt.assert_equal(
+                atoms.calc.results[key], deserialized.calc.results[key]
+            )
+    if atoms.constraints:
+        for a, b in zip(atoms.constraints, deserialized.constraints):
+            assert repr(a) == repr(b)
+
+    if deserialized.constraints:
+        for a, b in zip(atoms.constraints, deserialized.constraints):
+            assert repr(a) == repr(b)
+
+
+def test_unsupported_type():
+    """Test serialization of an unsupported type."""
+    atoms = ase.Atoms("H2", positions=[[0, 0, 0], [0, 0, 1]])
+    fig, ax = plt.subplots()
+    ax.plot([1, 2, 3], [1, 2, 3])
+    atoms.info["unsupported"] = fig  # Matplotlib figure is not supported
+    with pytest.raises(TypeError):
+        znjson.dumps(atoms, cls=znjson.ZnEncoder.from_converters([ASEConverter]))
---
+++ packages/zndraw/zndraw-0.5.11a3.tar.gz/zndraw-0.5.11a3/zndraw/figure.py
@@ -0,0 +1,60 @@
+import base64
+import dataclasses
+import io
+
+import matplotlib.pyplot as plt
+import znjson
+
+
+@dataclasses.dataclass(frozen=True)
+class Figure:
+    """Visualize a file or a matplotlib figure.
+
+
+    Parameters
+    ----------
+    path : str | None
+        Path to the image file.
+    figure : plt.Figure | None
+        Matplotlib figure object.
+
+    Examples
+    --------
+    >>> import matplotlib.pyplot as plt
+    >>> from zndraw import ZnDraw, Figure
+    >>> vis = ZnDraw(url="http://localhost:8000", token="test_token")
+    >>> fig, ax = plt.subplots()
+    >>> ax.plot([1, 2, 3], [1, 2, 3])
+    >>> vis.figures["mtpl"] = Figure.from_matplotlib(fig)
+    >>> vis.figures["png"] = Figure.from_path("path/to/image.png")
+    """
+
+    base64: str | None = None
+
+    @classmethod
+    def from_path(cls, path: str) -> "Figure":
+        """Create a Figure from a file path."""
+        with open(path, "rb") as image_file:
+            content = base64.b64encode(image_file.read()).decode("utf-8")
+        return cls(base64=content)
+
+    @classmethod
+    def from_matplotlib(cls, figure: plt.Figure) -> "Figure":
+        """Create a Figure from a matplotlib figure."""
+        buf = io.BytesIO()
+        figure.savefig(buf, format="png")
+        buf.seek(0)
+        content = base64.b64encode(buf.read()).decode("utf-8")
+        return cls(base64=content)
+
+
+class FigureConverter(znjson.ConverterBase):
+    level = 100
+    instance = Figure
+    representation = "zndraw.Figure"
+
+    def encode(self, obj: Figure) -> dict:
+        return {"base64": obj.base64}
+
+    def decode(self, value: dict) -> Figure:
+        return Figure(base64=value["base64"])
