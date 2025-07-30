4538fbb193fa7e295f9a49720cf2d3f196e50282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:23 +0000

    Add package audreylab-summarypostqc/0.1.1/audreylab_summarypostqc-0.1.1-py3-none-any.whl

---
+++ packages/audreylab-summarypostqc/audreylab_summarypostqc-0.1.1-py3-none-any.whl/audreylab_summarypostqc/__init__.py
@@ -0,0 +1,13 @@
+"""
+AudreyLab-SummaryPostQC
+
+A Python CLI tool to generate QQ and Manhattan plots from GWAS summary statistics.
+Developed by Etienne Kabongo, Audrey Grant Lab, McGill University.
+"""
+
+__version__ = "0.1.1"
+__author__ = "Etienne Kabongo"
+__email__ = "etienne.kabongo@mcgill.ca"
+__license__ = "MIT"
+__all__ = []  # Optionnel : à remplir si tu exposes des fonctions
+
