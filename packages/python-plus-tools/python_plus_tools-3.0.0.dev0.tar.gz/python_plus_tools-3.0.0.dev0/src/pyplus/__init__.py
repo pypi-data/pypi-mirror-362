1866a290b72b27163840d8b040c1bba3a52fe783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:26:59 +0000

    Add package python-plus-tools/3.0.0.dev0/python_plus_tools-3.0.0.dev0.tar.gz

---
+++ packages/python-plus-tools/python_plus_tools-3.0.0.dev0.tar.gz/python_plus_tools-3.0.0.dev0/src/pyplus/__init__.py
@@ -0,0 +1,26 @@
+"""
+The python Plus - Pyplus
+================
+the python's plus library.\n
+"""
+
+from .core import *
+from .tools import *
+from . import tools, science
+
+if get_config("import.lazy", True, ALL):
+    class LazyImport:
+        def __init__(self, module_name):
+            self.module_name = module_name
+            self._module = None
+
+        def __getattr__(self, name):
+            from importlib import import_module
+            if self._module is None:
+                self._module = import_module(self.module_name)
+            return getattr(self._module, name)
+    
+    advancedlib = LazyImport("advancedlib.all")
+    modulellib = LazyImport("modulelib")
+
+__version__ = get_version("main")
