84286dc67a6b2299002b8011db11aac263994f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:39 +0000

    Add package ecss-chat-client/1.3.37/ecss_chat_client-1.3.37-py3-none-any.whl

---
+++ packages/ecss-chat-client/ecss_chat_client-1.3.37-py3-none-any.whl/ecss_chat_client/types/rooms.py
@@ -0,0 +1,28 @@
+from enum import StrEnum
+
+
+class RoomTypes(StrEnum):
+    """Типы комнат в чате."""
+
+    DIRECT = 'd'
+    PRIVATE = 'p'
+    TELECONFERENCE = 'tc'
+    SUPERGROUP = 's'
+    TOPIC = 't'
+    THREAD_ROOM = 'thread'
+
+
+class RoomSyncShowType(StrEnum):
+    """Типы показа комнат в синхронизации комнат."""
+
+    ONLY_OPEN = 'except'
+    HIDDEN = 'only'
+    ALL = 'include'
+
+
+class RoomSettings(StrEnum):
+    """Настройки комнаты."""
+
+    ACTIVATE_READ_ONLY = 'ro'
+    ACTIVATE_IS_CHANNEL = 'isChannel'
+    ACTIVATE_REACTION = 'react'
