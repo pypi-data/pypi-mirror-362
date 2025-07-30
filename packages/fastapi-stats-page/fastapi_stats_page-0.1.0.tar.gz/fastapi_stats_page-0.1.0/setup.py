bdc7a7c6107a09279dea20a7f01c36bb0ebf7db9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:17 +0000

    Add package fastapi-stats-page/0.1.0/fastapi_stats_page-0.1.0.tar.gz

---
+++ packages/fastapi-stats-page/fastapi_stats_page-0.1.0.tar.gz/fastapi_stats_page-0.1.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme:
+    docs = readme.read()
+
+setup(
+    name="fastapi-stats-page",
+    version="0.1.0",
+    description="FastAPI visitor statistics with middleware and stats page",
+    author="Jackson Rodger",
+    long_description=docs,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "fastapi",
+        "jinja2",
+        "uvicorn",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: FastAPI",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
