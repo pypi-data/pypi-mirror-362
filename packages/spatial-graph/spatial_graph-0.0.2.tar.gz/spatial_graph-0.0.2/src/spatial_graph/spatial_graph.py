d2f13b3493dbec6d5674918e727b7dd0dfc17302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:26 +0000

    Add package spatial-graph/0.0.2/spatial_graph-0.0.2.tar.gz

---
+++ packages/spatial-graph/spatial_graph-0.0.2.tar.gz/spatial_graph-0.0.2/src/spatial_graph/spatial_graph.py
@@ -0,0 +1,99 @@
+from typing import ClassVar
+
+import numpy as np
+
+from .dtypes import DType
+from .graph import Graph
+from .rtree import LineRTree, PointRTree
+
+
+class SpatialGraph(Graph):
+    edge_inclusion_values: ClassVar[list[str]] = ["incident", "leaving", "entering"]
+
+    def __init__(
+        self,
+        ndims,
+        node_dtype,
+        node_attr_dtypes,
+        edge_attr_dtypes,
+        position_attr,
+        directed=False,
+    ):
+        if position_attr not in node_attr_dtypes:
+            raise ValueError(
+                f"position attribute {position_attr!r} not defined in "
+                "'node_attr_dtypes'"
+            )
+        super().__init__(node_dtype, node_attr_dtypes, edge_attr_dtypes, directed)
+
+        self.ndims = ndims
+        self.position_attr = position_attr
+        self.coord_dtype = DType(node_attr_dtypes[position_attr]).base
+        self._node_rtree = PointRTree(node_dtype, self.coord_dtype, ndims)
+        self._edge_rtree = LineRTree(f"{node_dtype}[2]", self.coord_dtype, ndims)
+
+    def add_node(self, node, **kwargs):
+        position = self._get_position(kwargs)
+        self._node_rtree.insert_point_item(node, position)
+        super().add_node(node, **kwargs)
+
+    def add_nodes(self, nodes, **kwargs):
+        positions = self._get_position(kwargs)
+        self._node_rtree.insert_point_items(nodes, positions)
+        super().add_nodes(nodes, **kwargs)
+
+    def add_edge(self, edge, **kwargs):
+        edge = np.array(edge, dtype=self.node_dtype)
+        position_u = getattr(self.node_attrs[edge[0]], self.position_attr)
+        position_v = getattr(self.node_attrs[edge[1]], self.position_attr)
+        self._edge_rtree.insert_line(edge, position_u, position_v)
+        super().add_edge(edge, **kwargs)
+
+    def add_edges(self, edges, **kwargs):
+        starts = getattr(self.node_attrs[edges[:, 0]], self.position_attr)
+        ends = getattr(self.node_attrs[edges[:, 1]], self.position_attr)
+        self._edge_rtree.insert_lines(edges, starts, ends)
+        super().add_edges(edges, **kwargs)
+
+    @property
+    def roi(self):
+        return self._node_rtree.bounding_box()
+
+    def query_nodes_in_roi(self, roi):
+        return self._node_rtree.search(roi[0], roi[1])
+
+    def query_edges_in_roi(self, roi):
+        return self._edge_rtree.search(roi[0], roi[1])
+
+    def query_nearest_nodes(self, point, k, return_distances=False):
+        return self._node_rtree.nearest(point, k, return_distances)
+
+    def query_nearest_edges(self, point, k, return_distances=False):
+        return self._edge_rtree.nearest(point, k, return_distances)
+
+    @property
+    def nodes(self):
+        return super().nodes()
+
+    @property
+    def edges(self):
+        return self.query_edges_in_roi(self.roi)
+
+    def remove_nodes(self, nodes):
+        positions = getattr(self.node_attrs[nodes], self.position_attr)
+        self._node_rtree.delete_items(nodes, positions)
+        if not self.directed:
+            edges = self.edges_by_nodes(nodes)
+        else:
+            edges = np.concatenate(
+                self.in_edges_by_nodes(nodes), self.out_edges_by_nodes(nodes)
+            )
+        positions_u = getattr(self.node_attrs[edges[:, 0]], self.position_attr)
+        positions_v = getattr(self.node_attrs[edges[:, 1]], self.position_attr)
+        self._edge_rtree.delete_items(edges, positions_u, positions_v)
+        super().remove_nodes(nodes)
+
+    def _get_position(self, kwargs):
+        if self.position_attr in kwargs:
+            return kwargs[self.position_attr]
+        raise RuntimeError(f"position attribute '{self.position_attr}' not given")
---
+++ packages/spatial-graph/spatial_graph-0.0.2.tar.gz/spatial_graph-0.0.2/tests/test_spatial_graph.py
@@ -0,0 +1,99 @@
+import numpy as np
+import pytest
+
+import spatial_graph as sg
+
+node_dtypes = ["int8", "uint8", "int16", "uint16"]
+node_attr_dtypes = [
+    {"position": "double[4]"},
+    {"position": "int[4]"},
+]
+edge_attr_dtypes = [
+    {},
+    {"score": "float64"},
+    {"score": "float64", "color": "uint8"},
+]
+
+
+@pytest.mark.parametrize("node_dtype", node_dtypes)
+@pytest.mark.parametrize("node_attr_dtypes", node_attr_dtypes)
+@pytest.mark.parametrize("edge_attr_dtypes", edge_attr_dtypes)
+@pytest.mark.parametrize("directed", [True, False])
+def test_construction(node_dtype, node_attr_dtypes, edge_attr_dtypes, directed):
+    sg.SpatialGraph(
+        ndims=4,
+        node_dtype=node_dtype,
+        node_attr_dtypes=node_attr_dtypes,
+        edge_attr_dtypes=edge_attr_dtypes,
+        position_attr="position",
+        directed=directed,
+    )
+
+
+def test_roi_query():
+    graph = sg.SpatialGraph(
+        ndims=3,
+        node_dtype="uint64",
+        node_attr_dtypes={"position": "double[3]"},
+        edge_attr_dtypes={"score": "float32"},
+        position_attr="position",
+        directed=False,
+    )
+
+    graph.add_nodes(
+        np.array([1, 2, 3, 4, 5], dtype="uint64"),
+        position=np.array(
+            [
+                [0.1, 0.1, 0.1],
+                [0.2, 0.2, 0.2],
+                [0.3, 0.3, 0.3],
+                [0.4, 0.4, 0.4],
+                [0.5, 0.5, 0.5],
+            ],
+            dtype="double",
+        ),
+    )
+
+    graph.add_edges(
+        np.array([[1, 2], [3, 4], [5, 1]], dtype="uint64"),
+        score=np.array([0.2, 0.3, 0.4], dtype="float32"),
+    )
+
+    nodes = graph.query_nodes_in_roi(np.array([[0.0, 0.0, 0.0], [0.25, 0.25, 0.25]]))
+    edges = graph.query_edges_in_roi(np.array([[0.0, 0.0, 0.0], [0.25, 0.25, 0.25]]))
+
+    assert list(sorted(nodes)) == [1, 2]
+    np.testing.assert_array_equal(edges, [[1, 2], [5, 1]])
+
+    # query in a ROI that does not contain any nodes/edges
+
+    nodes = graph.query_nodes_in_roi(np.array([[1.0, 1.0, 1.0], [1.25, 1.25, 1.25]]))
+    edges = graph.query_edges_in_roi(np.array([[1.0, 1.0, 1.0], [1.25, 1.25, 1.25]]))
+
+    assert len(nodes) == 0
+    assert len(edges) == 0
+
+
+def test_delete():
+    graph = sg.SpatialGraph(
+        ndims=3,
+        node_dtype="uint64",
+        node_attr_dtypes={"position": "double[3]"},
+        edge_attr_dtypes={"score": "float32"},
+        position_attr="position",
+        directed=False,
+    )
+    nodes = np.arange(0, 100_000).astype("uint64")
+    graph.add_nodes(
+        nodes,
+        position=np.random.random(size=(100_000, 3)).astype("double"),
+    )
+    edges = np.random.randint(0, 100_000, size=(10_000, 2)).astype("uint64")
+    graph.add_edges(
+        edges,
+        score=np.random.random(size=(10_000,)).astype("float32"),
+    )
+
+    graph.remove_nodes(nodes[:1000])
+
+    assert len(graph) == 99_000
