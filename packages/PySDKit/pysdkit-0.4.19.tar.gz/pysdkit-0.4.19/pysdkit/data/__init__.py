f858a150fd86d1b4b376687bc3d3ea7f7beb18b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:39 +0000

    Add package PySDKit/0.4.19/pysdkit-0.4.19.tar.gz

---
+++ packages/PySDKit/pysdkit-0.4.19.tar.gz/pysdkit-0.4.19/pysdkit/data/__init__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2024/7/22 22:56
+@author: Whenxuan Wang
+@email: wwhenxuan@gmail.com
+"""
+
+# A series of functions for generating 1D NumPy signals
+from ._generator import generate_sin_signal, generate_cos_signal
+from ._generator import (
+    generate_square_wave,
+    generate_triangle_wave,
+    generate_sawtooth_wave,
+)
+from ._generator import generate_am_signal, generate_exponential_signal
+
+# Generator for 1D univariate time series data
+from ._time_series import generate_time_series
+
+# Generate the main test sample signal
+from ._generator import test_emd
+
+# Generate the test function for HHT
+from ._generator import test_hht
+
+# Generator for 1D univariate
+from ._generator import test_univariate_signal
+
+# Generator for 1D multivariate
+from ._generator import test_multivariate_signal
+
+# add Gaussian noise
+from ._add_noise import add_noise
+
+# Generate a grid matrix given an input and output range
+from ._image import get_meshgrid_2D
+
+# Test case for loading a 2D grayscale image
+from ._image import test_grayscale
+
+# Test case for univariate 2D image
+from ._image import test_univariate_image
+
+# Test case for multivariate 2D image
+from ._image import test_multivariate_image
+
+# Test case for univariate 3D cube
+from ._cube import test_univariate_cube
+
+# Test case for multivariate 3D cube
+from ._cube import test_multivariate_cube
