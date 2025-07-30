8c1dda7b3f964d2b5e98337f7d65fbda9fd600a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:22 +0000

    Add package ol-openedx-otel-monitoring/0.1.1/ol_openedx_otel_monitoring-0.1.1-py3-none-any.whl

---
+++ packages/ol-openedx-otel-monitoring/ol_openedx_otel_monitoring-0.1.1-py3-none-any.whl/ol_openedx_otel_monitoring/__init__.py
@@ -0,0 +1,7 @@
+"""
+This is an integration of OTel Monitoring
+"""
+
+__version__ = "0.1.1"
+
+default_app_config = "ol_openedx_otel_monitoring.apps.OTelMonitoringConfig"  # pylint: disable=invalid-name
