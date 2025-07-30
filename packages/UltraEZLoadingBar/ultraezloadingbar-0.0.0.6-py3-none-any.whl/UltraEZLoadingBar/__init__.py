281fbe4e3909318a77a0f463d124ee500969ace6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:45:09 +0000

    Add package UltraEZLoadingBar/0.0.0.6/ultraezloadingbar-0.0.0.6-py3-none-any.whl

---
+++ packages/UltraEZLoadingBar/ultraezloadingbar-0.0.0.6-py3-none-any.whl/UltraEZLoadingBar/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+versionnumber = "0.0.0.7"
+
+from .Core import loading_bar # Loading Bar Function
+from .Core import new_line # New Line Function
+from .Core import colored_text # Colored Text Function
+from .Core import generatekey # Generate Key Function
+from .Core import enhancedloading_bar # Enhanced Loading Bar
+from .Core import cls # Clear Screen Function
+from .Core import version # Version Variable
