ed43390168f227f4ccac28fe862d6db63a3f01f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:39 +0000

    Add package nkunyim-util/0.1.8/nkunyim_util-0.1.8-py3-none-any.whl

---
+++ packages/nkunyim-util/nkunyim_util-0.1.8-py3-none-any.whl/nkunyim_util/models/access_model.py
@@ -0,0 +1,55 @@
+from typing import List, Optional
+from pydantic import BaseModel
+from uuid import UUID
+
+
+
+class ItemModel(BaseModel):
+    _id: UUID
+    seq: int
+    id: UUID
+    name: str
+    title: str
+    caption: str
+    icon: str
+    path: str
+    route: str
+    tags: Optional[str]
+    perms: Optional[List[str]]
+
+
+class MenuModel(BaseModel):
+    _id: UUID
+    node: str
+    seq: int
+    id: UUID
+    name: str
+    title: str
+    caption: str
+    icon: str
+    path: str
+    route: str
+    colour: str
+    tags: Optional[str]
+    items: Optional[List[ItemModel]]
+
+
+class NavModel(BaseModel):
+    node: str
+    menus: List[MenuModel]
+ 
+
+class RoleModel(BaseModel):
+    id: UUID
+    name: str
+    title: str
+    is_admin: str
+       
+    
+class AccessModel(BaseModel):
+    id: UUID
+    role: Optional[RoleModel]
+    name: str
+    tags: str
+    navs: Optional[List[NavModel]]
+    uix: dict
