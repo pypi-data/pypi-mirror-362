a1162589c94549c667b4cc0d5355ecaf337d5565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 09:10:05 +0000

    Add package optype/0.12.0/optype-0.12.0-py3-none-any.whl

---
+++ packages/optype/optype-0.12.0-py3-none-any.whl/optype/numpy/__init__.py
@@ -0,0 +1,45 @@
+# ruff: noqa: F403
+from . import (
+    _any_array,
+    _any_dtype,
+    _array,
+    _dtype,
+    _is,
+    _literals,
+    _scalar,
+    _sequence_nd,
+    _shape,
+    _to,
+    _ufunc,
+    compat,
+    ctypeslib,
+    random,
+)
+from ._any_array import *
+from ._any_dtype import *
+from ._array import *
+from ._dtype import *
+from ._is import *
+from ._literals import *
+from ._scalar import *
+from ._sequence_nd import *
+from ._shape import *
+from ._to import *
+from ._ufunc import *
+
+__all__ = ["compat", "ctypeslib", "random"]
+__all__ += _literals.__all__
+__all__ += _array.__all__
+__all__ += _any_array.__all__
+__all__ += _sequence_nd.__all__
+__all__ += _dtype.__all__
+__all__ += _any_dtype.__all__
+__all__ += _shape.__all__
+__all__ += _is.__all__
+__all__ += _to.__all__
+__all__ += _ufunc.__all__
+__all__ += _scalar.__all__
+
+
+def __dir__() -> list[str]:
+    return __all__
