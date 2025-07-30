dab6e87931706eac0dcd0c325a2ea72cd379e469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:08:33 +0000

    Add package mseep-3xpl-mcp/0.1.0/mseep_3xpl_mcp-0.1.0.tar.gz

---
+++ packages/mseep-3xpl-mcp/mseep_3xpl_mcp-0.1.0.tar.gz/mseep_3xpl_mcp-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mseep-3xpl-mcp',
+    version='0.1.0',
+    description='MCP server for interacting with 48 blockchains via 3xpl JSON API',
+    long_description="Package managed by MseeP.ai",
+    long_description_content_type="text/plain",
+    author='mseep',
+    author_email='support@skydeck.ai',
+    maintainer='mseep',
+    maintainer_email='support@skydeck.ai',
+    url='https://github.com/mseep',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['mcp[cli]>=1.6.0'],
+    keywords=['mseep'],
+)
