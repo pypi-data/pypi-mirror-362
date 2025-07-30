d763a18d48d42cce8b1e5d5439b89e92a329bee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:48 +0000

    Add package scrapy-zen/0.9.8/scrapy_zen-0.9.8-py3-none-any.whl

---
+++ packages/scrapy-zen/scrapy_zen-0.9.8-py3-none-any.whl/scrapy_zen/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.9.8"
+
+from w3lib.url import canonicalize_url
+
+
+def normalize_url(value: str) -> str:
+    if "http://" in value or "https://" in value:
+        return canonicalize_url(value)
+    return value
