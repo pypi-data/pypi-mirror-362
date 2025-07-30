388c39dc836eb17213541aba2e10897a5d424219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:43 +0000

    Add package kintera/0.7.6/kintera-0.7.6-cp310-cp310-macosx_14_0_arm64.whl

---
+++ packages/kintera/kintera-0.7.6-cp310-cp310-macosx_14_0_arm64.whl/kintera/kintera/kinetics/coagulation.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+// C/C++
+#include <set>
+
+// torch
+#include <torch/nn/cloneable.h>
+#include <torch/nn/functional.h>
+#include <torch/nn/module.h>
+#include <torch/nn/modules/common.h>
+#include <torch/nn/modules/container/any.h>
+
+// kintera
+#include "arrhenius.hpp"
+
+namespace kintera {
+
+struct CoagulationOptions : public ArrheniusOptions {
+  CoagulationOptions() = default;
+  CoagulationOptions(const ArrheniusOptions& arrhenius)
+      : ArrheniusOptions(arrhenius) {}
+};
+
+void add_to_vapor_cloud(std::set<std::string>& vapor_set,
+                        std::set<std::string>& cloud_set,
+                        CoagulationOptions op);
+
+}  // namespace kintera
