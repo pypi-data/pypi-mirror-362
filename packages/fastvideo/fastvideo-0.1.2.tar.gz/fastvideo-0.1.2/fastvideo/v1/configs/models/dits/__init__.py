4b6a8eb127f166345bc80006ce6261a875b3e632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:34:04 +0000

    Add package fastvideo/0.1.2/fastvideo-0.1.2.tar.gz

---
+++ packages/fastvideo/fastvideo-0.1.2.tar.gz/fastvideo-0.1.2/fastvideo/v1/configs/models/dits/__init__.py
@@ -0,0 +1,5 @@
+from fastvideo.v1.configs.models.dits.hunyuanvideo import HunyuanVideoConfig
+from fastvideo.v1.configs.models.dits.stepvideo import StepVideoConfig
+from fastvideo.v1.configs.models.dits.wanvideo import WanVideoConfig
+
+__all__ = ["HunyuanVideoConfig", "WanVideoConfig", "StepVideoConfig"]
