1a2cda5f03ad544da744193a6b630a3d3849b3ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:33:59 +0000

    Add package tigo-python/0.1.6/tigo_python-0.1.6-py3-none-any.whl

---
+++ packages/tigo-python/tigo_python-0.1.6-py3-none-any.whl/tigo_python/__init__.py
@@ -0,0 +1,24 @@
+# tigo_python/__init__.py
+"""
+A modern Python library for interacting with the Tigo Energy API.
+
+This library provides easy access to Tigo Energy's monitoring API,
+allowing users to retrieve system information, performance data,
+and analytics for their solar installations.
+"""
+
+from .client import TigoClient
+from .auth import TigoAuthenticator
+from .exceptions import TigoAPIError, TigoAuthenticationError
+import importlib.metadata
+
+
+__version__ = "0.1.6"
+__author__ = "Matt Dreyer"
+__email__ = "matt_dreyer@hotmail.com"
+
+__all__ = [
+    "TigoClient",
+    "TigoAPIError", 
+    "TigoAuthenticationError",
+]
