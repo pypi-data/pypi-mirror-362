3192ff04f824f84ab70807af5e9490c9d12127e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:44 +0000

    Add package mseep-arrakis-mcp-server/0.1.0/mseep_arrakis_mcp_server-0.1.0.tar.gz

---
+++ packages/mseep-arrakis-mcp-server/mseep_arrakis_mcp_server-0.1.0.tar.gz/mseep_arrakis_mcp_server-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mseep-arrakis-mcp-server',
+    version='0.1.0',
+    description='Add your description here',
+    long_description="Package managed by MseeP.ai",
+    long_description_content_type="text/plain",
+    author='mseep',
+    author_email='support@skydeck.ai',
+    maintainer='mseep',
+    maintainer_email='support@skydeck.ai',
+    url='https://github.com/mseep',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['httpx>=0.28.1', 'mcp[cli]>=1.3.0', 'py-arrakis>=1.1.2', 'requests>=2.32.3'],
+    keywords=['mseep'],
+)
