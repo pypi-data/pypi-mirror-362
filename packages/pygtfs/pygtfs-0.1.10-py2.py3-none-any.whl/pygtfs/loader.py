8d43dee22d2742ada5c1d078f07772af1132ef91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:06:57 +0000

    Add package pygtfs/0.1.10/pygtfs-0.1.10-py2.py3-none-any.whl

---
+++ packages/pygtfs/pygtfs-0.1.10-py2.py3-none-any.whl/pygtfs/loader.py
@@ -0,0 +1,173 @@
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+from datetime import date
+import logging
+import sys
+
+import six
+from sqlalchemy import and_
+from sqlalchemy.sql.expression import select, join
+
+from .gtfs_entities import (Feed, Service, ServiceException, gtfs_required,
+                            Translation, Stop, Trip, ShapePoint, _stop_translations,
+                            _trip_shapes, gtfs_calendar, gtfs_all)
+from . import feed
+
+logger = logging.getLogger(__name__)
+
+
+def list_feeds(schedule):
+    for (i, a_feed) in enumerate(schedule.feeds):
+        print("{0}. id {1.feed_id}, name {1.feed_name}, "
+              "loaded on {1.feed_append_date}".format(i, a_feed))
+
+
+def delete_feed(schedule, feed_filename, interactive=False):
+
+    feed_name = feed.derive_feed_name(feed_filename)
+    feeds_with_name = schedule.session.query(Feed).filter(Feed.feed_name == feed_name).all()
+    delete_all = not interactive
+    for matching_feed in feeds_with_name:
+        if not delete_all:
+            print("Found feed ({0.feed_id}) named {0.feed_name} loaded on {0.feed_append_date}".format(matching_feed))
+            logger.info("Found feed ({0.feed_id}) named {0.feed_name} loaded on {0.feed_append_date}".format(matching_feed))
+            ans = ""
+            while ans not in ("K", "O", "A"):
+                ans = six.moves.input("(K)eep / (O)verwrite / overwrite (A)ll ? ").upper()
+            if ans == "K":
+                continue
+            if ans == "A":
+                delete_all = True
+        # you get here if ans is A or O, and if delete_all
+        schedule.drop_feed(matching_feed.feed_id)
+
+
+def overwrite_feed(schedule, feed_filename, *args, **kwargs):
+    interactive = kwargs.pop('interactive', False)
+    delete_feed(schedule, feed_filename, interactive=interactive)
+    append_feed(schedule, feed_filename, *args, **kwargs)
+
+
+def append_feed(schedule, feed_filename, strip_fields=True,
+                chunk_size=5000, agency_id_override=None, ignore_files=()):
+
+    fd = feed.Feed(feed_filename, strip_fields)
+
+    gtfs_tables = {}
+    for gtfs_class in gtfs_all:
+        gtfs_filename = gtfs_class.__tablename__ + '.txt'
+
+        if gtfs_filename in ignore_files:
+            logger.info("ignoring file %s as requested" % gtfs_filename)
+            continue
+
+        print('Loading GTFS data for %s:' % gtfs_class)
+        logger.info('Loading GTFS data for %s:' % gtfs_class)
+
+        try:
+            # We ignore the feed supplied feed id, because we create our own
+            # later.
+            gtfs_tables[gtfs_class] = fd.read_table(gtfs_filename,
+                                                    set(c.name for c in gtfs_class.__table__.columns) - {'feed_id'})
+        except (KeyError, IOError):
+            if gtfs_class in gtfs_required:
+                raise IOError('Error: could not find %s' % gtfs_filename)
+
+    if len(set(gtfs_tables) & gtfs_calendar) == 0:
+        raise PygtfsException('Must have Calendar.txt or Calendar_dates.txt')
+
+
+    # create new feed
+    feed_entry = Feed(feed_name=fd.feed_name, feed_append_date=date.today())
+    schedule.session.add(feed_entry)
+    schedule.session.flush()
+    feed_id = feed_entry.feed_id
+    for gtfs_class in gtfs_all:
+        if gtfs_class not in gtfs_tables:
+            continue
+        gtfs_table = gtfs_tables[gtfs_class]
+
+
+        for i, record in enumerate(gtfs_table):
+            if not record:
+                # Empty row.
+                continue
+
+            try:
+                instance = gtfs_class(feed_id=feed_id, **record._asdict())
+            except:
+                print("Failure while writing {0}".format(record))
+                logger.error("Failure while writing {0}".format(record))
+                raise
+            schedule.session.add(instance)
+
+            if isinstance(instance, ServiceException):
+                service = schedule.session.execute(
+                    select(Service).where(Service.service_id == instance.service_id).where(Service.feed_id == feed_id)
+                ).one_or_none()
+                if service is None:
+                    # ServiceException was added for a day that has no associated regular service
+                    # Create a dummy service object for this service exception
+                    dummy = Service(
+                        feed_id=feed_id,
+                        service_id=instance.service_id,
+                        monday='0',
+                        tuesday='0',
+                        wednesday='0',
+                        thursday='0',
+                        friday='0',
+                        saturday='0',
+                        sunday='0',
+                        start_date=instance.date.strftime('%Y%m%d'),
+                        end_date=instance.date.strftime('%Y%m%d'))
+                    schedule.session.add(dummy)
+
+            if i % chunk_size == 0 and i > 0:
+                schedule.session.flush()
+                sys.stdout.write('.')
+                sys.stdout.flush()
+        print('%d record%s read for %s.' % ((i+1), '' if i == 0 else 's',
+                                            gtfs_class))
+        logger.info('%d record%s read for %s.' % ((i+1), '' if i == 0 else 's',
+                                            gtfs_class))
+    schedule.session.flush()
+    schedule.session.commit()
+    # load many to many relationships
+    if Translation in gtfs_tables:
+        print('Mapping translations to stops')
+        logger.info('Mapping translations to stops')
+        q = (schedule.session.query(
+                Stop.feed_id.label('stop_feed_id'),
+                Translation.feed_id.label('translation_feed_id'),
+                Stop.stop_id.label('stop_id'),
+                Translation.trans_id.label('trans_id'),
+                Translation.lang.label('lang'))
+            .filter(Stop.feed_id==feed_id)
+            .filter(Translation.feed_id==feed_id)
+            .filter(Stop.stop_name==Translation.trans_id)
+            )
+        upd = _stop_translations.insert().from_select(
+                ['stop_feed_id', 'translation_feed_id', 'stop_id', 'trans_id', 'lang'], q)
+        schedule.session.execute(upd)
+    if ShapePoint in gtfs_tables:
+        print('Mapping shapes to trips')
+        logger.info('Mapping shapes to trips')
+        q = (schedule.session.query(
+                Trip.feed_id.label('trip_feed_id'),
+                ShapePoint.feed_id.label('shape_feed_id'),
+                Trip.trip_id.label('trip_id'),
+                ShapePoint.shape_id.label('shape_id'),
+                ShapePoint.shape_pt_sequence.label('shape_pt_sequence'))
+            .filter(Trip.feed_id==feed_id)
+            .filter(ShapePoint.feed_id==feed_id)
+            .filter(ShapePoint.shape_id==Trip.shape_id)
+            )
+        upd = _trip_shapes.insert().from_select(
+                ['trip_feed_id', 'shape_feed_id', 'trip_id', 'shape_id', 'shape_pt_sequence'], q)
+        schedule.session.execute(upd)
+    schedule.session.commit()
+
+    print('Complete.')
+    logger.info('Complete.')
+    return schedule
---
+++ packages/pygtfs/pygtfs-0.1.10-py2.py3-none-any.whl/pygtfs/test/test.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import datetime
+import os.path
+
+# use unittest2 for Python2.6 compatibility.
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+from pygtfs import overwrite_feed
+from pygtfs import Schedule
+
+from sqlalchemy.orm import Query
+
+
+class TestSchedule(unittest.TestCase):
+    def setUp(self):
+        self.schedule = Schedule(":memory:")
+        data_location = os.path.join(os.path.dirname(__file__),
+                                     "data", "sample_feed")
+        overwrite_feed(self.schedule, data_location)
+
+    def test_routes(self):
+        self.assertEqual(self.schedule.routes[0].route_id, "AB")
+
+    def test_services(self):
+        ser = [service.service_id for service in self.schedule.services]
+        self.assertEqual(ser, ["FULLW", "WE"])
+
+    def test_stops(self):
+        self.assertEqual([st.stop_id for st in self.schedule.stops],
+                         ['FUR_CREEK_RES', 'BEATTY_AIRPORT', 'BULLFROG',
+                          'STAGECOACH', 'NADAV', 'NANAA', 'DADAN', 'EMSI',
+                          'AMV'])
+
+    def test_route_trips(self):
+        self.assertEqual([tr.trip_id for tr in self.schedule.routes[0].trips],
+                         ['AB1', 'AB2'])
+
+    def test_trip_stop_times(self):
+        self.assertEqual([(st.arrival_time if st.arrival_time else None,
+                           st.departure_time if st.departure_time else None) for st in self.schedule.routes[0].trips[0].stop_times],
+                        [(datetime.timedelta(0, 28800), datetime.timedelta(0, 28800)),
+                         (datetime.timedelta(0, 29400), datetime.timedelta(0, 29700))])
+
+    def test_service_trips(self):
+        self.assertEqual([tr.trip_id for tr in self.schedule.services[1].trips],
+                         ['AAMV1', 'AAMV2', 'AAMV3', 'AAMV4'])
+
+    def test_stop_stop_times(self):
+        self.assertEqual([(st.arrival_time, st.departure_time) for st in self.schedule.stops[0].stop_times],
+                         [(datetime.timedelta(0, 33600), datetime.timedelta(0, 33600)),
+                          (datetime.timedelta(0, 39600), datetime.timedelta(0, 39600))])
+
+    def test_stop_translations(self):
+        self.assertEqual(sorted([(tr.lang, tr.translation)
+                                 for tr in self.schedule.stops[0].translations]),
+                         [('EN', 'Furnace Creek Resort (Demo)'), ('HE', 'אתר הנופש ערוץ הכבשן (דמו)')])
+
+    def test_agencies(self):
+        self.assertEqual([ag.agency_id for ag in self.schedule.agencies],
+                         ['DTA'])
+
+    def test_agency_routes(self):
+        self.assertEqual([rt.route_id for rt in self.schedule.agencies[0].routes],
+                         ['AAMV', 'AB', 'BFC', 'CITY', 'EXT', 'STBA'])
+
+    def test_transfers(self):
+        self.assertEqual(3, len(self.schedule.stops[0].transfers_from))
+        self.assertEqual(0, len(self.schedule.stops[0].transfers_to))
+        self.assertEqual(0, len(self.schedule.stops[1].transfers_from))
+        self.assertEqual(3, len(self.schedule.stops[1].transfers_to))
+        self.assertEqual([
+            (0, 'BEATTY_AIRPORT', '', ''),
+            (3, 'BEATTY_AIRPORT', '', 'AAMV4'),
+            (1, 'BEATTY_AIRPORT', 'STBA', ''),
+        ], [(tr.transfer_type, tr.to_stop_id, tr.to_route_id, tr.to_trip_id) for tr in self.schedule.stops[1].transfers_to])
+
+    def test_transfers_route_to_route(self):
+        self.assertEqual(1, len(self.schedule.agencies[0].routes[0].transfers_from))
+        self.assertEqual(0, len(self.schedule.agencies[0].routes[0].transfers_to))
+        self.assertEqual(0, len(self.schedule.agencies[0].routes[5].transfers_from))
+        self.assertEqual(1, len(self.schedule.agencies[0].routes[5].transfers_to))
+        self.assertEqual([1], [tr.transfer_type for tr in self.schedule.agencies[0].routes[0].transfers_from])
+        self.assertEqual([], [tr.transfer_type for tr in self.schedule.agencies[0].routes[0].transfers_to])
+        self.assertEqual([], [tr.transfer_type for tr in self.schedule.agencies[0].routes[5].transfers_from])
+        self.assertEqual([1], [tr.transfer_type for tr in self.schedule.agencies[0].routes[5].transfers_to])
+
+    def test_transfers_trip_to_trip(self):
+        self.assertEqual(1, len(self.schedule.trips[8].transfers_from))
+        self.assertEqual(0, len(self.schedule.trips[8].transfers_to))
+        self.assertEqual(0, len(self.schedule.trips[10].transfers_from))
+        self.assertEqual(1, len(self.schedule.trips[10].transfers_to))
+        self.assertEqual([3], [tr.transfer_type for tr in self.schedule.trips[8].transfers_from])
+        self.assertEqual([], [tr.transfer_type for tr in self.schedule.trips[8].transfers_to])
+        self.assertEqual([], [tr.transfer_type for tr in self.schedule.trips[10].transfers_from])
+        self.assertEqual([3], [tr.transfer_type for tr in self.schedule.trips[10].transfers_to])
+
+    def test_trips_bikes_allowed(self):
+        for t in self.schedule.trips:
+            self.assertIsNone(t.bikes_allowed)
+        t = self.schedule.trips[0]
+        t.bikes_allowed = 0
+        t.bikes_allowed = 1
+        t.bikes_allowed = 2
+        with self.assertRaises(ValueError):
+            t.bikes_allowed = 3
+        with self.assertRaises(ValueError):
+            t.bikes_allowed = -1
+        self.assertEqual(t.bikes_allowed, 2)
+
+    def test_query_methods(self):
+        """
+            Test that the object query getters return
+            :py:class:`sqlalchemy.orm.Query` objects
+        """
+        # Ideally assertIsInstance would be used, but this is not Python 2.6
+        # compatile
+        self.assertEqual(isinstance(self.schedule.agencies_query, Query),
+                         True)
+
+class TestIgnoreFiles(unittest.TestCase):
+    def setUp(self):
+        self.schedule = Schedule(":memory:")
+        data_location = os.path.join(os.path.dirname(__file__),
+                                     "data", "sample_feed")
+        overwrite_feed(self.schedule, data_location, ignore_files="routes.txt")
+
+    def test_routes(self):
+        self.assertFalse(self.schedule.routes)
+
+    def test_services(self):
+        ser = [service.service_id for service in self.schedule.services]
+        self.assertEqual(ser, ["FULLW", "WE"])
+
+if __name__ == '__main__':
+    unittest.main()
