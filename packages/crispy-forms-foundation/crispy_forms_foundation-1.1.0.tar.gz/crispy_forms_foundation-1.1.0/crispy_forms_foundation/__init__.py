d2258fb132938c767e8f045d2d6a4736741cdf69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 02:44:24 +0000

    Add package crispy-forms-foundation/1.1.0/crispy_forms_foundation-1.1.0.tar.gz

---
+++ packages/crispy-forms-foundation/crispy_forms_foundation-1.1.0.tar.gz/crispy_forms_foundation-1.1.0/crispy_forms_foundation/__init__.py
@@ -0,0 +1,6 @@
+"""Django application to add 'django-crispy-forms' layout objects for 'Foundation for sites'"""  # noqa: E501
+from importlib.metadata import version
+
+
+__pkgname__ = "crispy-forms-foundation"
+__version__ = version(__pkgname__)
