f4aed441d98028a482c97a3d5c579234630a37cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:53 +0000

    Add package adt-db/0.1.1/adt_db-0.1.1.tar.gz

---
+++ packages/adt-db/adt_db-0.1.1.tar.gz/adt_db-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+import tomllib
+
+with open("pyproject.toml", "rb") as f:
+    pyproject = tomllib.load(f)
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="adt_db",
+    version=pyproject["project"]["version"],
+    description="Agent de Tourisme database manipulation library.",
+    package_dir={"": "adt_db"},
+    packages=find_packages(where="adt_db"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/agent-de-tourisme/adt-juil-db",
+    author="Maxime Blanchard, Khadim Fall, Wissem Mejri",
+    license="GPL-3",
+    install_requires=pyproject["project"]["dependencies"],
+    python_requires=pyproject["project"]["requires-python"]
+)
