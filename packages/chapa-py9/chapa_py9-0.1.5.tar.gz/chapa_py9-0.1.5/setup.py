988e46d7f6fa23389d23118fcebba7324dc3031e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:17 +0000

    Add package chapa-py9/0.1.5/chapa_py9-0.1.5.tar.gz

---
+++ packages/chapa-py9/chapa_py9-0.1.5.tar.gz/chapa_py9-0.1.5/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+import os
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+version_number = os.environ.get('CHAPA_VERSION', '0.1.5')
+
+setuptools.setup(
+    name='chapa-py9',
+    version=version_number,
+    author='Temkin Mengistu (Chapi)',
+    author_email='chapimenge3@gmail.com',
+    description='Python SDK for Chapa API https://developer.chapa.co',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/chapimenge3/chapa',
+    packages=['chapa'],
+    package_dir={'chapa': 'chapa'},
+    project_urls={
+        'Source': 'https://github.com/chapimenge3/chapa',
+        'Bug Tracker': 'https://github.com/chapimenge3/chapa/issues',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'httpx>=0.22.0',
+    ],
+)
