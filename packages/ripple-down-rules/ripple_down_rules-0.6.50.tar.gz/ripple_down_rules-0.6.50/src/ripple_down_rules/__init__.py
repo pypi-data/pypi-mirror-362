df0279c25b3794d0b83c83f7d3140e796b8398f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:07:04 +0000

    Add package ripple-down-rules/0.6.50/ripple_down_rules-0.6.50.tar.gz

---
+++ packages/ripple-down-rules/ripple_down_rules-0.6.50.tar.gz/ripple_down_rules-0.6.50/src/ripple_down_rules/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "0.6.50"
+
+import logging
+import sys
+
+logger = logging.Logger("rdr")
+logger.setLevel(logging.INFO)
+
+try:
+    from PyQt6.QtWidgets import QApplication
+    app = QApplication(sys.argv)
+except ImportError:
+    pass
+
+from .datastructures.dataclasses import CaseQuery
+from .rdr_decorators import RDRDecorator
+from .rdr import MultiClassRDR, SingleClassRDR, GeneralRDR
