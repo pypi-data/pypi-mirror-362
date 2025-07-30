ae92093440c134f8c426ed3addc7cfd3d249cd4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:31 +0000

    Add package mseep-litserve/1.0.0/mseep_litserve-1.0.0.tar.gz

---
+++ packages/mseep-litserve/mseep_litserve-1.0.0.tar.gz/mseep_litserve-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mseep-litserve',
+    version='1.0.0',
+    description='Lightweight AI server.',
+    long_description="Package managed by MseeP.ai",
+    long_description_content_type="text/plain",
+    author='mseep',
+    author_email='support@skydeck.ai',
+    maintainer='mseep',
+    maintainer_email='support@skydeck.ai',
+    url='https://github.com/mseep',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['fastapi>=0.100', 'pyzmq>=22.0.0', 'uvicorn[standard]>=0.29.0'],
+    keywords=['mseep', 'deep learning', 'pytorch', 'AI'],
+)
