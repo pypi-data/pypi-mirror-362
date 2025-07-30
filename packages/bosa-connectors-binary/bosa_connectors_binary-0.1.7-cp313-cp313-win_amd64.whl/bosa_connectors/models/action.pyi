4d41027dccbbbcec850016d522640fd8d4e2cf4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 04:27:13 +0000

    Add package bosa-connectors-binary/0.1.7/bosa_connectors_binary-0.1.7-cp313-cp313-win_amd64.whl

---
+++ packages/bosa-connectors-binary/bosa_connectors_binary-0.1.7-cp313-cp313-win_amd64.whl/bosa_connectors/models/action.pyi
@@ -0,0 +1,16 @@
+from bosa_connectors.models.file import ConnectorFile as ConnectorFile
+from pydantic import BaseModel
+from typing import Any
+
+class ActionResponseData(BaseModel):
+    """Response data model with data and meta information."""
+    data: list[Any] | dict[str, Any] | ConnectorFile
+    meta: dict[str, Any] | None
+
+class InitialExecutorRequest(BaseModel):
+    """Initial executor request model."""
+    params: dict[str, Any]
+    headers: dict[str, str] | None
+    max_attempts: int | None
+    token: str | None
+    timeout: int | None
