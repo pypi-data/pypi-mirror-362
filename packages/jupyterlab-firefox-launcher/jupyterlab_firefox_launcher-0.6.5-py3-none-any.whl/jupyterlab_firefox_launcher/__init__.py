4344049c6fbec06a8faaee80eeb3eeca3f4452ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:06 +0000

    Add package jupyterlab-firefox-launcher/0.6.5/jupyterlab_firefox_launcher-0.6.5-py3-none-any.whl

---
+++ packages/jupyterlab-firefox-launcher/jupyterlab_firefox_launcher-0.6.5-py3-none-any.whl/jupyterlab_firefox_launcher/__init__.py
@@ -0,0 +1,35 @@
+
+"""
+JupyterLab Firefox Launcher Extension
+
+A minimal Firefox launcher for JupyterLab that uses jupyter-server-proxy
+for process management. This extension is designed to work seamlessly
+with SlurmSpawner environments.
+"""
+
+import os
+from importlib.metadata import version
+
+# Get version from package metadata
+__version__ = version("jupyterlab-firefox-launcher")
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+
+def _jupyter_server_extension_points():
+    """
+    Minimal entry points - jupyter-server-proxy handles everything
+    """
+    return []
+
+
+# For backward compatibility - but we don't actually load anything
+def load_jupyter_server_extension(server_app):
+    """
+    Minimal server extension loader - jupyter-server-proxy handles everything
+    """
+    server_app.log.info("Firefox Desktop extension (minimal) - using jupyter-server-proxy")
+
+
+_load_jupyter_server_extension = load_jupyter_server_extension
+_jupyter_server_extension_paths = _jupyter_server_extension_points
