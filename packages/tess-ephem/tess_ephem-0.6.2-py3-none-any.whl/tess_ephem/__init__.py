87e706f16b04b0081a2d4427763cd0c8e76d7fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:11 +0000

    Add package tess-ephem/0.6.2/tess_ephem-0.6.2-py3-none-any.whl

---
+++ packages/tess-ephem/tess_ephem-0.6.2-py3-none-any.whl/tess_ephem/__init__.py
@@ -0,0 +1,10 @@
+import logging
+
+# Configure logging
+log = logging.getLogger(__name__)
+log.addHandler(logging.StreamHandler())
+
+from .ephem import TessEphem, ephem  # noqa: E402
+
+__version__ = "0.6.2"
+__all__ = ["ephem", "TessEphem"]
