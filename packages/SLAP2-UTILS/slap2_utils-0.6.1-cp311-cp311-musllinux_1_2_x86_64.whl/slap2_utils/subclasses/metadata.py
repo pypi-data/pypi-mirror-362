10a941db5b68d1fa73d92f6de121bd550df615ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:07:41 +0000

    Add package SLAP2-UTILS/0.6.1/slap2_utils-0.6.1-cp311-cp311-musllinux_1_2_x86_64.whl

---
+++ packages/SLAP2-UTILS/slap2_utils-0.6.1-cp311-cp311-musllinux_1_2_x86_64.whl/slap2_utils/subclasses/metadata.py
@@ -0,0 +1,89 @@
+# Subclasses of the primary SLAP2 Datafile
+import h5py
+from .slapROI import slapROI
+from .acquisitionContainer import AcquisitionContainer
+
+class MetaData():
+
+    """
+            A class used to load a SLAP2 binary metadatafile for data processing. This is also an attribute for the datafile object
+
+            Attributes
+            ----------
+            metadatafile : str
+                The name of the metadata file.
+            AcquisitionContainer : ``AcquisitionContainer``
+                An instance of the AcquisitionContainer class that contain information related to the acquisition.
+            acqDurationCycles : list
+                A list containing 2 int values for cycle duration
+            acqDuration_s : int
+                The total time (in seconds) for the whole acquisition.
+            acquisitionPathIdx : int
+                The number that represent the path that the acquisitiion take place (either 1 or 2).
+            acquisitionPathName : str
+                The string that reiterate which path the acquisition takes place (either 'path 1' or 'path 2')  .
+            aomActive : int
+                Binary number that reporesent whether aom is action (0 = false, 1 = yes).
+            aomVoltage : float
+                The number of voltage in aom (in float).
+            channelsSave : int
+                The number that represent which channel saved the data. 
+            dmdPixelsPerColumn : int
+                The number of pixel along the column of the dmd. 
+            dmdPixelsPerRow : int
+                The number of pixel along the row of the dmd.    
+            enableStack : list
+                Binary number that reporesent whether stack is enabled (0 = false, 1 = yes).
+            linePeriod_s : float
+                The time it takes to conduct 1 slice (1/linePeriod_s gives the line rate for the microscope).
+            remoteFocusPosition_um : int
+                A number that represent the remoteFocusPosition for the center of the stack. 
+            samplesPerLine : int
+                The line rate for the microscope (1/samplesPerLine gives the linePeriod_s).
+
+            
+            Descriptions for methods:
+            --------------------------
+
+            Methods
+            ----------
+            __init__() :
+                Fill the fields mentioned above. The method also loads the AcquisitionContainer using the __init__() method for the AcquisitionContainer
+
+            Return
+            -------
+                Self with populated fields. 
+                
+            """
+
+    def __init__(self, metadatafile):
+        self.metadatafile = metadatafile
+        self.AcquisitionContainer = None
+        self.acqDurationCycles = ''
+        self.acqDuration_s = ''
+        self.acquisitionPathIdx = ''
+        self.acquisitionPathName = ''
+        self.aomActive = ''
+        self.aomVoltage = ''
+        self.channelsSave = ''
+        self.dmdPixelsPerColumn = ''
+        self.dmdPixelsPerRow = ''
+        self.enableStack = ''
+        self.linePeriod_s = ''
+        #machineConfiguration <HDF5 group "/machineConfiguration" (3 members)>
+        self.remoteFocusPosition_um = ''
+        self.samplesPerLine = ''
+
+        with h5py.File(metadatafile, 'r') as hdf_file:
+        # Read the contents of the HDF5 file into the metaData dictionary
+
+            metaData = dict(hdf_file)
+            for _key in metaData.keys():
+                try:
+                    metaData[_key] = hdf_file[_key][:][0][0]
+                    setattr(self, _key, hdf_file[_key][:][0][0])
+                except:   
+                    metaData[_key] = hdf_file[_key]
+                    
+                
+            self.AcquisitionContainer = AcquisitionContainer(self.metadatafile)
---
+++ packages/SLAP2-UTILS/slap2_utils-0.6.1-cp311-cp311-musllinux_1_2_x86_64.whl/slap2_utils/utils/plots/slap2Plots.py
@@ -0,0 +1,135 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib import cm
+from ..roi_utils import roiImg
+
+def roiOverlay(datafile, refimg, roiIdx, display=True):
+    """
+    Plots contours of SLAP2 ROIs over a reference image with color-coded super pixels.
+
+    Parameters
+    ----------
+    datafile : ``SLAP2_Utils Datafile Object``
+        The datafile containing metadata and header information.
+    refimg : ``np.ndarray``
+        Reference image; can be 3D (t, x, y) or 2D (x, y).
+    roiIdx : int or list of int
+        Single ROI index or a list of ROI indices to overlay.
+    display : bool, ``optional``
+        Whether to display the plot immediately. Default is True.
+
+    Returns
+    -------
+    ax : ``matplotlib.axes.Axes``
+        Matplotlib Axes object with the plotted contours
+    """
+    assert type(roiIdx) == int or type(roiIdx) == list
+    if type(roiIdx) == int:
+        roiIdx = [roiIdx]
+    
+    if len(refimg.shape) == 3:
+        refimg = np.mean(refimg, axis=0)
+    
+    fig, ax = plt.subplots(1,figsize=(24,16), dpi=100)
+    ax.imshow(refimg, cmap='Greys_r', vmax=np.max(refimg)*.45)
+    
+    for _roiIdx in roiIdx:
+        img = roiImg(datafile, _roiIdx)
+        _roiColor = mpl.colors.rgb2hex(cm.Spectral(_roiIdx/len(datafile.metaData.AcquisitionContainer.ROIs)))
+        ax.contour(img, colors=_roiColor)
+        
+    ax.set_xticks([],[])
+    ax.set_yticks([],[])
+    if display:
+        plt.show()
+    return ax 
+
+def roiOverlaySuperPix(datafile, refimg, roiIdx, display=True):
+    """Plot pixel-wise traces of fluorescence changes over time.
+
+    This function plots the traces of fluorescence changes for each super pixel over a specified time period.
+
+    Parameters
+    ----------
+    dffData : ``np.array``
+        The delta F/F data of super pixels.
+    hz : float
+        Sampling rate in Hertz.
+    start : int
+        Start frame index.
+    end : int
+        End frame index.
+
+    Returns
+    -------
+    ax : ``matplotlib.axes.Axes``
+        Matplotlib axes object with the plotted contours
+    """
+    assert type(roiIdx) == int
+    if type(roiIdx) == int:
+        roiIdx = [roiIdx]
+    
+    if len(refimg.shape) == 3:
+        refimg = np.mean(refimg, axis=0)
+    
+    fig, ax = plt.subplots(1,figsize=(24,16), dpi=100)
+    
+    ax.imshow(refimg, cmap='Greys_r', vmax=np.max(refimg)*.45)
+    
+    for _roiIdx in roiIdx:
+        img = roiImg(datafile, _roiIdx)
+        _roiColor = mpl.colors.rgb2hex(cm.Spectral(_roiIdx/len(datafile.metaData.AcquisitionContainer.ROIs)))
+        ax.contour(img, colors=_roiColor)
+
+    yIndex = list(np.unique(np.where(img==1)[1]))
+    xIndex = list(np.unique(np.where(img==1)[0]))
+    barRow = (np.max(xIndex)+5, np.max(xIndex)+15)
+
+    superPixBar = np.zeros((img.shape[0], img.shape[1], 3))
+    for i, col in enumerate( range(4, len(yIndex)-4, 1)):
+        superPixBar[barRow[0]:barRow[1], yIndex[col], :] = cm.Spectral(i/(len(yIndex)-10))[:3]
+        
+    all_channels_zero = np.all(superPixBar == 0, axis=-1)
+    mask_rgb = np.repeat(all_channels_zero[:, :, np.newaxis], 3, axis=2)
+    _maskedBar = np.ma.masked_where(mask_rgb, superPixBar)
+    _maskedBar = superPixBar.copy()
+    _maskedBar[all_channels_zero, :] = [0, 0, 0]  # Set to black or make transparent
+
+   
+    alpha = np.ones((800, 1280, 4))  # Create an RGBA for alpha
+    alpha[all_channels_zero] = [0, 0, 0, 0]  # Make masked areas fully transparent
+    _maskedBar = np.concatenate((_maskedBar, alpha[:, :, 3:]), axis=-1)                
+    ax.imshow(_maskedBar)
+    ax.set_xticks([],[])
+    ax.set_yticks([],[])
+    if display:
+        plt.show()
+    return ax
+
+def plotPixTrace(dffData, hz, start, end):
+    """Plot pixel-wise traces of fluorescence changes over time.
+
+    This function plots the traces of fluorescence changes for each super pixel over a specified time period.
+
+    Parameters
+    ----------
+    dffData : ``np.array``
+        The delta F/F data of super pixels.
+    hz : float
+        Sampling rate in Hertz.
+    start : int
+        Start frame index.
+    end : int
+        End frame index.
+
+    Returns
+    -------
+    None
+    """
+    plt.figure(figsize=(24, 16))
+    time = np.arange(0, int(end-start)/hz, 1/hz)
+    for j in range(dffData.shape[0]):
+        _traceColor = mpl.colors.rgb2hex(cm.Spectral(j/dffData.shape[0]))
+        plt.plot(time, dffData[j, start:end]+(.15*j), color=_traceColor, alpha=.8, zorder=dffData.shape[0]-j+5)
+    plt.show()
