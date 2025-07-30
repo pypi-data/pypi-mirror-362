e6f8cab1b5c672851a630aa1bc36f2bc25d1ec25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:18 +0000

    Add package normtransform/0.2.0/normtransform-0.2.0-cp311-cp311-macosx_14_0_arm64.whl

---
+++ packages/normtransform/normtransform-0.2.0-cp311-cp311-macosx_14_0_arm64.whl/normtransform/dummy_trans.py
@@ -0,0 +1,13 @@
+import math
+
+def do_something():
+    a = 3
+    b = 4
+    c = math.sqrt(a**2 + b**2)
+    return c
+
+def do_something_else():
+    a = 5
+    b = 12  
+    c = math.sqrt(a**2 + b**2)
+    return c
