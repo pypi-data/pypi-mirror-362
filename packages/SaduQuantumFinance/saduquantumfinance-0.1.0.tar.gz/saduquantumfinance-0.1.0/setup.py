e4060e26665103c62d2ca62ed6caee039f34e87f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:33 +0000

    Add package SaduQuantumFinance/0.1.0/saduquantumfinance-0.1.0.tar.gz

---
+++ packages/SaduQuantumFinance/saduquantumfinance-0.1.0.tar.gz/saduquantumfinance-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SaduQuantumFinance',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A package that deals with Quantum Finance, Provides plotting visuals',
+    author='SaduPostMagazine',
+    author_email='sadupostrivista@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+    ]
+)
