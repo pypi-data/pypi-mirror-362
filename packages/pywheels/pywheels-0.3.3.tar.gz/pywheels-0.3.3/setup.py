1d6d7fc236d8b4ae0d651d72262f383d4a10aad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:07:59 +0000

    Add package pywheels/0.3.3/pywheels-0.3.3.tar.gz

---
+++ packages/pywheels/pywheels-0.3.3.tar.gz/pywheels-0.3.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(
+    name = 'pywheels',
+    version = '0.3.3',
+    packages = find_packages(),
+    description = 'Light-weight Python wheels',
+    author = 'parkcai',
+    author_email = 'sun_retailer@163.com',
+    url = 'https://github.com/parkcai/pywheels',
+    include_package_data = True,
+    package_data = {
+        'pywheels': ['locales/**/LC_MESSAGES/*.mo'],
+    },
+)
