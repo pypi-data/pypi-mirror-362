40f5c398bf4f3fb9ec73b33e9ebea79791eedcf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:54 +0000

    Add package duguidlab-motion-correction/2.0.1/duguidlab_motion_correction-2.0.1-cp312-cp312-musllinux_1_2_x86_64.whl

---
+++ packages/duguidlab-motion-correction/duguidlab_motion_correction-2.0.1-cp312-cp312-musllinux_1_2_x86_64.whl/sima/__init__.py
@@ -0,0 +1,12 @@
+"""SIMA: Python package for sequential image analysis.
+Licensed under the GNU GPL version 2 or later.
+Documentation: http://www.losonczylab.org/sima
+Version 1.3.2"""
+
+from sima.imaging import ImagingDataset
+from sima.sequence import Sequence
+
+import numpy.testing as testing
+test = testing.test
+
+__version__ = '1.3.2'
