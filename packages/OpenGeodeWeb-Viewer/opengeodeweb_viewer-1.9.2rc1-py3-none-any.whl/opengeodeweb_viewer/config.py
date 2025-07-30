00032b570ed29be5e9708629918fff6f73e10628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:28 +0000

    Add package OpenGeodeWeb-Viewer/1.9.2rc1/opengeodeweb_viewer-1.9.2rc1-py3-none-any.whl

---
+++ packages/OpenGeodeWeb-Viewer/opengeodeweb_viewer-1.9.2rc1-py3-none-any.whl/opengeodeweb_viewer/config.py
@@ -0,0 +1,32 @@
+import os
+from sys import platform
+
+
+def default_config():
+    os.environ["DEFAULT_HOST"] = "localhost"
+    os.environ["DEFAULT_PORT"] = "1234"
+
+
+def prod_config():
+    default_config()
+    os.environ["DATA_FOLDER_PATH"] = "/data/"
+
+
+def dev_config():
+    default_config()
+    if platform == "linux":
+        os.environ["DATA_FOLDER_PATH"] = "/temp/OpenGeodeWeb_Data/"
+    elif platform == "win32":
+        os.environ["DATA_FOLDER_PATH"] = os.path.join(
+            "C:/Users", os.getlogin(), "OpenGeodeWeb_Data"
+        )
+    if not os.path.exists(os.environ.get("DATA_FOLDER_PATH")):
+        os.mkdir(os.environ.get("DATA_FOLDER_PATH"))
+
+
+def test_config(path):
+    default_config()
+    print(f"{os.path.dirname(__file__)=}", flush=True)
+    os.environ["DATA_FOLDER_PATH"] = os.path.join(path, "data")
+
+    print(f"{os.environ.get('DATA_FOLDER_PATH')=}", flush=True)
