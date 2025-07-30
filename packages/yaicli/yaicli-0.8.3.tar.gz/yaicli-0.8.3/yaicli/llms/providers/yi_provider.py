c7bf59e91333b28c04fc8147d6e057cd0bec1d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:32 +0000

    Add package yaicli/0.8.3/yaicli-0.8.3.tar.gz

---
+++ packages/yaicli/yaicli-0.8.3.tar.gz/yaicli-0.8.3/yaicli/llms/providers/yi_provider.py
@@ -0,0 +1,15 @@
+from .openai_provider import OpenAIProvider
+
+
+class YiProvider(OpenAIProvider):
+    """Lingyiwanwu provider implementation based on openai-compatible API"""
+
+    DEFAULT_BASE_URL = "https://api.lingyiwanwu.com/v1"
+    COMPLETION_PARAMS_KEYS = {
+        "model": "MODEL",
+        "temperature": "TEMPERATURE",
+        "top_p": "TOP_P",
+        "max_tokens": "MAX_TOKENS",
+        "timeout": "TIMEOUT",
+        "extra_body": "EXTRA_BODY",
+    }
