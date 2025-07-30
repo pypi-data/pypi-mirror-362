666a9071cc010b6e70176fb6f31fe8e78f556e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 14:08:17 +0000

    Add package mseep-mcpcrashcourse/0.1.0/mseep_mcpcrashcourse-0.1.0.tar.gz

---
+++ packages/mseep-mcpcrashcourse/mseep_mcpcrashcourse-0.1.0.tar.gz/mseep_mcpcrashcourse-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mseep-mcpcrashcourse',
+    version='0.1.0',
+    description='Add your description here',
+    long_description="Package managed by MseeP.ai",
+    long_description_content_type="text/plain",
+    author='mseep',
+    author_email='support@skydeck.ai',
+    maintainer='mseep',
+    maintainer_email='support@skydeck.ai',
+    url='https://github.com/mseep',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['langchain-groq>=0.3.2', 'mcp-use>=1.2.7', 'mcp[cli]>=1.6.0'],
+    keywords=['mseep'],
+)
