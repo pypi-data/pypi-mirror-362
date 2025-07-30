b3d1c60e0ba174a5205eb7523a6be0aa84763103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 03:34:01 +0000

    Add package mcp-software-ccopyright/0.1.6/mcp_software_ccopyright-0.1.6.tar.gz

---
+++ packages/mcp-software-ccopyright/mcp_software_ccopyright-0.1.6.tar.gz/mcp_software_ccopyright-0.1.6/src/mcp_software_ccopyright/yu/processors/swift_processor.py
@@ -0,0 +1,14 @@
+from mcp_software_ccopyright.yu.processors import FileProcessorBase
+from mcp_software_ccopyright.yu.processors import BlankLineFilter
+from mcp_software_ccopyright.yu.processors.c_style_comment_block_filter import CStyleCommentBlockFilter
+from mcp_software_ccopyright.yu.processors.double_slash_comment_filter import DoubleSlashCommentFilter
+
+
+class SwiftProcessor(FileProcessorBase):
+    expected_extensions = ['.swift']
+
+    def __init__(self):
+        self.filters.append(BlankLineFilter())
+        self.filters.append(CStyleCommentBlockFilter())
+        self.filters.append(DoubleSlashCommentFilter())
+        return
