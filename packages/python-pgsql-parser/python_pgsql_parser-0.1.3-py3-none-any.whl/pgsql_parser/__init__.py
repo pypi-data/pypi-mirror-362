77243b103f6a8a096e2e0d9459dfe6b0d39e013d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:49 +0000

    Add package python-pgsql-parser/0.1.3/python_pgsql_parser-0.1.3-py3-none-any.whl

---
+++ packages/python-pgsql-parser/python_pgsql_parser-0.1.3-py3-none-any.whl/pgsql_parser/__init__.py
@@ -0,0 +1,3 @@
+from .sqlparser import SQLLexer, SQLParser, TokenType, Token, Table, Column, PrimaryKey, ForeignKey, Constraint
+
+__all__ = ["SQLLexer", "SQLParser", "TokenType", "Token", "Table", "Column", "PrimaryKey", "ForeignKey", "Constraint"]
