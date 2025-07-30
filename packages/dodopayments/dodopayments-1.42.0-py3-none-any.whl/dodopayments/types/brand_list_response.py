29182beb93721abb3e217dbdfbd49483fedfef87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 08:06:56 +0000

    Add package dodopayments/1.42.0/dodopayments-1.42.0-py3-none-any.whl

---
+++ packages/dodopayments/dodopayments-1.42.0-py3-none-any.whl/dodopayments/types/brand_list_response.py
@@ -0,0 +1,40 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Optional
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["BrandListResponse", "Item"]
+
+
+class Item(BaseModel):
+    brand_id: str
+
+    business_id: str
+
+    enabled: bool
+
+    statement_descriptor: str
+
+    verification_enabled: bool
+
+    verification_status: Literal["Success", "Fail", "Review", "Hold"]
+
+    description: Optional[str] = None
+
+    image: Optional[str] = None
+
+    name: Optional[str] = None
+
+    reason_for_hold: Optional[str] = None
+    """Incase the brand verification fails or is put on hold"""
+
+    support_email: Optional[str] = None
+
+    url: Optional[str] = None
+
+
+class BrandListResponse(BaseModel):
+    items: List[Item]
+    """List of brands for this business"""
