57fd961d125a3722f993e45a22ab68243b24387c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:21 +0000

    Add package laygo/0.1.2/laygo-0.1.2-py3-none-any.whl

---
+++ packages/laygo/laygo-0.1.2-py3-none-any.whl/laygo/__init__.py
@@ -0,0 +1,19 @@
+"""
+Laygo - A lightweight Python library for building resilient, in-memory data pipelines
+"""
+
+__version__ = "0.1.2"
+
+from .errors import ErrorHandler
+from .helpers import PipelineContext
+from .pipeline import Pipeline
+from .transformers.parallel import ParallelTransformer
+from .transformers.transformer import Transformer
+
+__all__ = [
+  "Pipeline",
+  "Transformer",
+  "ParallelTransformer",
+  "PipelineContext",
+  "ErrorHandler",
+]
