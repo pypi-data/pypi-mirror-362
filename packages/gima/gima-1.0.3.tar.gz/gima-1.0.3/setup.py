e2e25e0ebcf5c8df9c8b358e2718c66f0d702d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:59 +0000

    Add package gima/1.0.3/gima-1.0.3.tar.gz

---
+++ packages/gima/gima-1.0.3.tar.gz/gima-1.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='gima',
+    version='1.0.3',
+    author='Arkadiusz Hypki',
+    description='Software which simplifies managing many git repositories through a console',
+    packages=['gima', 'gima/db'],
+    include_package_data=True,
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'pygit2'
+    ],
+)
