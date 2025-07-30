476fe27726c34ce63692a03fc5566553096f5440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 01:33:47 +0000

    Add package dbt-sl-sdk/0.13.0/dbt_sl_sdk-0.13.0-py3-none-any.whl

---
+++ packages/dbt-sl-sdk/dbt_sl_sdk-0.13.0-py3-none-any.whl/dbtsl/models/__init__.py
@@ -0,0 +1,53 @@
+"""The models used for the SDK.
+
+NOTE: this will be deleted in the future and will be replaced by
+generated code from our GraphQL schema.
+"""
+
+from .base import BaseModel, GraphQLFragmentMixin
+from .dimension import Dimension, DimensionType
+from .entity import Entity, EntityType
+from .measure import AggregationType, Measure
+from .metric import AsyncMetric, Metric, MetricType, SyncMetric
+from .query import QueryResult
+from .saved_query import (
+    Export,
+    ExportConfig,
+    ExportDestinationType,
+    SavedQuery,
+    SavedQueryGroupByParam,
+    SavedQueryMetricParam,
+    SavedQueryQueryParams,
+    SavedQueryWhereParam,
+)
+from .time import DatePart, TimeGranularity
+
+# Only importing this so it registers aliases
+_ = QueryResult
+
+BaseModel._register_subclasses()  # pyright: ignore[reportPrivateUsage]
+GraphQLFragmentMixin._register_subclasses()  # pyright: ignore[reportPrivateUsage]
+
+__all__ = [
+    "AggregationType",
+    "AsyncMetric",
+    "DatePart",
+    "Dimension",
+    "DimensionType",
+    "Entity",
+    "EntityType",
+    "Export",
+    "ExportConfig",
+    "ExportDestinationType",
+    "Measure",
+    "Metric",
+    "MetricType",
+    "SavedQuery",
+    "SavedQuery",
+    "SavedQueryGroupByParam",
+    "SavedQueryMetricParam",
+    "SavedQueryQueryParams",
+    "SavedQueryWhereParam",
+    "SyncMetric",
+    "TimeGranularity",
+]
