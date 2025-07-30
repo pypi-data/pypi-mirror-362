9a290121443a7fa4a92e7aeedd4cf89f6dd8892d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:28 +0000

    Add package trac-ticketrpc/0.9.3/trac_ticketrpc-0.9.3-py3-none-any.whl

---
+++ packages/trac-ticketrpc/trac_ticketrpc-0.9.3-py3-none-any.whl/tracticketrpc/__init__.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# --------------------------------------------------------------------------------------------------
+# trac-ticketrpc - JSON-RPC Plugin to manage tickets in Trac SCM
+#
+# Copyright (c) 2025, Frank Sommer.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# --------------------------------------------------------------------------------------------------
+
+"""
+Constants of JSON-RPC interface for Trac ticket management.
+"""
+
+# plugin version
+VERSION = '0.9.3'
+
+# HTTP request path for ticket
+HTTP_REQUEST_PATH = '/ticketrpc'
+
+# HTTP response codes
+HTTP_OK = 200
+HTTP_METHOD_NOT_ALLOWED = 405
+HTTP_UNSUPPORTED_MEDIA_TYPE = 415
+
+# JSON RPC error codes
+JSON_RPC_PARSE_ERROR = -32700
+JSON_RPC_INVALID_REQUEST = -32600
+JSON_RPC_METHOD_NOT_FOUND = -32601
+JSON_RPC_INVALID_PARAMS = -32602
+JSON_RPC_CONTENT_TYPE_ERROR = -32050
+JSON_RPC_ACCESS_DENIED = 1000
