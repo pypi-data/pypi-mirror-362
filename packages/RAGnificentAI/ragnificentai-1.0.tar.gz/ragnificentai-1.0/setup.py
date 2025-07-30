8ebd10ff352daa1b43a223b883a86d79a1e26f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:07:53 +0000

    Add package RAGnificentAI/1.0/ragnificentai-1.0.tar.gz

---
+++ packages/RAGnificentAI/ragnificentai-1.0.tar.gz/ragnificentai-1.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RAGnificentAI',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        "pydantic",
+        "langchain",
+        "langchain-core",
+        "langchain-community",
+        "langchain-groq",
+        "langchain-openai",
+        "langgraph",
+        "python-dotenv",
+    ],
+    author='K. M. Abul Farhad-Ibn-Alam',
+    author_email='abulfarhad.ibnalam@gmail.com',
+    description='A simple RAG chatbot supporting multiple LLM providers',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Abul-Farhad/simple-rag',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence"
+    ],
+    python_requires='>=3.9',
+    keywords='llm chatbot rag openai groq gemini',
+    project_urls={
+        # 'Bug Reports': 'https://github.com/Abul-Farhad/simple-rag/issues',
+        'Source': 'https://github.com/Abul-Farhad/simple-rag',
+    },
+    license="Custom",
+    license_files=('LICENSE',),
+)
