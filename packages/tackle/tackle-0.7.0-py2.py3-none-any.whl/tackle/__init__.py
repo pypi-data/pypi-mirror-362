31ccfafbab58fdedc57210fc3b36c48beab38a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 05:12:50 +0000

    Add package tackle/0.7.0/tackle-0.7.0-py2.py3-none-any.whl

---
+++ packages/tackle/tackle-0.7.0-py2.py3-none-any.whl/tackle/__init__.py
@@ -0,0 +1,29 @@
+"""Main package for tackle."""
+__version__ = "0.7.0"  # x-release-please-version
+
+from pydantic import Field
+
+from tackle.context import Context
+from tackle.decorators import public, private
+from tackle.factory import new_context
+from tackle.main import tackle
+from tackle.models import BaseHook, HookCallInput
+from tackle.settings import settings
+from tackle.types import DocumentKeyType, DocumentType, DocumentValueType
+from tackle.utils.hooks import get_hook
+
+__all__ = [
+    'tackle',
+    'BaseHook',
+    'public',
+    'private',
+    'Field',
+    'HookCallInput',
+    'Context',
+    'settings',
+    'get_hook',
+    'new_context',
+    'DocumentKeyType',
+    'DocumentType',
+    'DocumentValueType',
+]
