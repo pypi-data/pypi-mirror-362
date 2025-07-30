80defb0b9add7649a7d17eda937dc246a45ee70d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:14 +0000

    Add package insightfinderai/2.0.0/insightfinderai-2.0.0-py3-none-any.whl

---
+++ packages/insightfinderai/insightfinderai-2.0.0-py3-none-any.whl/insightfinderai/config.py
@@ -0,0 +1,7 @@
+DEFAULT_API_URL = "https://ai.insightfinder.com"
+
+# API Endpoints
+CHATBOT_ENDPOINT = "api/external/v1/chatbot/stream-with-type"
+EVALUATION_ENDPOINT = "api/external/v1/evaluation/bias-hallu"
+SAFETY_EVALUATION_ENDPOINT = "api/external/v1/evaluation/safety"
+TRACE_PROJECT_NAME_ENDPOINT = "api/external/v1/chatbot/get-trace-project-name-v2"
