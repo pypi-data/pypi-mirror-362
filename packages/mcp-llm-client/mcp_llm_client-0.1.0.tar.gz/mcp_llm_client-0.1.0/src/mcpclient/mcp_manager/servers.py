9748243a17255b71e6cc4213042b6ec8bb033821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:04 +0000

    Add package mcp-llm-client/0.1.0/mcp_llm_client-0.1.0.tar.gz

---
+++ packages/mcp-llm-client/mcp_llm_client-0.1.0.tar.gz/mcp_llm_client-0.1.0/src/mcpclient/mcp_manager/servers.py
@@ -0,0 +1,68 @@
+import os
+import json
+from mcp_oauth import OAuthClient
+from ..config.logger import logger
+
+
+class Servers:
+    """
+    Extrae las credenciales de cada uno de los servers en la configuracion `.config.json`. Aternativamente puede pasarse otro archivo `.json`
+    como argumento `config_file_path`
+    """
+
+    def __init__(
+        self, config_file_path: str = "config.json", app_name: str = "mcp-llm-client"
+    ):
+        self.app_name = app_name
+        self.config_file_path: str = config_file_path
+        self.mcp_servers: dict[str, dict] | None = {}
+        self.load_servers()
+
+    def load_servers(self):
+        """Carga cada uno de los credenciales de los servidores que se le conectan desde el `.config`"""
+        with open(f"{os.getcwd()}{os.path.sep}{self.config_file_path}", "r") as file:
+            json_config: dict = json.loads(file.read())
+        self.mcp_servers = json_config["mcp_servers"]
+
+        for server in self.mcp_servers.values():
+            self.create_oauth_client(server=server)
+
+    def create_oauth_client(self, server: dict[str, dict]) -> None:
+        """Genera un oauth client para este servidor, usando los datos proporcionados en `config.json`"""
+        oauth_client: OAuthClient | None = None
+
+        if "auth" in server.keys() and (
+            "required" in server["auth"].keys() and server["auth"]["required"]
+        ):
+            server_url: str = server["auth"]["server"]
+            server_name: str | None = (
+                server["name"] if "name" in server.keys() else None
+            )
+            server_full_name: str = (
+                f""" "{server_name}[{server_url}]" """
+                if server_name is not None
+                else f""" "[{server_url}]" """
+            )
+
+            username: str = None
+            password: str = None
+
+            try:
+                if "secrets" in server["auth"].keys():
+                    username = server["auth"]["secrets"]["username"]
+                    password = server["auth"]["secrets"]["password"]
+            except:
+                pass
+
+            if username is None or password is None:
+                logger.info(
+                    f"Server {server_full_name} require manual login in webbrowser that will be open."
+                )
+            oauth_client = OAuthClient(
+                client_name=self.app_name,
+                server_url=server_url,
+                authorized_username=username,
+                authorized_username_password=password,
+            )
+
+        server["oauth_client"] = oauth_client
