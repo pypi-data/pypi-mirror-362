4a6fce6573248d2d52fda7b082f4de2e15ca9689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:56 +0000

    Add package ansys-magnet-segmentation-toolkit/0.8.5/ansys_magnet_segmentation_toolkit-0.8.5.tar.gz

---
+++ packages/ansys-magnet-segmentation-toolkit/ansys_magnet_segmentation_toolkit-0.8.5.tar.gz/ansys_magnet_segmentation_toolkit-0.8.5/README.rst
@@ -0,0 +1,69 @@
+Magnet Segmentation Toolkit
+===========================
+
+|pyansys| |PythonVersion| |GH-CI| |MIT| |black|
+
+.. |pyansys| image:: https://img.shields.io/badge/Py-Ansys-ffc107.svg?logo=data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAIAAACQkWg2AAABDklEQVQ4jWNgoDfg5mD8vE7q/3bpVyskbW0sMRUwofHD7Dh5OBkZGBgW7/3W2tZpa2tLQEOyOzeEsfumlK2tbVpaGj4N6jIs1lpsDAwMJ278sveMY2BgCA0NFRISwqkhyQ1q/Nyd3zg4OBgYGNjZ2ePi4rB5loGBhZnhxTLJ/9ulv26Q4uVk1NXV/f///////69du4Zdg78lx//t0v+3S88rFISInD59GqIH2esIJ8G9O2/XVwhjzpw5EAam1xkkBJn/bJX+v1365hxxuCAfH9+3b9/+////48cPuNehNsS7cDEzMTAwMMzb+Q2u4dOnT2vWrMHu9ZtzxP9vl/69RVpCkBlZ3N7enoDXBwEAAA+YYitOilMVAAAAAElFTkSuQmCC
+   :target: https://docs.pyansys.com/
+   :alt: PyAnsys
+
+.. |PythonVersion| image:: https://img.shields.io/badge/python-3.7+-blue.svg
+   :target: https://www.python.org/downloads/
+
+.. |GH-CI| image:: https://github.com/ansys/magnet-segmentation-toolkit/actions/workflows/ci_cd.yml/badge.svg
+   :target: https://github.com/ansys/magnet-segmentation-toolkit/blob/main/.github/workflows/ci_cd.yml
+
+.. |MIT| image:: https://img.shields.io/badge/License-MIT-yellow.svg
+   :target: https://opensource.org/licenses/MIT
+
+.. |black| image:: https://img.shields.io/badge/code%20style-black-000000.svg?style=flat
+  :target: https://github.com/psf/black
+  :alt: black
+
+.. reuse_start
+
+The Magnet Segmentation Toolkit is a Python wrapper for automating the segmentation
+and skew of interior permanent magnet (IPM) and surface permanent magnet (SPM)
+motors using Ansys Electronics Desktop (AEDT). You can launch this toolkit from the AEDT UI
+or launch it directly from a Python console. This toolkit can also be launched as desktop
+application based on `Pyside6 <https://doc.qt.io/qtforpython-6/quickstart.html>`_.
+
+Requirements
+~~~~~~~~~~~~
+In addition to the runtime dependencies listed in
+`Getting started <https://magnet.segmentation.toolkit.docs.pyansys.com/version/stable/Getting_started.html>`_,
+the Magnet Segmentation Toolkit requires AEDT 2023 R1 or later. This toolkit also supports the AEDT
+Student Version for 2023 R1 or later.
+
+Documentation and issues
+~~~~~~~~~~~~~~~~~~~~~~~~
+Documentation for the latest stable release of the Magnet Segmentation Toolkit is hosted at
+`Magnet Segmentation Toolkit documentation <https://magnet.segmentation.toolkit.docs.pyansys.com/version/stable/index.html>`_.
+
+In the upper right corner of the documentation's title bar, there is an option for switching from
+viewing the documentation for the latest stable release to viewing the documentation for the
+development version or previously released versions.
+
+On the `Magnet Segmentation Toolkit Issues <https://github.com/ansys/magnet-segmentation-toolkit/issues>`_
+page, you can create issues to report bugs and request new features. On the `Discussions <https://discuss.ansys.com/>`_
+page on the Ansys Developer portal, you can post questions, share ideas, and get community feedback.
+
+Distributing
+~~~~~~~~~~~~
+This project is vectored to be an open-source project. For the time being, feel
+free to distribute it internally, but direct users to visit the `Releases
+<https://github.com/ansys/magnet-segmentation-toolkit/releases>`__ page
+
+Security
+~~~~~~~~
+The versions that are still supported for security updates can be found at
+the `Security guidelines <https://github.com/ansys/magnet-segmentation-toolkit/blob/main/SECURITY.md>`_
+site. Information on how to report vulnerabilities is also found.
+
+License
+~~~~~~~
+The Magnet Segmentation Toolkit is licensed under the MIT license.
+
+This toolkit makes no commercial claim over Ansys whatsoever. The use of this toolkit
+requires a legally licensed copy of AEDT. For more information, see the
+`Ansys Electronics <https://www.ansys.com/products/electronics>`_ page on the Ansys website.
---
+++ packages/ansys-magnet-segmentation-toolkit/ansys_magnet_segmentation_toolkit-0.8.5.tar.gz/ansys_magnet_segmentation_toolkit-0.8.5/src/ansys/aedt/toolkits/magnet_segmentation/run_toolkit.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import atexit
+import importlib
+import multiprocessing
+import os
+import sys
+import time
+
+from ansys.aedt.toolkits.common.utils import check_backend_communication
+from ansys.aedt.toolkits.common.utils import clean_python_processes
+from ansys.aedt.toolkits.common.utils import find_free_port
+from ansys.aedt.toolkits.common.utils import is_server_running
+from ansys.aedt.toolkits.common.utils import process_desktop_properties
+from ansys.aedt.toolkits.common.utils import wait_for_server
+
+backend = None
+ui = None
+
+
+def get_backend():
+    global backend
+    if backend is None:
+        backend = importlib.import_module("ansys.aedt.toolkits.magnet_segmentation.backend.run_backend")
+    return backend
+
+
+def get_ui():
+    global ui
+    if ui is None:
+        ui = importlib.import_module("ansys.aedt.toolkits.magnet_segmentation.ui.run_frontend")
+    return ui
+
+
+from ansys.aedt.toolkits.magnet_segmentation.backend.models import properties as backend_properties
+from ansys.aedt.toolkits.magnet_segmentation.ui.models import properties as frontend_properties
+from ansys.aedt.toolkits.magnet_segmentation.ui.splash import show_splash_screen
+
+# # Set environment variables
+# os.environ["AEDT_TOOLKIT_HIGH_RESOLUTION"] = "False"
+# os.environ["AEDT_TOOLKIT_THEME"] = "ansys_dark.json"
+
+
+def start_backend(pp):
+    """Start the backend process."""
+    backend_imported = get_backend()
+
+    print(f"Starting backend on port {pp}...")
+    backend_imported.run_backend(pp)
+
+
+def start_frontend(backend_url, backend_port):
+    """Start the frontend process."""
+    ui_imported = get_ui()
+    print("Starting frontend...")
+    ui_imported.run_frontend(backend_url, backend_port)
+
+
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+
+    is_linux = os.name == "posix"
+    new_port = find_free_port(backend_properties.url, backend_properties.port)
+    if not new_port:
+        raise Exception(f"No free ports available in {backend_properties.url}")
+
+    backend_properties.port = new_port
+    frontend_properties.backend_port = new_port
+    url = frontend_properties.backend_url
+    port = frontend_properties.backend_port
+    url_call = f"http://{url}:{port}"
+    python_path = sys.executable
+
+    def terminate_processes():
+        print("Terminating backend and frontend processes...")
+        backend_process.terminate()
+        frontend_process.terminate()
+        backend_process.join()
+        frontend_process.join()
+        print("Processes terminated.")
+
+    # Clean python processes when script ends
+    atexit.register(clean_python_processes, url, port)
+
+    # Check if backend is already running
+    if is_server_running(server=url, port=port):
+        raise Exception(f"A process is already running at: {url_call}")
+
+    if not is_linux:
+        # Launch splash
+        splash_process = multiprocessing.Process(target=show_splash_screen)
+        splash_process.start()
+
+    # Launch backend process
+    backend_process = multiprocessing.Process(target=start_backend, args=(new_port,))
+    backend_process.start()
+
+    # Wait for backend to start
+    count = 0
+    while not check_backend_communication(url_call) and count < 10:
+        time.sleep(1)
+        count += 1
+
+    if not check_backend_communication(url_call):
+        raise Exception("Backend communication is not working.")
+
+    # Connect to AEDT session if necessary
+    process_desktop_properties(is_linux, url_call)
+
+    if not is_linux:
+        splash_process.join()
+
+    # Launch frontend process
+    frontend_process = multiprocessing.Process(target=start_frontend, args=(url, port))
+    frontend_process.start()
+
+    # Wait for backend confirmation
+    if not wait_for_server(server=url, port=port):
+        raise Exception(f"Backend did not start properly at {url_call}")
+
+    # Keep frontend running
+    frontend_process.join()
+
+    terminate_processes()
