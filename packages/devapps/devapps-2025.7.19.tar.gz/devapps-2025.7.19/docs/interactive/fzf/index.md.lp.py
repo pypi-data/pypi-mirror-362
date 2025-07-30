ff0158954ea17d4e5a786fae655753cba010e6ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 12:07:19 +0000

    Add package devapps/2025.7.19/devapps-2025.7.19.tar.gz

---
+++ packages/devapps/devapps-2025.7.19.tar.gz/devapps-2025.7.19/docs/interactive/fzf/index.md.lp.py
@@ -0,0 +1,18 @@
+{
+    '25239f42c84f0c86e2a5bb658125f4cd': {
+        'formatted': '![](img/k2.svg)',
+        'res': '![](img/k2.svg)',
+    },
+    '5893e7b6d81cd5d60130cf93d3af5eae': {
+        'formatted': '![](img/k0.1.svg)',
+        'res': '![](img/k0.1.svg)',
+    },
+    '8c597f65471280555b8570992b087870': {
+        'formatted': '![](img/k0.svg)',
+        'res': '![](img/k0.svg)',
+    },
+    'f969cb6b9a57bd892bc520bc0ce5879e': {
+        'formatted': '![](img/k1.svg)',
+        'res': '![](img/k1.svg)',
+    },
+}
