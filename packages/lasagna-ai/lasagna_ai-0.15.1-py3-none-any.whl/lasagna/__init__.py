a05274e80f71cf21e2b0a96ba322d04eff9fd25b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 13:19:56 +0000

    Add package lasagna-ai/0.15.1/lasagna_ai-0.15.1-py3-none-any.whl

---
+++ packages/lasagna-ai/lasagna_ai-0.15.1-py3-none-any.whl/lasagna/__init__.py
@@ -0,0 +1,53 @@
+from .types import *
+
+from .agent_runner import run
+
+from .agent_util import (
+    bind_model,
+    partial_bind_model,
+    recursive_extract_messages,
+    extract_last_message,
+    flat_messages,
+    override_system_prompt,
+    strip_tool_calls_and_results,
+    strip_all_but_last_human_message,
+    noop_callback,
+    MessageExtractor,
+    build_standard_message_extractor,
+    default_message_extractor,
+    build_simple_agent,
+    build_extraction_agent,
+    build_agent_chainer,
+    build_agent_router,
+    build_static_output_agent,
+)
+
+from .easy import (
+    easy_ask,
+    easy_extract,
+)
+
+__all__ = [
+    'run',
+    'bind_model',
+    'partial_bind_model',
+    'recursive_extract_messages',
+    'extract_last_message',
+    'flat_messages',
+    'override_system_prompt',
+    'strip_tool_calls_and_results',
+    'strip_all_but_last_human_message',
+    'noop_callback',
+    'MessageExtractor',
+    'build_standard_message_extractor',
+    'default_message_extractor',
+    'build_simple_agent',
+    'build_extraction_agent',
+    'build_agent_chainer',
+    'build_agent_router',
+    'build_static_output_agent',
+    'easy_ask',
+    'easy_extract',
+]
+
+__version__ = "0.15.1"
