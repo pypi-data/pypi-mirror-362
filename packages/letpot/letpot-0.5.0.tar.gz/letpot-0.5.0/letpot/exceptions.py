c7f9e9d4fee051212fc89c3f83e94f1118d996a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:31 +0000

    Add package letpot/0.5.0/letpot-0.5.0.tar.gz

---
+++ packages/letpot/letpot-0.5.0.tar.gz/letpot-0.5.0/letpot/exceptions.py
@@ -0,0 +1,13 @@
+"""Exceptions for Python client for LetPot hydroponic gardens."""
+
+
+class LetPotException(Exception):
+    """Generic exception."""
+
+
+class LetPotConnectionException(LetPotException):
+    """LetPot connection exception."""
+
+
+class LetPotAuthenticationException(LetPotException):
+    """LetPot authentication exception."""
