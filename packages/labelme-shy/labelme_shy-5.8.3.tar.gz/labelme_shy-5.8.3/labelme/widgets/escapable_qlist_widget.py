4388ac0df0122ca108db71c1ea75c16788b4a98e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 06:06:25 +0000

    Add package labelme-shy/5.8.3/labelme_shy-5.8.3.tar.gz

---
+++ packages/labelme-shy/labelme_shy-5.8.3.tar.gz/labelme_shy-5.8.3/labelme/widgets/escapable_qlist_widget.py
@@ -0,0 +1,9 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
+
+
+class EscapableQListWidget(QtWidgets.QListWidget):
+    def keyPressEvent(self, event):
+        super(EscapableQListWidget, self).keyPressEvent(event)
+        if event.key() == Qt.Key_Escape:  # type: ignore[attr-defined]
+            self.clearSelection()
