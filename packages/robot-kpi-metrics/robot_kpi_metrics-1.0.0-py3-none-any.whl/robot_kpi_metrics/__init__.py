5d3556e134fc5fa542aab82fc2bdc4ec891bf324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 16:08:59 +0000

    Add package robot-kpi-metrics/1.0.0/robot_kpi_metrics-1.0.0-py3-none-any.whl

---
+++ packages/robot-kpi-metrics/robot_kpi_metrics-1.0.0-py3-none-any.whl/robot_kpi_metrics/__init__.py
@@ -0,0 +1,2 @@
+# Robot KPI Metrics
+# Custom KPI report generator for Robot Framework
---
+++ packages/robot-kpi-metrics/robot_kpi_metrics-1.0.0-py3-none-any.whl/robot_kpi_metrics/test_results.py
@@ -0,0 +1,25 @@
+from robot.api import ResultVisitor
+from robot.utils.markuputils import html_format
+
+
+class TestResults(ResultVisitor):
+
+    def __init__(self, test_list):
+        self.test_list = test_list
+    
+    def visit_test(self, test):
+        suite_name = test.parent if test.parent else test.parent.name
+        test_json = {
+            "Suite Name" : suite_name,
+            "Test Name" : test.name,
+            "Test Id" : test.id,
+            "Status" : test.status,
+            "Documentation" : html_format(test.doc),
+            "Time" : test.elapsedtime,
+            # "Message" : html_format(test.message),
+            "Message" : str(test.message).replace("*HTML*",""),
+            "Tags" : test.tags,
+            'start_time': test.starttime,
+            'end_time': test.endtime,
+        }
+        self.test_list.append(test_json)
