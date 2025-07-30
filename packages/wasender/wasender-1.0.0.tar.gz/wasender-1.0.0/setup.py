489dc1f890240354ebcf1f71b09b85a646d23672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:08:41 +0000

    Add package wasender/1.0.0/wasender-1.0.0.tar.gz

---
+++ packages/wasender/wasender-1.0.0.tar.gz/wasender-1.0.0/setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup, find_packages
+
+setup(
+name='wasender',
+version='1.0.0',
+description='It is like the messenger where you send the mesaages and perform your activities',
+author='Rena Gondaliya',
+author_email='mypa2k04@gmail.com',
+license='MIT',
+packages=find_packages(),
+classifiers=[
+"Development Status :: 4 - Beta",
+"Intended Audience :: Developers",
+"Topic :: Communications :: Chat",
+"License :: OSI Approved :: MIT License",
+"Programming Language :: Python :: 3",
+"Programming Language :: Python :: 3.6",
+"Programming Language :: Python :: 3.7",
+"Programming Language :: Python :: 3.8",
+"Programming Language :: Python :: 3.9",
+"Programming Language :: Python :: 3.10",
+"Operating System :: OS Independent",
+],
+python_requires='>=3.6',
+)
