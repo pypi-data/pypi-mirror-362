e96e280799ba7c8f4a73f7d585ce4448ef52bb44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 11:06:24 +0000

    Add package apapi/0.6.0/apapi-0.6.0-py3-none-any.whl

---
+++ packages/apapi/apapi-0.6.0-py3-none-any.whl/apapi/__init__.py
@@ -0,0 +1,27 @@
+"""
+.. include:: ../README.md
+"""
+
+import logging
+
+from . import utils
+from .__version__ import (
+    __author__,
+    __author_email__,
+    __copyright__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
+from .alm import ALMConnection
+from .audit import AuditConnection
+from .authentication import BasicAuth, OAuth2NonRotatable, OAuth2Rotatable
+from .basic_connection import BasicConnection
+from .bulk import BulkConnection
+from .connection import Connection
+from .transactional import TransactionalConnection
+
+# Set default logging handler to avoid "No handler found" warnings.
+logging.getLogger(__name__).addHandler(logging.NullHandler())
---
+++ packages/apapi/apapi-0.6.0-py3-none-any.whl/apapi/connection.py
@@ -0,0 +1,22 @@
+"""
+apapi.connection
+
+This module provides Connection class, which contains all available API functions.
+"""
+from __future__ import annotations
+
+from __future__ import annotations
+
+from .alm import ALMConnection
+from .audit import AuditConnection
+from .bulk import BulkConnection
+from .transactional import TransactionalConnection
+
+
+class Connection(
+    ALMConnection,
+    AuditConnection,
+    BulkConnection,
+    TransactionalConnection,
+):
+    """Anaplan connection session. Provides all available API functions."""
