4e16eaa1737d45b3e2af1a072f2838f870766d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:47 +0000

    Add package ytsaurus-client-yc-auth/0.1.0/ytsaurus_client_yc_auth-0.1.0.tar.gz

---
+++ packages/ytsaurus-client-yc-auth/ytsaurus_client_yc_auth-0.1.0.tar.gz/ytsaurus_client_yc_auth-0.1.0/setup.py
@@ -0,0 +1,28 @@
+import os
+from distutils.core import setup
+from pathlib import Path
+from setuptools import find_namespace_packages
+
+setup(
+    name='ytsaurus-client-yc-auth',
+    version='0.1.0',
+    license='Apache-2.0',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    author="Yandex LLC",
+    author_email="cloud@support.yandex.ru",
+    packages=find_namespace_packages(include=('yc_managed_ytsaurus_auth',)),
+    install_requires=[
+        'ytsaurus-client>=0.13.33,<0.14.0',
+    ],
+    python_requires=">=3.8",
+    description='Yandex Cloud auth for YTsaurus client',
+    long_description_content_type='text/markdown',
+    long_description='Yandex Cloud auth for YTsaurus client',
+)
