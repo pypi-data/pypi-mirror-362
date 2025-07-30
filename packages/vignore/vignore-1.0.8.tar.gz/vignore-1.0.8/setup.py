36dcc43b7f9ab955916429d5da3cfb592f96c1bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 18:06:45 +0000

    Add package vignore/1.0.8/vignore-1.0.8.tar.gz

---
+++ packages/vignore/vignore-1.0.8.tar.gz/vignore-1.0.8/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="vignore",
+    version="1.0.8",
+    packages=["vignore"],
+    description="Visualize ignored files and directories by rules.",
+    url="https://github.com/james4ever0/vignore",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "numpy",
+        "rich",
+        "textual",
+        "jinja2",
+        "beartype",
+        "aiofiles",
+        "asyncio",
+        "humanize"
+    ],
+    entry_points="""
+        [console_scripts]
+        vignore=vignore.cli:main
+    """,
+)
