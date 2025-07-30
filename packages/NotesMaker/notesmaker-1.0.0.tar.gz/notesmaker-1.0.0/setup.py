f3819b9e1d9ada45baaa739ca9f08332aafdcb8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 07:09:26 +0000

    Add package NotesMaker/1.0.0/notesmaker-1.0.0.tar.gz

---
+++ packages/NotesMaker/notesmaker-1.0.0.tar.gz/notesmaker-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NotesMaker",
+    version="1.0.0",
+    description="This is an updated version of NotesMaker.py",
+    packages=find_packages(),
+    python_requires=">=3.6"
+)
