fc8611898f17b4086f5eeaf9f77808d11c32b22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-07-30 15:08:15 +0000

    Add package mseep-hayhooks/1.0.0/mseep_hayhooks-1.0.0.tar.gz

---
+++ packages/mseep-hayhooks/mseep_hayhooks-1.0.0.tar.gz/mseep_hayhooks-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mseep-hayhooks',
+    version='1.0.0',
+    description='Grab and deploy Haystack pipelines',
+    long_description="Package managed by MseeP.ai",
+    long_description_content_type="text/plain",
+    author='mseep',
+    author_email='support@skydeck.ai',
+    maintainer='mseep',
+    maintainer_email='support@skydeck.ai',
+    url='https://github.com/mseep',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=['haystack-ai>=2.11.0', 'fastapi[standard]', 'typer', 'uvicorn', 'rich', 'requests', 'python-multipart', 'loguru', 'pydantic-settings', 'python-dotenv', 'docstring-parser'],
+    keywords=['mseep'],
+)
